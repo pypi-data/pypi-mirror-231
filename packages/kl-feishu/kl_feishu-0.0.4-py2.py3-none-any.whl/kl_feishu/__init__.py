823a72d582bdf4a73d5e362187ce036b63d8b20b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:06:07 +0000

    Add package kl-feishu/0.0.4/kl_feishu-0.0.4-py2.py3-none-any.whl

---
+++ packages/kl-feishu/kl_feishu-0.0.4-py2.py3-none-any.whl/kl_feishu/__init__.py
@@ -0,0 +1,15 @@
+from .version import (
+    __author__,
+    __author_email__,
+    __build__,
+    __cake__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
+from .feishu import FeiShuHelp, CardMsgRoot
+
+# __all__ = ["FeishuHelp", "CardMsgRoot"]
