13441368af13419cfd773951fe04a1069a4a504d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:43 +0000

    Add package arcee-py/0.0.11/arcee_py-0.0.11-py3-none-any.whl

---
+++ packages/arcee-py/arcee_py-0.0.11-py3-none-any.whl/arcee/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.0.11"
+
+
+from arcee.api import get_dalm, train_dalm, upload_doc, upload_docs
+from arcee.config import ARCEE_API_KEY, ARCEE_APP_URL
+from arcee.dalm import DALM
+
+if not ARCEE_API_KEY:
+    raise Exception(f"ARCEE_API_KEY must be in the environment. You can retrieve your API key from {ARCEE_APP_URL}")
+
+
+__all__ = ["upload_docs", "upload_doc", "train_dalm", "get_dalm", "DALM"]
