8f92b11a318ff1f6bb7e22392c019c30d2e1783f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 15:06:28 +0000

    Add package covalent-hpc-plugin/0.0.3/covalent-hpc-plugin-0.0.3.tar.gz

---
+++ packages/covalent-hpc-plugin/covalent-hpc-plugin-0.0.3.tar.gz/covalent-hpc-plugin-0.0.3/setup.py
@@ -0,0 +1,77 @@
+# Copyright 2021 Agnostiq Inc.
+#
+# This file is part of Covalent.
+#
+# Licensed under the GNU Affero General Public License 3.0 (the "License").
+# A copy of the License may be obtained with this software package or at
+#
+#      https://www.gnu.org/licenses/agpl-3.0.en.html
+#
+# Use of this file is prohibited except in compliance with the License. Any
+# modifications or derivative works of this file must retain this copyright
+# notice, and modified files must contain a notice indicating that they have
+# been altered from the originals.
+#
+# Covalent is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the License for more details.
+#
+# Relief from the License may be granted by purchasing a commercial license.
+
+import site
+import sys
+
+from setuptools import find_packages, setup
+
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+plugins_list = ["hpc = covalent_hpc_plugin.hpc"]
+
+setup_info = {
+    "name": "covalent-hpc-plugin",
+    "packages": find_packages("."),
+    "version": "0.0.3",
+    "maintainer": "Andrew S. Rosen",
+    "url": "https://github.com/quantum-accelerators/covalent-hpc-plugin",
+    "license": "GNU Affero GPL v3.0",
+    "author": "Andrew S. Rosen",
+    "description": "Covalent HPC Plugin",
+    "long_description": open("README.md").read(),
+    "long_description_content_type": "text/markdown",
+    "include_package_data": True,
+    "install_requires": [
+        "asyncssh>=2.10.1",
+        "aiofiles>=0.8.0",
+        "cloudpickle>=2.0.0",
+        "covalent>=0.220.0",
+        "psij-python>=0.9.0",
+    ],
+    "classifiers": [
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "Environment :: Plugins",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: Other/Proprietary License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Adaptive Technologies",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Interface Engine/Protocol Translator",
+        "Topic :: Software Development",
+        "Topic :: System :: Distributed Computing",
+    ],
+    "entry_points": {
+        "covalent.executor.executor_plugins": plugins_list,
+    },
+}
+
+if __name__ == "__main__":
+    setup(**setup_info)
