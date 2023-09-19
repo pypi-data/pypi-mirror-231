e709729d59c745135c3d9b30898e8ce32c68ea7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:04:50 +0000

    Add package botanix/0.2.4/botanix-0.2.4.tar.gz

---
+++ packages/botanix/botanix-0.2.4.tar.gz/botanix-0.2.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='botanix',
+    version='0.2.4',
+    license='MIT',
+    author="Ali Kheyrollahi",
+    author_email='aliostad@gmail.com',
+    packages=find_packages(exclude=['tests', 'samples']),
+    url='https://github.com/aliostad/botanix',
+    keywords='Telegram bot',
+    install_requires=[
+          'python-telegram-bot',
+      ],
+
+)
