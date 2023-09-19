d9a2b401e45bda3dc466a8f077438f6e78a87466
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:04:36 +0000

    Add package resumer/0.1.0/resumer-0.1.0.tar.gz

---
+++ packages/resumer/resumer-0.1.0.tar.gz/resumer-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+print(find_packages(exclude=['tests*']))
+
+setup(
+    name='resumer',
+    version='0.1.0',
+    packages=find_packages(exclude=['tests*']),
+    include_package_data=True,
+    description="pandoc based generator with advanced filter support",
+    author="Zackary W",
+    license="MIT",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/zackaryw/resumer",
+    install_requires=[
+        "pydantic",
+        "click",
+    ],
+    entry_points={
+        'console_scripts': [
+            'resumer = resumer.cli:main',
+        ]
+    }
+)
