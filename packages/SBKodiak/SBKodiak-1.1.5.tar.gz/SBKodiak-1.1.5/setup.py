d83b9a45e38880a71492ccc3f5b7848e581c808c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:05:37 +0000

    Add package SBKodiak/1.1.5/SBKodiak-1.1.5.tar.gz

---
+++ packages/SBKodiak/SBKodiak-1.1.5.tar.gz/SBKodiak-1.1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='SBKodiak',
+      version='1.1.5',
+      description='SanBlaze Python control of Kodiak Modules',
+      author='Matt Holsey',
+      author_email='mholsey@sanblaze.com',
+      url='https://www.sanblaze.com/',
+      license='SanBlaze Technology ltd',
+      packages=['SBKodiak', 
+				'SBKodiak.docs',
+				'SBKodiak.tests'],
+	  install_requires=[
+			    'paramiko',
+			    'requests',
+				'urllib3',
+		  ],
+      include_package_data=True,
+      zip_safe=False)
