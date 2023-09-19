3c49719a0e99826df07e450ce761ecfc8810b37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:38 +0000

    Add package r-leaflet/0.0.1/r-leaflet-0.0.1.tar.gz

---
+++ packages/r-leaflet/r-leaflet-0.0.1.tar.gz/r-leaflet-0.0.1/README.md
@@ -0,0 +1,4 @@
+# r-leaflet
+r-leaflet - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/r-leaflet/r-leaflet-0.0.1.tar.gz/r-leaflet-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='r-leaflet',
+    version="1.0.0",
+    description='r-leaflet - listed by Cycode to prevent confusion',
+    long_description="r-leaflet",
+    long_description_content_type='text/markdown',
+)
