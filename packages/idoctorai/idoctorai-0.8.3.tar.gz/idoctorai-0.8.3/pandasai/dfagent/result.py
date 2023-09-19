f4d67146dd9821ff2c3951dd0b4f8a202732fd32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 16:04:50 +0000

    Add package idoctorai/0.8.3/idoctorai-0.8.3.tar.gz

---
+++ packages/idoctorai/idoctorai-0.8.3.tar.gz/idoctorai-0.8.3/pandasai/dfagent/result.py
@@ -0,0 +1,7 @@
+class Result:
+
+    def __init__(self, status=None, error=None, content=None, picpath=None):
+        self.status = status
+        self.error = error
+        self.content = content
+        self.picpath = picpath
