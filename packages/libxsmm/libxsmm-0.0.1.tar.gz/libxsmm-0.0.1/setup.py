26a8bef412a0996b80b9867957da38bd5782a3c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:31 +0000

    Add package libxsmm/0.0.1/libxsmm-0.0.1.tar.gz

---
+++ packages/libxsmm/libxsmm-0.0.1.tar.gz/libxsmm-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='libxsmm',
+    version="1.0.0",
+    description='libxsmm - listed by Cycode to prevent confusion',
+    long_description="libxsmm",
+    long_description_content_type='text/markdown',
+)
