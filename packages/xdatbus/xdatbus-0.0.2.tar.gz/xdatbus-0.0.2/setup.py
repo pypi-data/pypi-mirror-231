6d582fc61155199e63ed9862f6180923bd0122c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:08:17 +0000

    Add package xdatbus/0.0.2/xdatbus-0.0.2.tar.gz

---
+++ packages/xdatbus/xdatbus-0.0.2.tar.gz/xdatbus-0.0.2/setup.py
@@ -0,0 +1,35 @@
+import codecs
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+# you need to change all these
+VERSION = '0.0.2'
+DESCRIPTION = 'just for test'
+LONG_DESCRIPTION = 'just for test'
+
+setup(
+    name="xdatbus",
+    version=VERSION,
+    author="Jiacheng Wang",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'vasp', 'xdatcar', 'aimd'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Science/Research",
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
