ba64f12bf8865e7ab7bc8a1add3377354b48c882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:05:13 +0000

    Add package coretex/1.0.32/coretex-1.0.32-py3-none-any.whl

---
+++ packages/coretex/coretex-1.0.32-py3-none-any.whl/coretex/_task/_local.py
@@ -0,0 +1,147 @@
+#     Copyright (C) 2023  Coretex LLC
+
+#     This file is part of Coretex.ai
+
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU Affero General Public License as
+#     published by the Free Software Foundation, either version 3 of the
+#     License, or (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU Affero General Public License for more details.
+
+#     You should have received a copy of the GNU Affero General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from typing import Tuple, Optional, List, Dict, Any
+from getpass import getpass
+
+import logging
+import os
+import json
+import sys
+
+from tap import Tap
+
+import psutil
+
+from ._base_callback import TaskCallback
+from .. import folder_manager
+from ..coretex import Experiment, ExperimentStatus, BaseParameter, validateParameters, parameter_factory
+from ..networking import networkManager
+
+
+class LocalTaskCallback(TaskCallback):
+
+    def onStart(self) -> None:
+        super().onStart()
+
+        folder_manager.clearTempFiles()
+
+    def onSuccess(self) -> None:
+        super().onSuccess()
+
+        self._experiment.updateStatus(ExperimentStatus.completedWithSuccess)
+
+    def onKeyboardInterrupt(self) -> None:
+        super().onKeyboardInterrupt()
+
+        logging.getLogger("coretexpylib").info(">> [Coretex] Stopping the experiment")
+
+        self._experiment.updateStatus(ExperimentStatus.stopping)
+
+        experimentProcess = psutil.Process(os.getpid())
+        children = experimentProcess.children(recursive = True)
+
+        logging.getLogger("coretexpylib").debug(f">> [Coretex] Number of child processes: {len(children)}")
+
+        for process in children:
+            process.kill()
+
+        for process in children:
+            process.wait()
+
+        self._experiment.updateStatus(ExperimentStatus.stopped)
+
+    def onException(self, exception: BaseException) -> None:
+        super().onException(exception)
+
+        self._experiment.updateStatus(ExperimentStatus.completedWithError)
+
+
+class LocalArgumentParser(Tap):
+
+    username: Optional[str]
+    password: Optional[str]
+
+    spaceId: int
+    name: Optional[str]
+    description: Optional[str]
+
+    def configure(self) -> None:
+        self.add_argument("--username", nargs = "?", type = str, default = None)
+        self.add_argument("--password", nargs = "?", type = str, default = None)
+
+        self.add_argument("--spaceId", type = int)
+        self.add_argument("--name", nargs = "?", type = str, default = None)
+        self.add_argument("--description", nargs = "?", type = str, default = None)
+
+
+def _readExperimentConfig() -> List[BaseParameter]:
+    parameters: List[BaseParameter] = []
+
+    with open("./experiment.config", "rb") as configFile:
+        configContent: Dict[str, Any] = json.load(configFile)
+        parametersJson = configContent["parameters"]
+
+        if not isinstance(parametersJson, list):
+            raise ValueError(">> [Coretex] Invalid experiment.config file. Property 'parameters' must be an array")
+
+        for parameterJson in parametersJson:
+            parameter = parameter_factory.create(parameterJson)
+            parameters.append(parameter)
+
+    parameterValidationResults = validateParameters(parameters, verbose = True)
+    if not all(parameterValidationResults.values()):
+        # Using this to make the parameter errors more readable without scrolling through the console
+        sys.exit(1)
+
+    return parameters
+
+
+def _processLocal(args: Optional[List[str]] = None) -> Tuple[int, TaskCallback]:
+    parser, unknown = LocalArgumentParser().parse_known_args(args)
+
+    if parser.username is not None and parser.password is not None:
+        logging.getLogger("coretexpylib").info(">> [Coretex] Logging in with provided credentials")
+        response = networkManager.authenticate(parser.username, parser.password)
+    elif networkManager.hasStoredCredentials:
+        logging.getLogger("coretexpylib").info(">> [Coretex] Logging in with stored credentials")
+        response = networkManager.authenticateWithStoredCredentials()
+    else:
+        logging.getLogger("coretexpylib").info(">> [Coretex] Credentials not provided/stored")
+
+        username = input("Enter your username: ")
+        password = getpass("Enter your password: ")
+
+        response = networkManager.authenticate(username, password)
+
+    if response.hasFailed():
+        raise RuntimeError(">> [Coretex] Failed to authenticate")
+
+    if not os.path.exists("experiment.config"):
+        raise FileNotFoundError(">> [Coretex] \"experiment.config\" file not found")
+
+    experiment: Experiment = Experiment.runLocal(
+        parser.spaceId,
+        parser.name,
+        parser.description,
+        [parameter.encode() for parameter in _readExperimentConfig()]
+    )
+
+    logging.getLogger("coretexpylib").info(f">> [Coretex] Created local experiment with ID \"{experiment.id}\"")
+    experiment.updateStatus(ExperimentStatus.preparingToStart)
+
+    return experiment.id, LocalTaskCallback(experiment, response.json["refresh_token"])
