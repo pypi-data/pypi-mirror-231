15b2b602002bf8c66998b096a05903be8d8b12d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:04:28 +0000

    Add package calc-basic-movin/0.0.1/calc_basic_movin-0.0.1.tar.gz

---
+++ packages/calc-basic-movin/calc_basic_movin-0.0.1.tar.gz/calc_basic_movin-0.0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'calc_basic_movin',
+    version = '0.0.1',
+    description= 'Trial Basic calculator library',
+    #long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url = '',
+    author = 'Movin Fernandes',
+    author_email= 'movin.fernandes@hotmail.com',
+    license='MIT',
+    keywords='basic',
+    packages=find_packages(),
+    install_requires = '' 
+)
