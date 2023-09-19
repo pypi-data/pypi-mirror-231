fb60bce004c80f3d7cfd1c9b397cb9ea9926636e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:05:33 +0000

    Add package invenio-records-resources/4.11.2/invenio_records_resources-4.11.2-py2.py3-none-any.whl

---
+++ packages/invenio-records-resources/invenio_records_resources-4.11.2-py2.py3-none-any.whl/invenio_records_resources/services/records/params/pagination.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-Records-Resources is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Pagination parameter interpreter API."""
+
+from ....pagination import Pagination
+from ...errors import QuerystringValidationError
+from .base import ParamInterpreter
+
+
+class PaginationParam(ParamInterpreter):
+    """Pagination evaluator."""
+
+    def apply(self, identity, search, params):
+        """Evaluate the query str on the search."""
+        options = self.config.pagination_options
+
+        default_size = options["default_results_per_page"]
+
+        params.setdefault("size", default_size)
+        params.setdefault("page", 1)
+
+        p = Pagination(
+            params["size"],
+            params["page"],
+            options["default_max_results"],
+        )
+
+        if not p.valid():
+            raise QuerystringValidationError("Invalid pagination parameters.")
+
+        return search[p.from_idx : p.to_idx]
