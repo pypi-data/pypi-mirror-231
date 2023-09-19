7f7709b4fdf59b57ff1ef4ec2ec72ba608610714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:06:36 +0000

    Add package axsqlalchemy/0.0.51/axsqlalchemy-0.0.51-py3-none-any.whl

---
+++ packages/axsqlalchemy/axsqlalchemy-0.0.51-py3-none-any.whl/axsqlalchemy/schema.py
@@ -0,0 +1,33 @@
+from uuid import UUID
+from datetime import datetime
+from pydantic import BaseModel as _BaseModel, Field, root_validator
+
+
+class BaseModelAt(_BaseModel):
+    created_at: datetime = Field(default_factory=datetime.now)
+    updated_at: datetime | None = None
+
+
+class BaseModel(BaseModelAt):
+    class Config:
+        orm_mode = True
+
+
+class BaseModelUUID(BaseModel):
+    id: UUID | None = None
+
+
+class BaseModelInt(BaseModel):
+    id: int | None = None
+
+
+class IgnoreDeactiveSchema(BaseModel): 
+    @root_validator(pre=True)
+    def validate_value(cls, values):
+        if 'is_active' in values and not values['is_active']:
+            raise ValueError        
+        return values
+    
+    class Config:
+        skip_invalid_collections_items = True 
+
