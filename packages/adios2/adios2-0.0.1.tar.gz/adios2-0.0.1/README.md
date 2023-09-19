6f9bc03d5e5c504b3184d005d7ff497a14bc58c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package adios2/0.0.1/adios2-0.0.1.tar.gz

---
+++ packages/adios2/adios2-0.0.1.tar.gz/adios2-0.0.1/README.md
@@ -0,0 +1,4 @@
+# adios2
+adios2 - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/adios2/adios2-0.0.1.tar.gz/adios2-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='adios2',
+    version="1.0.0",
+    description='adios2 - listed by Cycode to prevent confusion',
+    long_description="adios2",
+    long_description_content_type='text/markdown',
+)
