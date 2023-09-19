64794739c977ce7934123ef6e4f5ccaca5e62016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package r-cmplot/0.0.1/r-cmplot-0.0.1.tar.gz

---
+++ packages/r-cmplot/r-cmplot-0.0.1.tar.gz/r-cmplot-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='r-cmplot',
+    version="1.0.0",
+    description='r-cmplot - listed by Cycode to prevent confusion',
+    long_description="r-cmplot",
+    long_description_content_type='text/markdown',
+)
