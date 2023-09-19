2c084c1dd1ef7488bde0af9587d1c00252cc585c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:04:41 +0000

    Add package nvfuser-cu117/0.0.19.dev20230918/nvfuser_cu117-0.0.19.dev20230918-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/nvfuser-cu117/nvfuser_cu117-0.0.19.dev20230918-cp310-cp310-manylinux_2_28_x86_64.whl/nvfuser/include/nvfuser/kernel_db/utils.h
@@ -0,0 +1,37 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#pragma once
+#include <string>
+#include <vector>
+
+#include <c10/macros/Export.h>
+
+namespace nvfuser {
+
+//! Helper methods to faciliate moving data between data buffers and files based
+//! on what type of data is being copied.
+
+TORCH_CUDA_CU_API bool append_to_text_file(
+    const std::string& file_path,
+    const std::string& src);
+
+TORCH_CUDA_CU_API bool copy_from_binary_file(
+    const std::string& file_path,
+    std::vector<char>& dst);
+TORCH_CUDA_CU_API bool copy_from_text_file(
+    const std::string& file_path,
+    std::string& dst);
+
+TORCH_CUDA_CU_API bool copy_to_binary_file(
+    const std::string& file_path,
+    const std::vector<char>& dst);
+TORCH_CUDA_CU_API bool copy_to_text_file(
+    const std::string& file_path,
+    const std::string& src);
+
+} // namespace nvfuser
---
+++ packages/nvfuser-cu117/nvfuser_cu117-0.0.19.dev20230918-cp310-cp310-manylinux_2_28_x86_64.whl/nvfuser/include/nvfuser/scheduler/matmul.h
@@ -0,0 +1,23 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#pragma once
+
+#include <ATen/core/ivalue.h>
+#include <exceptions.h>
+
+#include <fusion.h>
+#include <mma_type.h>
+#include <scheduler/matmul_heuristic.h>
+
+namespace nvfuser {
+
+TORCH_CUDA_CU_API void scheduleMatmul(
+    Fusion* fusion,
+    const MatmulParams& params);
+
+} // namespace nvfuser
