86e465646645f55f37ac9ae9852fa99577fb5c7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:05:59 +0000

    Add package invenio-pages/3.1.0/invenio-pages-3.1.0.tar.gz

---
+++ packages/invenio-pages/invenio-pages-3.1.0.tar.gz/invenio-pages-3.1.0/invenio_pages/services/schemas.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 CERN.
+#
+# Invenio-Pages is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""Pages schema."""
+
+from datetime import timezone
+
+from marshmallow import Schema, fields
+from marshmallow_utils.fields import SanitizedHTML, TZDateTime
+
+
+class PageSchema(Schema):
+    """Schema for page."""
+
+    id = fields.String()
+    url = fields.String(metadata={"create_only": True})
+    title = fields.String()
+    content = SanitizedHTML()
+    description = fields.String()
+    template_name = fields.String()
+    created = TZDateTime(timezone=timezone.utc, format="iso", dump_only=True)
+    updated = TZDateTime(timezone=timezone.utc, format="iso", dump_only=True)
