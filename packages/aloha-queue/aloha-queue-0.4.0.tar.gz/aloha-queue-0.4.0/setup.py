fbb9b0220e4203965957a3476773eb083a20acb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 16:05:18 +0000

    Add package aloha-queue/0.4.0/aloha-queue-0.4.0.tar.gz

---
+++ packages/aloha-queue/aloha-queue-0.4.0.tar.gz/aloha-queue-0.4.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="aloha-queue",
+    version="0.4.0",
+    packages=find_packages(),
+    author="Junle Yan",
+    author_email="junleyan.hi@gmail.com",
+    license="MIT"
+)
