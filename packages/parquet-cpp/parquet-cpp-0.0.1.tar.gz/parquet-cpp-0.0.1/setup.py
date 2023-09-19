bdcdababfe8b2091c540908322c68e4d23301b1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package parquet-cpp/0.0.1/parquet-cpp-0.0.1.tar.gz

---
+++ packages/parquet-cpp/parquet-cpp-0.0.1.tar.gz/parquet-cpp-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='parquet-cpp',
+    version="1.0.0",
+    description='parquet-cpp - listed by Cycode to prevent confusion',
+    long_description="parquet-cpp",
+    long_description_content_type='text/markdown',
+)
