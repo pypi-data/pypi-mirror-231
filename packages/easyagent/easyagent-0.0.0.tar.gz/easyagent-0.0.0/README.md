07c3768a502cb6925a37fc426fcbc6dbe8aaeb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:08:18 +0000

    Add package easyagent/0.0.0/easyagent-0.0.0.tar.gz

---
+++ packages/easyagent/easyagent-0.0.0.tar.gz/easyagent-0.0.0/README.md
@@ -0,0 +1,12 @@
+# Repository For Exploring Agents
+
+## Simple Agent That Executes And Plans
+
+A simple agent that executes and plans - `agent.py`.
+
+### Building out agents in teams
+
+
+### Built-In Logging Module
+
+
---
+++ packages/easyagent/easyagent-0.0.0.tar.gz/easyagent-0.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='easyagent',
+    version='0.0.0',
+    url='https://github.com/mypackage.git',
+    author='Jacky Wong',
+    author_email='colabdog@gmail.com',
+    description='Description of my package',
+    packages=find_packages(),    
+    install_requires=[],
+)
