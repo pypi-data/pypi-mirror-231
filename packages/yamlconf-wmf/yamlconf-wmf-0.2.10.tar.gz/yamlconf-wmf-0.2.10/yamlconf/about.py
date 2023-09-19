033bde11a192699975fcf0ed3c421d07d0da277e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:21 +0000

    Add package yamlconf-wmf/0.2.10/yamlconf-wmf-0.2.10.tar.gz

---
+++ packages/yamlconf-wmf/yamlconf-wmf-0.2.10.tar.gz/yamlconf-wmf-0.2.10/yamlconf/about.py
@@ -0,0 +1,11 @@
+__name__ = "yamlconf-wmf"
+__version__ = "0.2.10"
+__author__ = "Aaron Halfaker"
+__author_email__ = "aaron.halfaker@gmail.com"
+__description__ = "Yaml-based configuration file reader. This is a fork of the original yamlfconf package. " \
+                  "This fork is maintained by WMF Machine Learning Team"
+__license__ = "MIT"
+__url__ = "https://github.com/wikimedia/yamlconf"
+
+all = [__name__, __version__, __author__, __author_email__, __description__,
+       __license__, __url__]
