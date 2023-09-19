3f30b3f1f28b3f014fd4789e9e09cc201bfec692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:05:44 +0000

    Add package patchwork-core/0.2.5/patchwork-core-0.2.5.tar.gz

---
+++ packages/patchwork-core/patchwork-core-0.2.5.tar.gz/patchwork-core-0.2.5/tests/core/test_fnpath.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from typing import Type
+
+import pytest
+from pydantic import BaseModel
+
+from patchwork.core.typing import FuncPath
+
+
+class Result:
+    pass
+
+
+def example_function():
+    return Result
+
+
+async def example_coroutine():
+    return Result
+
+
+def test_function_no_type():
+    class M(BaseModel):
+        path: FuncPath
+
+    m = M(path='tests.core.test_fnpath:example_function')
+    assert m.path() == Result
+
+
+def test_function_default():
+    class M(BaseModel):
+        path: FuncPath = example_function
+
+    m = M()
+    assert m.path() == Result
+
+
+@pytest.mark.asyncio
+async def test_coroutine_no_type():
+    class M(BaseModel):
+        path: FuncPath
+
+    m = M(path='tests.core.test_fnpath:example_coroutine')
+    assert await m.path() == Result
+
+
+@pytest.mark.asyncio
+async def test_coroutine_default():
+    class M(BaseModel):
+        path: FuncPath = example_coroutine
+
+    m = M()
+    assert await m.path() == Result
