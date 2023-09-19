094239ffcd5d41e767ac12ce564af5b5e58fc1fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:39 +0000

    Add package pango/0.0.1/pango-0.0.1.tar.gz

---
+++ packages/pango/pango-0.0.1.tar.gz/pango-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pango',
+    version="1.0.0",
+    description='pango - listed by Cycode to prevent confusion',
+    long_description="pango",
+    long_description_content_type='text/markdown',
+)
