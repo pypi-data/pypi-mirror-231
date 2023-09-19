375a0276a953fca73bf3dc3dabe2f49f7212eb2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 15:07:37 +0000

    Add package recompyle/0.2.1/recompyle-0.2.1-py3-none-any.whl

---
+++ packages/recompyle/recompyle-0.2.1-py3-none-any.whl/recompyle/__init__.py
@@ -0,0 +1,4 @@
+from recompyle.applied import flat_profile, shallow_call_profiler
+from recompyle.decorators import rewrite_wrap_calls, wrap_calls
+
+__all__ = ["rewrite_wrap_calls", "wrap_calls", "flat_profile", "shallow_call_profiler"]
---
+++ packages/recompyle/recompyle-0.2.1-py3-none-any.whl/recompyle/applied/__init__.py
@@ -0,0 +1,4 @@
+"""Example tools that use recompyle features."""
+from recompyle.applied.flat_profiler import flat_profile, shallow_call_profiler
+
+__all__ = ["flat_profile", "shallow_call_profiler"]
