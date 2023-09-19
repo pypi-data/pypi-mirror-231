945530a0a92f5e41d592676e918eec2033886b26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:06:23 +0000

    Add package ntnx-vmm-py-client/4.0.3a1/ntnx_vmm_py_client-4.0.3a1-py3-none-any.whl

---
+++ packages/ntnx-vmm-py-client/ntnx_vmm_py_client-4.0.3a1-py3-none-any.whl/ntnx_vmm_py_client/api/__init__.py
@@ -0,0 +1,12 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_vmm_py_client.api.esxi_vm_api import EsxiVmApi
+from ntnx_vmm_py_client.api.images_api import ImagesApi
+from ntnx_vmm_py_client.api.placement_policies_api import PlacementPoliciesApi
+from ntnx_vmm_py_client.api.rate_limits_api import RateLimitsApi
+from ntnx_vmm_py_client.api.vm_api import VmApi
+from ntnx_vmm_py_client.api.vm_data_protection_api import VmDataProtectionApi
