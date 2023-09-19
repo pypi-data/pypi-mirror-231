dc78415e66f4e3e626e834bccf1203e041173e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:37 +0000

    Add package r-packrat/0.0.1/r-packrat-0.0.1.tar.gz

---
+++ packages/r-packrat/r-packrat-0.0.1.tar.gz/r-packrat-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='r-packrat',
+    version="1.0.0",
+    description='r-packrat - listed by Cycode to prevent confusion',
+    long_description="r-packrat",
+    long_description_content_type='text/markdown',
+)
