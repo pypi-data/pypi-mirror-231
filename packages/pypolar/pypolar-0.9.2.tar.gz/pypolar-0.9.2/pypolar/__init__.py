40155f2a60a6531d01f355d85d03a713a5459c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 16:05:01 +0000

    Add package pypolar/0.9.2/pypolar-0.9.2.tar.gz

---
+++ packages/pypolar/pypolar-0.9.2.tar.gz/pypolar-0.9.2/pypolar/__init__.py
@@ -0,0 +1,17 @@
+__version__ = '0.9.2'
+__author__ = 'Scott Prahl'
+__email__ = 'scott.prahl@oit.edu'
+__copyright__ = 'Copyright 2020-23, Scott Prahl'
+__license__ = 'MIT'
+__url__ = 'https://github.com/scottprahl/pypolar.git'
+
+from .fresnel import *
+from .jones import *
+from .mueller import *
+from .visualization import *
+
+from .sym_fresnel import *
+from .sym_jones import *
+from .sym_mueller import *
+
+from .ellipsometry import *
