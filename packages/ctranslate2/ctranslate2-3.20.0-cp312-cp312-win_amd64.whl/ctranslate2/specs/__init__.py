76030daa635c56ab7f7c5478bccfa7b65f93fd5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:00 +0000

    Add package ctranslate2/3.20.0/ctranslate2-3.20.0-cp312-cp312-win_amd64.whl

---
+++ packages/ctranslate2/ctranslate2-3.20.0-cp312-cp312-win_amd64.whl/ctranslate2/specs/__init__.py
@@ -0,0 +1,16 @@
+from ctranslate2.specs.attention_spec import RotaryScalingType
+from ctranslate2.specs.common_spec import Activation, EmbeddingsMerge
+from ctranslate2.specs.model_spec import (
+    LanguageModelSpec,
+    LayerSpec,
+    ModelSpec,
+    SequenceToSequenceModelSpec,
+)
+from ctranslate2.specs.transformer_spec import (
+    TransformerDecoderModelSpec,
+    TransformerDecoderSpec,
+    TransformerEncoderModelSpec,
+    TransformerEncoderSpec,
+    TransformerSpec,
+)
+from ctranslate2.specs.whisper_spec import WhisperSpec
