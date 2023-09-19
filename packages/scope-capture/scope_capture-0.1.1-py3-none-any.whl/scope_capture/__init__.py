79e796a639fa7019a6e78d308cc4840c651ddafd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:04:47 +0000

    Add package scope-capture/0.1.1/scope_capture-0.1.1-py3-none-any.whl

---
+++ packages/scope-capture/scope_capture-0.1.1-py3-none-any.whl/scope_capture/__init__.py
@@ -0,0 +1,27 @@
+import inspect
+from types import FunctionType
+
+def _make_cell(value):
+    fn = (lambda x: lambda: x)(value)
+    return fn.__closure__[0]
+
+def capture(f):
+    try:
+        frame = inspect.currentframe()
+        fake_globals = {}
+        fake_globals.update(f.__globals__)
+        fake_globals.update(frame.f_back.f_locals)
+        captured_cells = []
+        if f.__closure__:
+            for cell in f.__closure__:
+                captured_cells.append(_make_cell(cell.cell_contents))
+        call_fn = FunctionType(
+            code=f.__code__,
+            globals=fake_globals,
+            name=f.__name__,
+            argdefs=f.__defaults__,
+            closure=tuple(captured_cells),
+        )
+        return call_fn
+    finally:
+        del frame
