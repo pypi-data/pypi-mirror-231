f6208f5425c697be6c90ca4a3854f343d009399c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:06:23 +0000

    Add package ntnx-prism-py-client/4.0.3a2/ntnx-prism-py-client-4.0.3a2.tar.gz

---
+++ packages/ntnx-prism-py-client/ntnx-prism-py-client-4.0.3a2.tar.gz/ntnx-prism-py-client-4.0.3a2/ntnx_prism_py_client/api/__init__.py
@@ -0,0 +1,15 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_prism_py_client.api.alert_email_configuration_api import AlertEmailConfigurationApi
+from ntnx_prism_py_client.api.alerts_api import AlertsApi
+from ntnx_prism_py_client.api.audits_api import AuditsApi
+from ntnx_prism_py_client.api.categories_api import CategoriesApi
+from ntnx_prism_py_client.api.events_api import EventsApi
+from ntnx_prism_py_client.api.manage_alert_api import ManageAlertApi
+from ntnx_prism_py_client.api.system_defined_policies_api import SystemDefinedPoliciesApi
+from ntnx_prism_py_client.api.task_api import TaskApi
+from ntnx_prism_py_client.api.user_defined_policies_api import UserDefinedPoliciesApi
