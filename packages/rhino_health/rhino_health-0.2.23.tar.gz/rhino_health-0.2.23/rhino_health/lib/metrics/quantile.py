693bb1270d36c59b955f1dec6985c9d9f7cb8d33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:05:20 +0000

    Add package rhino_health/0.2.23/rhino_health-0.2.23.tar.gz

---
+++ packages/rhino_health/rhino_health-0.2.23.tar.gz/rhino_health-0.2.23/rhino_health/lib/metrics/quantile.py
@@ -0,0 +1,37 @@
+from typing import Union
+
+from rhino_health.lib.metrics.base_metric import BaseMetric
+from rhino_health.lib.metrics.filter_variable import FilterVariableTypeOrColumnName
+
+
+class Median(BaseMetric):
+    """
+    Returns the median of entries for a specified VARIABLE
+    """
+
+    variable: FilterVariableTypeOrColumnName
+
+    @classmethod
+    def metric_name(cls):
+        return "median"
+
+    @classmethod
+    def uses_cloud_aggregation(cls):
+        return True
+
+
+class Percentile(BaseMetric):
+    """
+    Returns the k-percentile of entries for a specified VARIABLE
+    """
+
+    variable: FilterVariableTypeOrColumnName
+    percentile: Union[int, float]
+
+    @classmethod
+    def metric_name(cls):
+        return "percentile"
+
+    @classmethod
+    def uses_cloud_aggregation(cls):
+        return True
