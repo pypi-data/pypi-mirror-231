00bf92894cd3b7620c66351c012191eb4677e6cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 16:05:27 +0000

    Add package ooba-api-client/0.1.0a1/ooba_api_client-0.1.0a1-py3-none-any.whl

---
+++ packages/ooba-api-client/ooba_api_client-0.1.0a1-py3-none-any.whl/ooba_api/prompts.py
@@ -0,0 +1,57 @@
+import textwrap
+from typing import TYPE_CHECKING
+
+import pydantic
+
+
+class Prompt(pydantic.BaseModel):
+    negative_prompt: str | None = None
+    prompt: str
+
+    def full_prompt(self) -> str:
+        return self.prompt
+
+
+class InstructPrompt(Prompt):
+    """
+    Used for instructions
+    """
+
+    instruct_template: str = "{prompt}"
+
+    def full_prompt(self) -> str:
+        return self.instruct_template.format(prompt=self.prompt)
+
+
+if not TYPE_CHECKING:
+    Messages = pydantic.conlist(dict, min_length=1)
+else:
+    Messages = list
+
+
+class ChatPrompt(Prompt):
+    """
+    Used for chat
+
+    Not tested, not implemented, just placeholder
+    """
+
+    messages: Messages
+
+
+class LlamaInstructPrompt(Prompt):
+    """
+    Used for llama, llama 2, code llama, etc
+    """
+
+    system_prompt: str = ""
+    instruct_template: str = textwrap.dedent(
+        """
+        [INST] <<SYS>> {system_prompt} <</SYS>> {user_prompt} [/INST]
+        """
+    ).strip()
+
+    def full_prompt(self) -> str:
+        return self.instruct_template.format(
+            system_prompt=self.system_prompt, user_prompt=self.prompt
+        )
