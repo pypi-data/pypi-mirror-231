e12213e9b729e215816af3fd78290dfb1aecb8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package graphite2/0.0.1/graphite2-0.0.1.tar.gz

---
+++ packages/graphite2/graphite2-0.0.1.tar.gz/graphite2-0.0.1/README.md
@@ -0,0 +1,4 @@
+# graphite2
+graphite2 - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/graphite2/graphite2-0.0.1.tar.gz/graphite2-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='graphite2',
+    version="1.0.0",
+    description='graphite2 - listed by Cycode to prevent confusion',
+    long_description="graphite2",
+    long_description_content_type='text/markdown',
+)
