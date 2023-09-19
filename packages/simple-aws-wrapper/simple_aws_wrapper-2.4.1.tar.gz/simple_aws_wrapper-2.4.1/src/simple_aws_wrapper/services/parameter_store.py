849b716a0f6bc353e4a41f2615c1b4efe7cd7e06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:40 +0000

    Add package simple-aws-wrapper/2.4.1/simple_aws_wrapper-2.4.1.tar.gz

---
+++ packages/simple-aws-wrapper/simple_aws_wrapper-2.4.1.tar.gz/simple_aws_wrapper-2.4.1/src/simple_aws_wrapper/services/parameter_store.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from simple_aws_wrapper.config import AWSConfig
+from simple_aws_wrapper.const import services
+from simple_aws_wrapper.exceptions.exceptions import MissingConfigurationException, GenericException
+from simple_aws_wrapper.resource_manager import ResourceManager
+
+
+class ParameterStore:
+    """
+    Classe per la gestione del servizio ParameterStore di AWS
+    """
+
+    def __init__(self):
+        if not AWSConfig().is_configured():
+            raise MissingConfigurationException
+        self.region_name = AWSConfig().get_region_name()
+        self.endpoint_url = AWSConfig().get_endpoint_url()
+        if self.endpoint_url and self.endpoint_url != "":
+            self.client = ResourceManager.get_client(services.SSM, self.region_name, self.endpoint_url)
+        else:
+            self.client = ResourceManager.get_client(services.SSM, self.region_name)
+    def get_parameters_values_from_list(self, parameters_list: list) -> dict:
+        """
+        Funzione per il recupero dei valori dal servizio Parameter Store a partire dalla lista dei nomi dei parametri
+        da recuperare
+        :param parameters_list: lista dei nomi dei parametri di cui recuperare il valore
+        :return: dizionario {"<nome_parametro>": "<valore_parametro>"}
+        """
+        output_dict: dict = {}
+        for parameter in parameters_list:
+            try:
+                output_dict[parameter] = self.client.get_parameters(Names=[parameter], WithDecryption=True)["Parameters"][0][
+                    "Value"
+                ]
+            except:
+                print(f"Errore nel recupero della chiave {parameter} dal parameter store")
+                output_dict[parameter] = ""
+                raise GenericException
+        return output_dict
