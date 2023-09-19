ad14b7edb4b10652567cb56b42da0743ecf7c6d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:05:05 +0000

    Add package ttcore/0.1.19/ttcore-0.1.19.tar.gz

---
+++ packages/ttcore/ttcore-0.1.19.tar.gz/ttcore-0.1.19/setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="ttcore",
+    version="0.1.19",
+    author="Martin F",
+    author_email="pypi.org@tigerteamx.com",
+    description="Atomic Batteries Included. Used by https://tigerteamx.com to maximize producitivty.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tigerteamx/ttcore",
+    packages=['ttcore'],
+    package_data={
+        'ttcore': [
+            'docs.html',
+            'admin.html',
+        ]
+    },
+    install_requires=[],  # Used for dependencies
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': ['ttcore = ttcore.cli:cli']
+    },
+    keywords=['productivity', 'bottlepy', 'peewee'],
+    python_requires='>=3.8',
+    include_package_data=True,
+)
