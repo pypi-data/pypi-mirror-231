2c49b073de77805b5bcdb12749011ba7a50b2120
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package rust-gnu/0.0.1/rust-gnu-0.0.1.tar.gz

---
+++ packages/rust-gnu/rust-gnu-0.0.1.tar.gz/rust-gnu-0.0.1/README.md
@@ -0,0 +1,4 @@
+# rust-gnu
+rust-gnu - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/rust-gnu/rust-gnu-0.0.1.tar.gz/rust-gnu-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='rust-gnu',
+    version="1.0.0",
+    description='rust-gnu - listed by Cycode to prevent confusion',
+    long_description="rust-gnu",
+    long_description_content_type='text/markdown',
+)
