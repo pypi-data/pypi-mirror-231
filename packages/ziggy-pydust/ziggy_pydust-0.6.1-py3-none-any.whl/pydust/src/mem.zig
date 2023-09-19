2aa9041b9db6ed6b0004866a76be0284ac49f7e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:08:54 +0000

    Add package ziggy-pydust/0.6.1/ziggy_pydust-0.6.1-py3-none-any.whl

---
+++ packages/ziggy-pydust/ziggy_pydust-0.6.1-py3-none-any.whl/pydust/src/mem.zig
@@ -0,0 +1,51 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const ffi = @import("ffi.zig");
+
+pub const PyMemAllocator = struct {
+    const Self = @This();
+
+    pub fn allocator(self: *const Self) Allocator {
+        return .{
+            .ptr = @constCast(self),
+            .vtable = &.{
+                .alloc = alloc,
+                .resize = resize,
+                .free = free,
+            },
+        };
+    }
+
+    fn alloc(ctx: *anyopaque, len: usize, ptr_align: u8, ret_addr: usize) ?[*]u8 {
+        _ = ret_addr;
+        _ = ptr_align;
+        _ = ctx;
+        return @ptrCast(ffi.PyMem_Malloc(len));
+    }
+
+    fn resize(ctx: *anyopaque, buf: []u8, buf_align: u8, new_len: usize, ret_addr: usize) bool {
+        _ = ret_addr;
+        _ = buf_align;
+        _ = ctx;
+        return ffi.PyMem_Realloc(buf.ptr, new_len) != null;
+    }
+
+    fn free(ctx: *anyopaque, buf: []u8, buf_align: u8, ret_addr: usize) void {
+        _ = ret_addr;
+        _ = buf_align;
+        _ = ctx;
+        ffi.PyMem_Free(buf.ptr);
+    }
+}{};
