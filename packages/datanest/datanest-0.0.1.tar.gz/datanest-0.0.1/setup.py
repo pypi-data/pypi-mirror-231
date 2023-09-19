5785a4022e546d76fdf0064f1cc9ec9a02fe88ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:05:52 +0000

    Add package datanest/0.0.1/datanest-0.0.1.tar.gz

---
+++ packages/datanest/datanest-0.0.1.tar.gz/datanest-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='datanest',
+    version='0.0.1',
+    description='A group of Datasets for you project',
+    author='Ethan Barr',
+    author_email='ethanwbarr07@gmail.com',
+    url='https://github.com/Ethan-Barr/Datanest',
+    packages=find_packages(),
+    install_requires=[
+        # Add dependencies
+    ],
+)
