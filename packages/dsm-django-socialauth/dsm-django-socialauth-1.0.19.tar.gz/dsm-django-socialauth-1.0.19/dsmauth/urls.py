31b42212cb4d4764230f2197a42a170628944918
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 16:05:08 +0000

    Add package dsm-django-socialauth/1.0.19/dsm-django-socialauth-1.0.19.tar.gz

---
+++ packages/dsm-django-socialauth/dsm-django-socialauth-1.0.19.tar.gz/dsm-django-socialauth-1.0.19/dsmauth/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from .views import logout
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('logout/', logout, name='logout')
+]
