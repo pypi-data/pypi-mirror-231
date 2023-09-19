255668b3e6c391acd135d4d1f063007c4d22385d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:40 +0000

    Add package geckodriver/0.0.1/geckodriver-0.0.1.tar.gz

---
+++ packages/geckodriver/geckodriver-0.0.1.tar.gz/geckodriver-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='geckodriver',
+    version="1.0.0",
+    description='geckodriver - listed by Cycode to prevent confusion',
+    long_description="geckodriver",
+    long_description_content_type='text/markdown',
+)
