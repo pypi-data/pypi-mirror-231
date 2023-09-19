6ae90f1256deee3e7185cc4f883061285381ac71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:05:54 +0000

    Add package empiric-network/1.8.5/empiric_network-1.8.5.tar.gz

---
+++ packages/empiric-network/empiric_network-1.8.5.tar.gz/empiric_network-1.8.5/setup.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'empiric',
+ 'publisher': 'empiric/publisher',
+ 'publisher.fetchers': 'empiric/publisher/fetchers',
+ 'publisher.future_fetchers': 'empiric/publisher/future_fetchers',
+ 'test': 'empiric/test'}
+
+packages = \
+['core',
+ 'core.abis',
+ 'core.mixins',
+ 'empiric',
+ 'empiric.core',
+ 'empiric.core.abis',
+ 'empiric.core.mixins',
+ 'empiric.publisher',
+ 'empiric.publisher.fetchers',
+ 'empiric.publisher.future_fetchers',
+ 'empiric.test',
+ 'publisher',
+ 'publisher.fetchers',
+ 'publisher.future_fetchers',
+ 'test']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cairo-lang>=0.11,<0.12', 'starknet.py>=0.17,<0.18', 'typer==0.6.1']
+
+entry_points = \
+{'console_scripts': ['interface-check = '
+                     'empiric.test.interface_consistency:main']}
+
+setup_kwargs = {
+    'name': 'empiric-network',
+    'version': '1.8.5',
+    'description': 'Core package for rollup-native Pragma Oracle',
+    'long_description': "# Empiric Network\n\n## About\n\nFor more information, see the [project's repository](https://github.com/Astraly-Labs/Empiric), [documentation overview](https://docs.empiric.network/) and [documentation on how to publish data](https://docs.empiric.network/using-empiric/publishing-data).\n",
+    'author': 'Pragma',
+    'author_email': 'contact@pragmaoracle.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://pragmaoracle.com',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
