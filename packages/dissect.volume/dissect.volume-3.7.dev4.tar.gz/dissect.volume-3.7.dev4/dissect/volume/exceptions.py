b3a94a70cb22a55d8a4d46a4cd8d10153fe470a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:04:36 +0000

    Add package dissect.volume/3.7.dev4/dissect.volume-3.7.dev4.tar.gz

---
+++ packages/dissect.volume/dissect.volume-3.7.dev4.tar.gz/dissect.volume-3.7.dev4/dissect/volume/exceptions.py
@@ -0,0 +1,22 @@
+class Error(Exception):
+    pass
+
+
+class LVM2Error(Error):
+    pass
+
+
+class DiskError(Error):
+    pass
+
+
+class RAIDError(Error):
+    pass
+
+
+class DMError(Error):
+    pass
+
+
+class MDError(Error):
+    pass
