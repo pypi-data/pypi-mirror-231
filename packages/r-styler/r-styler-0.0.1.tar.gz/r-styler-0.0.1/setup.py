8a8f52a1ed28e0923a831d82355bc58564700d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package r-styler/0.0.1/r-styler-0.0.1.tar.gz

---
+++ packages/r-styler/r-styler-0.0.1.tar.gz/r-styler-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='r-styler',
+    version="1.0.0",
+    description='r-styler - listed by Cycode to prevent confusion',
+    long_description="r-styler",
+    long_description_content_type='text/markdown',
+)
