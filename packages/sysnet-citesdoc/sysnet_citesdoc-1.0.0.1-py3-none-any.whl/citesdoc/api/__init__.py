98d5a0ba20db8269335db4c3759fadecc9b0c122
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:04:47 +0000

    Add package sysnet-citesdoc/1.0.0.1/sysnet_citesdoc-1.0.0.1-py3-none-any.whl

---
+++ packages/sysnet-citesdoc/sysnet_citesdoc-1.0.0.1-py3-none-any.whl/citesdoc/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from citesdoc.api.admins_api import AdminsApi
+from citesdoc.api.developers_api import DevelopersApi
+from citesdoc.api.public_api import PublicApi
