b7a97d88b10ac5eb67a5a83cc7dd4d5dc37c9567
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package libglvnd-egl-cos7-ppc64le/0.0.1/libglvnd-egl-cos7-ppc64le-0.0.1.tar.gz

---
+++ packages/libglvnd-egl-cos7-ppc64le/libglvnd-egl-cos7-ppc64le-0.0.1.tar.gz/libglvnd-egl-cos7-ppc64le-0.0.1/README.md
@@ -0,0 +1,4 @@
+# libglvnd-egl-cos7-ppc64le
+libglvnd-egl-cos7-ppc64le - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/libglvnd-egl-cos7-ppc64le/libglvnd-egl-cos7-ppc64le-0.0.1.tar.gz/libglvnd-egl-cos7-ppc64le-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='libglvnd-egl-cos7-ppc64le',
+    version="1.0.0",
+    description='libglvnd-egl-cos7-ppc64le - listed by Cycode to prevent confusion',
+    long_description="libglvnd-egl-cos7-ppc64le",
+    long_description_content_type='text/markdown',
+)
