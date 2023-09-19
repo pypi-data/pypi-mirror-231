f8fec202d79250c1c464dca4975baa53fe8afaff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:37 +0000

    Add package coredns/0.0.1/coredns-0.0.1.tar.gz

---
+++ packages/coredns/coredns-0.0.1.tar.gz/coredns-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='coredns',
+    version="1.0.0",
+    description='coredns - listed by Cycode to prevent confusion',
+    long_description="coredns",
+    long_description_content_type='text/markdown',
+)
