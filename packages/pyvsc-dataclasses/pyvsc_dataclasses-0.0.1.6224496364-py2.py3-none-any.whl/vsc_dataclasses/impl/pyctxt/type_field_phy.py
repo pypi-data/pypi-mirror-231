8f36ebab82180d0f669ff46fafed4f6fdd76e593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:08:01 +0000

    Add package pyvsc-dataclasses/0.0.1.6224496364/pyvsc_dataclasses-0.0.1.6224496364-py2.py3-none-any.whl

---
+++ packages/pyvsc-dataclasses/pyvsc_dataclasses-0.0.1.6224496364-py2.py3-none-any.whl/vsc_dataclasses/impl/pyctxt/type_field_phy.py
@@ -0,0 +1,21 @@
+
+import vsc_dataclasses.impl.context as ctxt_api
+from .model_val import ModelVal
+from .type_field import TypeField
+
+
+
+class TypeFieldPhy(ctxt_api.TypeFieldPhy, TypeField):
+
+    def __init__(self, name, dtype, attr, init):
+        TypeField.__init__(self, name, dtype, attr)
+        self._init = ModelVal()
+        if init is not None:
+            self._init.setBits(init.bits())
+            self._init.set_val_u(init.val_u())
+
+    def getInit(self) -> 'ModelVal':
+        return self._init
+    
+    def accept(self, v):
+        v.visitTypeFieldPhy(self)
