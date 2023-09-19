27ca9405f8e9456a37bf0300b0fd5f8b7ee6c2d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:06:30 +0000

    Add package vecked/1.2.0/vecked-1.2.0-py3-none-any.whl

---
+++ packages/vecked/vecked-1.2.0-py3-none-any.whl/vecked/__init__.py
@@ -0,0 +1,11 @@
+from vecked.types import TNumeric
+from vecked.vector2 import Vector2, Vector2f, Vector2i
+from vecked.version import get_version
+
+__all__ = [
+    "TNumeric",
+    "Vector2",
+    "Vector2f",
+    "Vector2i",
+    "get_version",
+]
---
+++ packages/vecked/vecked-1.2.0-py3-none-any.whl/vecked/vector2/vector2f.py
@@ -0,0 +1,71 @@
+from __future__ import annotations
+
+from vecked.vector2.vector2 import Vector2
+
+
+class Vector2f(Vector2[float]):
+    """
+    A two-dimensional floating-point vector.
+    """
+
+    def __add__(self, other: object) -> Vector2f:
+        if isinstance(other, Vector2):
+            return self.add_vector2(other)
+
+        raise ValueError("cannot add %s to %s" % (repr(other), repr(self)))
+
+    def __mul__(self, other: object) -> Vector2f:
+        if isinstance(other, float | int):
+            return self.multiply_length(other)
+
+        if isinstance(other, Vector2):
+            return self.multiply_vector2(other)
+
+        raise ValueError("cannot multiply %s by %s" % (repr(self), repr(other)))
+
+    def __sub__(self, other: object) -> Vector2f:
+        if isinstance(other, Vector2):
+            return self.subtract_vector2(other)
+
+        raise ValueError("cannot subtract %s from %s" % (repr(other), repr(self)))
+
+    def add_vector2(self, length: Vector2[float | int]) -> Vector2f:
+        """
+        Returns the addition of this vector to another.
+        """
+
+        return self.__class__(
+            self._x + length.x,
+            self._y + length.y,
+        )
+
+    def multiply_length(self, length: float | int) -> Vector2f:
+        """
+        Returns the multiplication of this vector by a length in both
+        dimensions.
+        """
+
+        return self.__class__(
+            self._x * length,
+            self._y * length,
+        )
+
+    def multiply_vector2(self, length: Vector2[float | int]) -> Vector2f:
+        """
+        Returns the multiplication of this vector by another.
+        """
+
+        return self.__class__(
+            self._x * length.x,
+            self._y * length.y,
+        )
+
+    def subtract_vector2(self, length: Vector2[float | int]) -> Vector2f:
+        """
+        Returns the subtraction of a two-dimensional vector from this.
+        """
+
+        return Vector2f(
+            self._x - length.x,
+            self._y - length.y,
+        )
