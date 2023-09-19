8575c7382af9c761d6d96266906b1dd967877001
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:21 +0000

    Add package metaheuristic-designer/0.1.1/metaheuristic_designer-0.1.1-py3-none-any.whl

---
+++ packages/metaheuristic-designer/metaheuristic_designer-0.1.1-py3-none-any.whl/metaheuristic_designer/utils.py
@@ -0,0 +1,16 @@
+from enum import Enum 
+import numpy as np
+import json
+
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        elif isinstance(obj, Enum):
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
