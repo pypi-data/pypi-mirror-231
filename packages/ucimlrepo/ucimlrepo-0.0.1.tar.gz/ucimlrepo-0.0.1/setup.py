95d6f035e56f3e7f1a4a86c14e3af431ce9a3cc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:08:29 +0000

    Add package ucimlrepo/0.0.1/ucimlrepo-0.0.1.tar.gz

---
+++ packages/ucimlrepo/ucimlrepo-0.0.1.tar.gz/ucimlrepo-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+      name='ucimlrepo',
+      version='0.0.1',
+      description='A Python interface to import datasets from the UCI Machine Learning Repository',
+      url='https://github.com/uci-ml-repo/ucimlrepo',
+      author='Philip Truong',
+      author_email='philtr928@example.com',
+      license='MIT',
+      package_dir={'': 'src'},
+      packages=find_packages(where='src')
+)
