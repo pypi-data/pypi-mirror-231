e1539c00c72f1539c35f44970532e75edc53496e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:05:19 +0000

    Add package candle-nn/0.2.3/candle_nn-0.2.3.tar.gz

---
+++ packages/candle-nn/candle_nn-0.2.3.tar.gz/candle_nn-0.2.3/local_dependencies/candle-nn/src/lib.rs
@@ -0,0 +1,35 @@
+pub mod activation;
+pub mod batch_norm;
+pub mod conv;
+pub mod embedding;
+pub mod func;
+pub mod group_norm;
+pub mod init;
+pub mod layer_norm;
+pub mod linear;
+pub mod loss;
+pub mod ops;
+pub mod optim;
+pub mod rnn;
+pub mod var_builder;
+pub mod var_map;
+
+pub use activation::Activation;
+pub use batch_norm::{batch_norm, BatchNorm, BatchNormConfig};
+pub use conv::{
+    conv1d, conv2d, conv2d_no_bias, conv_transpose2d, conv_transpose2d_no_bias, Conv1d,
+    Conv1dConfig, Conv2d, Conv2dConfig, ConvTranspose2d, ConvTranspose2dConfig,
+};
+pub use embedding::{embedding, Embedding};
+pub use func::{func, Func};
+pub use group_norm::{group_norm, GroupNorm};
+pub use init::Init;
+pub use layer_norm::{layer_norm, rms_norm, LayerNorm, LayerNormConfig, RmsNorm};
+pub use linear::{linear, linear_no_bias, Linear};
+pub use ops::Dropout;
+pub use optim::{AdamW, Optimizer, ParamsAdamW, SGD};
+pub use rnn::{gru, lstm, GRUConfig, LSTMConfig, GRU, LSTM, RNN};
+pub use var_builder::VarBuilder;
+pub use var_map::VarMap;
+
+pub use candle::Module;
