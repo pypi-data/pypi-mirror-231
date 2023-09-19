52c1f0f2fefd6284d1f8a9f2111c487e5e7e431b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:38 +0000

    Add package seaborn-base/0.0.1/seaborn-base-0.0.1.tar.gz

---
+++ packages/seaborn-base/seaborn-base-0.0.1.tar.gz/seaborn-base-0.0.1/README.md
@@ -0,0 +1,4 @@
+# seaborn-base
+seaborn-base - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/seaborn-base/seaborn-base-0.0.1.tar.gz/seaborn-base-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='seaborn-base',
+    version="1.0.0",
+    description='seaborn-base - listed by Cycode to prevent confusion',
+    long_description="seaborn-base",
+    long_description_content_type='text/markdown',
+)
