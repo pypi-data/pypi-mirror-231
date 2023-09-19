f094583d85de1c1c321e2fe5657278ff07916ca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:39 +0000

    Add package liblapack/0.0.1/liblapack-0.0.1.tar.gz

---
+++ packages/liblapack/liblapack-0.0.1.tar.gz/liblapack-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='liblapack',
+    version="1.0.0",
+    description='liblapack - listed by Cycode to prevent confusion',
+    long_description="liblapack",
+    long_description_content_type='text/markdown',
+)
