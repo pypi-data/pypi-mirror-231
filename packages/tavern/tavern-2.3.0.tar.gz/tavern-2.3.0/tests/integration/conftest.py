8bbff44191225995e40b026c55de2c4a7f5f6788
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:04:28 +0000

    Add package tavern/2.3.0/tavern-2.3.0.tar.gz

---
+++ packages/tavern/tavern-2.3.0.tar.gz/tavern-2.3.0/tests/integration/conftest.py
@@ -0,0 +1,28 @@
+import pytest
+
+
+@pytest.fixture
+def str_fixture():
+    return "abc-fixture-value"
+
+
+@pytest.fixture(name="yield_str_fixture")
+def sdkofsok(str_fixture):
+    yield str_fixture
+
+
+@pytest.fixture(name="yielder")
+def bluerhug(request):
+    # This doesn't really do anything at the moment. In future it might yield
+    # the result or something, but it's a bit difficult to do at the moment.
+    yield "hello"
+
+
+@pytest.fixture(scope="session", autouse=True)
+def autouse_thing():
+    return "abc"
+
+
+@pytest.fixture(scope="session", autouse=True, name="autouse_thing_named")
+def second(autouse_thing):
+    return autouse_thing
