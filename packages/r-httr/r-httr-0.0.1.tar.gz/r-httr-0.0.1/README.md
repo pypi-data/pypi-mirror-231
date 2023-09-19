d25f8094f5807107aac29e92a0a4466e8c855c9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package r-httr/0.0.1/r-httr-0.0.1.tar.gz

---
+++ packages/r-httr/r-httr-0.0.1.tar.gz/r-httr-0.0.1/README.md
@@ -0,0 +1,4 @@
+# r-httr
+r-httr - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/r-httr/r-httr-0.0.1.tar.gz/r-httr-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='r-httr',
+    version="1.0.0",
+    description='r-httr - listed by Cycode to prevent confusion',
+    long_description="r-httr",
+    long_description_content_type='text/markdown',
+)
