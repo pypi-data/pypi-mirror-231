fa4848671767cca6d652b2c74c37ec1b1fc05414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 15:07:20 +0000

    Add package equoai/0.0.2/equoai-0.0.2.tar.gz

---
+++ packages/equoai/equoai-0.0.2.tar.gz/equoai-0.0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="equoai",
+    version="0.0.2",
+    author="David Hostler",
+    description="Python client for accessing the EquoAI platform and all its core services",
+    packages=["equoai"]
+)
