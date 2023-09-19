ff34765c349a4817f51679e68c91b08961f99a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package libsystemd0/0.0.1/libsystemd0-0.0.1.tar.gz

---
+++ packages/libsystemd0/libsystemd0-0.0.1.tar.gz/libsystemd0-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='libsystemd0',
+    version="1.0.0",
+    description='libsystemd0 - listed by Cycode to prevent confusion',
+    long_description="libsystemd0",
+    long_description_content_type='text/markdown',
+)
