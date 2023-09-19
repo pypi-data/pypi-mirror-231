502560394b1bb1626098e541803063c845836881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:08:21 +0000

    Add package dksinha/0.1/dksinha-0.1.tar.gz

---
+++ packages/dksinha/dksinha-0.1.tar.gz/dksinha-0.1/setup.py
@@ -0,0 +1,10 @@
+from  setuptools import setup
+
+setup(
+    name="dksinha",
+    version="0.1",
+    description="Hardcore library",
+    author="danishk sinha",
+    author_email="thisisdanishk@gmail.com",
+    packages=["dksinha"]
+)
