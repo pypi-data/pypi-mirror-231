a77c3a191b5b8523b311f910fd40f4df30252860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:04:05 +0000

    Add package packagenlp/0.0.25/packagenlp-0.0.25.tar.gz

---
+++ packages/packagenlp/packagenlp-0.0.25.tar.gz/packagenlp-0.0.25/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+setuptools.setup(
+    name="packagenlp",
+    packages=['packagenlp'],
+    version="0.0.25",
+    author="Business & Décision",
+    description="A package for NLP",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords=["NLP", "Français", "Langues", "Traitements"],
+    download_url="https://gitlab.com/business-decision-data-science/packagenlp/-/package_files/82622627/download",
+    python_requires='>=3.6',
+    install_requires=[
+        'pandas',
+        'nltk',
+        'spacy',
+        'unidecode',
+        'treetaggerwrapper',
+        'fr_core_news_sm',
+    ],
+    include_package_data=True,
+    package_data={'': ['data/stopWords_spacy_en.csv', 'data/stopWords_spacy_fr.csv']},
+) 
