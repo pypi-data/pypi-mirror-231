46fe97d1beebf2b84377691bffc07e7e532b6a7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:06:02 +0000

    Add package gigglygazelle-upgrader/0.1.0/gigglygazelle_upgrader-0.1.0-py3-none-any.whl

---
+++ packages/gigglygazelle-upgrader/gigglygazelle_upgrader-0.1.0-py3-none-any.whl/gigglygazelle_upgrader/upgrader.py
@@ -0,0 +1,23 @@
+import sys
+import time
+
+
+def upgrade(device: str, image_name: str) -> None:
+    # Pretend function that upgrades a device.
+    # It's very convincing.
+    print(f"Connecting to {device}...")
+    time.sleep(2)
+    print("Connected! Upgrading...")
+    _pretend_loading_bar()
+    print(f"{device} upgraede to {image_name}")
+
+
+def _pretend_loading_bar() -> None:
+    # As fake as Microsoft's
+    bar = ""
+    for i in range(9):
+        sys.stdout.write(bar + "\r")
+        sys.stdout.flush()
+        bar += "."
+        time.sleep(0.3)
+    print("..........")
