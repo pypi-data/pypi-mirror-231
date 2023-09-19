37b7f58085de3feb130381cfe6fce1aab1f62167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:05:28 +0000

    Add package sc-concurrent-log-handler/0.0.14/sc-concurrent-log-handler-0.0.14.tar.gz

---
+++ packages/sc-concurrent-log-handler/sc-concurrent-log-handler-0.0.14.tar.gz/sc-concurrent-log-handler-0.0.14/src/ssc_concurrent_log_handler/__version__.py
@@ -0,0 +1,10 @@
+__title__ = "sc-concurrent-log-handler"
+__description__ = (
+    "SSC Enhanced RotatingFileHandler replacement with concurrency, gzip and Windows support"
+)
+__url__ = ""
+__version__ = "0.0.14"
+__author__ = "Aric Yang"
+__author_email__ = "yangzhao9@baidu.com"
+__license__ = "http://www.apache.org/licenses/LICENSE-2.0"
+__copyright__ = "Copyright 2023 Aric Yang"
