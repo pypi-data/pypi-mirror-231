21c429ee11861a7934e5f7ea403f17a8edc39444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package nwchem/0.0.1/nwchem-0.0.1.tar.gz

---
+++ packages/nwchem/nwchem-0.0.1.tar.gz/nwchem-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='nwchem',
+    version="1.0.0",
+    description='nwchem - listed by Cycode to prevent confusion',
+    long_description="nwchem",
+    long_description_content_type='text/markdown',
+)
