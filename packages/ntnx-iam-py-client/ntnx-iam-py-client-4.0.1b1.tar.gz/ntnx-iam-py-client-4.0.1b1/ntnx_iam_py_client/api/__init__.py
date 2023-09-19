a0eaa6c4830e50594a096102b0592bcf0c6c1152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:06:24 +0000

    Add package ntnx-iam-py-client/4.0.1b1/ntnx-iam-py-client-4.0.1b1.tar.gz

---
+++ packages/ntnx-iam-py-client/ntnx-iam-py-client-4.0.1b1.tar.gz/ntnx-iam-py-client-4.0.1b1/ntnx_iam_py_client/api/__init__.py
@@ -0,0 +1,19 @@
+
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from ntnx_iam_py_client.api.api_key_api import ApiKeyApi
+from ntnx_iam_py_client.api.authorization_policy_api import AuthorizationPolicyApi
+from ntnx_iam_py_client.api.cert_auth_provider_api import CertAuthProviderApi
+from ntnx_iam_py_client.api.client_api import ClientApi
+from ntnx_iam_py_client.api.directory_service_api import DirectoryServiceApi
+from ntnx_iam_py_client.api.entity_api import EntityApi
+from ntnx_iam_py_client.api.operation_api import OperationApi
+from ntnx_iam_py_client.api.role_api import RoleApi
+from ntnx_iam_py_client.api.saml_identity_provider_api import SamlIdentityProviderApi
+from ntnx_iam_py_client.api.saml_sp_metadata_api import SamlSpMetadataApi
+from ntnx_iam_py_client.api.service_account_api import ServiceAccountApi
+from ntnx_iam_py_client.api.user_api import UserApi
+from ntnx_iam_py_client.api.user_group_api import UserGroupApi
