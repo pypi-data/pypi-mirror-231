37b6fd2c5b2b2f8b227f312d641f13b217d512ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:05:37 +0000

    Add package rabbitizer/1.7.9/rabbitizer-1.7.9-cp311-cp311-win32.whl

---
+++ packages/rabbitizer/rabbitizer-1.7.9-cp311-cp311-win32.whl/rabbitizer/__init__.pyi
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2022-2023 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from .Utils import *
+
+from .Enum import *
+from .InstrCategory import *
+from .InstrId import *
+from .InstrIdType import *
+from .OperandType import *
+from .AccessType import *
+
+from .RegGprO32 import *
+from .RegGprN32 import *
+
+from .RegCop1O32 import *
+from .RegCop1N32 import *
+from .RegCop1N64 import *
+
+from .Abi import *
+from .Config import *
+
+from .rabbitizer import *
+
+from .RegistersTracker import *
