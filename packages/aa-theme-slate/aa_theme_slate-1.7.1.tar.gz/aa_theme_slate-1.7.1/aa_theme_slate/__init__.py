ce3a7f9ef7b32fd0cbbfea1aac05e9f049dfd10e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:08:40 +0000

    Add package aa-theme-slate/1.7.1/aa_theme_slate-1.7.1.tar.gz

---
+++ packages/aa-theme-slate/aa_theme_slate-1.7.1.tar.gz/aa_theme_slate-1.7.1/aa_theme_slate/__init__.py
@@ -0,0 +1,11 @@
+"""
+Initialize the app
+"""
+
+# Standard Library
+from importlib import metadata
+
+__version__ = metadata.version("aa-theme-slate")
+__title__ = 'Bootstrap Theme "Slate" for Alliance Auth'
+
+del metadata
