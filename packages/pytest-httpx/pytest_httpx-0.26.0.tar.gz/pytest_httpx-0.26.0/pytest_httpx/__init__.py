97174f2a0f0331c0822602296375476d7c7594e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:04:42 +0000

    Add package pytest-httpx/0.26.0/pytest_httpx-0.26.0.tar.gz

---
+++ packages/pytest-httpx/pytest_httpx-0.26.0.tar.gz/pytest_httpx-0.26.0/pytest_httpx/__init__.py
@@ -0,0 +1,66 @@
+from collections.abc import Generator
+from typing import List
+
+import httpx
+import pytest
+from pytest import MonkeyPatch
+
+from pytest_httpx._httpx_mock import (
+    HTTPXMock,
+    _PytestSyncTransport,
+    _PytestAsyncTransport,
+)
+from pytest_httpx._httpx_internals import IteratorStream
+from pytest_httpx.version import __version__
+
+__all__ = (
+    "HTTPXMock",
+    "IteratorStream",
+    "__version__",
+)
+
+
+@pytest.fixture
+def assert_all_responses_were_requested() -> bool:
+    return True
+
+
+@pytest.fixture
+def non_mocked_hosts() -> List[str]:
+    return []
+
+
+@pytest.fixture
+def httpx_mock(
+    monkeypatch: MonkeyPatch,
+    assert_all_responses_were_requested: bool,
+    non_mocked_hosts: List[str],
+) -> Generator[HTTPXMock, None, None]:
+    # Ensure redirections to www hosts are handled transparently.
+    missing_www = [
+        f"www.{host}" for host in non_mocked_hosts if not host.startswith("www.")
+    ]
+    non_mocked_hosts += missing_www
+
+    mock = HTTPXMock()
+
+    # Mock synchronous requests
+    real_sync_transport = httpx.Client._transport_for_url
+    monkeypatch.setattr(
+        httpx.Client,
+        "_transport_for_url",
+        lambda self, url: real_sync_transport(self, url)
+        if url.host in non_mocked_hosts
+        else _PytestSyncTransport(real_sync_transport(self, url), mock),
+    )
+    # Mock asynchronous requests
+    real_async_transport = httpx.AsyncClient._transport_for_url
+    monkeypatch.setattr(
+        httpx.AsyncClient,
+        "_transport_for_url",
+        lambda self, url: real_async_transport(self, url)
+        if url.host in non_mocked_hosts
+        else _PytestAsyncTransport(real_async_transport(self, url), mock),
+    )
+    yield mock
+    mock.reset(assert_all_responses_were_requested)
