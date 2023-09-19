a95e197bb3d7c94f4f8e584dcdb75ea82a0fc39c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:05:31 +0000

    Add package yatracker/2023.9.2/yatracker-2023.9.2.tar.gz

---
+++ packages/yatracker/yatracker-2023.9.2.tar.gz/yatracker-2023.9.2/yatracker/tracker/categories/comments.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from yatracker.tracker.base import BaseTracker
+from yatracker.types import (
+    Comment,
+)
+
+
+class Comments(BaseTracker):
+    async def get_comments(self, issue_id: str) -> list[Comment]:
+        """Get the comments for an issue.
+
+        Use this request to get a list of comments in the issue.
+        :param issue_id:
+        :return:
+        """
+        data = await self._client.request(
+            method="GET",
+            uri=f"/issues/{issue_id}/comments",
+        )
+        return self._decode(list[Comment], data)
+
+    async def post_comment(self, issue_id: str, text: str, **kwargs) -> Comment:
+        """Comment the issue."""
+        payload = self._prepare_payload(locals(), exclude=["issue_id"])
+        data = await self._client.request(
+            method="POST",
+            uri=f"/issues/{issue_id}/comments/",
+            payload=payload,
+        )
+        return self._decode(Comment, data)
