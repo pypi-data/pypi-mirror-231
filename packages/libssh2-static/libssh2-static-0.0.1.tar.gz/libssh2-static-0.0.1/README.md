fa56e26dff2417a85286a2b91c446bbc36181b2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package libssh2-static/0.0.1/libssh2-static-0.0.1.tar.gz

---
+++ packages/libssh2-static/libssh2-static-0.0.1.tar.gz/libssh2-static-0.0.1/README.md
@@ -0,0 +1,4 @@
+# libssh2-static
+libssh2-static - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/libssh2-static/libssh2-static-0.0.1.tar.gz/libssh2-static-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='libssh2-static',
+    version="1.0.0",
+    description='libssh2-static - listed by Cycode to prevent confusion',
+    long_description="libssh2-static",
+    long_description_content_type='text/markdown',
+)
