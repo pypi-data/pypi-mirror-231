6dfae154a59933f1dacb0f0016de3abefd7b2c67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:07:39 +0000

    Add package MHcal/0.1/MHcal-0.1.tar.gz

---
+++ packages/MHcal/MHcal-0.1.tar.gz/MHcal-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='MHcal',
+    version='0.1',
+    description='A simple Python library for basic math operations',
+    author='danishk sinha',
+    author_email='thisisdanishk@gmail.com',
+    packages=['MHcal'],
+)
