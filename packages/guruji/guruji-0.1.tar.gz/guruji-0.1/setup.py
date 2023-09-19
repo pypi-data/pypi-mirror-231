94aae5de2e03fa0c35f49c046842748acc852bac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:04:34 +0000

    Add package guruji/0.1/guruji-0.1.tar.gz

---
+++ packages/guruji/guruji-0.1.tar.gz/guruji-0.1/setup.py
@@ -0,0 +1,10 @@
+from  setuptools import setup
+
+setup(
+    name="guruji",
+    version="0.1",
+    description="Hardcore library",
+    author="danishk sinha",
+    author_email="thisisdanishk@gmail.com",
+    packages=["guruji"]
+)
