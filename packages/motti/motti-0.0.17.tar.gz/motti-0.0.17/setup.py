767f460579afe9fd8714f9ef47bc38e0d673cf49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:04:46 +0000

    Add package motti/0.0.17/motti-0.0.17.tar.gz

---
+++ packages/motti/motti-0.0.17.tar.gz/motti-0.0.17/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='motti',
+      version='0.0.17',
+      description='Hello world',
+      author='lup1n',
+      author_email='780966523@qq.com',
+      license='MIT',
+      packages=['motti'],
+      zip_safe=False)
