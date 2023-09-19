f662a245772774f84d2798096efa661f1334ea5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:05 +0000

    Add package galaxy-ng/4.7.3/galaxy-ng-4.7.3.tar.gz

---
+++ packages/galaxy-ng/galaxy-ng-4.7.3.tar.gz/galaxy-ng-4.7.3/galaxy_ng/tests/integration/api/test_api_base.py
@@ -0,0 +1,27 @@
+from ..utils import get_client
+
+
+def test_galaxy_api_root(ansible_config, artifact):
+    """Test galaxy API root."""
+
+    # TODO: change to `basic_user` profile when can access pulp-v3 api root
+    config = ansible_config("admin")
+    api_prefix = config.get("api_prefix")
+    api_prefix = api_prefix.rstrip("/")
+
+    api_client = get_client(
+        config=config,
+        request_token=True,
+        require_auth=True
+    )
+
+    # verify api root works
+    response = api_client(api_prefix + '/')
+    assert "v3" in response["available_versions"]
+    assert "pulp-v3" in response["available_versions"]
+
+    v3_root = api_client(api_prefix + '/' + response['available_versions']['v3'])
+    assert "published" in v3_root
+
+    pulp_root = api_client(api_prefix + '/' + response['available_versions']['pulp-v3'])
+    assert "tasks" in pulp_root
---
+++ packages/galaxy-ng/galaxy-ng-4.7.3.tar.gz/galaxy-ng-4.7.3/galaxy_ng/tests/integration/api/test_container_signing.py
@@ -0,0 +1,78 @@
+"""Tests related to container signing.
+
+See: https://issues.redhat.com/browse/AAH-1358
+"""
+import subprocess
+import time
+
+import pytest
+
+from galaxy_ng.tests.integration.utils import get_client
+
+
+@pytest.fixture(scope="function")
+def flags(ansible_config):
+    api_client = get_client(config=ansible_config("admin"), request_token=True, require_auth=True)
+    api_prefix = api_client.config.get("api_prefix").rstrip("/")
+    return api_client(f"{api_prefix}/_ui/v1/feature-flags/")
+
+
+@pytest.mark.parametrize(
+    "require_auth",
+    [
+        True,
+        False,
+    ],
+)
+@pytest.mark.standalone_only
+def test_push_and_sign_a_container(ansible_config, flags, require_auth):
+    can_sign = flags.get("container_signing")
+    if not can_sign:
+        pytest.skip("GALAXY_CONTAINER_SIGNING_SERVICE is not configured")
+
+    # Pull alpine image
+    subprocess.check_call(["docker", "pull", "alpine"])
+    # Tag the image
+    subprocess.check_call(["docker", "tag", "alpine", "localhost:5001/alpine:latest"])
+
+    # Login to local registy with tls verify disabled
+    subprocess.check_call(["docker", "login", "-u", "admin", "-p", "admin", "localhost:5001"])
+    # Push image to local registry
+    subprocess.check_call(["docker", "push", "localhost:5001/alpine:latest"])
+
+    # Get an API client running with admin user credentials
+    client = get_client(
+        config=ansible_config("admin"),
+        request_token=True,
+        require_auth=require_auth
+    )
+    api_prefix = client.config.get("api_prefix").rstrip("/")
+
+    # Get the pulp_href for the pushed image
+    image = client(
+        f"{api_prefix}/pulp/api/v3/repositories/container/container-push/?name=alpine"
+    )
+    container_href = image["results"][0]["pulp_href"]
+
+    # Get the pulp_href for signing service
+    signing_service = client(
+        f"{api_prefix}/pulp/api/v3/signing-services/?name=container-default"
+    )
+    ss_href = signing_service["results"][0]["pulp_href"]
+
+    # Sign the image
+    client(f"{container_href}/sign/", method="POST", args={"manifest_signing_service": ss_href})
+
+    # sleep 2 second2
+    time.sleep(2)
+
+    repo = client(container_href)
+    latest_version_href = repo["latest_version_href"]
+
+    # Check the image is signed on the latest version
+    latest_version = client(latest_version_href)
+    assert latest_version["content_summary"]["added"]["container.signature"]["count"] > 0
+
+    # Check the sign state is set on the UI API
+    ee = client(f"{api_prefix}/v3/plugin/execution-environments/repositories/?name=alpine")
+    assert ee["data"][0]["pulp"]["repository"]["sign_state"] == "signed"
