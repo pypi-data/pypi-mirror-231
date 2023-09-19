9d2ed12fd27c1a312b15f7d01ade58beefcc30ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:56 +0000

    Add package manim-studio/0.2.8/manim_studio-0.2.8-py3-none-any.whl

---
+++ packages/manim-studio/manim_studio-0.2.8-py3-none-any.whl/manim_studio/main.py
@@ -0,0 +1,26 @@
+from PyQt6.QtWidgets import QApplication
+import sys
+
+from .editor_widget import EditorWidget
+from .preview_widget import PreviewWidget
+from .live_scene import LiveScene
+from .communicate import Communicate
+from .ai_widget import AIWidget
+
+
+def main(scene_type=LiveScene) -> None:
+    app = QApplication(sys.argv)
+    communicate = Communicate()
+    scene = scene_type(communicate)
+    editor_window = EditorWidget(communicate, scene)
+    preview_window = PreviewWidget(communicate, scene)
+    preview_window.showFullScreen()
+    editor_window.show()
+    if AIWidget is not None:
+        ai_window = AIWidget(communicate)
+        ai_window.show()
+    sys.exit(app.exec())
+
+
+if __name__ == "__main__":
+    main(LiveScene)
