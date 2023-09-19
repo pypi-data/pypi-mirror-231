7f956f0ef80e7ddae00b7448eea597caa813c784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:08:43 +0000

    Add package mvtcr/0.1.2/mvtcr-0.1.2.tar.gz

---
+++ packages/mvtcr/mvtcr-0.1.2.tar.gz/mvtcr-0.1.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mvtcr',
+    version='0.1.2',
+    description='mvTCR: A multimodal generative model to learn a unified representation across TCR sequences and scRNAseq data for joint analysis of single-cell immune profiling data',
+    author='Felix Drost, Yang An, Lisa M Dratva, Rik GH Lindeboom, Muzlifah Haniffa, Sarah A Teichmann, Fabian Theis, Mohammad Lotfollahi, Benjamin Schubert',
+    maintainer='Felix Drost, Yang An,  Irene Bonafonte Pardàs, Jan-Philipp Leusch',
+    url='https://github.com/SchubertLab/mvTCR',
+    packages=find_packages(include=['tcr_embedding', 'tcr_embedding.*']),
+    install_requires=[
+        'scanpy==1.7.0',
+        'scirpy>=0.7',
+        'numba==0.52.0',
+        'pandas==1.2.3',
+        'anndata==0.8.0',
+        'leidenalg==0.8.4',
+        'comet-ml',
+        'numpy==1.20.3',
+        'scikit-learn==0.24.1',
+        'scrublet==0.2.3',
+        'tqdm',
+        'optuna==2.10.1',
+        'umap-learn==0.5.1',
+        'matplotlib==3.6.3',
+        'sqlalchemy==1.4.26',
+        'torch>=1.8.0'],
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only',
+        'Operating System :: Microsoft',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS',
+        'Environment :: GPU :: NVIDIA CUDA',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Healthcare Industry',
+    ],
+)
