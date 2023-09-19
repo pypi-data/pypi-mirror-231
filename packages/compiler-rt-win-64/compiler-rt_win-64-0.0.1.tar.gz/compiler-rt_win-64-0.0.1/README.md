7444550498df1ff1c1021123656c97f58fb185a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:38 +0000

    Add package compiler-rt-win-64/0.0.1/compiler-rt_win-64-0.0.1.tar.gz

---
+++ packages/compiler-rt-win-64/compiler-rt_win-64-0.0.1.tar.gz/compiler-rt_win-64-0.0.1/README.md
@@ -0,0 +1,4 @@
+# compiler-rt_win-64
+compiler-rt_win-64 - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/compiler-rt-win-64/compiler-rt_win-64-0.0.1.tar.gz/compiler-rt_win-64-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='compiler-rt_win-64',
+    version="1.0.0",
+    description='compiler-rt_win-64 - listed by Cycode to prevent confusion',
+    long_description="compiler-rt_win-64",
+    long_description_content_type='text/markdown',
+)
