aeae186981c2cb7535562fc9a2eb131245e19a46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:04:52 +0000

    Add package dlt/0.3.18a0/dlt-0.3.18a0-py3-none-any.whl

---
+++ packages/dlt/dlt-0.3.18a0-py3-none-any.whl/dlt/common/libs/pyarrow.py
@@ -0,0 +1,68 @@
+from typing import Any, Tuple
+from dlt import version
+from dlt.common.exceptions import MissingDependencyException
+
+from dlt.common.destination.capabilities import DestinationCapabilitiesContext
+
+try:
+    import pyarrow
+    import pyarrow.parquet
+except ModuleNotFoundError:
+    raise MissingDependencyException("DLT parquet Helpers", [f"{version.DLT_PKG_NAME}[parquet]"], "DLT Helpers for for parquet.")
+
+
+def get_py_arrow_datatype(column_type: str, caps: DestinationCapabilitiesContext, tz: str) -> Any:
+    if column_type == "text":
+        return pyarrow.string()
+    elif column_type == "double":
+        return pyarrow.float64()
+    elif column_type == "bool":
+        return pyarrow.bool_()
+    elif column_type == "timestamp":
+        return get_py_arrow_timestamp(caps.timestamp_precision, tz)
+    elif column_type == "bigint":
+        return pyarrow.int64()
+    elif column_type == "binary":
+        return pyarrow.binary()
+    elif column_type == "complex":
+        # return pyarrow.struct([pyarrow.field('json', pyarrow.string())])
+        return pyarrow.string()
+    elif column_type == "decimal":
+        return get_py_arrow_numeric(caps.decimal_precision)
+    elif column_type == "wei":
+        return get_py_arrow_numeric(caps.wei_precision)
+    elif column_type == "date":
+        return pyarrow.date32()
+    elif column_type == "time":
+        return get_py_arrow_time(caps.timestamp_precision)
+    else:
+        raise ValueError(column_type)
+
+
+def get_py_arrow_timestamp(precision: int, tz: str) -> Any:
+    if precision == 0:
+        return pyarrow.timestamp("s", tz=tz)
+    if precision <= 3:
+        return pyarrow.timestamp("ms", tz=tz)
+    if precision <= 6:
+        return pyarrow.timestamp("us", tz=tz)
+    return pyarrow.timestamp("ns", tz=tz)
+
+
+def get_py_arrow_time(precision: int) -> Any:
+    if precision == 0:
+        return pyarrow.time32("s")
+    elif precision <= 3:
+        return pyarrow.time32("ms")
+    elif precision <= 6:
+        return pyarrow.time64("us")
+    return pyarrow.time64("ns")
+
+
+def get_py_arrow_numeric(precision: Tuple[int, int]) -> Any:
+    if precision[0] <= 38:
+        return pyarrow.decimal128(*precision)
+    if precision[0] <= 76:
+        return pyarrow.decimal256(*precision)
+    # for higher precision use max precision and trim scale to leave the most significant part
+    return pyarrow.decimal256(76, max(0, 76 - (precision[0] - precision[1])))
---
+++ packages/dlt/dlt-0.3.18a0-py3-none-any.whl/dlt/helpers/dbt/__init__.py
@@ -0,0 +1,89 @@
+import contextlib
+from typing import List
+import pkg_resources
+import semver
+
+from dlt.common.runners import Venv
+from dlt.common.destination.reference import DestinationClientDwhConfiguration
+from dlt.common.configuration.specs import CredentialsWithDefault
+from dlt.common.typing import TSecretValue
+from dlt.version import get_installed_requirement_string
+
+from dlt.helpers.dbt.runner import create_runner, DBTPackageRunner
+
+DEFAULT_DBT_VERSION = ">=1.1,<1.6"
+
+# a map of destination names to dbt package names in case they don't match the pure destination name
+DBT_DESTINATION_MAP = {
+    "athena": "athena-community",
+    "motherduck": "duckdb",
+    "mssql": "sqlserver",
+}
+
+
+def _default_profile_name(credentials: DestinationClientDwhConfiguration) -> str:
+    profile_name = credentials.destination_name
+    # in case of credentials with default add default to the profile name
+    if isinstance(credentials.credentials, CredentialsWithDefault):
+        if credentials.credentials.has_default_credentials():
+            profile_name += "_default"
+    elif profile_name == 'snowflake':
+        if getattr(credentials.credentials, 'private_key', None):
+            # snowflake with private key is a separate profile
+            profile_name += '_pkey'
+    return profile_name
+
+
+def _create_dbt_deps(destination_names: List[str], dbt_version: str = DEFAULT_DBT_VERSION) -> List[str]:
+    if dbt_version:
+        # if parses as version use "==" operator
+        with contextlib.suppress(ValueError):
+            semver.parse(dbt_version)
+            dbt_version = "==" + dbt_version
+    else:
+        dbt_version = ""
+
+    all_packages = destination_names + ["core"]
+    for idx, package in enumerate(all_packages):
+        # TODO: if we have more cases like this, move to destination capabilities
+        package = DBT_DESTINATION_MAP.get(package, package)
+        package_w_ver = "dbt-" + package + dbt_version
+        # verify package
+        pkg_resources.Requirement.parse(package_w_ver)
+        all_packages[idx] = package_w_ver
+
+    dlt_requirement = get_installed_requirement_string()
+
+    return all_packages + [dlt_requirement]
+
+
+def restore_venv(venv_dir: str, destination_names: List[str], dbt_version: str = DEFAULT_DBT_VERSION) -> Venv:
+    venv = Venv.restore(venv_dir)
+    venv.add_dependencies(_create_dbt_deps(destination_names, dbt_version))
+    return venv
+
+
+def create_venv(venv_dir: str, destination_names: List[str], dbt_version: str = DEFAULT_DBT_VERSION) -> Venv:
+    return Venv.create(venv_dir, _create_dbt_deps(destination_names, dbt_version))
+
+
+def package_runner(
+    venv: Venv,
+    destination_configuration: DestinationClientDwhConfiguration,
+    working_dir: str,
+    package_location: str,
+    package_repository_branch: str = None,
+    package_repository_ssh_key: TSecretValue = TSecretValue(""),  # noqa
+    auto_full_refresh_when_out_of_sync: bool = None
+) -> DBTPackageRunner:
+    default_profile_name = _default_profile_name(destination_configuration)
+    return create_runner(
+        venv,
+        destination_configuration,
+        working_dir,
+        package_location,
+        package_repository_branch=package_repository_branch,
+        package_repository_ssh_key=package_repository_ssh_key,
+        package_profile_name=default_profile_name,
+        auto_full_refresh_when_out_of_sync=auto_full_refresh_when_out_of_sync
+    )
