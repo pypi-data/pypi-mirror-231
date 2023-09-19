ac53c02e195fb0855b493fd9c966a37f6421d595
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package ispc/0.0.1/ispc-0.0.1.tar.gz

---
+++ packages/ispc/ispc-0.0.1.tar.gz/ispc-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='ispc',
+    version="1.0.0",
+    description='ispc - listed by Cycode to prevent confusion',
+    long_description="ispc",
+    long_description_content_type='text/markdown',
+)
