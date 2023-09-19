5be60e965d99e20ef512119be4edad66467d3d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:37 +0000

    Add package r-zip/0.0.1/r-zip-0.0.1.tar.gz

---
+++ packages/r-zip/r-zip-0.0.1.tar.gz/r-zip-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='r-zip',
+    version="1.0.0",
+    description='r-zip - listed by Cycode to prevent confusion',
+    long_description="r-zip",
+    long_description_content_type='text/markdown',
+)
