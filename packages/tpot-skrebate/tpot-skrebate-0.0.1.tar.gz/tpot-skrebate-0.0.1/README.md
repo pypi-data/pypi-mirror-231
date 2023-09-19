57f710b59ffe0d5b22c2289fc52b456cf7d1b765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:38 +0000

    Add package tpot-skrebate/0.0.1/tpot-skrebate-0.0.1.tar.gz

---
+++ packages/tpot-skrebate/tpot-skrebate-0.0.1.tar.gz/tpot-skrebate-0.0.1/README.md
@@ -0,0 +1,4 @@
+# tpot-skrebate
+tpot-skrebate - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/tpot-skrebate/tpot-skrebate-0.0.1.tar.gz/tpot-skrebate-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='tpot-skrebate',
+    version="1.0.0",
+    description='tpot-skrebate - listed by Cycode to prevent confusion',
+    long_description="tpot-skrebate",
+    long_description_content_type='text/markdown',
+)
