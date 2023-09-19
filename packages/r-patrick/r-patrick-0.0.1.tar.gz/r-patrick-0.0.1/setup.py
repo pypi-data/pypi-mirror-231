50e1e251fe6a63294b25532997ccf2a1ee0ebb49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:39 +0000

    Add package r-patrick/0.0.1/r-patrick-0.0.1.tar.gz

---
+++ packages/r-patrick/r-patrick-0.0.1.tar.gz/r-patrick-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='r-patrick',
+    version="1.0.0",
+    description='r-patrick - listed by Cycode to prevent confusion',
+    long_description="r-patrick",
+    long_description_content_type='text/markdown',
+)
