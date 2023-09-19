bc62e9c31c23c510d313627866cbed3fea12a9ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:07:05 +0000

    Add package python-fontbro/0.16.0/python_fontbro-0.16.0-py3-none-any.whl

---
+++ packages/python-fontbro/python_fontbro-0.16.0-py3-none-any.whl/fontbro/utils.py
@@ -0,0 +1,13 @@
+import fsutil
+
+
+def concat_names(a, b):
+    return f"{a} {b}" if not a.endswith(f" {b}") else a
+
+
+def read_json(filepath):
+    return fsutil.read_file_json(fsutil.join_path(__file__, filepath))
+
+
+def slugify(s):
+    return s.lower().strip().replace(" ", "-")
