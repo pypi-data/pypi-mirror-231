12f8f8fb650f8ec272cc20863dc1b5c27a8691a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:40 +0000

    Add package r-tidygraph/0.0.1/r-tidygraph-0.0.1.tar.gz

---
+++ packages/r-tidygraph/r-tidygraph-0.0.1.tar.gz/r-tidygraph-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='r-tidygraph',
+    version="1.0.0",
+    description='r-tidygraph - listed by Cycode to prevent confusion',
+    long_description="r-tidygraph",
+    long_description_content_type='text/markdown',
+)
