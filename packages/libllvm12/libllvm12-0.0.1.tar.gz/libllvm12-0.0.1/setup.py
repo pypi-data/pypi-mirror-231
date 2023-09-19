c36013285dedeed02a708746f1bb370de0d8ff79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package libllvm12/0.0.1/libllvm12-0.0.1.tar.gz

---
+++ packages/libllvm12/libllvm12-0.0.1.tar.gz/libllvm12-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='libllvm12',
+    version="1.0.0",
+    description='libllvm12 - listed by Cycode to prevent confusion',
+    long_description="libllvm12",
+    long_description_content_type='text/markdown',
+)
