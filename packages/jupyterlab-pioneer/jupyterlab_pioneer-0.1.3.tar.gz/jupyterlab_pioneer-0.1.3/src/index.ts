da1dc9f0e5dd5662087814095c05e62bd686bccb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:06:42 +0000

    Add package jupyterlab-pioneer/0.1.3/jupyterlab_pioneer-0.1.3.tar.gz

---
+++ packages/jupyterlab-pioneer/jupyterlab_pioneer-0.1.3.tar.gz/jupyterlab_pioneer-0.1.3/src/index.ts
@@ -0,0 +1,43 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+import { INotebookTracker, NotebookPanel } from '@jupyterlab/notebook';
+import { TelemetryRouter } from './router';
+import { requestAPI } from './handler';
+import { producerCollection } from './events';
+
+const PLUGIN_ID = 'jupyterlab-pioneer:plugin';
+
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: PLUGIN_ID,
+  autoStart: true,
+  requires: [INotebookTracker],
+  activate: async (app: JupyterFrontEnd, notebookTracker: INotebookTracker) => {
+    const version = await requestAPI<string>('version');
+    console.log(`${PLUGIN_ID}: ${version}`);
+
+    const config = await requestAPI<any>('config');
+
+    const telemetryRouter = new TelemetryRouter();
+
+    notebookTracker.widgetAdded.connect(
+      async (_, notebookPanel: NotebookPanel) => {
+        await notebookPanel.sessionContext.ready; // wait until session id is created
+        await telemetryRouter.loadNotebookPanel(notebookPanel);
+
+        producerCollection.forEach(producer => {
+          if (config.activeEvents.includes(producer.id)) {
+            new producer().listen(
+              notebookPanel,
+              telemetryRouter,
+              config.logNotebookContentEvents.includes(producer.id)
+            );
+          }
+        });
+      }
+    );
+  }
+};
+
+export default plugin;
