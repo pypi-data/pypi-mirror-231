b4ee0b051086786da72a427c72cd4dac272fc0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 05:04:47 +0000

    Add package comicbox/1.0.0rc0/comicbox-1.0.0rc0.tar.gz

---
+++ packages/comicbox/comicbox-1.0.0rc0.tar.gz/comicbox-1.0.0rc0/comicbox/version.py
@@ -0,0 +1,18 @@
+"""Package name and version."""
+
+from importlib.metadata import PackageNotFoundError, version
+
+PACKAGE_NAME = "comicbox"
+
+
+def get_version():
+    """Get the current installed comicbox version."""
+    try:
+        v = version(PACKAGE_NAME)
+    except PackageNotFoundError:
+        v = "dev"
+    return v
+
+
+VERSION = get_version()
+DEFAULT_TAGGER = f"{PACKAGE_NAME} {VERSION}"
