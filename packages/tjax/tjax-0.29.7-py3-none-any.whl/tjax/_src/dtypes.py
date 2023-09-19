b75bf86e73f71094c88636a50dfabf7feb454d29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 15:07:46 +0000

    Add package tjax/0.29.7/tjax-0.29.7-py3-none-any.whl

---
+++ packages/tjax/tjax-0.29.7-py3-none-any.whl/tjax/_src/dtypes.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from typing import Any, TypedDict
+
+import jax.numpy as jnp
+
+__all__ = ['default_rtol',
+           'default_atol',
+           'default_tols']
+
+
+class Tols(TypedDict):
+    rtol: float
+    atol: float
+
+
+rtol_dict = {jnp.bfloat16: 1e-4,
+             jnp.float16: 1e-3, jnp.float32: 1e-4, jnp.float64: 1e-5,
+             jnp.complex64: 1e-4, jnp.complex128: 1e-5}
+atol_dict = {jnp.bfloat16: 1e-2,
+             jnp.float16: 1e-3, jnp.float32: 1e-6, jnp.float64: 1e-8,
+             jnp.complex64: 1e-6, jnp.complex128: 1e-8}
+
+
+def default_rtol(dtype: type[jnp.number[Any]]) -> float:
+    if not jnp.issubdtype(dtype, jnp.inexact):
+        return 0.0
+    return rtol_dict[dtype]
+
+
+def default_atol(dtype: type[jnp.number[Any]]) -> float:
+    if not jnp.issubdtype(dtype, jnp.inexact):
+        return 1.0
+    return atol_dict[dtype]
+
+
+def default_tols(dtype: type[jnp.number[Any]],
+                 *,
+                 rtol: float | None = None,
+                 atol: float | None = None) -> Tols:
+    return Tols(rtol=default_rtol(dtype) if rtol is None else rtol,
+                atol=default_atol(dtype) if atol is None else atol)
