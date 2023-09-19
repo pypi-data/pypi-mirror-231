027165244f848999dae578fefe6e89fd8e896b13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:08:20 +0000

    Add package scale-llm-engine/0.0.0b14/scale_llm_engine-0.0.0b14-py3-none-any.whl

---
+++ packages/scale-llm-engine/scale_llm_engine-0.0.0b14-py3-none-any.whl/llmengine/__init__.py
@@ -0,0 +1,69 @@
+# Copyright 2023 Scale AI. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__version__ = "0.0.0.beta13"
+
+from typing import Sequence
+
+from llmengine.completion import Completion
+from llmengine.data_types import (
+    CancelFineTuneResponse,
+    CompletionOutput,
+    CompletionStreamOutput,
+    CompletionStreamResponse,
+    CompletionSyncResponse,
+    CreateFineTuneRequest,
+    CreateFineTuneResponse,
+    DeleteFileResponse,
+    DeleteLLMEndpointResponse,
+    GetFileContentResponse,
+    GetFileResponse,
+    GetFineTuneResponse,
+    GetLLMEndpointResponse,
+    ListFilesResponse,
+    ListFineTunesResponse,
+    ListLLMEndpointsResponse,
+    ModelDownloadRequest,
+    ModelDownloadResponse,
+    UploadFileResponse,
+)
+from llmengine.file import File
+from llmengine.fine_tuning import FineTune
+from llmengine.model import Model
+
+__all__: Sequence[str] = (
+    "CancelFineTuneResponse",
+    "Completion",
+    "CompletionOutput",
+    "CompletionStreamOutput",
+    "CompletionStreamResponse",
+    "CompletionSyncResponse",
+    "CreateFineTuneRequest",
+    "CreateFineTuneResponse",
+    "DeleteFileResponse",
+    "DeleteLLMEndpointResponse",
+    "ModelDownloadRequest",
+    "ModelDownloadResponse",
+    "GetFileContentResponse",
+    "File",
+    "FineTune",
+    "GetFileResponse",
+    "GetFineTuneResponse",
+    "GetLLMEndpointResponse",
+    "ListFilesResponse",
+    "ListFineTunesResponse",
+    "ListLLMEndpointsResponse",
+    "Model",
+    "UploadFileResponse",
+)
