60ffe5ccdf95689282629154937486c9ab42d484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:40 +0000

    Add package nanoflann/0.0.1/nanoflann-0.0.1.tar.gz

---
+++ packages/nanoflann/nanoflann-0.0.1.tar.gz/nanoflann-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='nanoflann',
+    version="1.0.0",
+    description='nanoflann - listed by Cycode to prevent confusion',
+    long_description="nanoflann",
+    long_description_content_type='text/markdown',
+)
