4ccb16bfa77844fbaacd71a20afc475730d9f70d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:04:17 +0000

    Add package cca-zoo/2.3.4/cca_zoo-2.3.4-py3-none-any.whl

---
+++ packages/cca-zoo/cca_zoo-2.3.4-py3-none-any.whl/cca_zoo/deep/callbacks.py
@@ -0,0 +1,11 @@
+from pytorch_lightning import Callback, LightningModule, Trainer
+
+
+class CorrelationCallback(Callback):
+    def on_validation_epoch_end(
+        self, trainer: Trainer, pl_module: LightningModule
+    ) -> None:
+        pl_module.log(
+            "val/corr",
+            pl_module.correlation(trainer.val_dataloaders[0]).sum(),
+        )
---
+++ packages/cca-zoo/cca_zoo-2.3.4-py3-none-any.whl/cca_zoo/visualisation/__init__.py
@@ -0,0 +1,15 @@
+from .explained_variance import ExplainedVarianceDisplay
+from .scores import ScoreDisplay
+from .explained_covariance import ExplainedCovarianceDisplay
+from .weights import WeightHeatmapDisplay
+from .correlation import CorrelationHeatmapDisplay
+from .covariance import CovarianceHeatmapDisplay
+
+__all__ = [
+    "ExplainedVarianceDisplay",
+    "ScoreDisplay",
+    "ExplainedCovarianceDisplay",
+    "WeightHeatmapDisplay",
+    "CorrelationHeatmapDisplay",
+    "CovarianceHeatmapDisplay",
+]
