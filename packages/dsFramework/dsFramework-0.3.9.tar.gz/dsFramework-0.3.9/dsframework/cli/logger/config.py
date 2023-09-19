8f1b4cf634860594415ef679299087b6eac8a806
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:04:57 +0000

    Add package dsFramework/0.3.9/dsFramework-0.3.9.tar.gz

---
+++ packages/dsFramework/dsFramework-0.3.9.tar.gz/dsFramework-0.3.9/dsframework/cli/logger/config.py
@@ -0,0 +1,7 @@
+pubsub_config = {
+  'enable': 'True',
+  'level': 'INFO',
+  'stg_project_id': 'dozi-stg-ds-apps-1',
+  'prd_project_id': 'dozi-prd-ds-apps-1',
+  'topic_id': 'gae-logs-export-to-datadog'
+}
