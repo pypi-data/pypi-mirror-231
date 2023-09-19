128f2b123a363cc4c9ea11c04cb49ba9cbd4c895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package rabbitmq-server/0.0.1/rabbitmq-server-0.0.1.tar.gz

---
+++ packages/rabbitmq-server/rabbitmq-server-0.0.1.tar.gz/rabbitmq-server-0.0.1/README.md
@@ -0,0 +1,4 @@
+# rabbitmq-server
+rabbitmq-server - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/rabbitmq-server/rabbitmq-server-0.0.1.tar.gz/rabbitmq-server-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='rabbitmq-server',
+    version="1.0.0",
+    description='rabbitmq-server - listed by Cycode to prevent confusion',
+    long_description="rabbitmq-server",
+    long_description_content_type='text/markdown',
+)
