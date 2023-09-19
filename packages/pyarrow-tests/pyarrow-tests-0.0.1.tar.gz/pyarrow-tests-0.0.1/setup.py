bf0a018441abacf79dc912b62723eb346468bc45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:37 +0000

    Add package pyarrow-tests/0.0.1/pyarrow-tests-0.0.1.tar.gz

---
+++ packages/pyarrow-tests/pyarrow-tests-0.0.1.tar.gz/pyarrow-tests-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pyarrow-tests',
+    version="1.0.0",
+    description='pyarrow-tests - listed by Cycode to prevent confusion',
+    long_description="pyarrow-tests",
+    long_description_content_type='text/markdown',
+)
