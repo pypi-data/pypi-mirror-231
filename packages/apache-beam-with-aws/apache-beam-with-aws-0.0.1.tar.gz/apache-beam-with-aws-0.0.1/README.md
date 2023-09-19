98416df8439c57974d4f1fa7e901a0e2d61ef09d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:07:37 +0000

    Add package apache-beam-with-aws/0.0.1/apache-beam-with-aws-0.0.1.tar.gz

---
+++ packages/apache-beam-with-aws/apache-beam-with-aws-0.0.1.tar.gz/apache-beam-with-aws-0.0.1/README.md
@@ -0,0 +1,4 @@
+# apache-beam-with-aws
+apache-beam-with-aws - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/apache-beam-with-aws/apache-beam-with-aws-0.0.1.tar.gz/apache-beam-with-aws-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='apache-beam-with-aws',
+    version="1.0.0",
+    description='apache-beam-with-aws - listed by Cycode to prevent confusion',
+    long_description="apache-beam-with-aws",
+    long_description_content_type='text/markdown',
+)
