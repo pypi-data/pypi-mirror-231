d31fd22ae83aee1ac4026494cb872b00eb75b629
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:09:01 +0000

    Add package packtivity/0.16.2/packtivity-0.16.2.tar.gz

---
+++ packages/packtivity/packtivity-0.16.2.tar.gz/packtivity-0.16.2/setup.py
@@ -0,0 +1,68 @@
+import os
+from setuptools import setup, find_packages
+from pathlib import Path
+
+deps = [
+    "requests[security]",
+    "jsonschema",
+    "jsonref",
+    "pyyaml",
+    "click",
+    "glob2",
+    "jsonpointer",
+    "jsonpath-rw",
+    "jq>=1.0.0",
+    "yadage-schemas",
+    "mock",
+    "checksumdir",
+]
+
+if not "READTHEDOCS" in os.environ:
+    deps += ["jq"]
+
+this_directory = Path(__file__).parent
+
+setup(
+    name="packtivity",
+    version="0.16.2",
+    description="packtivity - general purpose schema + bindings for PROV activities",
+    long_description=(this_directory / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yadage/packtivity",
+    author="Lukas Heinrich",
+    author_email="lukas.heinrich@cern.ch",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    include_package_data=True,
+    install_requires=deps,
+    extras_require={
+        "celery": [
+            "celery>=5.0.0",
+            "redis",
+        ]
+    },
+    entry_points={
+        "console_scripts": [
+            "packtivity-run=packtivity.cli:runcli",
+            "packtivity-util=packtivity.cli:utilcli",
+            "packtivity-validate=packtivity.cli:validatecli",
+            "packtivity-checkproxy=packtivity.cli:checkproxy",
+        ],
+    },
+    dependency_links=[],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Operating System :: OS Independent",
+    ],
+)
