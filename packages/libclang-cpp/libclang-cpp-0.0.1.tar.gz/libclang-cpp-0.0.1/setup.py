81f929def4a26462930052d3da62ace0752100b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package libclang-cpp/0.0.1/libclang-cpp-0.0.1.tar.gz

---
+++ packages/libclang-cpp/libclang-cpp-0.0.1.tar.gz/libclang-cpp-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='libclang-cpp',
+    version="1.0.0",
+    description='libclang-cpp - listed by Cycode to prevent confusion',
+    long_description="libclang-cpp",
+    long_description_content_type='text/markdown',
+)
