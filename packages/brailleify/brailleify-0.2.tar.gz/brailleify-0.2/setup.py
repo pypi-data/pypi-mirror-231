f935c15c3d4c16be0857bf18f20d238c7f2c95f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:08:48 +0000

    Add package brailleify/0.2/brailleify-0.2.tar.gz

---
+++ packages/brailleify/brailleify-0.2.tar.gz/brailleify-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brailleify',
+    version='0.2',
+    packages=find_packages(),
+    author='Michael Erickson',
+    author_email='releif@protonmail.com',
+    description='A Python package for converting text to Braille.'
+)
