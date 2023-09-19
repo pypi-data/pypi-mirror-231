3f9221f38111e1e5ef03394eaeebc808cde308f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:08:40 +0000

    Add package Agent-Function-Calling/0.2/Agent_Function_Calling-0.2.tar.gz

---
+++ packages/Agent-Function-Calling/Agent_Function_Calling-0.2.tar.gz/Agent_Function_Calling-0.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Agent_Function_Calling",
+    version="0.2",
+    description="A tool to easily integrate custom functions with OpenAI's GPT models.",
+    author="Thierry Mayrand",
+    author_email="thierry.mayrand.ads@gmail.com",
+    license="MIT",  # or another license you're using
+    packages=find_packages(),
+    install_requires=[
+        "openai",
+        "dotenv",
+        "PyYAML",
+    ],
+    entry_points={
+        'console_scripts': [
+            'run_agent=Clean_agent.main:run_conversation',  # Update with your package name
+        ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",  # or "4 - Beta" or "5 - Production/Stable"
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
