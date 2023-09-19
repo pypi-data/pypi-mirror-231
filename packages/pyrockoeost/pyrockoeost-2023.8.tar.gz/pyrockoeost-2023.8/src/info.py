12810027cd30f90953059a2bdadc3ef4caeac664
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:06:52 +0000

    Add package pyrockoeost/2023.8/pyrockoeost-2023.8.tar.gz

---
+++ packages/pyrockoeost/pyrockoeost-2023.8.tar.gz/pyrockoeost-2023.8/src/info.py
@@ -0,0 +1,7 @@
+# This module is automatically created from setup.py
+git_sha1 = None
+local_modifications = None
+version = '2023.007'
+long_version = 'pyrockoeost-2023.007-2023-09-06_20:01:23'  # noqa
+installed_date = '2023-09-06_20:01:23'
+src_path = 'C:\\Users\\Kylian\\Desktop\\VACATION ALL\\pyrockoeost'
