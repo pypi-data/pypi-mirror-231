af577c65e70fe3fadf20c92419ece28fe110cfaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 15:08:10 +0000

    Add package cbssa/0.0.5/cbssa-0.0.5-py3-none-any.whl

---
+++ packages/cbssa/cbssa-0.0.5-py3-none-any.whl/cbssa/__init__.py
@@ -0,0 +1,7 @@
+from . import bayesian
+from . import data_binning
+from . import legacy
+from . import linear_regression
+from . import logistic_regression
+from . import model_evaluation
+from . import utils
