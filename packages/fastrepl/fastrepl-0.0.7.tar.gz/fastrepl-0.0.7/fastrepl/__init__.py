5d9baffe18e43841b3786f4fdbe8be608e85f2e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 04:06:28 +0000

    Add package fastrepl/0.0.7/fastrepl-0.0.7.tar.gz

---
+++ packages/fastrepl/fastrepl-0.0.7.tar.gz/fastrepl-0.0.7/fastrepl/__init__.py
@@ -0,0 +1,22 @@
+from fastrepl.eval import (
+    load_metric,
+    HumanClassifierRich,
+    LLMGradingHead,
+    LLMClassificationHead,
+    LLMGradingHeadCOT,
+    LLMClassificationHeadCOT,
+    Evaluator,
+    SimpleEvaluator,
+    RAGEvaluator,
+)
+
+from fastrepl.utils import DEBUG
+
+from fastrepl.repl import Updatable
+
+from fastrepl.runner import (
+    LocalRunner,
+    RemoteRunner,
+)
+
+from fastrepl.pytest_plugin import TestReport
