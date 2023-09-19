bbb154ed5fbb186abb61aa92145d50852f015f52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 03:06:22 +0000

    Add package aws-c-http/0.0.1/aws-c-http-0.0.1.tar.gz

---
+++ packages/aws-c-http/aws-c-http-0.0.1.tar.gz/aws-c-http-0.0.1/README.md
@@ -0,0 +1,4 @@
+# aws-c-http
+aws-c-http - listed by Cycode to prevent dependency confusion.
+Please contact us if you are the owner of this package.
+research@cycode.com
---
+++ packages/aws-c-http/aws-c-http-0.0.1.tar.gz/aws-c-http-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='aws-c-http',
+    version="1.0.0",
+    description='aws-c-http - listed by Cycode to prevent confusion',
+    long_description="aws-c-http",
+    long_description_content_type='text/markdown',
+)
