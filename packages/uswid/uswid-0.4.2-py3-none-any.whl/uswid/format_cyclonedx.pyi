5692f9fafa29347ce60b4341bac5695c067bd9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-19 14:06:37 +0000

    Add package uswid/0.4.2/uswid-0.4.2-py3-none-any.whl

---
+++ packages/uswid/uswid-0.4.2-py3-none-any.whl/uswid/format_cyclonedx.pyi
@@ -0,0 +1,8 @@
+from .container import uSwidContainer as uSwidContainer
+from .entity import uSwidEntityRole as uSwidEntityRole
+from .format import uSwidFormatBase as uSwidFormatBase
+from .identity import uSwidIdentity as uSwidIdentity
+
+class uSwidFormatCycloneDX(uSwidFormatBase):
+    def __init__(self) -> None: ...
+    def save(self, container: uSwidContainer) -> bytes: ...
