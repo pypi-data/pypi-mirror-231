e23d2d2ac57f055505f1f0170b1fff11fcc40a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:38 +0000

    Add package ixbrowser-local-api/1.0.1/ixbrowser-local-api-1.0.1.zip

---
+++ packages/ixbrowser-local-api/ixbrowser-local-api-1.0.1.zip/ixbrowser-local-api-1.0.1/ixbrowser_local_api/utils.py
@@ -0,0 +1,60 @@
+import sys
+import requests
+from datetime import datetime
+from .consts import Consts
+from .errors import UnexpectedError, HttpError, ResponseError
+
+
+HTTP_CODE_FOR_SUCCESS = 200
+RESULT_CODE_FOR_SUCCESS = 0
+
+
+class Utils(object):
+
+    SHOW_REQUEST_LOG = False
+
+    @staticmethod
+    def now():
+        return int(datetime.now().timestamp() * 1000)
+
+    @staticmethod
+    def get_api_response(url, params=None):
+        """
+        get api response
+        :param url:
+        :param params:
+        :return:
+        """
+        r = None
+        error_msg = None
+        if Utils.SHOW_REQUEST_LOG:
+            print('request url=', url)
+            print('request params=', params)
+        try:
+            r = requests.post(url, json=params, timeout=20)
+        except Exception as e:
+            error_msg = 'exception desc:' + str(e)
+
+        if error_msg is None:
+            if r.status_code == HTTP_CODE_FOR_SUCCESS:
+                if Utils.SHOW_REQUEST_LOG:
+                    print('response string=', r.text)
+                result = r.json()
+                if 'error' in result:
+                    if 'code' in result['error']:
+                        if result['error']['code'] == RESULT_CODE_FOR_SUCCESS:
+                            if 'data' in result:
+                                return result['data']
+                            else:
+                                return True
+                        else:
+                            raise ResponseError(result['error'])
+                    else:
+                        raise UnexpectedError("The returned data does not contain the 'error.code' key")
+                else:
+                    raise UnexpectedError("The returned data does not contain the 'error' key")
+            else:
+                raise HttpError(r.status_code)
+        else:
+            raise UnexpectedError(error_msg)
+
