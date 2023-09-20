1da38abd8ec3044c3e29bd903fc9c8677dd7ccc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:06:43 +0000

    Add package drawzero/0.4.1/drawzero-0.4.1-py3-none-any.whl

---
+++ packages/drawzero/drawzero-0.4.1-py3-none-any.whl/drawzero/examples/99_errors.py
@@ -0,0 +1,37 @@
+import os
+import traceback
+
+os.environ['EJUDGE_MODE'] = 'true'
+
+
+from drawzero import *
+line('nocolor', (100, 100), ('asdf', ))
+
+
+
+class ExceptionPrinter:
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        if exc_traceback:
+            traceback_len = 0
+            cur = exc_traceback
+            while cur:
+                traceback_len += 1
+                cur = cur.tb_next
+            print(f'Traceback is {traceback_len} deep')
+            print(''.join(traceback.format_tb(exc_traceback)), end='')
+            print(exc_value)
+        return True
+
+# import drawzero
+# print(help(drawzero))
+# exit()
+#
+#
+s = ExceptionPrinter()
+
+with s: line('nocolor', (100, 100), (200, 200))
+with s: line(C.red, (100, 200), (200, 'baz'))
+with s: rect('violet', (100, 200, 300, 'baz'))
