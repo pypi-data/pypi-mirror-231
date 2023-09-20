b83175c59f9ad8770080aed78345961321cd3ea9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:19 +0000

    Add package SG1AB/1.0.2.4/SG1AB-1.0.2.4-py3-none-any.whl

---
+++ packages/SG1AB/SG1AB-1.0.2.4-py3-none-any.whl/SG1AB/__init__.py
@@ -0,0 +1,23 @@
+# 아키텍처별로 사용할 모듈을 가져옵니다.
+import platform
+import os
+import shutil
+
+__all__ = ['nnfw_api_pybind']
+
+# 복사할 .so 파일을 저장할 디렉토리 경로
+destination_directory = os.getcwd()
+
+# 현재 아키텍처를 확인합니다.
+architecture = platform.machine()
+
+if architecture == 'x86_64':
+    from .x86_64 import nnfw_api_pybind
+elif architecture == 'armv7l':
+    from .armv7l import nnfw_api_pybind
+elif architecture == 'aarch64':
+    from .aarch64 import nnfw_api_pybind
+else:
+    raise ImportError(f"Unsupported architecture: {architecture}")
+
+
