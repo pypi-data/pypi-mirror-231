6f03ebba512df845816ade385d67d75994a1b7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:06:17 +0000

    Add package instructor/0.2.8/instructor-0.2.8.tar.gz

---
+++ packages/instructor/instructor-0.2.8.tar.gz/instructor-0.2.8/instructor/__init__.py
@@ -0,0 +1,14 @@
+from .function_calls import OpenAISchema, openai_function, openai_schema
+from .dsl import MultiTask, Maybe, llm_validator, CitationMixin
+from .patch import patch
+
+__all__ = [
+    "OpenAISchema",
+    "openai_function",
+    "CitationMixin",
+    "MultiTask",
+    "Maybe",
+    "openai_schema",
+    "patch",
+    "llm_validator",
+]
