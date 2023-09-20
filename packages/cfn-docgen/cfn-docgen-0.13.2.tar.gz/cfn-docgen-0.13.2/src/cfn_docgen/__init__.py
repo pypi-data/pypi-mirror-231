e6944830d7aed766b2ced13b98dbf7a7ff9bc0c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:09:53 +0000

    Add package cfn-docgen/0.13.2/cfn-docgen-0.13.2.tar.gz

---
+++ packages/cfn-docgen/cfn-docgen-0.13.2.tar.gz/cfn-docgen-0.13.2/src/cfn_docgen/__init__.py
@@ -0,0 +1,16 @@
+from cfn_docgen.domain.model.cfn_document_generator import (
+    CfnDocumentDestination as __CfnDocumentDestination
+)
+from cfn_docgen.domain.services.cfn_docgen_service import (
+    CfnDocgenService as __CfnDocgenService,
+    CfnDocgenServiceCommandInput as __CfnDocgenServiceCommandInput,
+    CfnTemplateSource as __CfnTemplateSource
+)
+
+
+__version__="0.13.2"
+
+CfnDocgenService = __CfnDocgenService
+CfnDocgenServiceCommandInput = __CfnDocgenServiceCommandInput
+CfnTemplateSource = __CfnTemplateSource
+CfnDocumentDestination = __CfnDocumentDestination
