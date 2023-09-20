99a1b1995eaa88d02e9f09f1ac8ce45f43b47352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:23 +0000

    Add package entegywrapper/2.0.0/entegywrapper-2.0.0-py3-none-any.whl

---
+++ packages/entegywrapper/entegywrapper-2.0.0-py3-none-any.whl/entegywrapper/Plugins/extAuth.py
@@ -0,0 +1,35 @@
+from entegywrapper.errors import EntegyFailedRequestError
+
+
+def external_authentication(self, profile_id: str, device_id: str) -> dict:
+    """
+    Authenticates a user's login via an external system.
+
+    Parameters
+    ----------
+        `profile_id` (`str`): the profileId that is logging in
+        `device_id` (`int`): the deviceId that is logging in
+
+    Returns
+    -------
+        `dict`: the response from the API
+    """
+    data = {"profileId": profile_id, "deviceId": device_id, "requestVersion": 1}
+
+    response = self.post(
+        self.api_endpoint + "/v2/Authentication/ExternalProfile", data=data
+    )
+
+    match response["response"]:
+        case 200:
+            return response
+        case 401:
+            raise EntegyFailedRequestError("Profile Doesn't Exist")
+        case 406:
+            raise EntegyFailedRequestError("Invalid Device Id")
+        case 407:
+            raise EntegyFailedRequestError("Profile is Banned")
+        case 408:
+            raise EntegyFailedRequestError("Profile is not Active")
+        case _:
+            raise EntegyFailedRequestError("Unknown error")
