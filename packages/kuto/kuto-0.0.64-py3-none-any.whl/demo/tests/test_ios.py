7a1d9aaa37ac79e5002af25cb1ad0018d7942c37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 16:07:15 +0000

    Add package kuto/0.0.64/kuto-0.0.64-py3-none-any.whl

---
+++ packages/kuto/kuto-0.0.64-py3-none-any.whl/demo/tests/test_ios.py
@@ -0,0 +1,29 @@
+import kuto
+from kuto.ios import Case
+
+from page.ios_page import IndexPage, MyPage, SettingPage
+
+
+class TestIosDemo(Case):
+    """IOS应用demo"""
+
+    def start(self):
+        self.index_page = IndexPage(self.driver)
+        self.my_page = MyPage(self.driver)
+        self.set_page = SettingPage(self.driver)
+
+    def test_go_setting(self):
+        self.index_page.myTab.click()
+        self.my_page.settingBtn.click()
+        self.set_page.about.assert_exists()
+
+
+if __name__ == '__main__':
+    """仅执行本模块"""
+    kuto.main(
+        platform="ios",
+        device_id="00008101-000E646A3C29003A",
+        pkg_name='com.qizhidao.company'
+    )
+
+
