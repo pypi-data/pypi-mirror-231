bf38f04f56d63814f316838cbecb98b44ca4d5bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:34 +0000

    Add package pdfTableJson/0.0.16/pdfTableJson-0.0.16-py3-none-any.whl

---
+++ packages/pdfTableJson/pdfTableJson-0.0.16-py3-none-any.whl/pdfTableJson/plus_data.py
@@ -0,0 +1,8 @@
+import sys, pathlib, fitz
+
+def getMoreData():
+    fname = sys.argv[1]  # get document filename
+    with fitz.open(fname) as doc:  # open document
+        text = chr(12).join([page.get_text() for page in doc])
+    # write as a binary file to support non-ASCII characters
+    pathlib.Path(fname + ".txt").write_bytes(text.encode())
