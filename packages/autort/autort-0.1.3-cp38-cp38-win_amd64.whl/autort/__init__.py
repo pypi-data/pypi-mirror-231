a03e30de631828fd988b8ac6badc524b26d20296
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 14:06:10 +0000

    Add package autort/0.1.3/autort-0.1.3-cp38-cp38-win_amd64.whl

---
+++ packages/autort/autort-0.1.3-cp38-cp38-win_amd64.whl/autort/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT license.
+
+raise RuntimeError("AutoRT from PyPI is not up-to-date, please install pip packages from: https://github.com/microsoft/antares/releases")
