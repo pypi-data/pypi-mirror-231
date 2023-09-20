3b674aa5037cadee05e8f454c63b4b99a510bebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:07:34 +0000

    Add package pdfTableJson/0.0.3/pdfTableJson-0.0.3.tar.gz

---
+++ packages/pdfTableJson/pdfTableJson-0.0.3.tar.gz/pdfTableJson-0.0.3/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='pdfTableJson',
+    version='0.0.3',
+    license = 'GNU AFFERO GPL 3.0',
+    description='PDF Table to JSON',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='hielosan',
+    author_email='hielosan@naver.com',
+    url='https://github.com/yousojeong/pdfTableJson/',
+    project_urls={
+        "Bug Tracker": "https://github.com/yousojeong/pdfTableJson/issues",
+    },
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'PyMuPDF',
+        'pdfTableJson',
+    ],
+    packages=find_packages(exclude=[]),
+    keywords=['pdf', 'table', 'json', 'converter', 'cv', 'openCV'],
+    python_requires='>=3.8',
+    package_data={},
+    zip_safe=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+    ],
+)
