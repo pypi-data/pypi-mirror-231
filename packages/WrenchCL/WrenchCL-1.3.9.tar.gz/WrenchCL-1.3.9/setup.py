956204d77cf0881df55a6bc04073056f616d96ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 14:07:11 +0000

    Add package WrenchCL/1.3.9/WrenchCL-1.3.9.tar.gz

---
+++ packages/WrenchCL/WrenchCL-1.3.9.tar.gz/WrenchCL-1.3.9/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+# Read the long description from README.md or README.rst
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='WrenchCL',
+    version='v1.3.9',
+    author='willem@wrench.ai',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # or "text/x-rst" if you used reStructuredText
+    packages=find_packages(),
+    install_requires=[
+        'colorama~=0.4.6',
+        'pandas~=1.5.3',
+        'requests~=2.29.0',
+        'psycopg2~=2.9.6',
+        'python-dotenv~=1.0.0',
+        'openai~=0.27.4',
+        'tenacity~=8.2.2',
+    ],
+)
