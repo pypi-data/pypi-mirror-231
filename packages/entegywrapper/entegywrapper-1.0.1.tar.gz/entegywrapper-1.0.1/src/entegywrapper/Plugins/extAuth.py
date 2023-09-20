7dc67ac8371fa80794b48204afd1d9b2acc5ea25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:28 +0000

    Add package entegywrapper/1.0.1/entegywrapper-1.0.1.tar.gz

---
+++ packages/entegywrapper/entegywrapper-1.0.1.tar.gz/entegywrapper-1.0.1/src/entegywrapper/Plugins/extAuth.py
@@ -0,0 +1,38 @@
+import json
+
+from typing import Any
+
+
+def external_authentication(
+    self,
+    profile_id: str,
+    device_id: str,
+    *,
+    request_version: int = 1
+) -> dict[str, Any]:
+    """
+    Authenticate a user's login via an external system.
+
+    Parameters
+    ----------
+        `profile_id` (`str`): the profile ID that is logging in
+        `device_id` (`int`): the device ID that is logging in
+        `request_version` (`int`, optional): the version of the request; defaults to `1`
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "profileId": profile_id,
+        "deviceId": device_id,
+        "requestVersion": request_version,
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/Authentication/ExternalProfile",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
