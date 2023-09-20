51672648eda65af62148ecf8652baf3edaf15f1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:06:30 +0000

    Add package flowtask/4.7.12/flowtask-4.7.12-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/flowtask/flowtask-4.7.12-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/flowtask/events/events/__init__.py
@@ -0,0 +1,26 @@
+"""FlowTask Events.
+
+Event System for Flowtask.
+"""
+from .abstract import AbstractEvent
+from .log import LogEvent
+from .notify_event import NotifyEvent
+from .dummy import Dummy
+from .webhook import WebHook
+from .file import FileDelete, FileCopy
+from .teams import TeamsMessage
+from .sendfile import SendFile
+from .alerts import Alert
+
+__all__ = (
+    'LogEvent',
+    'AbstractEvent',
+    'NotifyEvent',
+    'Dummy',
+    'WebHook',
+    'FileDelete',
+    'FileCopy',
+    'TeamsMessage',
+    'SendFile',
+    'Alert',
+)
