aa179fe0148ff4f0c45a2d4cd0c3ba714e391246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:06:32 +0000

    Add package compmec-shape/1.0.0/compmec_shape-1.0.0.tar.gz

---
+++ packages/compmec-shape/compmec_shape-1.0.0.tar.gz/compmec_shape-1.0.0/src/compmec/shape/__init__.py
@@ -0,0 +1,24 @@
+"""
+This module analyse sections of beams by using the boundary element method
+It uses mainly curves as boundary to compute the elements.
+For the moment, it only uses polygon shapes:
+    retangular is a 4-side shape while a circle is a polygon with many sides
+"""
+from compmec.shape.curve import IntegratePlanar, PlanarCurve
+from compmec.shape.jordancurve import IntegrateJordan, JordanCurve
+from compmec.shape.plot import ShapePloter
+from compmec.shape.polygon import Point2D
+from compmec.shape.primitive import Primitive
+from compmec.shape.shape import (
+    ConnectedShape,
+    DisjointShape,
+    EmptyShape,
+    IntegrateShape,
+    SimpleShape,
+    WholeShape,
+)
+
+__version__ = "1.0.0"
+
+if __name__ == "__main__":
+    pass
