7cb7ca8f66c5e1718cc3367ccf6415c95b2fd34e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:18 +0000

    Add package entegywrapper/0.1.8/entegywrapper-0.1.8-py3-none-any.whl

---
+++ packages/entegywrapper/entegywrapper-0.1.8-py3-none-any.whl/entegywrapper/Content/documents.py
@@ -0,0 +1,99 @@
+import requests, json, os, sys
+
+sys.path.append(os.path.dirname(__file__))
+from icons import icon
+
+
+def addDocuments(self, templateType, moduleId, fileDocuments):
+    """
+    Return all user profiles
+
+    Arguments:
+        templateType -- The template type of the page you're adding the documents to
+
+        moduleId -- The moduleId of the page you're adding the documents to
+
+        fileDocuments -- The file documents you want to add
+
+        e.g.
+
+        [
+            {
+                "name":"Power Point",
+                "externalReference":"document-56454",
+                "icon":icon["Film Reel"],
+                "fileUrl":"https://example.org/powerpoints/session/44444/powerpoint.ppt"
+            },
+            {
+                "name":"Demonstration Video",
+                "externalReference":"document-56455",
+                "icon":icon["Film Reel"],
+                "fileUrl":"https://example.org/powerpoints/session/44444/demonstrationvideo.mp4"
+            }
+        ]
+
+    Returns:
+        Base response object
+    """
+    data = {
+        "projectId": self.projectID,
+        "apiKey": self.getKey(),
+        "templateType": templateType,
+        "moduleId": moduleId,
+        "fileDocuments": fileDocuments,
+    }
+
+    resp = requests.post(
+        self.APIEndpoint + "/v2/Document/AddFile",
+        headers=self.headers,
+        data=json.dumps(data),
+    )
+    if resp == None:
+        raise Exception("No reponse received from API")
+    output = resp.json()
+    return output
+
+
+def addExternalContentDocuments(self, templateType, moduleId, externalContentItems):
+    """
+    Return all user profiles
+
+    Arguments:
+        templateType -- The template type of the page you're adding the documents to
+
+        moduleId -- The moduleId of the page you're adding the documents to
+
+        externalContentItems -- The external content documents you want to add
+
+        e.g.
+
+        [
+            {
+                "name": "Test External Content",
+                "icon": 69,
+                "externalReference": "ext_content",
+                "fileUrl": "https://www.youtube.com/watch?v=ieWm9T_GgSA",
+                "type": "YouTube"
+            }
+        ]
+
+    Returns:
+        Base response object
+    """
+    data = {
+        "projectId": self.projectID,
+        "apiKey": self.getKey(),
+        "templateType": templateType,
+        "moduleId": moduleId,
+        "externalContentItems": externalContentItems,
+    }
+
+    resp = requests.post(
+        self.APIEndpoint + "/v2/Document/AddExternalContent",
+        headers=self.headers,
+        data=json.dumps(data),
+    )
+    if resp == None:
+        raise Exception("No reponse received from API")
+    output = resp.json()
+    return output
---
+++ packages/entegywrapper/entegywrapper-0.1.8-py3-none-any.whl/entegywrapper/Profiles/profileLinks.py
@@ -0,0 +1,224 @@
+import requests, json
+
+
+def selectedProfileLinks(self, profileId, returnLimit=100):
+    """
+    Return all the profile links the profile ha
+
+    Arguments:
+        profileId -- User profile ID
+
+        returnLimit -- The index and amount of results to return, if not provided is defaulted to 0 and 100
+
+    Returns:
+        Base response object
+    """
+    data = {
+        "projectId": self.projectID,
+        "apiKey": self.getKey(),
+        "profileID": profileId,
+    }
+
+    resp = requests.post(
+        self.APIEndpoint + "/v2/ProfileLink/Selected/",
+        headers=self.headers,
+        data=json.dumps(data),
+    )
+    if resp == None:
+        raise Exception("No reponse received from API")
+    output = resp.json()
+    return output
+
+
+def pageProfileLinks(self, templateType, moduleId, returnLimit=100):
+    """
+    Gets all the profiles linked to a Content Page
+
+    Arguments:
+        templateType -- The templateType of the page
+
+        moduleId -- The moduleId of the page
+
+        returnLimit -- The index and amount of results to return, if not provided is defaulted to 0 and 100
+
+    Returns:
+        Pagination response and list of profile objects
+    """
+    data = {
+        "projectId": self.projectID,
+        "apiKey": self.getKey(),
+        "templateType": templateType,
+        "moduleId": moduleId,
+    }
+
+    resp = requests.post(
+        self.APIEndpoint + "/v2/ProfileLink/Page/",
+        headers=self.headers,
+        data=json.dumps(data),
+    )
+    if resp == None:
+        raise Exception("No reponse received from API")
+    output = resp.json()
+    return output
+
+
+def selectProfileLink(self, profileId, link):
+    """
+    Allows you to select a link for a profile
+
+    Arguments:
+        profileId -- User profile ID
+
+        link -- The link you wish to select
+
+        e.g.
+
+        {
+            "templateType":"session",
+            "moduleId":1
+        }
+
+    Returns:
+        Base response object
+    """
+    data = {
+        "projectId": self.projectID,
+        "apiKey": self.getKey(),
+        "profileID": profileId,
+        "link": link,
+    }
+
+    resp = requests.post(
+        self.APIEndpoint + "/v2/ProfileLink/Select/",
+        headers=self.headers,
+        data=json.dumps(data),
+    )
+    if resp == None:
+        raise Exception("No reponse received from API")
+    output = resp.json()
+    return output
+
+
+def multiSelectProfileLinks(self, profiles):
+    """
+    Allows you to select multiple pages on multiple profiles at once
+
+    Arguments:
+        profiles-- 	A list of profile references with link objects within. Refer to the example JSON
+
+        e.g.
+
+        [{
+            "profileId":"ff11c742-346e-4874-9e24-efe6980a7453",
+            "links":[
+            {
+                "templateType":"sesSiOn",
+                "moduleId":1
+            },
+            {
+                "templateType":"sesSiOn",
+                "moduleId":2
+            },
+            {
+                "templateType":"sesSiOn",
+                "moduleId":3
+            }]
+        },
+        {
+            "profileId":"3e7026f6-cb67-452b-b962-125731807855",
+            "links":[
+            {
+                "templateType":"sesSiOn",
+                "moduleId":4
+            },
+            {
+                "templateType":"sesSiOn",
+                "moduleId":2
+            },
+            {
+                "templateType":"sesSiOn",
+                "moduleId":3
+            }]
+        }]
+
+    Returns:
+        Base response object
+    """
+    data = {"projectId": self.projectID, "apiKey": self.getKey(), "profiles": profiles}
+
+    resp = requests.post(
+        self.APIEndpoint + "/v2/ProfileLink/MultiSelect/",
+        headers=self.headers,
+        data=json.dumps(data),
+    )
+    if resp == None:
+        raise Exception("No reponse received from API")
+    output = resp.json()
+    return output
+
+
+def deSelectProfileLinks(self, profileId, link):
+    """
+    Allows you to deselect a link for a profile
+
+    Arguments:
+        profileId -- The profileId of the profile
+
+        link -- The link you wish to select
+
+        e.g.
+
+        {
+            "templateType":"session",
+            "moduleId":1
+        }
+
+    Returns:
+        Base response object
+    """
+    data = {
+        "projectId": self.projectID,
+        "apiKey": self.getKey(),
+        "profileId": profileId,
+        "link": link,
+    }
+
+    resp = requests.post(
+        self.APIEndpoint + "/v2/ProfileLink/Deselect/",
+        headers=self.headers,
+        data=json.dumps(data),
+    )
+    if resp == None:
+        raise Exception("No reponse received from API")
+    output = resp.json()
+    return output
+
+
+def clearProfileLinks(self, profileId, templateType):
+    """
+    Allows you to clear all the selected links of a templateType on a single profile
+
+    Arguments:
+        profileId -- The profileId of the profile
+
+        templateType -- The templateType to clear links of
+
+    Returns:
+        Base response object
+    """
+    data = {
+        "projectId": self.projectID,
+        "apiKey": self.getKey(),
+        "profileId": profileId,
+        "templateType": templateType,
+    }
+
+    resp = requests.post(
+        self.APIEndpoint + "/v2/ProfileLink/Clear/",
+        headers=self.headers,
+        data=json.dumps(data),
+    )
+    if resp == None:
+        raise Exception("No reponse received from API")
+    output = resp.json()
+    return output
