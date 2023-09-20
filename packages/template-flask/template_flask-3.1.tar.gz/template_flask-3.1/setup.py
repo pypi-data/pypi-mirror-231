2dfb68d54fca885d63052038d6427a1e91a2b0a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 14:09:55 +0000

    Add package template-flask/3.1/template_flask-3.1.tar.gz

---
+++ packages/template-flask/template_flask-3.1.tar.gz/template_flask-3.1/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+keywords = ['flask', 'directory structure', 'cookie cutter'
+            'venv', 'bootstrap']
+
+with open("README.txt", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='template_flask',
+    version="3.1",
+    author='Zach Beebe',
+    author_email='z.beebe@yahoo.com',
+    description='Python module for creating a template flask app structure and virtual environment, along with Bootstrap examples',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/zacharybeebe/template_flask',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+    keywords=keywords,
+    python_requires='>=3.6',
+    py_modules=['template_flask'],
+    include_package_data=True
+)
