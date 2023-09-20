d7879c36e1db5751d1d36d864f35622cd03db0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:09:18 +0000

    Add package ibm-watson-machine-learning/1.0.322/ibm_watson_machine_learning-1.0.322.tar.gz

---
+++ packages/ibm-watson-machine-learning/ibm_watson_machine_learning-1.0.322.tar.gz/ibm_watson_machine_learning-1.0.322/ibm_watson_machine_learning/foundation_models/utils/enums.py
@@ -0,0 +1,28 @@
+#  -----------------------------------------------------------------------------------------
+#  (C) Copyright IBM Corp. 2023.
+#  https://opensource.org/licenses/BSD-3-Clause
+#  -----------------------------------------------------------------------------------------
+
+from enum import Enum
+
+__all__ = [
+    "ModelTypes",
+    "DecodingMethods",
+]
+
+
+class ModelTypes(Enum):
+    """Supported foundation models."""
+    FLAN_T5_XXL = "google/flan-t5-xxl"
+    FLAN_UL2 = "google/flan-ul2"
+    MT0_XXL = "bigscience/mt0-xxl"
+    GPT_NEOX = 'eleutherai/gpt-neox-20b'
+    MPT_7B_INSTRUCT2 = 'ibm/mpt-7b-instruct2'
+    STARCODER = 'bigcode/starcoder'
+    LLAMA_2_70B_CHAT = 'meta-llama/llama-2-70b-chat'
+
+
+class DecodingMethods(Enum):
+    """Supported decoding methods for text generation."""
+    SAMPLE = "sample"
+    GREEDY = "greedy"
