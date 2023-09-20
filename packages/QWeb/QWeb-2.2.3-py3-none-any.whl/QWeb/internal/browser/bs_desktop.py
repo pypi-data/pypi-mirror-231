6968c5dbb8c7b09f2abf952ca80c30e3a6db4fb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 16:07:21 +0000

    Add package QWeb/2.2.3/QWeb-2.2.3-py3-none-any.whl

---
+++ packages/QWeb/QWeb-2.2.3-py3-none-any.whl/QWeb/internal/browser/bs_desktop.py
@@ -0,0 +1,62 @@
+from __future__ import annotations
+import os
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+from typing import Any
+from robot.api import logger
+from QWeb.internal import browser, exceptions, util
+
+NAMES: dict[str, tuple[str, str]] = {
+    # Default  versions for different browsers.
+    'chrome': ('Chrome', 'latest'),
+    'ie': ('IE', 'latest'),
+    'internet explorer': ('IE', 'latest'),
+    'edge': ('Edge', 'latest'),
+    'firefox': ('Firefox', 'latest'),
+    'ff': ('Firefox', 'latest'),
+    'safari': ('Safari', '15.3')
+}
+
+OS: dict[str, str] = {
+    # Default versions for Windows and OSX.
+    'osx': '"Big Sur',
+    'windows': '10'
+}
+
+
+def open_browser(bs_browser: str, project_name: str, run_id: str, **kwargs: Any) -> WebDriver:
+    bs_key = util.get_rfw_variable_value('${APIKEY}') or os.environ.get('bskey')
+    bs_user = util.get_rfw_variable_value('${USERNAME}') or os.environ.get('bsuser')
+    bs_os = util.get_rfw_variable_value('${BSOS}') or 'windows'
+
+    desired_caps: dict = {
+        'bstack:options': {
+            "buildName": project_name,
+            "projectName": project_name,
+            "sessionName": run_id,
+            'os': bs_os,
+            'osVersion': util.get_rfw_variable_value('${BSOSVERSION}') or OS[bs_os.lower()],
+            'resolution': util.get_rfw_variable_value('${BSRESOLUTION}') or '1920x1080',
+            "local": util.get_rfw_variable_value('${BSLOCAL}') or "false",
+            "localIdentifier": util.get_rfw_variable_value('${BSLOCALID}') or '',
+            **kwargs,
+        },
+
+        'browserName': util.get_rfw_variable_value('${BROWSER}') or NAMES[bs_browser][0],
+        'browserVersion': util.get_rfw_variable_value('${BROWSERVERSION}') or NAMES[bs_browser][1]
+    }
+
+    # handle issue where any, even empty value in localIdentifier turns local to true
+    if desired_caps["bstack:options"]["local"] == 'false':
+        del desired_caps["bstack:options"]["localIdentifier"]
+
+    try:
+        executor_url = f'https://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+        driver = webdriver.Remote(command_executor=executor_url, desired_capabilities=desired_caps)
+        logger.info(f'BrowserStack session ID: {driver.session_id}', also_console=True)
+    except WebDriverException as e:
+        logger.error(e)
+        raise exceptions.QWebException('Incorrect Browserstack capabilities.')
+    browser.cache_browser(driver)
+    return driver
---
+++ packages/QWeb/QWeb-2.2.3-py3-none-any.whl/QWeb/internal/browser/safari.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+from typing import Optional, Any
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+from QWeb.internal import browser
+
+NAMES: list[str] = ["safari", "sf"]
+open_windows: list[str] = []
+
+
+def open_browser(port: int = 0,
+                 executable_path: str = '/usr/bin/safaridriver',
+                 reuse_service: bool = False,
+                 desired_capabilities: Optional[dict[str, Any]] = None,
+                 quiet: bool = False) -> WebDriver:
+
+    desired_capabilities = DesiredCapabilities.SAFARI
+
+    driver = webdriver.Safari(port, executable_path, reuse_service, desired_capabilities, quiet)
+    # If implicit_wait is not > 0 Safaridriver starts raising TimeoutExceptions
+    #    instead of proper exception types
+    driver.implicitly_wait(0.1)
+    driver.maximize_window()
+    browser.cache_browser(driver)
+    open_windows.append(driver.current_window_handle)
+    return driver
