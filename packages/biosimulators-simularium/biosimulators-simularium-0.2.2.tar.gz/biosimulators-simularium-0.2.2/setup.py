af8c5570344aa8ab1974da9278311761c80e8e9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:07:38 +0000

    Add package biosimulators-simularium/0.2.2/biosimulators-simularium-0.2.2.tar.gz

---
+++ packages/biosimulators-simularium/biosimulators-simularium-0.2.2.tar.gz/biosimulators-simularium-0.2.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='biosimulators-simularium',
+    version='0.2.2',
+    packages=find_packages(),
+    url='https://github.com/biosimulators/Biosimulators_simularium',
+    author='Alexander Patrie/BioSimulators Team',
+    author_email='info@biosimulators.org',
+    install_requires=[
+        'zarr',
+        'simulariumio[tutorial]',
+        'smoldyn',
+        'biosimulators-utils'
+    ],
+    entry_points={
+            'console_scripts': [
+                'biosimulators-simularium = biosimulators_simularium.__main__:main',
+            ],
+    },
+)
