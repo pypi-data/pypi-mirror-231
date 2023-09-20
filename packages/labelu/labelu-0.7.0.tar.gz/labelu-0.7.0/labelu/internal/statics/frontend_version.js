b21b4b0af52545dc30a86e2d15237b3ea0145772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 16:07:05 +0000

    Add package labelu/0.7.0/labelu-0.7.0.tar.gz

---
+++ packages/labelu/labelu-0.7.0.tar.gz/labelu-0.7.0/labelu/internal/statics/frontend_version.js
@@ -0,0 +1,14 @@
+(function () {
+  window.__frontend = {
+    version: '2.2.0',
+    deps: {
+      '@label-u/annotation': '2.2.0',
+      '@label-u/components': '2.2.0',
+      '@label-u/components-react': '1.0.0',
+      '@label-u/formatter': '1.0.1',
+      '@label-u/interface': '1.0.0',
+      '@label-u/video-editor-react': '1.0.0',
+      '@label-u/video-react': '1.0.0',
+    },
+  };
+})();
