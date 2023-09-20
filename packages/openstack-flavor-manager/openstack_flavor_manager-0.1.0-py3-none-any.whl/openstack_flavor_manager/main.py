bfa6cf5373ddb0b03a585bee78e188f8ccd716dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:07:18 +0000

    Add package openstack-flavor-manager/0.1.0/openstack_flavor_manager-0.1.0-py3-none-any.whl

---
+++ packages/openstack-flavor-manager/openstack_flavor_manager-0.1.0-py3-none-any.whl/openstack_flavor_manager/main.py
@@ -0,0 +1,35 @@
+import typer
+import logging
+from openstack_flavor_manager.reference import get_url
+from openstack_flavor_manager.cloud import Cloud
+from openstack_flavor_manager.ensure import Ensure
+
+app = typer.Typer(help="Client to manage OpenStack flavors")
+
+
+@app.command("ensure")
+def ensure(url: str, cloud: str = typer.Option("openstack", help="Cloud name"),
+           recommended: bool = typer.Option(False, help="Additionally install recommended flavors")) -> None:
+    flavour_definitions = get_url(url)
+    ensure_object = Ensure(cloud=Cloud(cloud), definitions=flavour_definitions, recommended=recommended)
+    ensure_object.ensure()
+
+
+# This is needed for:
+# 1) Global app option handling
+# 2) Prevent that the single "ensure" command is automatically ommited
+@app.callback()
+def callback(debug_logging: bool = typer.Option(False, "--debug", help="Enable debug logging")):
+    logging.basicConfig(
+        level=logging.INFO if not debug_logging else logging.DEBUG,
+        format="%(asctime)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S"
+    )
+
+
+def main():
+    app()
+
+
+if __name__ == "__main__":
+    main()
