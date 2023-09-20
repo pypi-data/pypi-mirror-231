9baf6596e9b82cd3214dea9e5aa704a70d8fde4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:06:50 +0000

    Add package 1RF21IS005/0.1/1RF21IS005-0.1.tar.gz

---
+++ packages/1RF21IS005/1RF21IS005-0.1.tar.gz/1RF21IS005-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="1RF21IS005",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        # Any dependencies your project might have.
+    ],
+)
