0f39e78e34b2ac38fb50dddddc0c32af4a495939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:09:53 +0000

    Add package kl-feishu/0.0.7/kl_feishu-0.0.7.tar.gz

---
+++ packages/kl-feishu/kl_feishu-0.0.7.tar.gz/kl_feishu-0.0.7/kl_feishu/__init__.py
@@ -0,0 +1,14 @@
+from .version import (
+    __author__,
+    __author_email__,
+    __copyright__,
+    __description__,
+    __license__,
+    __title__,
+    __url__,
+    __version__,
+)
+from .feishu import FeiShuHelp, CardMsgRoot
+from .feishu_def import GroupInfo,GroupMember
+
+# __all__ = ["FeishuHelp", "CardMsgRoot"]
