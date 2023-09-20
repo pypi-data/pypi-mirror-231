244b75c971e7f083fe68534d9aa2c84c38430c60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 14:07:21 +0000

    Add package instadump/0.2.0/instadump-0.2.0.tar.gz

---
+++ packages/instadump/instadump-0.2.0.tar.gz/instadump-0.2.0/instadump/lib/utils.py
@@ -0,0 +1,20 @@
+from dateutil.relativedelta import relativedelta
+
+import re
+
+
+def parse_period(period: str) -> relativedelta:
+    pattern = r"(\d+)([dmy])"
+
+    if result := re.match(pattern, period):
+        value, unit = result.groups()
+
+        match unit:
+            case "d":
+                return relativedelta(days=int(value))
+            case "m":
+                return relativedelta(months=int(value))
+            case "y":
+                return relativedelta(years=int(value))
+    else:
+        raise ValueError(f"Invalid period string: {period}")
