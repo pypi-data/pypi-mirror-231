83676dfc7407d8a9e724220edb89be7afcd5185d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:06:24 +0000

    Add package rpc-queue-manager/0.2/rpc_queue_manager-0.2.tar.gz

---
+++ packages/rpc-queue-manager/rpc_queue_manager-0.2.tar.gz/rpc_queue_manager-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='rpc_queue_manager',
+    version='0.2',
+    description='A simple library for interacting with RabbitMQ RPC in Django',
+    author='Surya Dev Singh',
+    author_email='surya.dev@careers360.com',
+    packages=['rpc_queue_manager'],
+    install_requires=[
+        'pika',
+    ],
+)
