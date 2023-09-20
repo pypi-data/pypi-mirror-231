c7c28d31b9e19668e5bf5c52cf17b1c17cc93425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:25 +0000

    Add package ixbrowser-local-api/1.0.0/ixbrowser-local-api-1.0.0.zip

---
+++ packages/ixbrowser-local-api/ixbrowser-local-api-1.0.0.zip/ixbrowser-local-api-1.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(name='ixbrowser-local-api',
+      version='1.0.0',
+      description='A client of ixBrowser local api',
+      author='IXSPY Team',
+      author_email='tech@ixspy.com',
+      url='https://github.com/ixspyinc/ixbrowser-local-api-python',
+      license='MIT',
+      keywords='',
+      packages=find_packages(),
+      install_requires=['requests >= 2']
+      )
