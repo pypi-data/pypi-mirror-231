072a0ac593887a757f591d621756e0f07afd6ea4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:07:17 +0000

    Add package unstructured-haystack/0.0.5/unstructured_haystack-0.0.5.tar.gz

---
+++ packages/unstructured-haystack/unstructured_haystack-0.0.5.tar.gz/unstructured_haystack-0.0.5/README.md
@@ -0,0 +1,35 @@
+# Unstructured Haystack
+
+[![PyPI - Version](https://img.shields.io/pypi/v/unstructured-haystack.svg)](https://pypi.org/project/unstructured-haystack)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/unstructured-haystack.svg)](https://pypi.org/project/unstructured-haystack)
+
+-----
+
+## Unstructured Connectors for Haystack
+
+This is an example Haystack 2.0 integration. It's an integration for Unstructured.io connectors. Please contribute 🚀
+
+The current version has 2 available Unstructured connectors:
+- **Discord**: `UnstructuredDiscordConnector`
+- **GitHub**: `UnstructuredGitHubConnector`
+- **Google Drive**: `UnstructuredGoogleDriveConnector`
+
+## How to use in a Haystack 2.0 Pipeline 
+For example, you can write documents fetched from Discord using the `UnstructuredDiscordConnector`:
+
+```python
+from haystack.preview import Pipeline
+from haystack.preview.components.writers import DocumentWriter
+from unstructured_haystack import UnstructuredDiscordConnector
+from chroma_haystack import ChromaDocumentStore
+
+# Chroma is used in-memory so we use the same instances in the two pipelines below
+document_store = ChromaDocumentStore()
+connector = UnstructuredDiscordConnector(api_key="UNSTRUCTURED_API_KEY", discord_token="DISCORD_TOKEN")
+
+indexing = Pipeline()
+indexing.add_component("connector", connector)
+indexing.add_component("writer", DocumentWriter(document_store))
+indexing.connect("connector.documents", "writer.documents")
+indexing.run({"connector": {"channels" : "993539071815200889", "period": 3, "output_dir" : "discord-example"}})
+```
---
+++ packages/unstructured-haystack/unstructured_haystack-0.0.5.tar.gz/unstructured_haystack-0.0.5/src/unstructured_haystack/unstuctured_haystack.py
@@ -0,0 +1,42 @@
+import subprocess
+from typing import Dict, List, Any, Optional
+from canals.serialization import default_to_dict, default_from_dict
+from haystack.preview import component, Document
+import glob, json
+
+@component
+class UnstructuredConnector:
+    def __init__(self, api_key: str, output_dir: str):
+        self.args = ["unstructured-ingest"]
+        self.opts = ["--api-key", api_key]
+       
+        self.output_dir = output_dir
+    
+    def to_dict(self) -> Dict[str, Any]:
+        return default_to_dict(self, args=self.args, opts = self.opts, output_dir=self.output_dir)
+    
+    @classmethod
+    def from_dict(cls, data: Dict[str, Any]) -> "UnstructuredConnector":
+        return default_from_dict(cls, data)
+    
+    @component.output_types(documents=List[Document])    
+    def run(self):
+        command = self.args + self.opts
+        process = subprocess.Popen(command, stdout=subprocess.PIPE)
+        output, error = process.communicate()
+
+        # Print output
+        if process.returncode == 0:
+            haystack_docs=[]
+            print(f'{self.output_dir}/**/*.json')
+            for json_file in glob.glob(f'{self.output_dir}/**/*.json', recursive=True):
+                with open(json_file,'r') as fin:
+                    unstructured_doc = json.load(fin)
+                for el in unstructured_doc:
+                    metadata = el['metadata']
+                    metadata['unstructured_type'] = el['type']
+                    haystack_docs.append(Document(content=el['text'], metadata=metadata))
+        else:
+            print('Command failed. Error:')
+            print(error)
+        return {"documents": haystack_docs}
