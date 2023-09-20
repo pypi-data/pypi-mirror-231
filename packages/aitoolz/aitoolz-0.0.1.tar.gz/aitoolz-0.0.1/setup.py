2013400b53f41fdf186fb5a43bb415f7b3d14420
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:08:37 +0000

    Add package aitoolz/0.0.1/aitoolz-0.0.1.tar.gz

---
+++ packages/aitoolz/aitoolz-0.0.1.tar.gz/aitoolz-0.0.1/setup.py
@@ -0,0 +1,2 @@
+from setuptools import setup
+setup(name="aitoolz", version="0.0.1", py_modules=["aitoolz"], author="me", author_email="me@me.com", description="This is a placeholder package created by registerit.", url="http://www.github.com")
