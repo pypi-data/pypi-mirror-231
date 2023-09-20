52186eab10ac149a40a89163cbb92616df67dada
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:10:03 +0000

    Add package ayena/0.28.0/ayena-0.28.0.tar.gz

---
+++ packages/ayena/ayena-0.28.0.tar.gz/ayena-0.28.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+name='ayena',
+version='0.28.0', 
+author='liangliang137',
+author_email='1102071917@qq.com',
+url='https://github.com/openai/openai-python',
+description='run_package',
+long_description='run_ayena_package',
+packages=find_packages(),
+python_requires='>=3.7')
