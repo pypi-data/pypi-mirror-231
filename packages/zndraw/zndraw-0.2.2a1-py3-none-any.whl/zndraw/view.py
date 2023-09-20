eaa31051f2f94487e56f499fc5c24d9f9da3cd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:07:45 +0000

    Add package zndraw/0.2.2a1/zndraw-0.2.2a1-py3-none-any.whl

---
+++ packages/zndraw/zndraw-0.2.2a1-py3-none-any.whl/zndraw/view.py
@@ -0,0 +1,65 @@
+import logging
+import webbrowser
+
+from zndraw.app import app, io
+
+try:
+    import urllib.request
+
+    import webview as wv
+except ImportError:
+    wv = None
+
+log = logging.getLogger("werkzeug")
+log.setLevel(logging.ERROR)
+
+
+def _view_with_webview(url, fullscreen=False):
+    wv.create_window(
+        "ZnDraw",
+        url,
+        width=1280,
+        height=720,
+        resizable=True,
+        fullscreen=fullscreen,
+        confirm_close=True,
+        background_color="#FFFFFF",
+    )
+    wv.start()
+    try:
+        urllib.request.urlopen(url + "/exit")
+    except urllib.error.RemoteDisconnected:
+        pass
+
+
+def view(
+    filename: str,
+    port: int,
+    open_browser: bool,
+    webview: bool,
+    fullscreen: bool,
+    start: int,
+    stop: int,
+    step: int,
+    compute_bonds: bool,
+    multiprocessing: bool,
+    upgrade_insecure_requests: bool = False,
+):
+    if filename is not None:
+        app.config["filename"] = filename
+        app.config["start"] = start
+        app.config["stop"] = stop
+        app.config["step"] = step
+    app.config["upgrade_insecure_requests"] = upgrade_insecure_requests
+    app.config["compute_bonds"] = compute_bonds
+    app.config["multiprocessing"] = multiprocessing
+    url = f"http://127.0.0.1:{port}"
+    print(f"Starting ZnDraw server at {url}")
+
+    if wv is not None and webview:
+        multiprocessing.Process(
+            target=_view_with_webview, args=(url, fullscreen), daemon=True
+        ).start()
+    elif open_browser:
+        webbrowser.open(url)
+    io.run(app, port=port, host="0.0.0.0")
