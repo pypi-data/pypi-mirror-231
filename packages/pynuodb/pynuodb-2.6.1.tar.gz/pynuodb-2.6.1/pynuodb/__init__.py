1144b71d14082adf8347efc09cd932942a7ec975
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:06:55 +0000

    Add package pynuodb/2.6.1/pynuodb-2.6.1.tar.gz

---
+++ packages/pynuodb/pynuodb-2.6.1.tar.gz/pynuodb-2.6.1/pynuodb/__init__.py
@@ -0,0 +1,13 @@
+"""Classes containing the exceptions for reporting errors.
+
+(C) Copyright 2013-2023 Dassault Systemes SE.  All Rights Reserved.
+
+This software is licensed under a BSD 3-Clause License.
+See the LICENSE file provided with this software.
+"""
+
+__version__ = '2.6.1'
+
+from .connection import *
+from .datatype import *
+from .exception import *
