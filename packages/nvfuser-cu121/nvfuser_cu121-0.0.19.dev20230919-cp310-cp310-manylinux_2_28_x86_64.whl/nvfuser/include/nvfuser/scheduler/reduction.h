b5bdc3a6f9f9ad792142a58686af15870686efab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:09:19 +0000

    Add package nvfuser-cu121/0.0.19.dev20230919/nvfuser_cu121-0.0.19.dev20230919-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/nvfuser-cu121/nvfuser_cu121-0.0.19.dev20230919-cp310-cp310-manylinux_2_28_x86_64.whl/nvfuser/include/nvfuser/scheduler/reduction.h
@@ -0,0 +1,34 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#pragma once
+
+#include <ATen/core/ivalue.h>
+#include <exceptions.h>
+
+#include <fusion.h>
+#include <scheduler/reduction_heuristic.h>
+
+namespace nvfuser {
+
+class SchedulerRuntimeInfo;
+class HeuristicSummary;
+
+TORCH_CUDA_CU_API std::shared_ptr<ReductionParams> getReductionHeuristics(
+    Fusion* fusion,
+    const at::ArrayRef<c10::IValue>& runtime_inputs,
+    HeuristicSummary* data_cache = nullptr);
+
+TORCH_CUDA_CU_API std::shared_ptr<ReductionParams> getReductionHeuristics(
+    Fusion* fusion,
+    SchedulerRuntimeInfo& runtime_info,
+    HeuristicSummary* data_cache = nullptr);
+
+TORCH_CUDA_CU_API void scheduleReduction(
+    Fusion* fusion,
+    const ReductionParams& rparams);
+} // namespace nvfuser
