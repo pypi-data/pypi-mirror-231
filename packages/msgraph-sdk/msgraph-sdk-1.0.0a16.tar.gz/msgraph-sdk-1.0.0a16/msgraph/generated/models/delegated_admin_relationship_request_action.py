3b2c3a5d8660fbb21b3ff882fde7ea5cafa70523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:06:03 +0000

    Add package msgraph-sdk/1.0.0a16/msgraph-sdk-1.0.0a16.tar.gz

---
+++ packages/msgraph-sdk/msgraph-sdk-1.0.0a16.tar.gz/msgraph-sdk-1.0.0a16/msgraph/generated/models/delegated_admin_relationship_request_action.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class DelegatedAdminRelationshipRequestAction(str, Enum):
+    LockForApproval = "lockForApproval",
+    Approve = "approve",
+    Terminate = "terminate",
+    UnknownFutureValue = "unknownFutureValue",
+
