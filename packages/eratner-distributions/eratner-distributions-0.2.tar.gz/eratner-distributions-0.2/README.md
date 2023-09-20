718a2df30a3fdc76108056efe8e3bca1ed123c74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 14:09:56 +0000

    Add package eratner-distributions/0.2/eratner-distributions-0.2.tar.gz

---
+++ packages/eratner-distributions/eratner-distributions-0.2.tar.gz/eratner-distributions-0.2/README.md
@@ -0,0 +1,7 @@
+### Table of Contents
+
+1. [Installation](#installation)
+
+## Installation <a name="installation"></a>
+
+You can install via `pip install eratner-distributions`
---
+++ packages/eratner-distributions/eratner-distributions-0.2.tar.gz/eratner-distributions-0.2/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='eratner-distributions',
+      version='0.2',
+      description='Gaussian distributions and Binomial Distributions',
+      packages=['distributions'],
+      zip_safe=False)
