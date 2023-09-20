149a77a6301e7d194a023fd3740fec9df1e2810a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 16:06:51 +0000

    Add package larvaworld/0.0.18/larvaworld-0.0.18-py3-none-any.whl

---
+++ packages/larvaworld/larvaworld-0.0.18-py3-none-any.whl/larvaworld/lib/model/__init__.py
@@ -0,0 +1,12 @@
+
+# from .object import Entity, ModelEntity, SpatialEntity
+
+# from .spatial import SpatialEntity
+
+from .object import *
+from .deb import *
+from .modules import *
+from .agents import *
+
+from .envs import *
+
