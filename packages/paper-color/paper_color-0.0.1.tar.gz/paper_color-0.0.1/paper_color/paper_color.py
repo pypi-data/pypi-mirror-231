e0d20887409cf8c8e22fd4e02902b9bd8ae8eb59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 16:06:57 +0000

    Add package paper-color/0.0.1/paper_color-0.0.1.tar.gz

---
+++ packages/paper-color/paper_color-0.0.1.tar.gz/paper_color-0.0.1/paper_color/paper_color.py
@@ -0,0 +1,16 @@
+
+
+
+
+def get_color(theme=None):
+    color_list = ['#3366FF',
+                  '#6AB520',
+                  '#0CA6FF',
+                  '#FFBF1E',
+                  '#FF4E2B']
+    return color_list
+
+
+if __name__ == '__main__':
+    color_list = get_color()
+    print(color_list)
