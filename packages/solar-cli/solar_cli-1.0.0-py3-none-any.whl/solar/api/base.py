efbceb4561468fed2b3f53148102180e7025abaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:06:59 +0000

    Add package solar-cli/1.0.0/solar_cli-1.0.0-py3-none-any.whl

---
+++ packages/solar-cli/solar_cli-1.0.0-py3-none-any.whl/solar/api/base.py
@@ -0,0 +1,207 @@
+import json
+import logging
+from typing import List, Optional
+
+import aiohttp
+import orjson
+from more_itertools import chunked
+
+from solar.types.analysis import AnalysisModel
+from solar.types.cluster_status import ClusterStatus
+
+# requests.packages.urllib3.disable_warnings()  # type: ignore
+logger = logging.getLogger("root")
+
+
+class ApiEngine:
+    """Base class for working with API Solr"""
+
+    def __init__(
+        self,
+        base_url: str,
+        collection: Optional[str] = None,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        id_col: str = "id",
+    ) -> None:
+        self.base_url = base_url
+        self.collection = collection
+        self.username = username
+        self.password = password
+        self.id_col = id_col
+        self.client = None
+
+    async def build_client(self):
+        """
+        aiohttp client initialization.
+        Have to used before calling .api_request
+        """
+
+        if self.password is not None and self.username is not None:
+            auth = aiohttp.BasicAuth(login=self.username, password=self.password)
+        else:
+            auth = None
+
+        self.client: Optional[aiohttp.ClientSession] = aiohttp.ClientSession(
+            auth=auth, connector=aiohttp.TCPConnector(verify_ssl=False)
+        )
+
+    async def close_client(self):
+        """Close aiohttp client and free resources"""
+
+        if self.client is None:
+            return
+
+        await self.client.close()
+
+    async def _fetch_ids(self, query: str = "*:*") -> Optional[List[str]]:
+        """Fetch collection documents IDs
+
+        Args:
+            query (str, optional): `query` parameter for Solr. Useful for filtering docs
+                Defaults to "*:*".
+
+        Returns:
+            Optional[List[str]]: array of documents IDs
+        """
+        url_path = f"/solr/{self.collection}/export"
+        content = await self.api_request(
+            method="GET",
+            path=url_path,
+            params={"q": query, "fl": self.id_col, "sort": f"{self.id_col} desc"},
+        )
+        if content is None:
+            return
+
+        data = orjson.loads(content)
+
+        body = data["response"]
+        ids = [i["id"] for i in body["docs"]]
+        return ids
+
+    async def api_request(
+        self,
+        *,
+        path: str,
+        params: dict = {},
+        method: str = "GET",
+        **kwargs,
+    ) -> Optional[str]:
+        """Create request to Solr API
+
+        Args:
+            path (str): URL path
+            params (dict, optional): query params.
+                Defaults to {}.
+            method (str, optional): request method (GET, POST, etc...).
+                Defaults to "GET".
+
+        Raises:
+            ValueError: .build_client() is not called before request
+
+        Returns:
+            Optional[aiohttp.ClientResponse]: Solr response.
+                If None - response status code is >= 400
+        """
+        if self.client is None:
+            raise ValueError(".build_client() have to be called before request")
+
+        async with self.client.request(
+            method=method,
+            url=self.base_url + path,
+            params=params,
+            **kwargs,
+        ) as resp:
+            if resp.status == 401:
+                logger.error("Auth error")
+                raise ValueError("Auth error :(")
+
+            if resp.status != 200:
+                text = await resp.text()
+                logger.error(f"{method} - {resp.url} - {text}")
+                return None
+
+            try:
+                return await resp.text(encoding="UTF-8")
+            except UnicodeDecodeError as e:
+                logger.error("Can't decode answer :( {e}")
+                return None
+
+    async def remove_collection(self, collection_name: str):
+        url = f"/solr/admin/collections?action=DELETE&name={collection_name}"
+        await self.api_request(path=url)
+
+    async def reload_collection(self, collection_name: str):
+        url = f"/solr/admin/collections?action=RELOAD&name={collection_name}"
+        await self.api_request(path=url)
+
+    async def cluster_status(self) -> ClusterStatus:
+        url = f"/solr/admin/collections?action=CLUSTERSTATUS&wt=json"
+        status = await self.api_request(
+            path=url,
+        )
+        if status is None:
+            raise ValueError("Error getting cluster status")
+
+        status = json.loads(status)
+        status_model = ClusterStatus(**status)
+        return status_model
+
+    async def create_alias(self, alias_name: str, collections: List[str]):
+        url = "/solr/admin/collections?action=CREATEALIAS"
+        params = dict(name=alias_name, collections=",".join(collections))
+
+        resp = await self.api_request(path=url, params=params)
+        if resp is None:
+            raise ValueError(f"Error creating alias {alias_name}")
+
+    async def remove_alias(self, alias_name: str):
+        url = "/solr/admin/collections?action=DELETEALIAS"
+        params = dict(name=alias_name)
+
+        resp = await self.api_request(path=url, params=params)
+        if resp is None:
+            raise ValueError(f"Error removing alias {alias_name}")
+
+    async def analyzer_step(self, field: str, analyzer: str, text: str):
+        url = f"/solr/{self.collection}/analysis/field"
+        params = {
+            "analysis.fieldname": field,
+            "analysis.fieldvalue": text,
+            "analysis.showmatch": "false",
+            "verbose_output": 0,
+        }
+        resp = await self.api_request(path=url, params=params)
+        if resp is None:
+            raise ValueError("Error fetching analysis result")
+
+        data = json.loads(resp)
+        analysis = AnalysisModel(**data)
+
+        parsed_tokens = []
+        for step_name, result in chunked(
+            analysis.analysis.field_names.content.index, 2
+        ):
+            if analyzer not in step_name:
+                continue
+
+            if isinstance(result, str):
+                analyzer_result = result
+                return analyzer_result
+
+            for i in result:
+                token = i["text"]
+                parsed_tokens.append(token)
+
+        analyzer_result = " ".join(parsed_tokens)
+        return analyzer_result
+
+    async def config_collections(self, config_name: str) -> List[str]:
+        url = f"/api/schema-designer/info?configSet={config_name}&wt=json"
+        resp = await self.api_request(path=url)
+        if resp is None:
+            raise ValueError(f"Error fetching {config_name=} collections :(")
+
+        data = json.loads(resp)
+        return data['collections']
+
---
+++ packages/solar-cli/solar_cli-1.0.0-py3-none-any.whl/solar/api/import_.py
@@ -0,0 +1,251 @@
+import json
+import logging
+import os
+import pathlib
+import time
+from typing import List, Optional, Union
+
+from more_itertools import chunked
+from rich import print
+from rich.progress import (
+    BarColumn,
+    Progress,
+    TaskProgressColumn,
+    TextColumn,
+    TimeRemainingColumn,
+)
+from solar import config
+
+from solar.api.base import ApiEngine
+
+logger = logging.getLogger("root")
+
+
+def recursive_field_remove(doc: dict, field: str):
+    if not isinstance(doc, (dict, list)):
+        return doc
+    if isinstance(doc, list):
+        return [recursive_field_remove(v, field) for v in doc]
+    return {k: recursive_field_remove(v, field) for k, v in doc.items() if k != field}
+
+
+class Importer(ApiEngine):
+    """Class with *import* methods"""
+
+    def _load_json(self, path: Union[str, pathlib.Path]):
+        """Load source `.json` file"""
+
+        if isinstance(path, pathlib.Path):
+            path_str = path.absolute().__str__()
+        else:
+            path_str = path
+
+        with open(path_str, "r", encoding="UTF-8") as f:
+            data = json.load(f)
+
+        if self.collection is None:
+            self.collection = data["collection"]
+        return data
+
+    async def _post_documents(self, docs: List[dict], collection_name: str):
+        """Send documents to Solr.
+
+        Args:
+            docs (List[dict]): array of docs
+
+        Raises:
+            ValueError: error sending docs
+        """
+        curr_time = round(time.time() * 1000)
+        for doc in docs:
+            doc.pop("_version_", None)
+            doc = recursive_field_remove(doc, "_root_")
+
+        url_path = f"/solr/{collection_name}/update"
+        headers = {
+            "Content-type": "application/json",
+        }
+
+        params = {
+            "_": curr_time,
+            "commitWithin": "5000",
+            "overwrite": "true",
+            "wt": "json",
+        }
+
+        doc_binary = json.dumps(docs, ensure_ascii=False).encode("utf-8")
+
+        res = await self.api_request(
+            method="POST",
+            path=url_path,
+            data=doc_binary,
+            headers=headers,
+            params=params,
+        )
+        return res
+
+    async def import_data(
+        self,
+        path: Union[str, pathlib.Path],
+        batch_size: int = 50,
+        overwrite: bool = True,
+        collection: Optional[str] = None,
+    ):
+        """Import data, saved as `.json` file
+
+        Args:
+            path (str): source `.json` path
+            batch_size (int, optional): how many documents will be sent by one request.
+                Defaults to 50.
+        """
+        data = self._load_json(path)
+        docs = data["docs"]
+        docs_ids = set([i[self.id_col] for i in docs])
+
+        print(f"Number of docs: {len(docs_ids)}")
+        if not overwrite:
+            print("Fetching IDs of collection documents...")
+            existing_ids = await self._fetch_ids(query="*:*")
+            if existing_ids is None:
+                raise ValueError("Error fetching collection IDs :(")
+
+            print(f"Number of docs found: {len(existing_ids)}")
+            existing_ids = set(existing_ids)
+            upload_ids = docs_ids - existing_ids
+
+            print(f"{len(upload_ids)} docs will be created")
+            seen = set()
+            upload_docs = []
+            for doc in docs:
+                doc_id = doc[self.id_col]
+                if doc_id not in seen and doc_id in upload_ids:
+                    upload_docs.append(doc)
+                    seen.add(doc_id)
+        else:
+            upload_docs = docs
+
+        del docs
+        del data
+
+        if len(upload_docs) == 0:
+            print("No documents to import :(")
+            return
+
+        if collection is None:
+            collection_name: str = self.collection
+        else:
+            collection_name = collection
+
+        print("Begin import with params:")
+        print(f"Source file: [bold]{path}")
+        print(f"Batch size: [bold]{batch_size}")
+        print(f"Collection: [bold]{collection_name}")
+
+        if not config.FORCE:
+            confirm = input("Correct? (y/n)").lower() == "y"
+            if not confirm:
+                print("[red]Отмена...")
+                return
+
+        num_docs = len(upload_docs)
+
+        with Progress(
+            TextColumn("[progress.description]{task.description}"),
+            BarColumn(),
+            TaskProgressColumn(),
+            TextColumn("({task.completed})"),
+            TimeRemainingColumn(),
+        ) as progress:
+            task = progress.add_task("Uploading...", total=num_docs)
+
+            for batch_docs in chunked(upload_docs, batch_size):
+                try:
+                    await self._post_documents(
+                        docs=batch_docs, collection_name=collection_name
+                    )
+                except Exception:
+                    print("[red]Ошибка :)")
+
+                progress.update(task, advance=batch_size)
+
+    async def _remove_config(self, name: str):
+        url = f"/api/cluster/configs/{name}?omitHeader=true"
+        r = await self.api_request(method="DELETE", path=url)
+        if r is None:
+            raise ValueError("Error deleting config :(")
+
+        print("[green]Ok")
+
+    async def create_collection(self, collection_name: str, config_name: str):
+        url = f"/solr/admin/collections?action=CREATE&name={collection_name}&numShards=1&replicationFactor=1&maxShardsPerNode=1&collection.configName={config_name}"
+        r = await self.api_request(path=url)
+        if r is None:
+            raise ValueError("Error creating collection :(")
+
+    async def import_configs(
+        self,
+        configs_path: Union[str, pathlib.Path],
+        overwrite: bool = False,
+        name: Optional[str] = None,
+    ):
+        """Import config
+
+        Args:
+            configs_path (Union[str, pathlib.Path]): folder with config root
+            overwrite (bool, optional): overwrite currently existing config with the same name
+                Defaults to False.
+            name (Optional[str], optional): create config with this name
+                if `None` - source folder name will be used
+                Defaults to None.
+
+        Raises:
+            ValueError: Error importing config
+        """
+        import io
+        import zipfile
+
+        overwrite_str = "true" if overwrite else "false"
+        cleanup_str = overwrite_str
+
+        if isinstance(configs_path, str):
+            configs_path = pathlib.Path(configs_path)
+
+        if name is None:
+            name = configs_path.name
+
+        print("Import params:")
+        print(f"Source config: [bold]{name}[/bold]")
+        print(f"Overwrite: [bold]{overwrite}[/bold]")
+
+        if not config.FORCE:
+            confirm = input("Correct? (y/n)")
+            if confirm.lower() != "y":
+                print("[red]Stopping...")
+                os._exit(1)
+
+        print("Creating new config...")
+        upload_url = "/solr/admin/configs"
+        params = dict(
+            action="UPLOAD", name=name, overwrite=overwrite_str
+        )
+        with io.BytesIO() as f:
+            with zipfile.ZipFile(f, "w") as zf:
+                for path in configs_path.rglob("*"):
+                    rel_path = path.relative_to(configs_path)
+                    zf.write(path, rel_path)
+
+            f.seek(0)
+
+            resp = await self.api_request(
+                method="POST", path=upload_url, params=params, data=f
+            )
+            if resp is None:
+                raise ValueError("Error sending config .zip archive :(")
+
+        if overwrite:
+            collections = await self.config_collections(config_name=name)
+            for c in collections:
+                print(f"Reloading collection {c}")
+                await self.reload_collection(c)
+
+        print("[bold green]Done!")
