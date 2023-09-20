bfeb32ec1f6d48c3ba6edaccb09fb4e9c457c79b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:33 +0000

    Add package panax/0.0.10/panax-0.0.10.tar.gz

---
+++ packages/panax/panax-0.0.10.tar.gz/panax-0.0.10/panax/__init__.py
@@ -0,0 +1,44 @@
+from wsgiref.simple_server import make_server
+from request import Request
+
+
+urls = []
+
+
+request = Request()
+
+
+def run_server(environ, star_response):
+    star_response('200 OK', [('Content-Type', 'text/html;charset=urf-8')])
+    url = environ['PATH_INFO']
+    func = None
+    for i in urls:
+        if i[0] == url:
+            func = i[2]
+            break
+    if func:
+        request.bind(environ)
+        response = str(func(request)).encode('utf-8')
+    else:
+        response = str({"code": 404, "msg": "Not Found!"}).encode('utf-8')
+    return [response, ]
+
+
+def route(url, method=['GET', 'POST']):
+    def wrapper(handler):
+        urls.append([url, method, handler])
+        return handler
+
+    return wrapper
+
+
+def run(host='127.0.0.1', port=8080, **kwargs):
+    '''
+    启动监听服务
+    '''
+    httpd = make_server(host, port, run_server)
+    print('Courage server starting up ...')
+    print('Listening on http://%s:%d/' % (host, port))
+    print('Use Ctrl-C to quit.')
+    print('')
+    httpd.serve_forever()
