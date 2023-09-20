f8c677c49aa671bd6c975bfbd4303597515efae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:10:06 +0000

    Add package movingclusterlib/0.1.1/movingclusterlib-0.1.1.tar.gz

---
+++ packages/movingclusterlib/movingclusterlib-0.1.1.tar.gz/movingclusterlib-0.1.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='movingclusterlib',
+    version='0.1.1',
+    author='Alexander Khlebushchev',
+    license='MIT',
+    packages=find_packages(),
+    zip_safe=False,
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'matplotlib>=3.6.0',
+        'numpy>=1.23.3',
+        'pandas>=1.5.0',
+        'datatool-python>=1.3.4',
+    ],
+)
