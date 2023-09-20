bf7da96c34862b9bfa87781e263a3460e7a56599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:08:50 +0000

    Add package flask-gordon/0.20.1/flask_gordon-0.20.1-py2.py3-none-any.whl

---
+++ packages/flask-gordon/flask_gordon-0.20.1-py2.py3-none-any.whl/flask_gordon/ext/functions.py
@@ -0,0 +1,19 @@
+import contextlib
+import sys
+
+
+def get_app_class(app):
+    # Is this a flask or celery application?
+    with contextlib.suppress(AttributeError):
+        _ = app.run
+        return "flask"
+
+    with contextlib.suppress(AttributeError):
+        _ = app.control.inspect
+        return "celery"
+
+    return None
+
+
+def is_celery_app():
+    return "celery.bin.celery" in sys.modules
