16d5cae0bdc4158db98b86eed0641fdb6e3bb597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:06:16 +0000

    Add package forestadmin-datasource-toolkit/1.0.0b18/forestadmin_datasource_toolkit-1.0.0b18.tar.gz

---
+++ packages/forestadmin-datasource-toolkit/forestadmin_datasource_toolkit-1.0.0b18.tar.gz/forestadmin_datasource_toolkit-1.0.0b18/forestadmin/datasource_toolkit/datasource_customizer/datasource_customizer.py
@@ -0,0 +1,34 @@
+from typing import Dict
+
+from forestadmin.datasource_toolkit.datasource_customizer.collection_customizer import CollectionCustomizer
+from forestadmin.datasource_toolkit.datasources import Datasource
+from forestadmin.datasource_toolkit.decorators.chart.types import DataSourceChartDefinition
+from forestadmin.datasource_toolkit.decorators.decorator_stack import DecoratorStack
+
+
+class DatasourceCustomizer:
+    def __init__(self) -> None:
+        self.composite_datasource: Datasource = Datasource()
+        self.stack = DecoratorStack(self.composite_datasource)
+
+    def add_datasource(self, datasource: Datasource, options: Dict):
+        # if "include" in options or "exclude" in options:
+        #     $datasource = new PublicationCollectionDatasourceDecorator($datasource);
+        #     $datasource->build();
+        #     $datasource->keepCollectionsMatching($options['include'] ?? [], $options['exclude'] ?? []);
+
+        # if "rename" in options:
+        #     $datasource = new RenameCollectionDatasourceDecorator($datasource);
+        #     $datasource->build();
+        #     $datasource->renameCollections($options['rename'] ?? []);
+
+        for collection in datasource.collections:
+            self.composite_datasource.add_collection(collection)
+
+        self.stack = DecoratorStack(self.composite_datasource)
+
+    def customize_collection(self, collection_name: str) -> CollectionCustomizer:
+        return CollectionCustomizer(self, self.stack, collection_name)
+
+    def add_chart(self, name: str, definition: DataSourceChartDefinition):
+        self.stack.chart.add_chart(name, definition)
