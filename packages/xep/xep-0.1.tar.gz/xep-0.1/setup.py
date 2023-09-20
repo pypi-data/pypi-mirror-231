77a4c0154b524d7ed34141f10d77d311af8f8339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:07:42 +0000

    Add package xep/0.1/xep-0.1.tar.gz

---
+++ packages/xep/xep-0.1.tar.gz/xep-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xep",
+    version="0.1",
+    packages=find_packages(),
+    license="",
+    description="",
+    url="https://github.com/Onanim90/xep",
+    author="Pavel Ilyin",
+    author_email="azunya.90@gmail.com",
+    install_requires=[
+        "openpyxl",
+        "pyyaml",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
