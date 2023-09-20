77b19eb0bab38f6ff1e400a267a2026192291a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:18 +0000

    Add package entegywrapper/1.0.1/entegywrapper-1.0.1-py3-none-any.whl

---
+++ packages/entegywrapper/entegywrapper-1.0.1-py3-none-any.whl/entegywrapper/Profiles/profileCustom.py
@@ -0,0 +1,190 @@
+import requests
+import json
+
+from typing import Any
+
+CustomField: type = dict[str, str | int | bool]
+
+
+def get_profile_custom(self, key: str) -> dict[str, Any]:
+    """
+    Returns a single custom field.
+
+    Parameters
+    ----------
+        `key` (`str`): the key of the custom field to return
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {"projectId": self.project_id, "apiKey": self.get_key(), "key": key}
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileCustomField",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
+
+
+def create_profile_custom(self, custom_field: CustomField) -> dict[str, Any]:
+    """
+    Creates a new Custom Field for Profiles.
+
+    There is a limited number of text custom fields allowed per project. Once a
+    custom field is created, it's type cannot be modified.
+
+    Parameters
+    ----------
+        `custom_field` (`CustomField`): the custom field you wish to create
+
+    The format of `custom_field` is as follows:
+    ```python
+        {
+            "key": "dietary-requirements",
+            "name": "Dietary requirements",
+            "type": "MultiChoice",
+            "options":[
+                {
+                    "name": "Halal"
+                },
+                {
+                    "name": "Vegan"
+                },
+                {
+                    "name": "Lactose Intolerant"
+                },
+                {
+                    "name": "Gluten Intolerant"
+                }
+            ]
+        }
+    ```
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "customField": custom_field,
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileCustomField/Create",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
+
+
+def update_profile_custom(
+    self,
+    key: str,
+    custom_field: CustomField
+) -> dict[str, Any]:
+    """
+    Updates custom profile field `key` with `customField`'s data.
+
+    Fields are optional, not including them omits them from being updated. You
+    can update the field's key by setting a different valid key within the
+    ProfileField object you pass in. You can delete multi choice options by
+    providing an empty or whitespace only `name` along with it's optionId. A
+    null `name` will omit the name from being updated. You can create new
+    options by not including an `optionId`. Unlike most endpoints that will
+    completely fail when invalid data is input, this endpoint will skip over any
+    invalid input and continue to create anything that is valid.
+
+    Parameters
+    ----------
+        `key` (`str`): the key of the custom field to return
+        `custom_field` (`CustomField`): the data to update the field with
+
+    The format of `custom_field` is as follows:
+    ```python
+        {
+            "options": [
+                {
+                    "optionId": 6,
+                    "name": ""
+                },
+                {
+                    "optionId": 7,
+                    "name": "Halal"
+                },
+                {
+                    "optionId": 8,
+                    "name": "Vegan"
+                },
+                {
+                    "name": "Lactose Intolerant"
+                },
+                {
+                    "name": "Gluten Intolerant"
+                }
+            ]
+        }
+    ```
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "key": key,
+        "customField": custom_field
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileCustomField/Update",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
+
+
+def delete_profile_custom(self, key: str) -> dict[str, Any]:
+    """
+    Deletes a custom field.
+
+    Parameters
+    ----------
+        `key` (`str`): the key of the custom field to delete
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "key": key
+    }
+
+    return self.delete(
+        self.api_endpoint + "/v2/ProfileCustomField/Delete",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
+
+
+def all_profile_custom(self) -> dict[str, Any]:
+    """
+    Returns all custom fields.
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key()
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileCustomField/All",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
---
+++ packages/entegywrapper/entegywrapper-1.0.1-py3-none-any.whl/entegywrapper/Profiles/profileLinks.py
@@ -0,0 +1,224 @@
+import json
+
+from typing import Any
+
+Link: type = dict
+"""
+The format of a `Link` is as follows:
+    ```python
+        {
+            "template_type": "session",
+            "moduleId":1
+        }
+    ```
+"""
+
+
+def selected_profile_links(
+    self,
+    profile_id: str,
+    *,
+    return_limit: int = 100
+) -> dict[str, Any]:
+    """
+    Return all the profile links the profile has.
+
+    Parameters
+    ----------
+        `profile_id` (`str`): the profileId of the profile
+        `return_limit` (`int`): the index and amount of results to return; defaults to 100
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "profileID": profile_id,
+        "pagination": {
+            "index": 0,
+            "limit": return_limit
+        },
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileLink/Selected/",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
+
+
+def page_profile_links(
+    self,
+    template_type: str,
+    module_id: str,
+    *,
+    return_limit: int = 100
+) -> dict[str, Any]:
+    """
+    Gets all the profiles linked to a Content Page.
+
+    Parameters
+    ----------
+        `template_type` (`str`): the template_type of the page
+        `module_id` (`int`): the moduleId of the page
+        `return_limit` (`int`): the maximum number of results to return; defaults to 100
+
+    Returns
+    -------
+        Pagination response and list of profile objects
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "templateType":template_type,
+        "moduleId": module_id,
+        "pagination": {
+            "index": 0,
+            "limit": return_limit
+        },
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileLink/Page/",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
+
+
+def select_profile_link(self, profile_id: str, link: Link) -> dict[str, Any]:
+    """
+    Allows you to select a link for a profile
+
+    Parameters
+    ----------
+        `profile_id` (`str`): the profileId of the profile
+        `link` (`Link`): the link you wish to select
+    ```
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "profileID": profile_id,
+        "link": link,
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileLink/Select/",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
+
+
+def multi_select_profile_links(
+    self,
+    profiles: list[str, str | list[Link]]
+) -> dict[str, Any]:
+    """
+    Allows you to select multiple pages on multiple profiles at once
+
+    Parameters
+    ----------
+        `profiles` (`list[str, str | list[Link]]`): list of profile references with link objects within
+
+    The format of `profiles` is as follows:
+    ```python
+        [
+            {
+                "profileId": "ff11c742-346e-4874-9e24-efe6980a7453",
+                "links": [
+                    {
+                        "template_type": "sesSiOn",
+                        "moduleId":1
+                    },
+                    {
+                        "template_type": "sesSiOn",
+                        "moduleId":2
+                    },
+                    {
+                        "template_type": "sesSiOn",
+                        "moduleId":3
+                    }
+                ]
+            },
+        ]
+    ```
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "profiles": profiles
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileLink/MultiSelect/",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
+
+
+def deselect_profile_links(self, profile_id: str, link: Link) -> dict[str, Any]:
+    """
+    Allows you to deselect a link for a profile.
+
+    Parameters
+    ----------
+        `profile_id` (`str`): the profileId of the profile
+        `link` (`Link`): the link you wish to deselect
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "profileId": profile_id,
+        "link": link,
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileLink/Deselect/",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
+
+
+def clear_profile_links(
+    self,
+    profile_id: str,
+    template_type: str
+) -> dict[str, Any]:
+    """
+    Allows you to clear all the selected links of a template_type on a single profile
+
+    Parameters
+    ----------
+        `profile_id` (`str`): the profileId of the profile
+        `template_type` (`str`): the template_type to clear links of
+
+    Returns
+    -------
+        `dict[str, Any]`: API response JSON
+    """
+    data = {
+        "projectId": self.project_id,
+        "apiKey": self.get_key(),
+        "profileId": profile_id,
+        "templateType":template_type,
+    }
+
+    return self.post(
+        self.api_endpoint + "/v2/ProfileLink/Clear/",
+        headers=self.headers,
+        data=json.dumps(data)
+    )
