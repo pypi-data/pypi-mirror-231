d6d58705d39661362ca86961f2494796be6093cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:10:03 +0000

    Add package ayena/0.28.0/ayena-0.28.0-py3-none-any.whl

---
+++ packages/ayena/ayena-0.28.0-py3-none-any.whl/ayena/api_resources/abstract/__init__.py
@@ -0,0 +1,13 @@
+# flake8: noqa
+
+from ayena.api_resources.abstract.api_resource import APIResource
+from ayena.api_resources.abstract.createable_api_resource import CreateableAPIResource
+from ayena.api_resources.abstract.deletable_api_resource import DeletableAPIResource
+from ayena.api_resources.abstract.listable_api_resource import ListableAPIResource
+from ayena.api_resources.abstract.nested_resource_class_methods import (
+    nested_resource_class_methods,
+)
+from ayena.api_resources.abstract.paginatable_api_resource import (
+    PaginatableAPIResource,
+)
+from ayena.api_resources.abstract.updateable_api_resource import UpdateableAPIResource
