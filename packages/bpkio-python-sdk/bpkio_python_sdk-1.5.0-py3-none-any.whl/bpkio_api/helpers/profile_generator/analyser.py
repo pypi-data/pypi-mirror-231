30ad5031a6e6972caf6a0576a6881a351fd7b5d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:09:20 +0000

    Add package bpkio-python-sdk/1.5.0/bpkio_python_sdk-1.5.0-py3-none-any.whl

---
+++ packages/bpkio-python-sdk/bpkio_python_sdk-1.5.0-py3-none-any.whl/bpkio_api/helpers/profile_generator/analyser.py
@@ -0,0 +1,19 @@
+class Message:
+    def __init__(self, level: str, message: str):
+        self.level = level
+        self.message = message
+
+
+class WarningMessage(Message):
+    def __init__(self, message: str):
+        super().__init__("warning", message)
+
+
+class ErrorMessage(Message):
+    def __init__(self, message: str):
+        super().__init__("error", message)
+
+
+class InfoMessage(Message):
+    def __init__(self, message: str):
+        super().__init__("info", message)
