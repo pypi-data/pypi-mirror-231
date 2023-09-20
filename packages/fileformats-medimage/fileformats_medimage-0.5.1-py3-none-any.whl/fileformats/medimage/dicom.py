453ae9615ce8befb634dd922366702e3fa7ae5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:06:24 +0000

    Add package fileformats-medimage/0.5.1/fileformats_medimage-0.5.1-py3-none-any.whl

---
+++ packages/fileformats-medimage/fileformats_medimage-0.5.1-py3-none-any.whl/fileformats/medimage/dicom.py
@@ -0,0 +1,29 @@
+from fileformats.core import mark
+from fileformats.generic import Directory, TypedSet
+from fileformats.application import Dicom
+from .base import MedicalImage
+
+# =====================================================================
+# Custom loader functions for different image types
+# =====================================================================
+
+
+class DicomCollection(MedicalImage):
+    """Base class for collections of DICOM files, which can either be stored within a
+    directory (DicomDir) or presented as a flat list (DicomSet)
+    """
+
+    content_types = (Dicom,)
+    iana_mime = None
+
+    @mark.extra
+    def series_number(self):
+        raise NotImplementedError
+
+
+class DicomDir(DicomCollection, Directory):
+    pass
+
+
+class DicomSet(DicomCollection, TypedSet):
+    pass
