0151accd20df83254e7a3a8d34db7fa169c3940a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:07:39 +0000

    Add package trey-trey/1.0.0/trey_trey-1.0.0.tar.gz

---
+++ packages/trey-trey/trey_trey-1.0.0.tar.gz/trey_trey-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='trey_trey',
+    version='1.0.0',
+    author='makshef',
+    author_email='m228-maks@yandex.ru',
+    description='--',
+    long_description='Readme content goes here',
+    long_description_content_type='text/markdown',
+    url='https://github.com/m228/Work.git',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[],
+)
