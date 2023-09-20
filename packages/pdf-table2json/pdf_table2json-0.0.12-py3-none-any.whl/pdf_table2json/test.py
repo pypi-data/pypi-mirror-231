5c7db123a6e4a06740915bac69ae4985e720068d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:07:47 +0000

    Add package pdf-table2json/0.0.12/pdf_table2json-0.0.12-py3-none-any.whl

---
+++ packages/pdf-table2json/pdf_table2json-0.0.12-py3-none-any.whl/pdf_table2json/test.py
@@ -0,0 +1,9 @@
+# pip install pdf-table2json
+
+from converter import main
+
+result = main("PATH/PDF_NAME.pdf")
+
+print("================================================")
+print(result)
+print("================================================")
