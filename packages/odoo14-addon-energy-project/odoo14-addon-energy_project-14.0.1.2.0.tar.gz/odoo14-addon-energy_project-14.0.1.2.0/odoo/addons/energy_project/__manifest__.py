03231c4a6725925937fb5aa5e2f4e6170d0bfd2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 16:07:26 +0000

    Add package odoo14-addon-energy-project/14.0.1.2.0/odoo14-addon-energy_project-14.0.1.2.0.tar.gz

---
+++ packages/odoo14-addon-energy-project/odoo14-addon-energy_project-14.0.1.2.0.tar.gz/odoo14-addon-energy_project-14.0.1.2.0/odoo/addons/energy_project/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Energy Projects",
+    "summary": """
+        Base module for energetic projects.
+        """,
+    "description": """
+            Base module for energetic projects.
+    """,
+    "author": "Coopdevs Treball SCCL & Som Energia SCCL",
+    "website": "https://coopdevs.org",
+    "category": "Customizations",
+    "version": "14.0.1.2.0",
+    "depends": [
+        "base",
+        "mail",
+    ],
+    "data": [
+        "security/res_groups.xml",
+        "security/ir.model.access.csv",
+        "security/ir_rule_data.xml",
+        "views/inscription_views.xml",
+    ],
+}
