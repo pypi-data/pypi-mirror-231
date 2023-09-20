40179ab9d69eefbfb2474b485aeab9ef1d73dd6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:13 +0000

    Add package hpvsim/1.2.4/hpvsim-1.2.4-py3-none-any.whl

---
+++ packages/hpvsim/hpvsim-1.2.4-py3-none-any.whl/hpvsim/version.py
@@ -0,0 +1,9 @@
+'''
+Version and license information.
+'''
+
+__all__ = ['__version__', '__versiondate__', '__license__']
+
+__version__ = '1.2.4'
+__versiondate__ = '2023-09-19'
+__license__ = f'HPVsim {__version__} ({__versiondate__}) — © 2023 by IDM'
