67fb1d4d4a233a38109087a7f0c8e2c3abf1ddd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 16:07:16 +0000

    Add package larvaworld/0.0.19/larvaworld-0.0.19-py3-none-any.whl

---
+++ packages/larvaworld/larvaworld-0.0.19-py3-none-any.whl/larvaworld/lib/__init__.py
@@ -0,0 +1,7 @@
+
+from . import aux, param, reg, plot, model, process, screen, sim, util
+# from .aux import *
+# from .reg import *
+# import aux, param
+# import aux, param, reg, plot, model, process, screen, sim, util
+from .process.dataset import ParamLarvaDataset,BaseLarvaDataset,LarvaDataset, LarvaDatasetCollection
