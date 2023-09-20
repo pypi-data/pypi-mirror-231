8f2b2b756bdf079d71293d318188476d008ecf1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:27 +0000

    Add package getpotatocsv/1.0.0/getpotatocsv-1.0.0-py3-none-any.whl

---
+++ packages/getpotatocsv/getpotatocsv-1.0.0-py3-none-any.whl/alertemail/alertemail.py
@@ -0,0 +1,29 @@
+import smtplib
+from email.mime.text import MIMEText
+
+def send(recipients,subject,message):
+    # Email details
+    smtp_server = 'mrelay.noc.sony.co.jp'
+    smtp_port = 25
+    sender = 'SCK-H5BAREVOS-NGK-SYSADMIN@sony.com'
+
+    # Create a MIME text object
+    msg = MIMEText(message)
+    msg['Subject'] = subject
+    msg['From'] = sender
+    #msg['To'] = recipient
+    msg['To'] = ', '.join(recipients)
+
+    try:
+        # Connect to the SMTP server
+        server = smtplib.SMTP(smtp_server, smtp_port)
+
+        # Send the email
+        server.sendmail(sender, recipients, msg.as_string())
+
+        print('Email sent successfully!')
+    except Exception as e:
+        print('An error occurred:', str(e))
+    finally:
+        # Disconnect from the SMTP server
+        server.quit()
