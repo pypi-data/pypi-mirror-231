249858841b2337040e35ef487a79f2489a9ec4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:34 +0000

    Add package instantweather/2.0.0/instantweather-2.0.0.tar.gz

---
+++ packages/instantweather/instantweather-2.0.0.tar.gz/instantweather-2.0.0/instantweather/__init__.py
@@ -0,0 +1,1 @@
+from instantweather.instantweather import Weather
---
+++ packages/instantweather/instantweather-2.0.0.tar.gz/instantweather-2.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+setup(
+    name='instantweather',
+    packages=['instantweather'],
+    version='2.0.0',
+    license='MIT',    
+    description='weather forecast data',
+    author='Anuj Pandey',    
+    url='https://github.com/newguy7/instantweather',
+    keywords=['weather','forecast','openweather'],    
+    install_requires=[
+        'requests',        
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
