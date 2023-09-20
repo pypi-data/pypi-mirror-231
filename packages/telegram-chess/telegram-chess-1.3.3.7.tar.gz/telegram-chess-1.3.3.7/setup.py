c4c230d0562b89f98a0e9004c7c439ad39d296ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:06:42 +0000

    Add package telegram-chess/1.3.3.7/telegram-chess-1.3.3.7.tar.gz

---
+++ packages/telegram-chess/telegram-chess-1.3.3.7.tar.gz/telegram-chess-1.3.3.7/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='telegram-chess',
+    version='1.3.3.7',
+    packages=find_packages(),
+    install_requires=[
+        'aiogram',
+        'python-chess',
+    ]
+)
---
+++ packages/telegram-chess/telegram-chess-1.3.3.7.tar.gz/telegram-chess-1.3.3.7/telegram_chess/__init__.py
@@ -0,0 +1,1 @@
+from .telegram_chess import TelegramChess
