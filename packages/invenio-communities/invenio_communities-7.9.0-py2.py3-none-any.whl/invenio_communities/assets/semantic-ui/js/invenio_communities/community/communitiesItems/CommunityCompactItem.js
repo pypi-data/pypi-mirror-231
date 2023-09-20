98f071c705754ae13a2e0bd24cd41f687926f13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 14:07:17 +0000

    Add package invenio-communities/7.9.0/invenio_communities-7.9.0-py2.py3-none-any.whl

---
+++ packages/invenio-communities/invenio_communities-7.9.0-py2.py3-none-any.whl/invenio_communities/assets/semantic-ui/js/invenio_communities/community/communitiesItems/CommunityCompactItem.js
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { CommunityCompactItemComputer } from "./CommunityCompactItemComputer";
+import { CommunityCompactItemMobile } from "./CommunityCompactItemMobile";
+
+export function CommunityCompactItem({
+  result,
+  actions,
+  extraLabels,
+  itemClassName,
+  showPermissionLabel,
+}) {
+  return (
+    <>
+      <CommunityCompactItemComputer
+        result={result}
+        actions={actions}
+        extraLabels={extraLabels}
+        itemClassName={itemClassName}
+        showPermissionLabel={showPermissionLabel}
+      />
+      <CommunityCompactItemMobile
+        result={result}
+        actions={actions}
+        extraLabels={extraLabels}
+        itemClassName={itemClassName}
+        showPermissionLabel={showPermissionLabel}
+      />
+    </>
+  );
+}
+
+CommunityCompactItem.propTypes = {
+  result: PropTypes.object.isRequired,
+  actions: PropTypes.node,
+  extraLabels: PropTypes.node,
+  itemClassName: PropTypes.string,
+  showPermissionLabel: PropTypes.bool,
+};
+
+CommunityCompactItem.defaultProps = {
+  actions: undefined,
+  extraLabels: undefined,
+  itemClassName: "",
+  showPermissionLabel: false,
+};
