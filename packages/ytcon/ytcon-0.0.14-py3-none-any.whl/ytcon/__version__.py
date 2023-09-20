ce7417495ee896cd23168386bf3f7e952c5522d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:08:46 +0000

    Add package ytcon/0.0.14/ytcon-0.0.14-py3-none-any.whl

---
+++ packages/ytcon/ytcon-0.0.14-py3-none-any.whl/ytcon/__version__.py
@@ -0,0 +1,7 @@
+"""
+Created for storing version number.
+The version number will be placed here based on the tag in Github using the tags_marker.py script.
+
+Having "0.0.14" in final releases is NOT allowed.
+"""
+__version__="0.0.14"
