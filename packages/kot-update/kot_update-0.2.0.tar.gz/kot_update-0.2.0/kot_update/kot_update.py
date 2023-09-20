f12699c29cf2ce8590cbb46159183e42ef12e3f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 04:06:51 +0000

    Add package kot-update/0.2.0/kot_update-0.2.0.tar.gz

---
+++ packages/kot-update/kot_update-0.2.0.tar.gz/kot_update-0.2.0/kot_update/kot_update.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import time
+from kot import KOT, KOT_Remote, HASHES, console
+from rich.progress import track
+
+class KOT_Update:
+    def __init__(self, cloud) -> None:
+        self.cloud = cloud
+        self.pre_update_dict = {}
+    def pre_update(self, key):
+        self.pre_update_dict[key] = self.cloud.get(key, encryption_key=None)
+        self.start_time = time.time()
+    def update(self):
+        console.log("[bold green] Update Started")
+        console.log("")
+        the_update_list = str(list(self.pre_update_dict))
+        console.log(f"[bold white] Updating: {the_update_list}")
+        error = []
+        for key in track(self.pre_update_dict, description="           ", console=console):
+            result = False
+            currently = self.pre_update_dict[key]
+            new = new = self.cloud.get(key, encryption_key=None)
+
+
+            if currently != new:
+                result = True
+            
+            
+            if not result:
+                error.append(key)
+
+        end_time = time.time()
+        took_time = int(end_time - self.start_time)
+        console.log(f" Update took {took_time}s")
+
+        for key in error:
+            console.log(f" {key}: [bold red]Failure")
+
+        console.log("")
+        if len(error) != 0:
+            console.log("[bold red] Updating Complated With Error")
+        else:
+            console.log("[bold green] Updating Complated Without any Error")
