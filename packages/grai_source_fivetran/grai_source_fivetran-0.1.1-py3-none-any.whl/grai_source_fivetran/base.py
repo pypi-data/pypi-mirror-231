bd90db46b814abfdb083c03155728e9561163858
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:07:50 +0000

    Add package grai_source_fivetran/0.1.1/grai_source_fivetran-0.1.1-py3-none-any.whl

---
+++ packages/grai_source_fivetran/grai_source_fivetran-0.1.1-py3-none-any.whl/grai_source_fivetran/base.py
@@ -0,0 +1,63 @@
+from functools import cache
+from typing import List, Optional, Tuple
+
+from grai_schemas.base import SourcedEdge, SourcedNode
+from grai_schemas.integrations.base import GraiIntegrationImplementation
+from grai_schemas.v1.source import SourceV1
+
+from grai_source_fivetran.adapters import adapt_to_client
+from grai_source_fivetran.loader import FivetranConnector, NamespaceTypes
+
+
+class FivetranIntegration(GraiIntegrationImplementation):
+    def __init__(
+        self,
+        source: SourceV1,
+        version: Optional[str] = None,
+        namespaces: Optional[NamespaceTypes] = None,
+        default_namespace: Optional[str] = None,
+        parallelization: int = 10,
+        api_key: Optional[str] = None,
+        api_secret: Optional[str] = None,
+        endpoint: Optional[str] = None,
+        limit: Optional[int] = None,
+    ):
+        super().__init__(source, version)
+
+        self.connector = FivetranConnector(
+            namespaces=namespaces,
+            default_namespace=default_namespace,
+            parallelization=parallelization,
+            api_key=api_key,
+            api_secret=api_secret,
+            endpoint=endpoint,
+            limit=limit,
+        )
+
+    def ready(self) -> bool:
+        self.connector.has_query_permissions()
+        return True
+
+    @cache
+    def get_nodes_and_edges(self) -> Tuple[List[SourcedNode], List[SourcedEdge]]:
+        nodes, edges = self.connector.get_nodes_and_edges()
+
+        nodes = adapt_to_client(nodes, self.source, self.version)
+        edges = adapt_to_client(edges, self.source, self.version)
+
+        node_hashes = {hash(node) for node in nodes}
+        if (n_hashes := len(node_hashes)) != len(nodes):
+            message = (
+                f"The Fivetran connection generated {len(nodes) - n_hashes} duplicated nodes. "
+                f"This is likely because there are multiple Fivetran tables with the same name. "
+                f"You can disambiguate these tables by identifying them with different namespaces. Please "
+                f"see the documentation for more information. https://docs.grai.io/integrations/fivetran"
+            )
+            raise ValueError(message)
+        return nodes, edges
+
+    def nodes(self) -> List[SourcedNode]:
+        return self.get_nodes_and_edges()[0]
+
+    def edges(self) -> List[SourcedEdge]:
+        return self.get_nodes_and_edges()[1]
