54e21c29a710fb3411ca94008f2e0947342580a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:09:53 +0000

    Add package dengineclient/2.0.0/dengineclient-2.0.0.tar.gz

---
+++ packages/dengineclient/dengineclient-2.0.0.tar.gz/dengineclient-2.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dengineclient",
+    version="2.0.0",
+    packages=find_packages(),
+    author="zhangmanni",
+    author_email="zhangmanni@didiglobal.com",
+    description="apollo and featurepalette client",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
+
