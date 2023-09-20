6fd06d23ccc8a6d685300bd01438bf83bd5389a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:08:41 +0000

    Add package nytid/4.2/nytid-4.2.tar.gz

---
+++ packages/nytid/nytid-4.2.tar.gz/nytid-4.2/src/nytid/cli/__init__.py
@@ -0,0 +1,24 @@
+"""The CLI of nytid"""
+
+import typer
+from nytid.cli import courses
+import typerconf
+from nytid.cli import schedule
+from nytid.cli import signupsheets
+from nytid.cli import hr
+
+import logging
+import sys
+
+logging.basicConfig(format=f"nytid: %(levelname)s: %(message)s")
+
+cli = typer.Typer()
+
+cli.add_typer(courses.cli, name="courses")
+typerconf.add_config_cmd(cli)
+cli.add_typer(schedule.cli, name="schedule")
+cli.add_typer(signupsheets.cli, name="signupsheets")
+cli.add_typer(hr.cli, name="hr")
+
+if __name__ == "__main__":
+    cli()
