797c5059431cf15aca19322ab57339cba654849d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:28 +0000

    Add package pyOpenSSL-SDK/0.0.0.post0/pyopenssl_sdk-0.0.0.post0-py3-none-any.whl

---
+++ packages/pyOpenSSL-SDK/pyopenssl_sdk-0.0.0.post0-py3-none-any.whl/rubberduck/install.sh
@@ -0,0 +1,7 @@
+i=$(basename "$0")
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$i was called at $dt"
+sleep 5
+echo "$i slept for 5 seconds"
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$i completed at $dt"
