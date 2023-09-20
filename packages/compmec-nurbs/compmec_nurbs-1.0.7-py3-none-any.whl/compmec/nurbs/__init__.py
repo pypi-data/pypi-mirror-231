5b9810da9dd88747264afed83cc0b8f40de4b3f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:07:21 +0000

    Add package compmec-nurbs/1.0.7/compmec_nurbs-1.0.7-py3-none-any.whl

---
+++ packages/compmec-nurbs/compmec_nurbs-1.0.7-py3-none-any.whl/compmec/nurbs/__init__.py
@@ -0,0 +1,10 @@
+from compmec.nurbs.advanced import Intersection, Projection
+from compmec.nurbs.calculus import Derivate, Integrate
+from compmec.nurbs.curves import Curve
+from compmec.nurbs.functions import Function
+from compmec.nurbs.knotspace import GeneratorKnotVector, KnotVector
+
+__version__ = "1.0.7"
+
+if __name__ == "__main__":
+    pass
