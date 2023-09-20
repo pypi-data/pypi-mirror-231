ecb139c8cb2ed6723c32f75081469c710c7a172b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 14:06:13 +0000

    Add package problem180meridian/0.1.2/problem180meridian-0.1.2.tar.gz

---
+++ packages/problem180meridian/problem180meridian-0.1.2.tar.gz/problem180meridian-0.1.2/setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+from io import open
+from setuptools import setup
+
+"""
+
+:authors: sergsadkov
+:license: Apache License, Version 2.0, see LICENSE file
+:copyright: (c) 2023 sergsadkov
+"""
+
+
+version = '0.1.2'
+
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='problem180meridian',
+    version=version,
+
+    author='sergsadkov',
+    author_email='sergsadkov@gmail.com',
+
+    description=u'Fixes 180 degree crossing issue for vector geodata',
+
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    url='https://github.com/sergsadkov/problem180meridian',
+    download_url='https://github.com/sergsadkov/problem180meridian/archive/main.zip',
+
+    license='Apache License, Version 2.0, see LICENSE file',
+
+    packages=['problem180meridian'],
+    install_requires=['numpy', 'GDAL'],
+
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ]
+)
---
+++ packages/problem180meridian/problem180meridian-0.1.2.tar.gz/problem180meridian-0.1.2/test/test_split180_multipolygon.py
@@ -0,0 +1,38 @@
+"""
+Testing problem180meridian.split180_multipolygon()
+"""
+
+from ..problem180meridian import split180_multipolygon
+
+
+# Testing integer coordinates without crossing 180th meridian
+def test_split180_int_nochange():
+
+    test_coordinates = [[((0, 0), (0, 10), (10, 10), (10, 0), (0, 0)), ((100, 100), (100, 110), (110, 110), (110, 100), (100, 100))]]
+    output = 'MULTIPOLYGON (((0 0,0 10,10 10,10 0,0 0)),((100 100,100 110,110 110,110 100,100 100)))'
+
+    test_output = split180_multipolygon(test_coordinates).ExportToWkt()
+    print(test_output)
+    assert output == test_output
+
+
+# Testing integer coordinates with crossing 180th meridian
+def test_split180_int_change():
+
+    test_coordinates = [[((100, 0), (100, 10), (-160, 10), (-160, 0), (100, 0))]]
+    output = 'MULTIPOLYGON (((100 0,100 10,180 10,180 0,100 0)),((-180 10,-160 10,-160 0,-180 0,-180 10)))'
+
+    test_output = split180_multipolygon(test_coordinates).ExportToWkt()
+    print(test_output)
+    assert output == test_output
+
+
+# Testing float coordinates without crossing 180th meridian
+def test_split180_float_nochange():
+
+    test_coordinates = [[((124.055513, -50.114067), (52.829571, -67.533366), (-92.538693, -70.016703), (-169.745357, -51.297079), (124.055513, -50.114067))]]
+    output = 'MULTIPOLYGON (((124.055513 -50.114067,52.829571 -67.533366,-92.538693 -70.016703,-169.745357 -51.297079,-180 -51.113823,180.0 -51.113823,124.055513 -50.114067)))'
+
+    test_output = split180_multipolygon(test_coordinates).ExportToWkt()
+    print(test_output)
+    assert output == test_output
