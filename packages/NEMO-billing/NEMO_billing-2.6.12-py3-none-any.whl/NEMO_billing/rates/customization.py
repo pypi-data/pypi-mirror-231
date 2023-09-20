be7aefcaa876c728da27f9be50fdfafce9073272
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:06:48 +0000

    Add package NEMO-billing/2.6.12/NEMO_billing-2.6.12-py3-none-any.whl

---
+++ packages/NEMO-billing/NEMO_billing-2.6.12-py3-none-any.whl/NEMO_billing/rates/customization.py
@@ -0,0 +1,15 @@
+from NEMO.decorators import customization
+from NEMO.views.customization import CustomizationBase
+
+
+@customization(key="billing_rates", title="Rates")
+class BillingRatesCustomization(CustomizationBase):
+    variables = {"rates_expand_table": "", "allow_blank_rate": "", "rates_daily_per_account": ""}
+
+    def save(self, request, element=None):
+        errors = super().save(request, element)
+        if not errors:
+            from NEMO.rates import rate_class
+
+            rate_class.load_rates()
+        return errors
