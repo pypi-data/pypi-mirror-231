a2c0916a4c3e6d3691c2a7adb7f5dad92f87dfcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:07:57 +0000

    Add package bw-hestia-bridge/0.2.0/bw_hestia_bridge-0.2.0.tar.gz

---
+++ packages/bw-hestia-bridge/bw_hestia_bridge-0.2.0.tar.gz/bw_hestia_bridge-0.2.0/bw_hestia_bridge/hestia_api/querying.py
@@ -0,0 +1,188 @@
+import re
+from typing import Any, Literal, Optional, Union
+
+import requests
+
+from ..utils import get_config
+from .base_api import base_api_data, nested_elements, valid_types
+
+
+def search_hestia(
+    query: Union[str, dict[str, str]],
+    node_type: Optional[str] = None,
+    fields: Optional[list[str]] = None,
+    limit: Optional[int] = 10,
+    how: Literal["or", "and", "exact"] = "or",
+) -> list[dict[str, str]]:
+    """
+    Search the Hestia database.
+
+    Parameters
+    ----------
+    query : str or dict
+        A string to match to names in the Hestia database, or a dict of the
+        form ``{"field_name": value}`` to match `field_name` instead of
+        "name". See the examples below to see how to make more complex
+        quieries.
+    node_type : str, optional (default: any type)
+        A valid type among "actor", "animal", "bibliography", "completeness",
+        "cycle", "emission", "impactassessment", "indicator",
+        "infrastructure", "input", "management", "measurement",
+        "organisation", "practice", "product", "property", "site", "source",
+        "term", "transformation", or "transport".
+    fields : list[str], optional (default: ["@type", "name", "@id"])
+        Fields that will be returned in the search results.
+    limit : int, optional (default: 10)
+        The maximum number of results that will be returned (best match come
+        first).
+    how : {"or", "and", "exact"}, optional (default: "or")
+        Whether the search tries to match any word in `query` ("or"), all
+        words in `query` ("and") or to match the whole query exactly
+        ("exact").
+
+    Returns
+    -------
+    A list of dicts containing the `fields` entries. Additionally, a "_score"
+    value is returned, indicating the accuracy of the match found in the 
+    Hestia database (results are sorted by decreasing "_score").
+
+    Examples
+    --------
+    One can refine the query by searching for nodes that have a product with a
+    name matching "Saplings" by using ::
+
+        search_hestia({"products.term.name": "Saplings"})
+
+    It is also possible to do multi-criteria searches as follow ::
+
+        search_hestia({"name": "Ouidah", "products.term.name": "Saplings"})
+    """
+    url, proxies, headers = base_api_data()
+
+    fields = fields or ["@type", "name", "@id"]
+
+    how = how or "or"
+
+    matches: list[dict] = []
+
+    if not isinstance(query, dict):
+        query = {"name": query}
+
+    # check the query
+    r = r"^(?P<path>\w+)\..+"
+
+    for k, v in query.items():
+        re_match = re.search(r, k)
+
+        qk = {}
+
+        if how in ("and", "or"):
+            qk[k] = {"query": v, "operator": how}
+        elif how == "exact":
+            qk[f"{k}.keyword"] = v
+        else:
+            raise ValueError(f"Invalid `how` argument: '{how}'.")
+
+        if re_match:
+            path = re_match.groupdict()["path"]
+
+            if path in nested_elements:
+                is_nested = True
+
+                matches.append({"nested": {"path": path, "query": {"match": qk}}})
+            else:
+                matches.append({"match": qk})
+        else:
+            matches.append({"match": qk})
+
+    if node_type:
+        assert (
+            node_type.lower() in valid_types
+        ), f"Valid `node_type` entries are {valid_types}"
+
+        matches.append({"match": {"@type": node_type[0].upper() + node_type[1:]}})
+
+    q: dict[str, Any] = {
+        "fields": fields,
+        "limit": limit,
+        "query": {"bool": {"must": matches}},
+    }
+
+    res = requests.post(
+        f"{url}/search", json=q, headers=headers, proxies=proxies
+    ).json()
+
+    return res.get("results", [])
+
+
+def get_hestia_node(
+    node_id: Union[str, dict[str, str]],
+    node_type: Optional[str] = None,
+    data_state: Optional[str] = None,
+) -> dict:
+    """
+    Download the Hestia node associated to `node`.
+
+    Parameters
+    ----------
+    node_id : str or dict[str, str]
+        Hestia ID for the node or dictionary describing the node (e.g. returned from
+        :func:`search_hestia`). If it's a dict, it must contain at least an "@type"
+        and an "@id" entry.
+    node_type : str, optional (default: try to autodetect)
+        A valid type among "actor", "animal", "bibliography", "completeness",
+        "cycle", "emission", "impactassessment", "indicator",
+        "infrastructure", "input", "management", "measurement",
+        "organisation", "practice", "product", "property", "site", "source",
+        "term", "transformation", or "transport". If not provided, will either
+        be taken from `node_id` if it is a dict, or default to "cycle".
+    data_state : str, optional (default: "recalculated")
+        Version of the data, by default, use "recalculated" to download the
+        more detailed version of the data. Use "original" to get the raw data.
+
+    Returns
+    -------
+    The dict associated to the JSON-LD entry describing `node` in the
+    Hestia database.
+    """
+    if isinstance(node_id, dict):
+        assert "@type" in node_id, "`node` must contain an '@type' entry."
+        assert "@id" in node_id, "`node` must contain an '@id' entry."
+
+        node_type = node_id["@type"]
+        node_id = node_id["@id"]
+    else:
+        node_type = node_type or get_node_type(node_id)
+
+    node_type = node_type.lower()
+
+    url, proxies, headers = base_api_data()
+
+    data_state = data_state or "recalculated"
+
+    req_url = f"{url}/{node_type}s/{node_id}?dataState={data_state}"
+
+    return requests.get(req_url, headers=headers, proxies=proxies).json()
+
+
+def get_node_type(node_id: str) -> str:
+    """
+    Get the node type from its Hestia ID
+
+    Parameters
+    ----------
+    node_id : str
+        Hestia ID for the node.
+
+    Raises
+    ------
+    ``ValueError`` if `node_id` is not found.
+    """
+    res = search_hestia({"@id": node_id}, how="exact")
+
+    if res:
+        return res[0]["@type"].lower()
+
+    api_type = "staging" if get_config("use_staging") else "stable"
+
+    raise ValueError(f"The {api_type} API found no node with ID {node_id}.")
---
+++ packages/bw-hestia-bridge/bw_hestia_bridge-0.2.0.tar.gz/bw_hestia_bridge-0.2.0/tests/test_queries.py
@@ -0,0 +1,96 @@
+from bw_hestia_bridge import get_hestia_node, search_hestia, set_config, get_cycle_graph
+
+
+def test_search():
+    """
+    Test the search function
+
+    These tests are dependent on the Staging API of Hestia, if some things
+    change on the Staging Server, they could fail. In such a case it could
+    be convenient to disable these tests if reality demands a rapid merge.
+    """
+    # test empty results
+    assert not search_hestia("nonexistant")
+
+    # test limits and nested search
+    set_config("use_staging", True)
+
+    assert len(search_hestia({"products.term.name": "Saplings"})) == 10
+    assert len(search_hestia({"products.term.name": "Saplings"}, limit=12)) == 12
+
+    # test combined queries
+    assert 0 < len(search_hestia({"name": "ouidah", "products.term.name": "Saplings"})) < 10
+
+    # test more or less precise match
+    res_or = search_hestia(
+        {"name": "conventional denmark", "product.term.name": "Maize"}, how="or")
+
+    res_and = search_hestia(
+        {"name": "conventional denmark", "product.term.name": "Maize"}, how="and")
+
+    assert len(res_and) < len(res_or)
+
+    # test exact match
+    res_or = search_hestia(
+        "Maize, grain, Denmark, 2009, Conventional-Non Irrigated")
+
+    res_exact = search_hestia(
+        "Maize, grain, Denmark, 2009, Conventional-Non Irrigated", how="exact")
+
+    assert len(res_or) > 1 and len(res_exact) == 1
+
+    # check exact with complex match
+    assert len(search_hestia(
+        {"inputs.term.name": 'Inorganic Nitrogen fertiliser, unspecified (kg N)'},
+        how="exact")) > 0
+
+
+def test_download():
+    """
+    Download complete node information from Hestia
+
+    These tests are dependent on the Staging API of Hestia, if some things
+    change on the Staging Server, they could fail. In such a case it could
+    be convenient to disable these tests if reality demands a rapid merge.
+    """
+    set_config("use_staging", True)
+
+    # test with search results
+    res = search_hestia({"name": "ouidah", "products.term.name": "Saplings"})
+
+    node = get_hestia_node(res[0])
+    node_r = get_hestia_node(res[0], data_state="original")
+
+    for k, v in res[0].items():
+        if k in node:
+            assert node[k] == v
+            assert node_r[k] == v
+
+    # test with node id and type
+    nid = res[1]["@id"]
+    ntype = res[1]["@type"]
+
+    node = get_hestia_node(nid, ntype)
+    node2 = get_hestia_node(nid)  # without type
+
+    assert node == node2
+
+    for k, v in res[1].items():
+        if k in node:
+            assert node[k] == v
+
+
+def test_get_cycle_graph():
+    """
+    Test to get other cycle which produces a product within the input tree
+    of a given cycle.
+
+    These tests are dependent on the Staging API of Hestia, if some things
+    change on the Staging Server, they could fail. In such a case it could
+    be convenient to disable these tests if reality demands a rapid merge.
+    """
+    set_config("use_staging", True)
+
+    products = [cycle["via"]["term"]["name"] for cycle in get_cycle_graph("zcuqxv54gn2x")]
+
+    assert "Maize, grain" in products
