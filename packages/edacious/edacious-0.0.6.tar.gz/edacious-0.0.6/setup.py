54071d1495d094acc6a8365b3e04c17b5cdfb848
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 03:09:41 +0000

    Add package edacious/0.0.6/edacious-0.0.6.tar.gz

---
+++ packages/edacious/edacious-0.0.6.tar.gz/edacious-0.0.6/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="edacious",
+    version="0.0.6",
+    author="Eldad Bishari",
+    author_email="eldad@1221tlv.org",
+    description="Edacious an Event Derive Architecture framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/eldad1221/edacious",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'quickbelog>=1.1.0',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
