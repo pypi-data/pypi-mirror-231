1ecb8fffcfde32fddf39e8afd54023ed636db195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 15:07:05 +0000

    Add package deeplay/0.0.2/deeplay-0.0.2.tar.gz

---
+++ packages/deeplay/deeplay-0.0.2.tar.gz/deeplay-0.0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="deeplay",
+    version="0.0.2",
+    license="MIT",
+    author="Benjamin Midtvedt",
+    author_email="email@example.com",
+    packages=find_packages("deeplay"),
+    package_dir={"": "deeplay"},
+    url="https://github.com/softmatterlab/deeplay",
+    keywords="example project",
+    install_requires=[],
+)
