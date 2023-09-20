7b96917f0d1645d687e3e9327c967960fbcf3c96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-20 05:07:39 +0000

    Add package jwtsign/1.0.8/jwtsign-1.0.8-cp38-cp38-manylinux_2_28_x86_64.whl

---
+++ packages/jwtsign/jwtsign-1.0.8-cp38-cp38-manylinux_2_28_x86_64.whl/jwtsign/jwtsign.pyi
@@ -0,0 +1,18 @@
+from typing import TypeVar, Generic
+
+ClaimsModel = TypeVar("ClaimsModel")
+
+class PyJwtEncoder(Generic[ClaimsModel]):
+    def __init__(self, private_key: bytes) -> None: ...
+    
+    def encode_claims_json_str(self, claims_str: str) -> str: ...
+    
+    def encode_claims_json_obj(self, claims: ClaimsModel) -> str: ...
+    
+
+class PyJwtDecoder(Generic[ClaimsModel]):
+
+    def __init__(self, public_key: bytes, leeway: int) -> None: ...
+
+    def decode(self, token: str) -> ClaimsModel: ...
+
