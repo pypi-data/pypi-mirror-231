e1c9e94148437adcc63fcd13baec1e5312da5ced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:08 +0000

    Add package pyjaws/0.1.6/pyjaws-0.1.6.tar.gz

---
+++ packages/pyjaws/pyjaws-0.1.6.tar.gz/pyjaws-0.1.6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+import pyjaws
+from pathlib import Path
+
+PACKAGE_REQUIREMENTS = [
+    "click",
+    "python-rapidjson==1.10",
+    "pydantic==1.10.5",
+    "Jinja2==3.1.2",
+    "networkx",
+    "GitPython==3.1.35",
+    "requests<2.30.0",
+    "urllib3<2",
+    "matplotlib",
+    "databricks-sdk==0.8.0",
+]
+
+current_dir = Path(__file__).parent.parent
+long_description = (current_dir / "README.md").read_text()
+
+setup(
+    name="pyjaws",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"": ["README.md", "api/templates/*.j2", "api/templates/macros/*.j2"]},
+    setup_requires=["setuptools", "wheel"],
+    install_requires=PACKAGE_REQUIREMENTS,
+    entry_points={"console_scripts": ["pyjaws = pyjaws.client.entrypoint:main"]},
+    version=pyjaws.__version__,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    authors=["Rafael Pierre"],
+)
