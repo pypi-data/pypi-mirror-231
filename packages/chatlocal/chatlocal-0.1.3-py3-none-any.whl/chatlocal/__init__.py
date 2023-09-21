bb18edd8c8d144cfd1b6ec484c25cde9aea1a3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 16:08:25 +0000

    Add package chatlocal/0.1.3/chatlocal-0.1.3-py3-none-any.whl

---
+++ packages/chatlocal/chatlocal-0.1.3-py3-none-any.whl/chatlocal/__init__.py
@@ -0,0 +1,7 @@
+from chatlocal.dataloader import DataLoader, FileType
+from chatlocal.settings import VectorStoreSettings
+from chatlocal.vectorstore import VectorStore
+
+__all__ = ["DataLoader", "VectorStore", "FileType", "VectorStoreSettings"]
+
+__version__ = "v0.1.3"
