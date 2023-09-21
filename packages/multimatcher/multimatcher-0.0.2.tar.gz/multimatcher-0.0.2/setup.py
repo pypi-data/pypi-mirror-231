8f5ad0dc7c294dc7eef4f09b8031b96866501c6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 14:07:46 +0000

    Add package multimatcher/0.0.2/multimatcher-0.0.2.tar.gz

---
+++ packages/multimatcher/multimatcher-0.0.2.tar.gz/multimatcher-0.0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='multimatcher',
+    version='0.0.2',
+    install_requires=[
+        'requests',
+        'importlib-metadata; python_version >= "3.8"',
+    ],
+)
