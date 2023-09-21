7b7f97aa6a2925421cc0a83877fd97de390cac15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:48 +0000

    Add package helloev31/0.0.6/helloev31-0.0.6-py3-none-any.whl

---
+++ packages/helloev31/helloev31-0.0.6-py3-none-any.whl/libname/hello.py
@@ -0,0 +1,14 @@
+def sayHello():
+    print("Hello Everyone")
+def add():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    return a+b
+def sub():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    return a-b
+def multiply():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    return a*b
