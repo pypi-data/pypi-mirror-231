fea3214d882eee48f268f29640ca713c8b11f13c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:39 +0000

    Add package libName2/0.0.2/libName2-0.0.2.tar.gz

---
+++ packages/libName2/libName2-0.0.2.tar.gz/libName2-0.0.2/libName/__init__.py
@@ -0,0 +1,1 @@
+from libName import main
---
+++ packages/libName2/libName2-0.0.2.tar.gz/libName2-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Streaming video data via networks'
+
+# Setting up
+setup(
+    name="libName2",
+    version=VERSION,
+    author="NeuralNine (Florian Dedov)",
+    author_email="<mail@neuralnine.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
