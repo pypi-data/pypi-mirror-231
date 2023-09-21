786ac4ea8acc3137274f34e315300af12d3cb57f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:46 +0000

    Add package nvfuser-cu118/0.0.19.dev20230920/nvfuser_cu118-0.0.19.dev20230920-cp310-cp310-manylinux_2_28_x86_64.whl

---
+++ packages/nvfuser-cu118/nvfuser_cu118-0.0.19.dev20230920-cp310-cp310-manylinux_2_28_x86_64.whl/nvfuser/include/nvfuser/scheduler/all_schedulers.h
@@ -0,0 +1,14 @@
+// clang-format off
+/*
+ * SPDX-FileCopyrightText: Copyright (c) 2023-present NVIDIA CORPORATION & AFFILIATES.
+ * All rights reserved.
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// clang-format on
+#pragma once
+#include <scheduler/matmul.h>
+#include <scheduler/no_op.h>
+#include <scheduler/normalization.h>
+#include <scheduler/pointwise.h>
+#include <scheduler/reduction.h>
+#include <scheduler/transpose.h>
---
+++ packages/nvfuser-cu118/nvfuser_cu118-0.0.19.dev20230920-cp310-cp310-manylinux_2_28_x86_64.whl/nvfuser/version.py
@@ -0,0 +1,1 @@
+_version_str = '0.0.19+git94e2d91'
