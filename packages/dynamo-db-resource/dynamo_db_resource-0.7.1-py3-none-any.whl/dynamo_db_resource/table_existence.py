affcd1a4c6b22fd062f361f7429c53402cf445f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 14:08:10 +0000

    Add package dynamo-db-resource/0.7.1/dynamo_db_resource-0.7.1-py3-none-any.whl

---
+++ packages/dynamo-db-resource/dynamo_db_resource-0.7.1-py3-none-any.whl/dynamo_db_resource/table_existence.py
@@ -0,0 +1,80 @@
+import boto3
+from os import environ as os_environ
+from .dynamo_db_table import _cast_table_name
+from ._schema import _json_schema_2_dynamo_db_type_switch
+
+__all__ = [
+    "create_dynamo_db_table_from_schema",
+    "delete_dynamo_db_table",
+    "convert_schema_to_infrastructure_code",
+]
+
+
+def create_dynamo_db_table_from_schema(json_schema, config=None, **resource_config):
+    if not config:
+        config = {}
+    if not resource_config:
+        resource_config = {"region_name": os_environ["AWS_REGION"]}
+
+    ddb = boto3.resource("dynamodb", **resource_config)
+    infrastructure = convert_schema_to_infrastructure_code(json_schema)
+    infrastructure["TableName"] = _cast_table_name(infrastructure["TableName"], config)
+    try:
+        ddb.create_table(**infrastructure)
+
+        print(f"successfully created table {infrastructure['TableName']}")
+    except Exception as e:
+        print(f"not created table {infrastructure['TableName']}")
+        print(e)
+        pass
+
+
+def delete_dynamo_db_table(
+    table_name: str, require_confirmation: bool = True, **resource_config
+):
+    if not resource_config:
+        resource_config = {"region_name": os_environ["AWS_REGION"]}
+    if require_confirmation:
+        decision = input(f"Are you sure to delete Dynamo DB table {table_name}\ny/n: ")
+        if decision != "y":
+            return
+
+    table_name = _cast_table_name(table_name)
+
+    ddb = boto3.client("dynamodb", **resource_config)
+    ddb.delete_table(TableName=table_name)
+
+
+def _create_attribute_definition(schema, attribute_name):
+    return {
+        "AttributeName": attribute_name,
+        "AttributeType": _json_schema_2_dynamo_db_type_switch[
+            schema["properties"][attribute_name]["type"]
+        ],
+    }
+
+
+def convert_schema_to_infrastructure_code(schema: dict) -> dict:
+    infrastructure_code = schema["$infrastructure"]
+    infrastructure_code["TableName"] = schema["title"]
+    infrastructure_code["AttributeDefinitions"] = list()
+
+    keys_to_define = set(
+        [key_schema["AttributeName"] for key_schema in infrastructure_code["KeySchema"]]
+    )
+    for index_type in ["LocalSecondaryIndexes", "GlobalSecondaryIndexes"]:
+        for index_schema in infrastructure_code.get(index_type, list()):
+            keys_to_define.update(
+                set(
+                    [
+                        key_schema["AttributeName"]
+                        for key_schema in index_schema["KeySchema"]
+                    ]
+                )
+            )
+
+    infrastructure_code["AttributeDefinitions"] = [
+        _create_attribute_definition(schema, key)
+        for key in sorted(list(keys_to_define))
+    ]
+    return infrastructure_code
---
+++ packages/dynamo-db-resource/dynamo_db_resource-0.7.1-py3-none-any.whl/tests/test_resource_config.py
@@ -0,0 +1,46 @@
+from pytest import fixture
+from os import environ as os_environ
+
+
+@fixture
+def os_env():
+
+    os_environ["ENV"] = "cloud"
+    os_environ["STAGE"] = "TEST"
+    os_environ["AWS_REGION"] = "eu-central-1"
+    os_environ["DYNAMO_DB_RESOURCE_SCHEMA_ORIGIN"] = "file"
+    os_environ["DYNAMO_DB_RESOURCE_SCHEMA_DIRECTORY"] = "test_data/tables/"
+
+
+def test_resource_config_unittest(os_env):
+    from dynamo_db_resource.resource_config import create_resource_config
+
+    config = create_resource_config("local", True)
+
+    assert config == {
+        "aws_access_key_id": "dummy",
+        "aws_secret_access_key": "dummy",
+        "endpoint_url": "http://localhost:8000",
+        "region_name": "dummy",
+    }
+
+
+def test_resource_config_aws_sam(os_env):
+    from dynamo_db_resource.resource_config import create_resource_config
+
+    config = create_resource_config("local", aws_sam=True)
+
+    assert config == {
+        "aws_access_key_id": "dummy",
+        "aws_secret_access_key": "dummy",
+        "endpoint_url": "http://docker.for.mac.localhost:8000",
+        "region_name": "dummy",
+    }
+
+
+def test_resource_config_cloud(os_env):
+    from dynamo_db_resource.resource_config import create_resource_config
+
+    config = create_resource_config(os_environ["AWS_REGION"])
+
+    assert config == {"region_name": os_environ["AWS_REGION"]}
