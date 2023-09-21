2c522f1c2b265a72fe16707f48c9db9229d0a56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:50 +0000

    Add package fastapi-websocket-rpc/0.1.25/fastapi_websocket_rpc-0.1.25.tar.gz

---
+++ packages/fastapi-websocket-rpc/fastapi_websocket_rpc-0.1.25.tar.gz/fastapi_websocket_rpc-0.1.25/fastapi_websocket_rpc/simplewebsocket.py
@@ -0,0 +1,44 @@
+import json
+from abc import ABC, abstractmethod
+
+from .utils import pydantic_serialize
+
+
+class SimpleWebSocket(ABC):
+    """
+    Abstract base class for all websocket related wrappers.
+    """
+
+    @abstractmethod
+    def send(self, msg):
+        pass
+
+    @abstractmethod
+    def recv(self):
+        pass
+
+    @abstractmethod
+    def close(self, code: int = 1000):
+        pass
+
+
+class JsonSerializingWebSocket(SimpleWebSocket):
+    def __init__(self, websocket: SimpleWebSocket):
+        self._websocket = websocket
+
+    def _serialize(self, msg):
+        return pydantic_serialize(msg)
+
+    def _deserialize(self, buffer):
+        return json.loads(buffer)
+
+    async def send(self, msg):
+        await self._websocket.send(self._serialize(msg))
+
+    async def recv(self):
+        msg = await self._websocket.recv()
+
+        return self._deserialize(msg)
+
+    async def close(self, code: int = 1000):
+        await self._websocket.close(code)
