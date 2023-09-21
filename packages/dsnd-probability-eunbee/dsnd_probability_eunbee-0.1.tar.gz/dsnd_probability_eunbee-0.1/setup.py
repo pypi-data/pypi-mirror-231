a1f29709f170d19b60078c6f8420c215800e58cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:06:56 +0000

    Add package dsnd-probability-eunbee/0.1/dsnd_probability_eunbee-0.1.tar.gz

---
+++ packages/dsnd-probability-eunbee/dsnd_probability_eunbee-0.1.tar.gz/dsnd_probability_eunbee-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dsnd_probability_eunbee',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['dsnd_probability_eunbee'],
+      zip_safe=False)
