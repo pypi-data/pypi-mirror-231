2a952e4d9f00d6a96cbf57386811264bde584892
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:36 +0000

    Add package LIFEsim/0.2.32/LIFEsim-0.2.32-py3-none-any.whl

---
+++ packages/LIFEsim/LIFEsim-0.2.32-py3-none-any.whl/lifesim/__init__.py
@@ -0,0 +1,16 @@
+__version__ = '0.2.32'
+
+from lifesim.core.core import Module, Bus
+
+from lifesim.instrument.instrument import Instrument
+from lifesim.instrument.transmission import TransmissionMap
+from lifesim.instrument.pn_exozodi import PhotonNoiseExozodi
+from lifesim.instrument.pn_localzodi import PhotonNoiseLocalzodi
+from lifesim.instrument.pn_star import PhotonNoiseStar
+
+from lifesim.util.importer import SpectrumImporter
+
+from lifesim.optimize.optimizer import Optimizer
+from lifesim.optimize.ahgs import AhgsModule
+
+from lifesim.gui.spectrum_gui import Gui
---
+++ packages/LIFEsim/LIFEsim-0.2.32-py3-none-any.whl/lifesim/core/core.py
@@ -0,0 +1,522 @@
+from __future__ import annotations
+import abc
+from abc import ABC
+from typing import Tuple, Union
+import warnings
+import os
+
+import yaml
+from numpy import ndarray, array
+import git
+
+from lifesim.core.data import Data
+
+
+class Module(ABC):
+    """
+    A `Module` is the most basic component of LIFEsim. A single `Module` should be used to
+    encapsulate a physical, (partially) self-contained model. Communication between two or more
+    Modules is done through sockets. See the LIFEsim ULM diagram or the inheritance and
+    implementation in :file:`lifesim.core.modules.py` and :file:`lifesim.instrument.*`
+
+    Attributes
+    ----------
+    name : str
+        Name of the module. The name is used as an identifier for different instances of modules
+        and should therefore be unique to a single instance of the `Bus` class.
+    data : dict
+        Deprecated. Store data in the respective location in the `Bus` class (`bus.data`)
+    sockets : dict
+        The sockets dictionary stores the properties of the sockets under the key equal to the
+        socket name
+
+    Examples
+    --------
+    When any module is created within LIFEsim, it should inherit the ``Module`` class and define
+    stub methods as follows
+
+    >>> from lifesim.core.core import Module
+    >>> class NewModule(Module):
+    ...     def __init__(self,
+    ...                  name: str):
+    ...         ().__init__(name=name)
+    ...     @abc.abstractmethod
+    ...     def new_method(self):
+    ...         pass
+    """
+
+    def __init__(self,
+                 name: str):
+        self.name = name
+        self.data = None
+        self.sockets = {}
+
+    def add_socket(self,
+                   s_name: str,
+                   s_type: type,
+                   s_number: int = 1):
+        """
+        Add a socket to the module.
+
+        Parameters
+        ----------
+        s_name : str
+            The name of the socket. This name is used as the key in the `sockets` dictionary. Must
+            be unique within a single module.
+        s_type : type
+            The socket will only accept connections with modules of the given type.
+        s_number : int
+            Specifies the number of module instances that can be connected to this socket.
+
+        Raises
+        ------
+        ValueError
+            If the a socket with the specified socket name already exists.
+
+        Examples
+        --------
+        In this example, a newly coded module needs an interface to the TransmissionModule. A
+        suitable socket can be added as follows
+
+        >>> import abc
+        >>> from lifesim.core.core import Module
+        >>> from lifesim.core.modules import TransmissionModule
+        >>> class NewModule(Module):
+        ...     def __init__(self,
+        ...                  name: str):
+        ...         ().__init__(name=name)
+        ...
+        ...         self.add_socket(s_name='transm',
+        ...                         s_type=TransmissionModule,
+        ...                         s_number=1)
+        ...     @abc.abstractmethod
+        ...     def new_method(self):
+        ...         pass
+        """
+
+        # check if socket already exists
+        self.socket_exists(s_name=s_name,
+                           should_exist=False)
+
+        # create socket with s_name as key
+        self.sockets[s_name] = {}
+
+        # add the maximum number of connected modules and the expected types to the socket dict
+        self.sockets[s_name]['s_type'] = s_type
+        self.sockets[s_name]['s_number'] = s_number
+
+        # add the socket data
+        self.sockets[s_name]['data'] = {}
+
+        # create list of modules
+        self.sockets[s_name]['modules'] = [None] * s_number
+
+    def run_socket(self,
+                   method: str,
+                   s_name: str,
+                   **kwargs):
+        """
+        A generic command for execution of a specific method in all modules connected to the
+        socket. This can be used to specify workflows and hierachy between different modules.
+
+        Parameters
+        ----------
+        s_name : str
+            Specifies the name of the socket that should send the run command.
+        method : str
+            Specifies the method that should be called in the connected module(s).
+        kwargs :
+            Can be used to pass any argument to the specified method within the connected
+            module(s).
+
+        Returns
+        -------
+        Passes trough any returns from the specified methods within the connected module(s). If
+        multiple modules are connected, the return variables are encapsulated in a list.
+
+        Raises
+        ------
+        ValueError
+            If there exist no sockets with the specified socket names.
+
+        Example
+        -------
+        Continuing the above example in ``add_socket``, any module added to the socket can be run
+        by using
+
+        >>> class New(NewModule):
+        ...         def __init__(self,
+        ...                      name: str):
+        ...             super().__init__(name=name)
+        ...
+        ...         def new_method(self):
+        ...             _, _, result, _, _ = self.run_socket(s_name='transm',
+        ...                                                  method='transmission_map',
+        ...                                                  map_selection='tm3')
+        """
+
+        self.socket_exists(s_name=s_name,
+                           should_exist=True)
+
+        if self.sockets[s_name]['s_number'] == 1:
+            return getattr(self.sockets[s_name]['modules'][0], method)(**kwargs)
+        else:
+            # TODO: Attach the name of the module to the returned variables in the list s.t. the
+            #       source of the individual returned variables can be identified
+            return [getattr(module, method)(**kwargs)
+                    for module in filter(None, self.sockets[s_name]['modules'])]
+
+    def update_socket(self,
+                      s_name: str,
+                      s_data: dict):
+        """
+        Deprecated. All data should be handled through the bus. Pushes changed data to all modules
+        connected to the bus.
+
+        Parameters
+        ----------
+        s_name : str
+            Specifies the name of the socket that should push data.
+        s_data : dict
+            Dictionary containing the updated data.
+
+        Raises
+        ------
+        ValueError
+            If there exist no sockets with the specified socket names.
+        """
+
+        # check if the socket exists
+        self.socket_exists(s_name=s_name,
+                           should_exist=True)
+
+        # set the new/updated data to the socket data
+        self.sockets[s_name]['data'].update(s_data)
+
+    def connect_module(self,
+                       module: Module):
+        """
+        Connect the specified module to a suitable and available (the number of module that can
+        connect to a single socket can be specified in the creation of the socket) socket. If
+        multiple sockets are suitable and available, the module is connected to only the first such
+        socket (in the order as they appear in the socket dictionary).
+
+        Parameters
+        ----------
+        module : Module
+            The module that should be connected to a socket.
+
+        Raises
+        ------
+        ValueError
+            If no socket accepting modules of the same type as the specified module exists.
+        ValueError
+            If none of the suitable sockets have an spot available.
+        """
+
+        # TODO: The described functionality is not quite reached since only the first socket of
+        #  acceptable type is used for any connection. In the case that the first socket of correct
+        #  type does not have free spots but later sockets of correct type do, this will lead to a
+        #  non-connection of the module.
+        s_name = None
+        # cycle through all sockets
+        for key in self.sockets.keys():
+            # if the type of the specified module is the same as the type of the socket safe the
+            # name of the socket an break
+            if isinstance(module, self.sockets[key]['s_type']):
+                s_name = key
+                break
+        if s_name is None:
+            # raise error if no sockets of the required type exist
+            raise ValueError('Cannot find socket that accepts module of type ' + str(type(module)))
+
+        # try to find an empty spot for the module in the previously identified socket
+        for i in range(self.sockets[s_name]['s_number']):
+            if self.sockets[s_name]['modules'][i] is None:
+                self.sockets[s_name]['modules'][i] = module
+                break
+            if i == self.sockets[s_name]['s_number'] - 1:
+                raise ValueError('All available socket spots for this module type are already '
+                                 'occupied')
+
+    def disconnect_module(self,
+                          module: Module):
+        """
+        Disconnect the specified module.
+
+        Parameters
+        ----------
+        module : Module
+            The module that should be disconnected.
+
+        Raises
+        ------
+        ValueError
+            If the module is not connected.
+        """
+
+        # search the sockets for the given module
+        s_index = None
+        for key in self.sockets.keys():
+            if module in self.sockets[key]['modules']:
+
+                # if the module is found, remove it from the socket
+                s_index = self.sockets[key]['modules'].index(module)
+                self.sockets[key]['modules'][s_index] = None
+                break
+
+        # if the module is not found raise an error
+        if s_index is None:
+            raise ValueError('The module ' + module.name + ' is not connected to the module '
+                             + self.name)
+
+    def socket_exists(self,
+                      s_name: str,
+                      should_exist: bool):
+        """
+        Checks for the existence of a socket of the specified name.
+
+        Parameters
+        ----------
+        s_name : str
+            Name of the socket.
+        should_exist : bool
+            If set to `true`, the exception is raised if the socket of given name does not exist.
+            If set to `false`, the exception is raised if the socket of given name does exist.
+
+        Raises
+        ------
+        ValueError
+            Depending of `should_exist`, the error is raised if a socket of given name does or does
+            not exist.
+        """
+        if should_exist and (s_name not in self.sockets.keys()):
+            raise ValueError('The socket ' + s_name + ' does not exist')
+        elif (not should_exist) and (s_name in self.sockets.keys()):
+            raise ValueError('A socket with the name ' + s_name + ' already exists')
+
+
+class Bus(object):
+    """
+    The `Bus` is responsible for handling and interfacing with modules, for creating connections
+    between modules, for data storage and for option handling. Every simulation run with LIFEsim
+    should be based of a bus.
+
+    Attributes
+    ----------
+    modules : dict
+        A dictionary containing all modules used in a simulation.
+    data : Data
+        The data and options of a simulation are stored in here in a `Data` class.
+    connections : list
+        A list keeping track over all connections made between modules via sockets.
+    """
+    def __init__(self):
+        self.modules = {}
+        self.data = Data()
+        self.connections = []
+
+    # TODO: Implement the use of configuration files.
+    @classmethod
+    def init_from_config(cls):
+        pass
+
+    def save_to_config(self):
+        pass
+
+    def connect(self,
+                module_names: Tuple[str, str]):
+        """
+        Establish a connection between two specified modules using available sockets
+
+        Parameters
+        ----------
+        module_names : Tuple[str, str]
+            A tuple containing the names of the two modules which will be connected.
+
+        Raises
+        ------
+        ValueError
+            If at least one of the specified modules does not exists. If two modules of same module
+            type are given
+        """
+
+        # check if modules exist
+        if module_names[0] not in self.modules.keys():
+            raise ValueError('The module ' + module_names[0] + ' does not exist')
+        elif module_names[1] not in self.modules.keys():
+            raise ValueError('The module ' + module_names[1] + ' does not exist')
+
+        # try to connect the modules in both directions
+        i = 0
+        try:
+            self.modules[module_names[0]].connect_module(self.modules[module_names[1]])
+
+            # note the establishment of the connection in the connection list of the bus
+            self.connections.append({module_names[0], module_names[1]})
+        except ValueError:
+            i += 1
+        try:
+            self.modules[module_names[1]].connect_module(self.modules[module_names[0]])
+
+            # note the establishment of the connection in the connection list of the bus
+            self.connections.append({module_names[1], module_names[0]})
+        except ValueError:
+            i += 1
+
+        # if a connection is not possible in either direction, raise an error that the connection
+        # was unsuccessful
+        if i == 2:
+            raise ValueError('The modules '
+                             + module_names[0] + ' and '
+                             + module_names[1] + ' could not be connected')
+
+    def disconnect(self,
+                   module_names: Tuple[str, str]):
+        """
+        Disconnect two previously connected modules.
+
+        Parameters
+        ----------
+        module_names : Tuple[str, str]
+            A tuple containing the names of the two modules which will be disconnected.
+
+        Raises
+        ------
+        Warning
+            If there exists no connection between the two specified modules.
+        Warning
+            If the connected modules cannot be disconnected.
+        """
+
+        # check if the modules are connected
+        if set(module_names) not in self.connections:
+            warnings.warn('The modules '
+                          + module_names[0] + ' and '
+                          + module_names[1] + ' are not connected')
+
+        # try to disconnect the two modules both ways
+        i = 0
+        try:
+            self.modules[module_names[0]].disconnect_module(self.modules[module_names[1]])
+
+            # if the disconnect is successful, remove it from the connections list
+            self.connections.remove({module_names[0], module_names[1]})
+        except ValueError:
+            i += 1
+        try:
+            self.modules[module_names[1]].disconnect_module(self.modules[module_names[0]])
+
+            # if the disconnect is successful, remove it from the connections list
+            self.connections.remove({module_names[1], module_names[0]})
+        except ValueError:
+            i += 1
+        if i == 2:
+            # raise a warning if the modules cannot be disconnected
+            warnings.warn('The modules '
+                          + module_names[0] + ' and '
+                          + module_names[1] + ' could not be disconnected')
+
+    def add_module(self,
+                   module: Module):
+        """
+        Adds a given module to the bus
+
+        Parameters
+        ----------
+        module : Module
+            Module that will be added to the bus
+
+        Raises
+        ------
+        ValueError
+            If the name of the module is already in use
+        """
+
+        # add module to the list of modules
+        if module.name in self.modules.keys():
+            raise ValueError('The module name ' + module.name + ' is already in use in this bus')
+        self.modules[module.name] = module
+
+        # point module to the data storage class
+        module.data = self.data
+
+    def write_config(self):
+        module_dict = {key: str(type(module)) for (key, module) in self.modules.items()}
+
+        # fetch version number and git commit hash if available
+        lifesim_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', '..'))
+        version_dict = {}
+
+        try:
+            repo = git.Repo(lifesim_path)
+            version_dict['git_sha'] = repo.head.object.hexsha
+            version_dict['git_branch'] = repo.active_branch.name
+        except (ValueError, git.exc.InvalidGitRepositoryError):
+            version_dict['git_sha'] = 'not a git repository'
+            version_dict['git_branch'] = 'not a git repository'
+
+        # TODO: This is extremely ugly. Fix this.
+        from lifesim.__init__ import __version__
+        version_dict['version'] = __version__
+
+        config_dict = {'array': convert_to_list(self.data.options.array),
+                       'models': convert_to_list(self.data.options.models),
+                       'optimization': convert_to_list(self.data.options.optimization),
+                       'other': convert_to_list(self.data.options.other),
+                       'modules': module_dict,
+                       'connections': convert_to_list(self.connections),
+                       'version': version_dict}
+
+        with open(self.data.options.other['output_path']
+                  + self.data.options.other['output_filename'] + '.yaml', 'w') as file:
+            documents = yaml.dump(config_dict, file)
+
+    def build_from_config(self,
+                          filename: str):
+        with open(filename) as file:
+            config_dict = yaml.load(file, Loader=yaml.FullLoader)
+
+        self.data.options.array = convert_to_np(config_dict['array'])
+        self.data.options.optimization = convert_to_np(config_dict['optimization'])
+        self.data.options.models = config_dict['models']
+        self.data.options.other = config_dict['other']
+
+        # TODO: Implement methods for rebuilding modules and connections from config file
+
+    def save(self):
+        print('Saving database and config files...')
+        self.write_config()
+        if self.data.catalog is not None:
+            self.data.export_catalog()
+        print('[Done]')
+
+
+def convert_to_list(inp: Union[dict, list]):
+
+    if isinstance(inp, dict):
+        out = {}
+        for k in inp.keys():
+            if isinstance(inp[k], ndarray):
+                out[k] = inp[k].tolist()
+            else:
+                out[k] = inp[k]
+    elif isinstance(inp, list):
+        out = []
+        for el in inp:
+            if isinstance(el, set):
+                out.append(list(el))
+            else:
+                out.append(el)
+    else:
+        raise ValueError('Unsupported input type')
+    return out
+
+def convert_to_np(inp: dict):
+    out = {}
+    for k in inp.keys():
+        if isinstance(inp[k], list):
+            out[k] = array(inp[k])
+        else:
+            out[k] = inp[k]
+    return out
