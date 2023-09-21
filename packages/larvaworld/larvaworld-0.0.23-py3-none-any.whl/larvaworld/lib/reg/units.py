58bc38e36f532fc7f53acef8634cf7c560a10562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:10 +0000

    Add package larvaworld/0.0.23/larvaworld-0.0.23-py3-none-any.whl

---
+++ packages/larvaworld/larvaworld-0.0.23-py3-none-any.whl/larvaworld/lib/reg/units.py
@@ -0,0 +1,26 @@
+import warnings
+
+
+
+warnings.simplefilter(action='ignore')
+from pint import UnitRegistry, errors
+
+
+
+# class MyUnitRegistry(UnitRegistry):
+#     def __getattr__(self, name):
+#         if name[0] == '_':
+#             try:
+#                 value = super(MyUnitRegistry, self).__getattr__(name)
+#                 return value
+#             except errors.UndefinedUnitError as e:
+#                 raise AttributeError()
+#         else:
+#             return super(MyUnitRegistry, self).__getattr__(name)
+
+
+units = UnitRegistry()
+units.default_format = "~P"
+units.setup_matplotlib(True)
+
+
