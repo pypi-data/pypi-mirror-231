c0f7fa5c187836cb0a7b15148509c502d5636d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:35 +0000

    Add package armcnc/1.0.4/armcnc-1.0.4.tar.gz

---
+++ packages/armcnc/armcnc-1.0.4.tar.gz/armcnc-1.0.4/armcnc/cnc/__init__.py
@@ -0,0 +1,19 @@
+"""
+******************************************************************************
+* @author  ARMCNC site:www.armcnc.net github:armcnc.github.io
+******************************************************************************
+"""
+
+from .status import Status
+from .command import Command
+from .ini_file import INIFile
+from .error_channel import ErrorChannel
+
+class CNC:
+
+    def __init__(self, framework):
+        self.framework = framework
+        self.status = Status()
+        self.command = Command()
+        self.ini_file = INIFile()
+        self.error_channel = ErrorChannel()
