309e1d0d9e1eedce562ef08dc59814f58a98bc15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:05:12 +0000

    Add package nb-cli-plugin-webui/0.2.0/nb_cli_plugin_webui-0.2.0-py3-none-any.whl

---
+++ packages/nb-cli-plugin-webui/nb_cli_plugin_webui-0.2.0-py3-none-any.whl/nb_cli_plugin_webui/core/configs/config.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from typing import Union
+
+from pydantic import ValidationError
+
+from nb_cli_plugin_webui.utils.store import get_config_file
+from nb_cli_plugin_webui.models.domain.config import WebUIConfig
+
+CONFIG_PATH = get_config_file("config.json")
+CONFIG_CACHE: Union[WebUIConfig, None] = None
+
+
+class Config:
+    def __init__(self, config_file: Path):
+        self.config_file = config_file
+
+    @property
+    def exist(self) -> bool:
+        return CONFIG_PATH.is_file()
+
+    def read(self, refresh: bool = False) -> WebUIConfig:
+        global CONFIG_CACHE
+        if not CONFIG_CACHE or refresh:
+            try:
+                CONFIG_CACHE = WebUIConfig.parse_file(self.config_file)
+            except ValidationError:
+                raise ValidationError("Config file is invalid.", WebUIConfig)
+        return CONFIG_CACHE
+
+    def store(self, data: WebUIConfig) -> None:
+        CONFIG_PATH.write_text(data.to_json(), encoding="utf-8")
+
+
+config = Config(CONFIG_PATH)
