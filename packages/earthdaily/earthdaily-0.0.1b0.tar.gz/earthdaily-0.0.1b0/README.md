a3e8c86e5198944c956c9dde839fd52a640938c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:47 +0000

    Add package earthdaily/0.0.1b0/earthdaily-0.0.1b0.tar.gz

---
+++ packages/earthdaily/earthdaily-0.0.1b0.tar.gz/earthdaily-0.0.1b0/README.md
@@ -0,0 +1,16 @@
+# Earthdaily Python Package
+
+## Your Gateway to the Stac Catalog Earth Data Store
+
+In the realm of geospatial data analysis and Earth observation, the EarthDaily Python package emerges as a powerful toolset that seamlessly connects you to the vast and invaluable Stac catalog Earth Data Store. This package is designed with the vision of simplifying and optimizing your workflow, ensuring that you can harness the full potential of Earth observation data with ease and efficiency.
+
+Our package is built upon a foundation of best practices, meticulously crafted to elevate your data analysis experience. With EarthDaily, you can effortlessly navigate the complexities of datacube creation, including crucial processes like conversion to reflectance and automatic clipping to your area of interest. Additionally, we've taken care to make EarthDaily fully compatible with Dask, enabling you to scale your data preprocessing tasks with confidence and precision.
+
+## Install
+
+### Using pip 
+
+`pip install earthdaily`
+
+### Planned : Using conda/mamba
+
---
+++ packages/earthdaily/earthdaily-0.0.1b0.tar.gz/earthdaily-0.0.1b0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+
+# Retrieve release number from text file VERSION.
+# See https://packaging.python.org/guides/single-sourcing-package-version/.
+with open("earthdaily/__init__.py", encoding="utf8") as f:
+    for line in f:
+        if line.startswith("__version__"):
+            version = line.split("=")[1].split('"')[1]
+
+setup(
+    name="earthdaily",
+    packages=find_packages(),
+    version=version,
+    description="earthdaily: easy authentication, search and retrieval of Earth Data Store collections data",
+    author="EarthDaily Agro",
+    python_requires=">=3.9",
+    install_requires=[
+        "numpy",
+        "scipy",
+        "scikit-learn",
+        "matplotlib",
+        "joblib",
+        "psutil",
+        "xarray",
+        "pandas",
+        "geopandas",
+        "rasterio",
+        "pystac-client",
+        "requests",
+        "xarray",
+        "rioxarray",
+        "h5netcdf ",
+        "netcdf4",
+        "pystac",
+        "stackstac",
+        "odc-stac",
+        "tqdm",
+        "rich",
+    ],
+    license="Commercial",
+    zip_safe=False,
+    keywords=["Earth Data Store", "earthdaily", "earthdailyagro", "stac"],
+)
