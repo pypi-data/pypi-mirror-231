d12b139ac3a3e1d0ee9b6e75c47cfdabc5b80a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:47 +0000

    Add package blosc2-btune/1.0.2/blosc2_btune-1.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/blosc2-btune/blosc2_btune-1.0.2-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/blosc2_btune/__init__.py
@@ -0,0 +1,31 @@
+##############################################################################
+# Btune for Blosc2 - Automatically choose the best codec/filter for your data
+#
+# Copyright (c) 2023 The Blosc Developers <blosc@blosc.org>
+# https://btune.blosc.org
+# License: GNU Affero General Public License v3.0
+# See LICENSE.txt for details about copyright and rights to use.
+##############################################################################
+
+VERSION = "1.0.2"
+
+import os
+import platform
+from pathlib import Path
+
+def print_libpath():
+    system = platform.system()
+    if system == "Linux":
+        libname = "libblosc2_btune.so"
+    elif system == "Darwin":
+        libname = "libblosc2_btune.so"
+    elif system == "Windows":
+        libname = "libblosc2_btune.dll"
+    else:
+        raise RuntimeError("Unsupported system: ", system)
+    libpath = os.path.abspath(Path(__file__).parent / libname)
+    print(libpath, end="")
+
+
+if __name__ == "__main__":
+    print_libpath()
