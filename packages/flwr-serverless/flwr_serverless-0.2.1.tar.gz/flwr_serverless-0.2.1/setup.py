3002f46dcfb03de8a1ab7d7a3c1099f0c924b3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 05:05:50 +0000

    Add package flwr-serverless/0.2.1/flwr_serverless-0.2.1.tar.gz

---
+++ packages/flwr-serverless/flwr_serverless-0.2.1.tar.gz/flwr_serverless-0.2.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    install_requires = f.read().splitlines()
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="flwr_serverless",
+    version="0.2.1",
+    description="A serverless federated learning library based on flwr",
+    url="https://github.com/kungfuai/flwr_serverless",
+    author="Kungfu AI",
+    author_email="zhangzhang.si@gmail.com",
+    license="MIT",
+    packages=find_packages("."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.6",
+    install_requires=install_requires,
+    include_package_data=True,
+    zip_safe=False,
+)
