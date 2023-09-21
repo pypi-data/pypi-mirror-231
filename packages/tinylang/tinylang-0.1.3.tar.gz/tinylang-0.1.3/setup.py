be2761a17885af8c68810902005db084e4304981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 16:08:13 +0000

    Add package tinylang/0.1.3/tinylang-0.1.3.tar.gz

---
+++ packages/tinylang/tinylang-0.1.3.tar.gz/tinylang-0.1.3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="tinylang",
+    version="0.1.3",
+    packages=find_packages(),
+    description="A tiny language interpreter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
