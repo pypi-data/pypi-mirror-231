85c96784df5b13430b156ec2713eb62c09dbaae0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:06:22 +0000

    Add package Vic-Lim-WX/0.2.8/Vic_Lim_WX-0.2.8.tar.gz

---
+++ packages/Vic-Lim-WX/Vic_Lim_WX-0.2.8.tar.gz/Vic_Lim_WX-0.2.8/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.2.8'
+DESCRIPTION = 'Manages data by converting them to dataframes'
+LONG_DESCRIPTION = ''' A package that provides a convenient way to manage and 
+    manipulate different types of data sources and convert them to Pandas 
+    DataFrames.
+    '''
+
+# Setting up
+setup(
+    name="Vic_Lim_WX",
+    version=VERSION,
+    author="vicLim88 (Vic Lim)",
+    author_email="<vic.lim@icloud.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(where="vic_lim_wx"),
+    package_dir={'': 'vic_lim_wx'},
+    py_modules=['vic_lim_wx'],
+    install_requires=[
+    ],
+    keywords=[
+        'python',
+        'data science'
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
