ab15b23fec768b9f7ced2d82b76537964af4f46a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:10 +0000

    Add package rabies/0.5.0/rabies-0.5.0.tar.gz

---
+++ packages/rabies/rabies-0.5.0.tar.gz/rabies-0.5.0/rabies/preprocess_pkg/utils.py
@@ -0,0 +1,222 @@
+import os
+import pathlib
+import SimpleITK as sitk
+import numpy as np
+from nipype.interfaces.base import (
+    traits, TraitedSpec, BaseInterfaceInputSpec,
+    File, BaseInterface
+)
+from rabies.utils import run_command
+
+def prep_bids_iter(layout, bold_only=False):
+    '''
+    This function takes as input a BIDSLayout, and generates iteration lists
+    for managing the workflow's iterables depending on whether --bold_only is
+    selected or not.
+    '''
+    import pathlib
+
+    scan_info = []
+    split_name = []
+    scan_list = []
+    run_iter = {}
+    bold_scan_list = []
+
+    subject_list = layout.get_subject()
+    if len(subject_list) == 0:
+        raise ValueError(
+            "No subject information could be retrieved from the BIDS directory. The 'sub-' specification is mandatory.")
+    if not bold_only:
+        anat_bids = layout.get(subject=subject_list, suffix=[
+                               'T2w', 'T1w'], extension=['nii', 'nii.gz'])
+        if len(anat_bids) == 0:
+            raise ValueError(
+                "No anatomical file with the suffix 'T2w' or 'T1w' were found among the BIDS directory.")
+    bold_bids = layout.get(subject=subject_list, suffix=[
+                           'bold'], extension=['nii', 'nii.gz'])
+    if len(bold_bids) == 0:
+        raise ValueError(
+            "No functional file with the suffix 'bold' were found among the BIDS directory.")
+
+    bold_dict = {}
+    for bold in bold_bids:
+        sub = bold.get_entities()['subject']
+        try:
+            ses = bold.get_entities()['session']
+        except:
+            ses = None
+
+        try:
+            run = bold.get_entities()['run']
+        except:
+            run = None
+
+        if sub not in list(bold_dict.keys()):
+            bold_dict[sub] = {}
+        if ses not in list(bold_dict[sub].keys()):
+            bold_dict[sub][ses] = {}
+
+        bold_list = layout.get(subject=sub, session=ses, run=run, suffix=[
+                               'bold'], extension=['nii', 'nii.gz'], return_type='filename')
+        bold_dict[sub][ses][run] = bold_list
+
+    # if not bold_only, then the bold_list and run_iter will be a dictionary with keys being the anat filename
+    # otherwise, it will be a list of bold scans themselves
+    for sub in list(bold_dict.keys()):
+        for ses in list(bold_dict[sub].keys()):
+            if not bold_only:
+                anat_list = layout.get(subject=sub, session=ses, suffix=[
+                                       'T2w', 'T1w'], extension=['nii', 'nii.gz'], return_type='filename')
+                if len(anat_list) == 0:
+                    raise ValueError(
+                        f'Missing an anatomical image for sub {sub} and ses- {ses}')
+                if len(anat_list) > 1:
+                    raise ValueError(
+                        f'Duplicate was found for the anatomical file sub- {sub}, ses- {ses}: {str(anat_list)}')
+                file = anat_list[0]
+                scan_list.append(file)
+                filename_template = pathlib.Path(file).name.rsplit(".nii")[0]
+                split_name.append(filename_template)
+                scan_info.append({'subject_id': sub, 'session': ses})
+                run_iter[filename_template] = []
+
+            for run in list(bold_dict[sub][ses].keys()):
+                bold_list = bold_dict[sub][ses][run]
+                if len(bold_list) > 1:
+                    raise ValueError(
+                        f'Duplicate was found for bold files sub- {sub}, ses- {ses} and run {run}: {str(bold_list)}')
+                file = bold_list[0]
+                bold_scan_list.append(file)
+                if bold_only:
+                    scan_list.append(file)
+                    filename_template = pathlib.Path(
+                        file).name.rsplit(".nii")[0]
+                    split_name.append(filename_template)
+                    scan_info.append(
+                        {'subject_id': sub, 'session': ses, 'run': run})
+                else:
+                    run_iter[filename_template].append(run)
+
+    return split_name, scan_info, run_iter, scan_list, bold_scan_list
+
+
+class BIDSDataGraberInputSpec(BaseInterfaceInputSpec):
+    bids_dir = traits.Str(exists=True, mandatory=True,
+                          desc="BIDS data directory")
+    suffix = traits.List(exists=True, mandatory=True,
+                         desc="Suffix to search for")
+    scan_info = traits.Dict(exists=True, mandatory=True,
+                            desc="Info required to find the scan")
+    run = traits.Any(exists=True, desc="Run number")
+
+
+class BIDSDataGraberOutputSpec(TraitedSpec):
+    out_file = File(
+        exists=True, desc="Selected file based on the provided parameters.")
+
+
+class BIDSDataGraber(BaseInterface):
+    """
+    This interface will select a single scan from the BIDS directory based on the
+    input specifications.
+    """
+
+    input_spec = BIDSDataGraberInputSpec
+    output_spec = BIDSDataGraberOutputSpec
+
+    def _run_interface(self, runtime):
+        subject_id = self.inputs.scan_info['subject_id']
+        session = self.inputs.scan_info['session']
+        if 'run' in (self.inputs.scan_info.keys()):
+            run = self.inputs.scan_info['run']
+        else:
+            run = self.inputs.run
+
+        from bids.layout import BIDSLayout
+        layout = BIDSLayout(self.inputs.bids_dir, validate=False)
+        try:
+            if run is None: # if there is no run spec to search, don't include it in the search
+                file_list = layout.get(subject=subject_id, session=session, extension=[
+                                  'nii', 'nii.gz'], suffix=self.inputs.suffix, return_type='filename')
+            else:
+                file_list = layout.get(subject=subject_id, session=session, run=run, extension=[
+                                  'nii', 'nii.gz'], suffix=self.inputs.suffix, return_type='filename')
+            if len(file_list) > 1:
+                raise ValueError(f'Provided BIDS spec lead to duplicates: \
+                    {str(self.inputs.suffix)} sub-{subject_id} ses-{session} run-{str(run)}')
+            elif len(file_list)==0:
+                raise ValueError(f'No file for found corresponding to the following BIDS spec: \
+                    {str(self.inputs.suffix)} sub-{subject_id} ses-{session} run-{str(run)}')
+        except:
+            raise ValueError(f'Error with BIDS spec: \
+                    {str(self.inputs.suffix)} sub-{subject_id} ses-{session} run-{str(run)}')
+
+        setattr(self, 'out_file', file_list[0])
+
+        return runtime
+
+    def _list_outputs(self):
+        return {'out_file': getattr(self, 'out_file')}
+
+
+def convert_to_RAS(img_file, out_dir=None):
+    # convert the input image to the RAS orientation convention
+    import os
+    import nibabel as nb
+    img = nb.load(img_file)
+    if nb.aff2axcodes(img.affine) == ('R', 'A', 'S'):
+        return img_file
+    else:
+        import pathlib  # Better path manipulation
+        split = pathlib.Path(img_file).name.rsplit(".nii")
+        if out_dir is None:
+            out_file = os.path.abspath(split[0]+'_RAS.nii.gz')
+        else:
+            out_file = out_dir+'/'+split[0]+'_RAS.nii.gz'
+            if not os.path.isdir(out_dir):
+                os.makedirs(out_dir)
+        nb.as_closest_canonical(img).to_filename(out_file)
+        return out_file
+
+
+def resample_template(template_file, mask_file, file_list, spacing='inputs_defined', rabies_data_type=8):
+    import os
+    import SimpleITK as sitk
+    import numpy as np
+    from rabies.utils import resample_image_spacing, run_command
+    from nipype import logging
+    log = logging.getLogger('nipype.workflow')
+
+    if spacing == 'inputs_defined':
+        file_list = list(np.asarray(file_list).flatten())
+        img = sitk.ReadImage(file_list[0], rabies_data_type)
+        low_dim = np.asarray(img.GetSpacing()[:3]).min()
+        for file in file_list[1:]:
+            img = sitk.ReadImage(file, rabies_data_type)
+            new_low_dim = np.asarray(img.GetSpacing()[:3]).min()
+            if new_low_dim < low_dim:
+                low_dim = new_low_dim
+        spacing = (low_dim, low_dim, low_dim)
+
+        template_image = sitk.ReadImage(
+            template_file, rabies_data_type)
+        template_dim = template_image.GetSpacing()
+        if np.asarray(template_dim[:3]).min() > low_dim:
+            log.info("The template retains its original resolution.")
+            return template_file, mask_file
+    else:
+        shape = spacing.split('x')
+        spacing = (float(shape[0]), float(shape[1]), float(shape[2]))
+
+    log.info(f"Resampling template to {spacing[0]}x{spacing[1]}x{spacing[2]}mm dimensions.")
+    resampled_template = os.path.abspath("resampled_template.nii.gz")
+    sitk.WriteImage(resample_image_spacing(sitk.ReadImage(
+        template_file, rabies_data_type), spacing), resampled_template)
+
+    # also resample the brain mask to ensure stable registrations further down
+    resampled_mask = os.path.abspath("resampled_mask.nii.gz")
+    command = f'antsApplyTransforms -d 3 -i {mask_file} -r {resampled_template} -o {resampled_mask} --verbose -n GenericLabel'
+    rc = run_command(command)
+
+    return resampled_template, resampled_mask
+
---
+++ packages/rabies/rabies-0.5.0.tar.gz/rabies-0.5.0/scripts/error_check_rabies.py
@@ -0,0 +1,141 @@
+#! /usr/bin/env python
+
+import SimpleITK as sitk
+import os
+import sys
+import numpy as np
+import tempfile
+import shutil
+import subprocess
+from rabies.utils import resample_image_spacing, copyInfo_4DImage
+
+if len(sys.argv) == 2:
+    tmppath = sys.argv[1]
+else:
+    tmppath = tempfile.mkdtemp()
+
+os.makedirs(tmppath+'/inputs', exist_ok=True)
+
+if 'XDG_DATA_HOME' in os.environ.keys():
+    rabies_path = os.environ['XDG_DATA_HOME']+'/rabies'
+else:
+    rabies_path = os.environ['HOME']+'/.local/share/rabies'
+
+template = f"{rabies_path}/DSURQE_40micron_average.nii.gz"
+mask = f"{rabies_path}/DSURQE_40micron_mask.nii.gz"
+
+img = sitk.ReadImage(template)
+spacing = (float(1), float(1), float(1))  # resample to 1mmx1mmx1mm
+resampled = resample_image_spacing(sitk.ReadImage(template), spacing)
+array = sitk.GetArrayFromImage(resampled)
+array_4d = np.repeat(array[np.newaxis, :, :, :], 15, axis=0)
+array_4d += np.random.normal(0, array_4d.mean()
+                             / 100, array_4d.shape)  # add gaussian noise
+sitk.WriteImage(resampled, tmppath+'/inputs/sub-token_T1w.nii.gz')
+sitk.WriteImage(resampled, tmppath+'/inputs/sub-token2_T1w.nii.gz')
+sitk.WriteImage(resampled, tmppath+'/inputs/sub-token3_T1w.nii.gz')
+sitk.WriteImage(sitk.GetImageFromArray(array_4d, isVector=False),
+                tmppath+'/inputs/sub-token_bold.nii.gz')
+
+resampled = resample_image_spacing(sitk.ReadImage(mask), spacing)
+array = sitk.GetArrayFromImage(resampled)
+array[array < 1] = 0
+array[array > 1] = 1
+binarized = sitk.GetImageFromArray(array, isVector=False)
+binarized.CopyInformation(resampled)
+sitk.WriteImage(binarized, tmppath+'/inputs/token_mask.nii.gz')
+array[:, :, :6] = 0
+binarized = sitk.GetImageFromArray(array, isVector=False)
+binarized.CopyInformation(resampled)
+sitk.WriteImage(binarized, tmppath+'/inputs/token_mask_half.nii.gz')
+
+
+sitk.WriteImage(copyInfo_4DImage(sitk.ReadImage(tmppath+'/inputs/sub-token_bold.nii.gz'), sitk.ReadImage(tmppath
+                + '/inputs/sub-token_T1w.nii.gz'), sitk.ReadImage(tmppath+'/inputs/sub-token_bold.nii.gz')), tmppath+'/inputs/sub-token_bold.nii.gz')
+
+command = f"rabies --verbose 1 preprocess {tmppath}/inputs {tmppath}/outputs --anat_inho_cor method=disable,otsu_thresh=2,multiotsu=false --bold_inho_cor method=disable,otsu_thresh=2,multiotsu=false \
+    --anat_template {tmppath}/inputs/sub-token_T1w.nii.gz --brain_mask {tmppath}/inputs/token_mask.nii.gz --WM_mask {tmppath}/inputs/token_mask.nii.gz --CSF_mask {tmppath}/inputs/token_mask.nii.gz --vascular_mask {tmppath}/inputs/token_mask.nii.gz --labels {tmppath}/inputs/token_mask.nii.gz \
+    --bold2anat_coreg registration=no_reg,masking=false,brain_extraction=false --commonspace_reg masking=false,brain_extraction=false,fast_commonspace=true,template_registration=no_reg --data_type int16 --bold_only --detect_dummy \
+    --tpattern seq-z --apply_STC --voxelwise_motion --isotropic_HMC"
+process = subprocess.run(
+    command,
+    check=True,
+    shell=True,
+    )
+
+shutil.rmtree(f'{tmppath}/outputs/')
+command = f"rabies --verbose 1 preprocess {tmppath}/inputs {tmppath}/outputs --anat_inho_cor method=disable,otsu_thresh=2,multiotsu=false --bold_inho_cor method=disable,otsu_thresh=2,multiotsu=false \
+    --anat_template {tmppath}/inputs/sub-token_T1w.nii.gz --brain_mask {tmppath}/inputs/token_mask.nii.gz --WM_mask {tmppath}/inputs/token_mask.nii.gz --CSF_mask {tmppath}/inputs/token_mask.nii.gz --vascular_mask {tmppath}/inputs/token_mask.nii.gz --labels {tmppath}/inputs/token_mask.nii.gz \
+    --bold2anat_coreg registration=no_reg,masking=true,brain_extraction=true --commonspace_reg masking=true,brain_extraction=true,fast_commonspace=true,template_registration=no_reg --data_type int16  \
+    --HMC_option 0"
+process = subprocess.run(
+    command,
+    check=True,
+    shell=True,
+    )
+
+command = f"rabies --verbose 1 confound_correction {tmppath}/outputs {tmppath}/outputs --ica_aroma apply=true,dim=0,random_seed=1 --frame_censoring FD_censoring=true,FD_threshold=0.05,DVARS_censoring=true,minimum_timepoint=3 --nativespace_analysis"
+process = subprocess.run(
+    command,
+    check=True,
+    shell=True,
+    )
+
+### Add subjects for the group analysis to run
+array_4d += np.random.normal(0, array_4d.mean()
+                             / 100, array_4d.shape)  # add gaussian noise
+sitk.WriteImage(sitk.GetImageFromArray(array_4d, isVector=False),
+                tmppath+'/inputs/sub-token2_bold.nii.gz')
+array_4d += np.random.normal(0, array_4d.mean()
+                             / 100, array_4d.shape)  # add gaussian noise
+sitk.WriteImage(sitk.GetImageFromArray(array_4d, isVector=False),
+                tmppath+'/inputs/sub-token3_bold.nii.gz')
+
+shutil.rmtree(f'{tmppath}/outputs/')
+command = f"rabies --verbose 1 preprocess {tmppath}/inputs {tmppath}/outputs --anat_inho_cor method=disable,otsu_thresh=2,multiotsu=false --bold_inho_cor method=disable,otsu_thresh=2,multiotsu=false \
+    --anat_template {tmppath}/inputs/sub-token_T1w.nii.gz --brain_mask {tmppath}/inputs/token_mask.nii.gz --WM_mask {tmppath}/inputs/token_mask_half.nii.gz --CSF_mask {tmppath}/inputs/token_mask_half.nii.gz --vascular_mask {tmppath}/inputs/token_mask_half.nii.gz --labels {tmppath}/inputs/token_mask.nii.gz \
+    --bold2anat_coreg registration=no_reg,masking=false,brain_extraction=false --commonspace_reg masking=false,brain_extraction=false,fast_commonspace=true,template_registration=no_reg --data_type int16  \
+    --HMC_option 0"
+process = subprocess.run(
+    command,
+    check=True,
+    shell=True,
+    )
+
+command = f"rabies --verbose 1 confound_correction --read_datasink {tmppath}/outputs {tmppath}/outputs --conf_list mot_6 --smoothing_filter 0.3"
+process = subprocess.run(
+    command,
+    check=True,
+    shell=True,
+    )
+
+command = f"rabies --verbose 1 analysis {tmppath}/outputs {tmppath}/outputs --DR_ICA --NPR_temporal_comp 1 --seed_list {tmppath}/inputs/token_mask_half.nii.gz"
+process = subprocess.run(
+    command,
+    check=True,
+    shell=True,
+    )
+
+shutil.rmtree(f'{tmppath}/outputs/confound_correction_main_wf')
+shutil.rmtree(f'{tmppath}/outputs/confound_correction_datasink')
+os.remove(f'{tmppath}/outputs/rabies_confound_correction.pkl')
+command = f"rabies --verbose 1 confound_correction {tmppath}/outputs {tmppath}/outputs"
+process = subprocess.run(
+    command,
+    check=True,
+    shell=True,
+    )
+
+shutil.rmtree(f'{tmppath}/outputs/analysis_main_wf')
+shutil.rmtree(f'{tmppath}/outputs/analysis_datasink')
+os.remove(f'{tmppath}/outputs/rabies_analysis.pkl')
+command = f"rabies --verbose 1 analysis {tmppath}/outputs {tmppath}/outputs --NPR_temporal_comp 1 --data_diagnosis --DR_ICA"
+process = subprocess.run(
+    command,
+    check=True,
+    shell=True,
+    )
+
+
+if not len(sys.argv) == 2:
+    shutil.rmtree(tmppath)
