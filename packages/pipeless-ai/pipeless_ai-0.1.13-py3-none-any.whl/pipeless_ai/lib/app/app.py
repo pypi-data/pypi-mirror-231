4f9dbd218bf8800c92ad4de68de335664de06208
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:39 +0000

    Add package pipeless-ai/0.1.13/pipeless_ai-0.1.13-py3-none-any.whl

---
+++ packages/pipeless-ai/pipeless_ai-0.1.13-py3-none-any.whl/pipeless_ai/lib/app/app.py
@@ -0,0 +1,41 @@
+import types
+from pipeless_ai.lib.timer import timer
+
+class PipelessApp():
+    """
+    Base class to handle an App workflow
+    """
+
+    def __init__(self):
+        self.original_frame = None
+        self.inference = types.SimpleNamespace()
+        self.plugins = types.SimpleNamespace()
+        self.__plugins_exec_graph = ()
+
+    @timer
+    def __before(self):
+        if hasattr(self, 'before') and callable(self.before):
+            self.before()
+
+    @timer
+    def __pre_process(self, frame):
+        if hasattr(self, 'pre_process') and callable(self.pre_process):
+            return self.pre_process(frame)
+        return frame
+
+    @timer
+    def __process(self, frame):
+        if hasattr(self, 'process') and callable(self.process):
+            return self.process(frame)
+        return frame
+
+    @timer
+    def __post_process(self, frame):
+        if hasattr(self, 'post_process') and callable(self.post_process):
+            return self.post_process(frame)
+        return frame
+
+    @timer
+    def __after(self):
+        if hasattr(self, 'after') and callable(self.after):
+            self.after()
