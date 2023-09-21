a87ddf0815794b98bf2d8b32dd2e34c084e6ba22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:06:50 +0000

    Add package brushit/0.1.0/brushit-0.1.0-py3-none-any.whl

---
+++ packages/brushit/brushit-0.1.0-py3-none-any.whl/brushit/cli.py
@@ -0,0 +1,47 @@
+"""some cli"""
+from itertools import chain
+from pathlib import Path
+import sys
+from typing import Annotated, Literal
+from pydantic import BaseModel, DirectoryPath, ValidationError
+import tyro
+from brushit import duster
+from brushit import controller
+from dataclasses import dataclass
+from rich import print
+from rich.table import Table
+
+
+class List(BaseModel):
+    root_folder: DirectoryPath = Path.cwd()
+    """The repo root folder."""
+
+    def main(self):
+        controller.list_branches(self.root_folder)
+
+
+class Branch(BaseModel):
+    """Upperlevel"""
+
+
+@dataclass
+class Clean:
+    """Clean your repo branches."""
+
+    dry_run: bool = True
+
+    root_folder: DirectoryPath = Path.cwd()
+    """The repo root folder."""
+
+    def main(self):
+        if self.dry_run:
+            cleaner = controller.branch_table
+        controller.clean_branches(self.root_folder, cleaner=cleaner)
+
+
+if __name__ == "__main__":
+    try:
+        out = tyro.cli(List | Clean)
+        out.main()
+    except ValidationError as err:
+        print(err)
