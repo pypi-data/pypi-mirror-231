7eb5761513b9aaf078285d92f1c7bba2ed4f6a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:08 +0000

    Add package fundamentalanalysis/0.3.1/fundamentalanalysis-0.3.1.tar.gz

---
+++ packages/fundamentalanalysis/fundamentalanalysis-0.3.1.tar.gz/fundamentalanalysis-0.3.1/fundamentalanalysis/__init__.py
@@ -0,0 +1,3 @@
+print(
+    "This package has been depreciated and greatly upgraded in the Finance Toolkit package. This package can be installed with: pip install financetoolkit -U"
+    "\nFor more information (including usage and examples) see: https://github.com/JerBouma/FinanceToolkit")
