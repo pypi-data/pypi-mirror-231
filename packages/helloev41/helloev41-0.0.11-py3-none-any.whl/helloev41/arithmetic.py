a75bf6f1e12e44d43146a5152be12c46612a5f08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:06:24 +0000

    Add package helloev41/0.0.11/helloev41-0.0.11-py3-none-any.whl

---
+++ packages/helloev41/helloev41-0.0.11-py3-none-any.whl/helloev41/arithmetic.py
@@ -0,0 +1,16 @@
+def add():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    return a+b
+def sub():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    return a-b
+def mul():
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    return a*b
+def div():
+     a = int(input("Enter a: "))
+     b = int(input("Enter b: "))
+     return a/b
