a52d9c144a82c0ae8b322aea2f0c48d34d83a309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:22 +0000

    Add package pytomography/1.1.0/pytomography-1.1.0.tar.gz

---
+++ packages/pytomography/pytomography-1.1.0.tar.gz/pytomography-1.1.0/src/pytomography/__init__.py
@@ -0,0 +1,26 @@
+import torch
+from importlib.metadata import version
+
+__version__: str = version('pytomography')
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+if device == "cpu":
+    print("PyTomography did not find a GPU available on this machine. If this is not expected, please check your CUDA installation.")
+dtype = torch.float32
+delta = 1e-11
+
+def set_dtype(dt):
+    global dtype
+    global delta
+    dtype = dt
+    torch.set_default_dtype(dt)
+    if dt==torch.float16:
+        delta = 1e-5
+    elif dt==torch.float32:
+        delta = 1e-11
+    
+def set_device(d):
+    global device
+    device = d
+    
+    
