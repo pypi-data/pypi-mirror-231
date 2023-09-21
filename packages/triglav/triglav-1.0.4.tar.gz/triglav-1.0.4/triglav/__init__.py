3fe8a3ca0d91e910c82e1ba8459db7018e052b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:33 +0000

    Add package triglav/1.0.4/triglav-1.0.4.tar.gz

---
+++ packages/triglav/triglav-1.0.4.tar.gz/triglav-1.0.4/triglav/__init__.py
@@ -0,0 +1,7 @@
+"""
+This module implements Triglav
+"""
+
+from .triglav import Triglav, CLRTransformer, Scaler, NoScale, ETCProx, NoResample
+
+__all__ = ['Triglav', 'CLRTransformer', 'Scaler', 'NoScale', 'ETCProx', 'NoResample']
