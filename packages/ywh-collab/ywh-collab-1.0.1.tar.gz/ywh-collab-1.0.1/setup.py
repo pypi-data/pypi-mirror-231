9ea0de2d8fcaab4c7d18862adefe60a06db2a930
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:29 +0000

    Add package ywh-collab/1.0.1/ywh-collab-1.0.1.tar.gz

---
+++ packages/ywh-collab/ywh-collab-1.0.1.tar.gz/ywh-collab-1.0.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.md").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="ywh-collab",
+    version="1.0.1",
+    license="Apache",
+    author="Aethios",
+    author_email="tom.chambaretaud@protonmail.com",
+    description="Yes We Hack private program comparator for hunters collaboration by Aethlios.",
+    packages=find_packages(),
+    install_requires=["requests"],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/AethliosIK/ywh-collab",
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+    ]
+)
