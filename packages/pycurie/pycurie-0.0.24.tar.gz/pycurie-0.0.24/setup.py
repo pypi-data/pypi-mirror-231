110f81568c0134e042a3472fd70ec53579d77ab8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:19 +0000

    Add package pycurie/0.0.24/pycurie-0.0.24.tar.gz

---
+++ packages/pycurie/pycurie-0.0.24.tar.gz/pycurie-0.0.24/setup.py
@@ -0,0 +1,26 @@
+# A simple setup script
+from setuptools import setup, find_packages
+
+setup(name='pycurie',
+      version='0.0.24',
+      author='Eric DiGioacchino',
+      url='https://github.com/EricDiGi/CurieKetl',
+      keywords=['etl', 'data', 'data engineering', 'data science', 'mlops', 'devops'],
+      classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3"
+        ],
+        long_description=open('../README.md').read(),
+        long_description_content_type='text/markdown',
+      packages=find_packages(),
+      entry_points={
+          'console_scripts': [
+              'curie = curie.__main__:main'
+          ]
+      },
+      package_data={
+            "curie.defaults": ["*"],
+            "curie.defaults.jinja": ["*.md.j2", "*.yaml.j2"]
+      }
+)
