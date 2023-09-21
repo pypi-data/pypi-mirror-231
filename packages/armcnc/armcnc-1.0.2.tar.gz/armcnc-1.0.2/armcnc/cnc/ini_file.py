883c2c60a2874bca9c0a4a0640975231eb293983
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:48 +0000

    Add package armcnc/1.0.2/armcnc-1.0.2.tar.gz

---
+++ packages/armcnc/armcnc-1.0.2.tar.gz/armcnc-1.0.2/armcnc/cnc/ini_file.py
@@ -0,0 +1,12 @@
+"""
+******************************************************************************
+* @author  ARMCNC site:www.armcnc.net github:armcnc.github.io
+******************************************************************************
+"""
+
+import linuxcnc
+
+class INIFile:
+
+    def __init__(self):
+        self.linuxcnc = linuxcnc
