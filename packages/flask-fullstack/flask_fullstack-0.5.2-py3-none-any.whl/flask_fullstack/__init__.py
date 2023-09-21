77323bba50df785d8027fe0dcd38618b72cce6bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:08 +0000

    Add package flask-fullstack/0.5.2/flask_fullstack-0.5.2-py3-none-any.whl

---
+++ packages/flask-fullstack/flask_fullstack-0.5.2-py3-none-any.whl/flask_fullstack/__init__.py
@@ -0,0 +1,42 @@
+from .base import Identifiable, UserRole
+from .core import Flask, configure_logging, configure_whooshee
+from .restx import (
+    DateTimeField,
+    FlaskTestClient,
+    Model,
+    PydanticModel,
+    RequestParser,
+    ResourceController,
+    ResponseDoc,
+    Undefined,
+    counter_parser,
+    password_parser,
+)
+from .siox import (
+    ClientEvent,
+    DuplexEvent,
+    EventController,
+    EventException,
+    EventSpace,
+    Namespace,
+    ServerEvent,
+    SocketIO,
+    SocketIOTestClient,
+)
+from .utils import (
+    IndexService,
+    JSONWithModel,
+    JSONWithSchema,
+    Nameable,
+    NamedProperties,
+    NamedPropertiesMeta,
+    NotImplementedField,
+    Searcher,
+    SQLAlchemy,
+    TypeEnum,
+    check_code,
+    dict_cut,
+    dict_reduce,
+    dict_rekey,
+    get_or_pop,
+)
