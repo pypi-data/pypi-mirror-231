850a631b3507384e3120777be2a3555741eebad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 14:07:10 +0000

    Add package example-emmo00/0.0.3/example_emmo00-0.0.3-py3-none-any.whl

---
+++ packages/example-emmo00/example_emmo00-0.0.3-py3-none-any.whl/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+setuptools.setup(
+ name='example_emmo00', 
+ version='0.1.0',
+ author="Emmanuel",
+ author_email="<author’s email>",
+ description="<Basic desc>",
+ packages=setuptools.find_packages(),
+ classifiers=[
+ "Programming Language :: Python :: 3",
+ "License :: OSI Approved :: MIT License",
+ "Operating System :: OS Independent",
+ ],
+)
