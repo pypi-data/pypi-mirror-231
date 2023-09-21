f99a62c69fa3085d194df4f4a88c44543666b526
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:48 +0000

    Add package sonarlight/0.1.0/sonarlight-0.1.0.tar.gz

---
+++ packages/sonarlight/sonarlight-0.1.0.tar.gz/sonarlight-0.1.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+VERSION = '0.1.0' 
+DESCRIPTION = 'sonarlight'
+LONG_DESCRIPTION = Path("README.md").read_text()
+
+setup(
+    name="sonarlight", 
+    version=VERSION,
+    author="Kenneth Thorø Martinsen",
+    author_email="kenneth2810@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    packages=find_packages(),
+    install_requires=["numpy", "pandas"],
+    keywords=['python'],
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Visualization",
+        "Topic :: Scientific/Engineering :: Oceanography",
+        "Topic :: Scientific/Engineering :: GIS"
+    ]
+)
