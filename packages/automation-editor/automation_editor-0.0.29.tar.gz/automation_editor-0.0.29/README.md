e304aecf8d0ad62004dbc5b847efd52b0aa27227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:49 +0000

    Add package automation-editor/0.0.29/automation_editor-0.0.29.tar.gz

---
+++ packages/automation-editor/automation_editor-0.0.29.tar.gz/automation_editor-0.0.29/README.md
@@ -0,0 +1,46 @@
+[![Downloads](https://static.pepy.tech/badge/automation-editor)](https://pepy.tech/project/automation-editor)
+
+[![Codacy Badge](https://app.codacy.com/project/badge/Grade/b7d32ed8600b4bd2a2f3e960f46f2ad0)](https://www.codacy.com/gh/JE-Chen/Integration-testing-environment/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=JE-Chen/Integration-testing-environment&amp;utm_campaign=Badge_Grade)
+
+[![AutomationEditor Stable Python3.9](https://github.com/Intergration-Automation-Testing/AutomationEditor/actions/workflows/stable_python3_9.yml/badge.svg)](https://github.com/Intergration-Automation-Testing/AutomationEditor/actions/workflows/stable_python3_9.yml)
+
+[![AutomationEditor Stable Python3.10](https://github.com/Intergration-Automation-Testing/AutomationEditor/actions/workflows/stable_python3_10.yml/badge.svg)](https://github.com/Intergration-Automation-Testing/AutomationEditor/actions/workflows/stable_python3_10.yml)
+
+[![AutomationEditor Stable Python3.11](https://github.com/Intergration-Automation-Testing/AutomationEditor/actions/workflows/stable_python3_11.yml/badge.svg)](https://github.com/Intergration-Automation-Testing/AutomationEditor/actions/workflows/stable_python3_11.yml)
+
+[![Documentation Status](https://readthedocs.org/projects/automationeditor/badge/?version=latest)](https://automationeditor.readthedocs.io/en/latest/?badge=latest)
+
+[Automation Editor Doc Click Here!](https://automationeditor.readthedocs.io/en/latest/)
+
+---
+# Table of contents
+> Editor components \
+> GUI components \
+> WEB components \
+> API components \
+> Load components
+---
+## What is Automation Editor
+> Project Kanban \
+> https://github.com/orgs/Integration-Automation/projects/2/views/1 \
+> * Native support AutoControl script.
+> * Native support APITestka script.
+> * Native support Load Density script.
+> * Native support WEB Runner script.
+> * Native support Mail Thunder script.
+> * Native support File Automation script.
+> * Native support Front Engine script.
+> * Automation instance monitor.
+> * Automation project support.
+> * Docs support.
+
+
+---
+
+### install
+#### we suggest install full package
+* pip install automation_editor[full_extension]
+#### if we don't want to use send after test
+* pip install automation_editor
+* 
+![Architecture Diagram](architecture_diagram/AutomationEditorArchitectureDiagram.drawio.png)
---
+++ packages/automation-editor/automation_editor-0.0.29.tar.gz/automation_editor-0.0.29/automation_editor/automation_editor_ui/editor_main/main_ui.py
@@ -0,0 +1,72 @@
+import sys
+from typing import List, Dict, Type
+
+from PySide6.QtCore import QTimer
+from PySide6.QtWidgets import QApplication, QWidget
+from je_editor import EditorMain, language_wrapper
+from qt_material import apply_stylesheet
+
+from automation_editor.automation_editor_ui.extend_multi_language.update_language_dict import update_language_dict
+from automation_editor.automation_editor_ui.menu.build_menubar import add_menu_to_menubar
+from automation_editor.automation_editor_ui.syntax.syntax_extend import \
+    syntax_extend_package
+
+EDITOR_EXTEND_TAB: Dict[str, Type[QWidget]] = {}
+
+
+class AutomationEditor(EditorMain):
+
+    def __init__(self, debug_mode: bool = False):
+        super().__init__()
+        self.current_run_code_window: List[QWidget] = list()
+        # Project compiler if user not choose this will use which to find
+        self.python_compiler = None
+        # Delete JEditor help
+        self.help_menu.deleteLater()
+        # Update language_dict
+        update_language_dict()
+        # Menu
+        add_menu_to_menubar(self)
+        syntax_extend_package(self)
+        # System tray change
+        self.system_tray.main_window = self
+        self.system_tray.setToolTip(language_wrapper.language_word_dict.get("automation_editor_application_name"))
+        # Tab
+        for widget_name, widget in EDITOR_EXTEND_TAB.items():
+            self.tab_widget.addTab(widget(), widget_name)
+        # Title
+        self.setWindowTitle(language_wrapper.language_word_dict.get("automation_editor_application_name"))
+        if debug_mode:
+            close_timer = QTimer(self)
+            close_timer.setInterval(10000)
+            close_timer.timeout.connect(self.debug_close)
+            close_timer.start()
+
+    def closeEvent(self, event) -> None:
+        for widget in self.current_run_code_window:
+            widget.close()
+        super().closeEvent(event)
+
+    @classmethod
+    def debug_close(cls) -> None:
+        """
+        Use to run CI test.
+        :return: None
+        """
+        sys.exit(0)
+
+
+def start_editor(debug_mode: bool = False, **kwargs) -> None:
+    """
+    Start editor instance
+    :return: None
+    """
+    new_editor = QApplication(sys.argv)
+    window = AutomationEditor(debug_mode, **kwargs)
+    apply_stylesheet(new_editor, theme="dark_amber.xml")
+    window.showMaximized()
+    try:
+        window.startup_setting()
+    except Exception as error:
+        print(repr(error))
+    sys.exit(new_editor.exec())
