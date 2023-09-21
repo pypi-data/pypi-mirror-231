dc658ef8d785a0faa1a5da99d3ce45500d6eade3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:06:26 +0000

    Add package pyVisualCrossing/0.1.3/pyVisualCrossing-0.1.3-py3-none-any.whl

---
+++ packages/pyVisualCrossing/pyVisualCrossing-0.1.3-py3-none-any.whl/pyVisualCrossing/__init__.py
@@ -0,0 +1,10 @@
+"""Python Wrapper for Visual Crossing Weather API."""
+from __future__ import annotations
+
+from pyVisualCrossing.api import VisualCrossing
+from pyVisualCrossing.data import ForecastData, ForecastDailyData, ForecastHourlyData
+
+__title__ = "pyVisualCrossing"
+__version__ = "0.1.3"
+__author__ = "briis"
+__license__ = "MIT"
