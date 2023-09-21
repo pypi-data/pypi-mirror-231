fee98e1c07d0160d569ff76244bb813d25c030e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:21 +0000

    Add package dfwx-openchat/0.0.1/dfwx_openchat-0.0.1.tar.gz

---
+++ packages/dfwx-openchat/dfwx_openchat-0.0.1.tar.gz/dfwx_openchat-0.0.1/dfwx_openchat/__init__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 David
+
+"""
+This module defines some common string constants.
+"""
+from builtins import str
+from builtins import bytes
+from . import protocol
+
+
+SDK_VERSION = b'0.8.90'
+DEFAULT_SERVICE_DOMAIN = b'bcebos.com'
+URL_PREFIX = b'/v1'
+DEFAULT_ENCODING = 'UTF-8'
