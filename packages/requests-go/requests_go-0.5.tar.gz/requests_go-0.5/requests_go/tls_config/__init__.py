3fb93857281510c18856da1b77428c557ac4cad4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 16:08:28 +0000

    Add package requests-go/0.5/requests_go-0.5.tar.gz

---
+++ packages/requests-go/requests_go-0.5.tar.gz/requests_go-0.5/requests_go/tls_config/__init__.py
@@ -0,0 +1,3 @@
+from .config import *
+from .convert_config import to_tls_config
+from .extensions import TLSExtensions, HTTP2Settings
---
+++ packages/requests-go/requests_go-0.5.tar.gz/requests_go-0.5/requests_go/tls_config/config.py
@@ -0,0 +1,82 @@
+import uuid
+
+from .extensions import TLSExtensions, HTTP2Settings
+
+
+class TLSConfig:
+    def __init__(self):
+        super(TLSConfig, self).__init__()
+        self._keys = [
+            "id",
+            "ja3",
+            "headers_order",
+            "force_http1",
+            "pseudo_header_order",
+            "tls_extensions",
+            "http2_settings",
+        ]
+        self.id: str = str(uuid.uuid4())    # session id, Used to maintain session
+        self.ja3: str = None  # tls ja3 value
+        self.headers_order: list[str] = None  # http headers order
+        self.force_http1: bool = False  # force http1 request
+        # :method
+        # :authority
+        # :scheme
+        # :path
+        # Example:
+        # [
+        #     ":method",
+        #     ":authority",
+        #     ":scheme",
+        #     ":path"
+        # ]
+        self.pseudo_header_order: list[str] = [
+            ":method",
+            ":authority",
+            ":scheme",
+            ":path"
+        ]  # HTTP2 Pseudo header order
+        self.tls_extensions: TLSExtensions = TLSExtensions()  # tls extensions
+        self.http2_settings: HTTP2Settings = HTTP2Settings()  # http2 extensions
+
+    def __str__(self):
+        return str(self.toJSON())
+
+    def __iter__(self):
+        for key in self._keys:
+            yield key, getattr(self, key)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    def __delitem__(self, key):
+        setattr(self, key, None)
+
+    def __delattr__(self, item):
+        setattr(self, item, None)
+
+    # JSON转类
+    def _fromJSON(self, config: dict):
+        for key, value in config.items():
+            if key in self._keys:
+                setattr(self, key, value)
+
+    # 类转JSON
+    def toJSON(self):
+        result = {}
+        for key in self._keys:
+            go_keys = key.split("_")
+            go_key = ""
+            for k in go_keys:
+                if k == "tls" or k == "http2" or k == "http1":
+                    go_key += k.upper()
+                else:
+                    go_key += k.title()
+            if key in ["tls_extensions", "http2_settings"]:
+                result[go_key] = getattr(self, key).toJSON()
+            else:
+                result[go_key] = getattr(self, key)
+        return result
