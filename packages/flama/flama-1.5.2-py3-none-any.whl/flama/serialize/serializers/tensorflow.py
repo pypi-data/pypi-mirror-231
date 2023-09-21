7d3b746e539372b287dc33ba00603de2759c21ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:36 +0000

    Add package flama/1.5.2/flama-1.5.2-py3-none-any.whl

---
+++ packages/flama/flama-1.5.2-py3-none-any.whl/flama/serialize/serializers/tensorflow.py
@@ -0,0 +1,47 @@
+import codecs
+import importlib.metadata
+import io
+import json
+import tarfile
+import typing as t
+from tempfile import TemporaryDirectory
+
+from flama.serialize.base import Serializer
+from flama.serialize.types import Framework
+
+try:
+    import tensorflow as tf
+except Exception:  # pragma: no cover
+    tf = None  # type: ignore[misc, assignment]
+
+
+class TensorFlowSerializer(Serializer):
+    lib = Framework.tensorflow
+
+    def dump(self, obj: t.Any, **kwargs) -> bytes:
+        assert tf is not None, "`tensorflow` must be installed to use TensorFlowSerializer."
+        buffer = io.BytesIO()
+        with TemporaryDirectory() as saved_model_dir, tarfile.open(fileobj=buffer, mode="w") as model_tar:
+            tf.keras.models.save_model(obj, saved_model_dir)  # type: ignore
+            model_tar.add(saved_model_dir, arcname="")
+        buffer.seek(0)
+        return codecs.encode(buffer.read(), "base64")
+
+    def load(self, model: bytes, **kwargs) -> t.Any:
+        assert tf is not None, "`tensorflow` must be installed to use TensorFlowSerializer."
+        with TemporaryDirectory() as saved_model_dir, tarfile.open(
+            fileobj=io.BytesIO(codecs.decode(model, "base64")), mode="r:"
+        ) as model_tar:
+            model_tar.extractall(saved_model_dir)
+            return tf.keras.models.load_model(saved_model_dir)  # type: ignore
+
+    def info(self, model: t.Any) -> t.Dict[str, t.Any]:
+        model_info: t.Dict[str, t.Any] = json.loads(model.to_json())
+        return model_info
+
+    def version(self) -> str:  # type: ignore[return]
+        for lib in ("tensorflow", "tensorflow-cpu", "tensorflow-gpu", "keras"):
+            try:
+                return importlib.metadata.version(lib)
+            except Exception:
+                pass
