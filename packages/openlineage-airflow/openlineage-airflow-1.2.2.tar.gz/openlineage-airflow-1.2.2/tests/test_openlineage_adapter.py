ff6ecb0bcce007956580ee92440f705a35f49fe9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:38 +0000

    Add package openlineage-airflow/1.2.2/openlineage-airflow-1.2.2.tar.gz

---
+++ packages/openlineage-airflow/openlineage-airflow-1.2.2.tar.gz/openlineage-airflow-1.2.2/tests/test_openlineage_adapter.py
@@ -0,0 +1,43 @@
+# Copyright 2018-2023 contributors to the OpenLineage project
+# SPDX-License-Identifier: Apache-2.0
+
+import logging
+import os
+from unittest.mock import patch
+
+from openlineage.airflow.adapter import OpenLineageAdapter
+
+
+@patch.dict(os.environ, {
+    "MARQUEZ_URL": "http://marquez:5000",
+    "MARQUEZ_API_KEY": "api-key"
+})
+def test_create_client_from_marquez_url():
+    client = OpenLineageAdapter().get_or_create_openlineage_client()
+    assert client.transport.url == "http://marquez:5000"
+
+
+@patch.dict(os.environ, {
+    "OPENLINEAGE_URL": "http://ol-api:5000",
+    "OPENLINEAGE_API_KEY": "api-key"
+})
+def test_create_client_from_ol_env():
+    client = OpenLineageAdapter().get_or_create_openlineage_client()
+    assert client.transport.url == "http://ol-api:5000"
+
+
+def test_setting_ol_adapter_log_level() -> None:
+    # DEBUG level set for `openlineage` logger in tests setup
+    default_log_level = logging.DEBUG
+    # without environment variable
+    OpenLineageAdapter()
+    parent_logger = logging.getLogger("openlineage.airflow")
+    logger = logging.getLogger("openlineage.airflow.adapter")
+    assert parent_logger.getEffectiveLevel() == default_log_level
+    assert logger.getEffectiveLevel() == default_log_level
+    with patch.dict(os.environ, {"OPENLINEAGE_AIRFLOW_LOGGING": "CRITICAL"}):
+        assert parent_logger.getEffectiveLevel() == default_log_level
+        assert logger.getEffectiveLevel() == default_log_level
+        OpenLineageAdapter()
+        assert parent_logger.getEffectiveLevel() == logging.CRITICAL
+        assert logger.getEffectiveLevel() == logging.CRITICAL
