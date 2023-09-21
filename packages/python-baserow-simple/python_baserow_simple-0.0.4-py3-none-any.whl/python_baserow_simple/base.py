f479c416d8a319d97e6f1374d490550a48869a3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:06:50 +0000

    Add package python-baserow-simple/0.0.4/python_baserow_simple-0.0.4-py3-none-any.whl

---
+++ packages/python-baserow-simple/python_baserow_simple-0.0.4-py3-none-any.whl/python_baserow_simple/base.py
@@ -0,0 +1,17 @@
+"""
+python_baserow_simple base module.
+
+This is the principal module of the python_baserow_simple project.
+here you put your main classes and objects.
+
+Be creative! do whatever you want!
+
+If you want to replace this with a Flask application run:
+
+    $ make init
+
+and then choose `flask` as template.
+"""
+
+# example constant variable
+NAME = "python_baserow_simple"
