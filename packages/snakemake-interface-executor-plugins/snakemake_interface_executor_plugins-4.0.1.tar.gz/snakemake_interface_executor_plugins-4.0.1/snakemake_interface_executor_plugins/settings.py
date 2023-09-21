ec139c39ab88be4026d9b9577714bc7af1a44ee6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:03 +0000

    Add package snakemake-interface-executor-plugins/4.0.1/snakemake_interface_executor_plugins-4.0.1.tar.gz

---
+++ packages/snakemake-interface-executor-plugins/snakemake_interface_executor_plugins-4.0.1.tar.gz/snakemake_interface_executor_plugins-4.0.1/snakemake_interface_executor_plugins/settings.py
@@ -0,0 +1,81 @@
+from abc import ABC, abstractmethod
+from typing import List, Optional, Set
+
+from snakemake_interface_common.settings import SettingsEnumBase
+
+
+class RemoteExecutionSettingsExecutorInterface(ABC):
+    @property
+    @abstractmethod
+    def jobname(self) -> str:
+        ...
+
+    @property
+    @abstractmethod
+    def jobscript(self) -> str:
+        ...
+
+    @property
+    @abstractmethod
+    def immediate_submit(self) -> bool:
+        ...
+
+    @property
+    @abstractmethod
+    def envvars(self) -> Optional[List[str]]:
+        ...
+
+    @property
+    @abstractmethod
+    def max_status_checks_per_second(self) -> float:
+        ...
+
+    @property
+    @abstractmethod
+    def seconds_between_status_checks(self) -> int:
+        ...
+
+
+class ExecMode(SettingsEnumBase):
+    """
+    Enum for execution mode of Snakemake.
+    This handles the behavior of e.g. the logger.
+    """
+
+    DEFAULT = 0
+    SUBPROCESS = 1
+    REMOTE = 2
+
+
+class ExecutionSettingsExecutorInterface(ABC):
+    @property
+    @abstractmethod
+    def keep_incomplete(self) -> bool:
+        ...
+
+
+class StorageSettingsExecutorInterface(ABC):
+    @property
+    @abstractmethod
+    def assume_shared_fs(self) -> bool:
+        ...
+
+
+class DeploymentMethod(SettingsEnumBase):
+    CONDA = 0
+    APPTAINER = 1
+    ENV_MODULES = 2
+
+
+class DeploymentSettingsExecutorInterface(ABC):
+    @property
+    @abstractmethod
+    def deployment_method(self) -> Set[DeploymentMethod]:
+        ...
+
+
+class GroupSettingsExecutorInterface(ABC):
+    @property
+    @abstractmethod
+    def local_groupid(self) -> str:
+        ...
