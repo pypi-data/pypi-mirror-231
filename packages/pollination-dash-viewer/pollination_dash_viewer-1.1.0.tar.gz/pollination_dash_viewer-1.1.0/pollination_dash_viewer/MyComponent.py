4dd23c87e57939afd47cb155b910df9bbf7b640a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:25 +0000

    Add package pollination-dash-viewer/1.1.0/pollination_dash_viewer-1.1.0.tar.gz

---
+++ packages/pollination-dash-viewer/pollination_dash_viewer-1.1.0.tar.gz/pollination_dash_viewer-1.1.0/pollination_dash_viewer/MyComponent.py
@@ -0,0 +1,29 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class MyComponent(Component):
+    """A MyComponent component.
+Component description
+
+Keyword arguments:
+
+- id (string; optional):
+    Unique ID to identify this component in Dash callbacks."""
+    _children_props = []
+    _base_nodes = ['children']
+    _namespace = 'pollination_dash_viewer'
+    _type = 'MyComponent'
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id']
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs and excess named props
+        args = {k: _locals[k] for k in _explicit_args}
+
+        super(MyComponent, self).__init__(**args)
---
+++ packages/pollination-dash-viewer/pollination_dash_viewer-1.1.0.tar.gz/pollination_dash_viewer-1.1.0/src/jl/''_mycomponent.jl
@@ -0,0 +1,18 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+export ''_mycomponent
+
+"""
+    ''_mycomponent(;kwargs...)
+
+A MyComponent component.
+Component description
+Keyword arguments:
+- `id` (String; optional): Unique ID to identify this component in Dash callbacks.
+"""
+function ''_mycomponent(; kwargs...)
+        available_props = Symbol[:id]
+        wild_props = Symbol[]
+        return Component("''_mycomponent", "MyComponent", "pollination_dash_viewer", available_props, wild_props; kwargs...)
+end
+
