21bd2c207265e5a4bf5784bbaffc03e5d1ea16a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:06:48 +0000

    Add package era-5g-client/0.6.0/era_5g_client-0.6.0-py3-none-any.whl

---
+++ packages/era-5g-client/era_5g_client-0.6.0-py3-none-any.whl/era_5g_client/exceptions.py
@@ -0,0 +1,26 @@
+class Era5gClientException(Exception):
+    """Common base for all exception that this client might raise."""
+
+    pass
+
+
+class FailedToConnect(Era5gClientException):
+    """Exception which is raised when the client could not connect to the
+    NetApp or Middleware."""
+
+    pass
+
+
+class FailedToObtainPlan(Era5gClientException):
+    """Exception which is raised when the client could not get the plan from
+    the Middleware."""
+
+    pass
+
+
+class NetAppNotReady(Era5gClientException):
+    """Raised when an operation was requested on NetApp which is not ready."""
+
+
+class FailedToSendData(Era5gClientException):
+    """Raised when the data could not be send."""
