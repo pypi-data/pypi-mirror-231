2777d95cbc7b7d070b42de1558d990a2f3c24f3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:05:23 +0000

    Add package skelly-synchronize/2023.9.1026/skelly_synchronize-2023.9.1026-py3-none-any.whl

---
+++ packages/skelly-synchronize/skelly_synchronize-2023.9.1026-py3-none-any.whl/skelly_synchronize/system/paths_and_file_names.py
@@ -0,0 +1,23 @@
+# directory names
+SYNCHRONIZED_VIDEOS_FOLDER_NAME = "synchronized_videos"
+RAW_VIDEOS_FOLDER_NAME = "raw_videos"
+AUDIO_FILES_FOLDER_NAME = "audio_files"
+TRIMMED_AUDIO_FOLDER_NAME = "trimmed_audio"
+
+# file names
+DEBUG_TOML_NAME = "synchronization_debug.toml"
+DEBUG_PLOT_NAME = "debug_plot.png"
+
+# debug dictionary keys
+RAW_VIDEO_NAME = "Raw_video_information"
+SYNCHRONIZED_VIDEO_NAME = "Synchronized_video_information"
+AUDIO_NAME = "Audio_information"
+LAG_DICTIONARY_NAME = "Lag_dictionary"
+
+# figshare info
+FIGSHARE_ZIP_FILE_URL = "https://figshare.com/ndownloader/files/41066489"
+FIGSHARE_SAMPLE_DATA_FILE_NAME = "skelly_synchronize_sample_data"
+
+# string constants
+SYNCED_VIDEO_PRECURSOR = "synced_"
+BRIGHTNESS_SUFFIX = "_brightness"
