2c6c7c3423bd331df4733658f97c846e87ee8a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:50 +0000

    Add package fastapi-websocket-rpc/0.1.25/fastapi_websocket_rpc-0.1.25-py3-none-any.whl

---
+++ packages/fastapi-websocket-rpc/fastapi_websocket_rpc-0.1.25-py3-none-any.whl/fastapi_websocket_rpc/__init__.py
@@ -0,0 +1,5 @@
+from .rpc_methods import RpcMethodsBase, RpcUtilityMethods
+from .websocket_rpc_client import WebSocketRpcClient
+from .websocket_rpc_endpoint import WebsocketRPCEndpoint
+from .rpc_channel import RpcChannel
+from .schemas import WebSocketFrameType
