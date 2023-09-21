c2edc6e80fb169cc6c2f9d6d6d50892e6d7fa075
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:11 +0000

    Add package pdm-bump/0.8.0a2/pdm_bump-0.8.0a2.tar.gz

---
+++ packages/pdm-bump/pdm_bump-0.8.0a2.tar.gz/pdm_bump-0.8.0a2/src/pdm_bump/vcs/__init__.py
@@ -0,0 +1,48 @@
+#
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021-2023 Carsten Igel.
+#
+# This file is part of pdm-bump
+# (see https://github.com/carstencodes/pdm-bump).
+#
+# This file is published using the MIT license.
+# Refer to LICENSE for more information
+#
+
+""""""
+
+
+# Justification: load module for decoupling
+from . import gitcli  # noqa: F401
+from .core import (
+    DefaultVcsProvider,
+    VcsProvider,
+    VcsProviderAggregator,
+    VcsProviderFactory,
+    VcsProviderRegistry,
+    vcs_provider,
+    vcs_providers,
+)
+from .history import (
+    Commit,
+    CommitStatistics,
+    CommitType,
+    ConventionalCommitParser,
+    History,
+)
+
+__all__ = (
+    "vcs_provider",
+    "vcs_providers",
+    "VcsProvider",
+    "VcsProviderAggregator",
+    "VcsProviderFactory",
+    "VcsProviderRegistry",
+    "DefaultVcsProvider",
+    "Commit",
+    "CommitType",
+    "CommitStatistics",
+    "History",
+    "ConventionalCommitParser",
+)
