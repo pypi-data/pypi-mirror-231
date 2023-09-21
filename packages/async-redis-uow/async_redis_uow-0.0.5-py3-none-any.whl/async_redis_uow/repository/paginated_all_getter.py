b0aebd23bd4ea9c0b0e6b3b28be745f65dd2c533
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:06:16 +0000

    Add package async-redis-uow/0.0.5/async_redis_uow-0.0.5-py3-none-any.whl

---
+++ packages/async-redis-uow/async_redis_uow-0.0.5-py3-none-any.whl/async_redis_uow/repository/paginated_all_getter.py
@@ -0,0 +1,16 @@
+from typing import Generic, List, Optional
+from .types import TIModel, TOModel
+from .all_getter import AllGetterRepo
+
+
+class PaginatedAllGetterRepo(AllGetterRepo[TIModel, TOModel], Generic[TIModel, TOModel]):
+    __abstract__ = True
+
+    async def page(self, filters: Optional[str] = None, count: Optional[int] = None, page: Optional[int] = None) -> List[TOModel]:
+        objs = await self.all(filters)
+
+        if count and page:
+            return objs[(count or 1) * page - count:count]
+
+        return objs
+
