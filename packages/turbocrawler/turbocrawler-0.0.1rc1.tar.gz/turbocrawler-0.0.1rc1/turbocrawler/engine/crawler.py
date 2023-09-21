96448d5385c2e31013a534484167b396fd6129f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 14:08:22 +0000

    Add package turbocrawler/0.0.1rc1/turbocrawler-0.0.1rc1.tar.gz

---
+++ packages/turbocrawler/turbocrawler-0.0.1rc1.tar.gz/turbocrawler-0.0.1rc1/turbocrawler/engine/crawler.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from typing import Any
+
+from turbocrawler.engine.base_queues.crawler_queue_base import CrawlerQueueABC
+from turbocrawler.engine.models import CrawlerRequest, CrawlerResponse
+
+
+class Crawler(ABC):
+    crawler_name: str
+    allowed_domains: list[str]
+    regex_rules: list[str] = []
+    time_between_requests: int | float = 0
+
+    crawler_queue: CrawlerQueueABC
+
+    def __init__(self) -> None:
+        ...
+
+    @abstractmethod
+    def start_crawler(self) -> None:
+        ...
+
+    @abstractmethod
+    def crawler_first_request(self) -> CrawlerResponse:
+        ...
+
+    # @abstractmethod
+    # def before_request(self):
+    #     ...
+
+    @abstractmethod
+    def process_request(self, crawler_request: CrawlerRequest) -> CrawlerResponse:
+        ...
+
+    # @abstractmethod
+    # def after_request(self):
+    #     ...
+
+    @abstractmethod
+    def parse_crawler_response(self, crawler_response: CrawlerResponse) -> Any:
+        ...
+
+    @abstractmethod
+    def stop_crawler(self) -> None:
+        ...
