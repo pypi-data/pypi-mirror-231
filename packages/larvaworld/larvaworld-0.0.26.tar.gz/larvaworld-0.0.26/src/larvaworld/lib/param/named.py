2f02d426cfd3b5e3c8c7463a640cffeb85c129c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:06 +0000

    Add package larvaworld/0.0.26/larvaworld-0.0.26.tar.gz

---
+++ packages/larvaworld/larvaworld-0.0.26.tar.gz/larvaworld-0.0.26/src/larvaworld/lib/param/named.py
@@ -0,0 +1,22 @@
+import param
+
+from larvaworld.lib.param import NestedConf
+
+
+
+__all__ = [
+    'Named',
+    'Grouped',
+]
+
+
+class Named(NestedConf) :
+    unique_id = param.String(None, doc='The unique ID of the entity')
+
+    def set_id(self, id):
+        self.unique_id = id
+
+
+class Grouped(Named) :
+    group = param.String(None, doc='The unique ID of the entity"s group')
+
