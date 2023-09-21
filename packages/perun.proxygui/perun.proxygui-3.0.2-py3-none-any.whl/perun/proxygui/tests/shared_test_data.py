ced96f6b7e2b1c9f8d0121c3388fbb3b42d00af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:28 +0000

    Add package perun.proxygui/3.0.2/perun.proxygui-3.0.2-py3-none-any.whl

---
+++ packages/perun.proxygui/perun.proxygui-3.0.2-py3-none-any.whl/perun/proxygui/tests/shared_test_data.py
@@ -0,0 +1,6 @@
+SHARED_TESTING_CONFIG = {
+    "adapters_manager": {"adapters": {}},
+    "attrs_cfg_path": "mock_path",
+    "perun_person_principal_names_attribute": "mock_name",
+}
+ATTRS_MAP = {}
