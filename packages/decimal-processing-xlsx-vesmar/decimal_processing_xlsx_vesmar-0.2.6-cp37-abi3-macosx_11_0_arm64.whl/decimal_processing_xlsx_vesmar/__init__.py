543b2cc39a1c3a68daa8e96d9fd383783be3d01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:24 +0000

    Add package decimal-processing-xlsx-vesmar/0.2.6/decimal_processing_xlsx_vesmar-0.2.6-cp37-abi3-macosx_11_0_arm64.whl

---
+++ packages/decimal-processing-xlsx-vesmar/decimal_processing_xlsx_vesmar-0.2.6-cp37-abi3-macosx_11_0_arm64.whl/decimal_processing_xlsx_vesmar/__init__.py
@@ -0,0 +1,5 @@
+from .decimal_processing_xlsx_vesmar import *
+
+__doc__ = decimal_processing_xlsx_vesmar.__doc__
+if hasattr(decimal_processing_xlsx_vesmar, "__all__"):
+    __all__ = decimal_processing_xlsx_vesmar.__all__
