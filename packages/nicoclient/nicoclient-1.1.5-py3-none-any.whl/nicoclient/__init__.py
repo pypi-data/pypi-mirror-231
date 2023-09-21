cceba38720d00c6b5cb9f9a7571bf5994a3b20db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:06:26 +0000

    Add package nicoclient/1.1.5/nicoclient-1.1.5-py3-none-any.whl

---
+++ packages/nicoclient/nicoclient-1.1.5-py3-none-any.whl/nicoclient/__init__.py
@@ -0,0 +1,5 @@
+from nicoclient.core.search import search_utattemita_videos, search_daily_trending_utatttemita_videos
+from nicoclient.html_page.playlist import get_playlist
+from nicoclient.html_page.video_page import get_video_by_id
+
+name = "nicoclient"
