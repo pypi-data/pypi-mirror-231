6a4918044328203ab9b4fe9601ebcf1f9ed8bc74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 05:05:48 +0000

    Add package conda-store-server/2023.9.1/conda_store_server-2023.9.1-py3-none-any.whl

---
+++ packages/conda-store-server/conda_store_server-2023.9.1-py3-none-any.whl/conda_store_server/server/dependencies.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, Request
+from sqlalchemy.orm import Session
+
+
+async def get_conda_store(request: Request):
+    return request.state.conda_store
+
+
+async def get_server(request: Request):
+    return request.state.server
+
+
+async def get_auth(request: Request):
+    return request.state.authentication
+
+
+async def get_entity(request: Request, auth=Depends(get_auth)):
+    return auth.authenticate_request(request)
+
+
+async def get_templates(request: Request):
+    return request.state.templates
+
+
+async def get_db(request: Request, conda_store=Depends(get_conda_store)) -> Session:
+    db = conda_store.session_factory()
+    try:
+        yield db
+    finally:
+        db.close()
