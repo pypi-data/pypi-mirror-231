45995d14738cc4e08948b4abbf253f5ec0b78ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:02 +0000

    Add package instacrawler/1.0.0/instacrawler-1.0.0.tar.gz

---
+++ packages/instacrawler/instacrawler-1.0.0.tar.gz/instacrawler-1.0.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'instacrawler',
+    version = '1.0.0',
+    description = "The fastest way to check someone's instagram feed/story.",
+    author = 'gibiee',
+    author_email = 'gibiee@naver.com',
+    url = 'https://github.com/gibiee/instacrawler',
+    download_url = '',
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    install_requires = ['pillow', 'beautifulsoup4', 'selenium'],
+    
+    python_requires ='>=3.6',
+    license = 'MIT',
+    keywords = ['instacrawler', 'instagram', 'crawler', 'crawling', 'instagram-crawler', 'instagram-crawling'],
+    classifiers = [
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
+)
