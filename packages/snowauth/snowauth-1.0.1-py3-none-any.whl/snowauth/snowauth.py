09e57f8dfd68c422b4821c561e568df228849dcb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:29 +0000

    Add package snowauth/1.0.1/snowauth-1.0.1-py3-none-any.whl

---
+++ packages/snowauth/snowauth-1.0.1-py3-none-any.whl/snowauth/snowauth.py
@@ -0,0 +1,28 @@
+import toml
+from snowflake.snowpark.session import Session
+
+def connect(header,file_name="creds.conf"):
+    """
+    Allows users to put configurations in a file with toml header
+    and pull to generate a session variable in snowpark.
+
+    Para:
+        header - Header of the toml file section to read from.
+        
+        file_name - File name to get credentials from. Default - "creds.conf"
+
+    Example:
+        >>> import snowauth
+        >>> session = snowauth.connect('example_connection')
+        >>> print(session)
+        ...
+        ... <snowflake.snowpark.session.Session: account=...>
+    """
+
+    with open(file_name, 'r') as f:
+        creds = toml.load(f)
+
+    if not creds:
+        raise Exception('Connection Name not Found!')
+    
+    return Session.builder.configs(creds[header]).create()
