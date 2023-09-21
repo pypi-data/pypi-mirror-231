e732a800c86064bed02158e10ee8d98d5cb93952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:06:08 +0000

    Add package Hough-TMF/0.0.2/Hough-TMF-0.0.2.tar.gz

---
+++ packages/Hough-TMF/Hough-TMF-0.0.2.tar.gz/Hough-TMF-0.0.2/setup.py
@@ -0,0 +1,42 @@
+import setuptools
+
+'''
+import pandas
+import os
+import cv2
+from skimage.transform import hough_line, hough_line_peaks
+from skimage.feature import canny
+import numpy as np
+import scipy
+from scipy.optimize import curve_fit
+
+import torch
+import torch.nn.functional as F
+#ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
+import numpy as np
+from numpy import roll
+
+'''
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Hough-TMF",
+    version="0.0.2",
+    author="Hao Lv",
+    author_email="lh21@apm.ac.cn",
+    description="A package for template matching using Torch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
+
+
