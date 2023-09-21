c361f4e09557aed99072f522a26effd2b2c9fb27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:07:40 +0000

    Add package byquant/3.8.12.0/byquant-3.8.12.0-py3-none-any.whl

---
+++ packages/byquant/byquant-3.8.12.0-py3-none-any.whl/byquant/bybot/__init__.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; py-indent-offset:4 -*-
+###############################################################################
+#
+# Author : Trabi
+# Email : info@byquant.com
+# Copyright (C) 2022-2023 ByQuant.com
+#
+#  ███████   ██    ██   ██████   ██    ██    ████    ██    ██  ████████  
+#  ██    ██   ██  ██   ██    ██  ██    ██   ██  ██   ███   ██     ██     
+#  ███████     ████    ██    ██  ██    ██  ████████  ██ ██ ██     ██         █████  █████  █████
+#  ██    ██     ██     ██   ██   ██    ██  ██    ██  ██   ███     ██         █      █   █  █ █ █
+#  ███████      ██      ███████   ██████   ██    ██  ██    ██     ██     ██  █████  █████  █ █ █
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+
+
+import os
+def checkVip(check_name,warn=True):
+    byroot_dir = os.path.dirname(__file__)
+    check_dir = byroot_dir+"/"+check_name
+    if os.path.exists(check_dir) and os.path.isdir(check_dir):
+        return True
+    else:
+        return False
+        
+if not checkVip('vip'):
+    print("No ByBot Permission, Please apply from https://byquant.com/by/bot/ ")
+
+else:
+    from . import strategy as strategy
+    from . import model as model
+    from . import signal as signal
+    from . import algo as algo
+    #from model import model
