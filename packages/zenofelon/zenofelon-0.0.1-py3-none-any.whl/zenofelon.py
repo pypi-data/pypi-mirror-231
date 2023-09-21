7b6ce6beca702c30a93185d23943774de6840c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 05:05:34 +0000

    Add package zenofelon/0.0.1/zenofelon-0.0.1-py3-none-any.whl

---
+++ packages/zenofelon/zenofelon-0.0.1-py3-none-any.whl/zenofelon.py
@@ -0,0 +1,18 @@
+
+print("""The Zen of Elon
+
+
+1) Question your requirements
+Make your requirements less dumb. Everyone's wrong, no matter who you are.
+
+2) Delete
+Try very hard to delete the part or process. If you're not adding things back in ten percent of the time, you are not deleting enough.
+
+3) Simplify and optimize
+The most common error of a smart engineer is to optimize a thing that should not exist.
+
+4) Accelerate cycle time
+You are moving too slowly — go faster. But don’t go faster before steps 1, 2 and 3 — if you are digging your grave, don’t dig it faster.
+
+5) Automate
+""")
