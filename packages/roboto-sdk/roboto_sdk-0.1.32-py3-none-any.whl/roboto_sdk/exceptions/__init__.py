d826459c8e68cfa6c9e3b64ddcdc602c00a0d43c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 14:06:19 +0000

    Add package roboto-sdk/0.1.32/roboto_sdk-0.1.32-py3-none-any.whl

---
+++ packages/roboto-sdk/roboto_sdk-0.1.32-py3-none-any.whl/roboto_sdk/exceptions/__init__.py
@@ -0,0 +1,27 @@
+from .domain import (
+    RobotoConditionException,
+    RobotoConflictException,
+    RobotoDomainException,
+    RobotoHttpExceptionParse,
+    RobotoInvalidRequestException,
+    RobotoLimitExceededException,
+    RobotoNoOrgProvidedException,
+    RobotoNotFoundException,
+    RobotoServiceException,
+    RobotoUnauthorizedException,
+    RobotoUnknownOperationException,
+)
+
+__all__ = [
+    "RobotoConditionException",
+    "RobotoConflictException",
+    "RobotoDomainException",
+    "RobotoHttpExceptionParse",
+    "RobotoInvalidRequestException",
+    "RobotoLimitExceededException",
+    "RobotoNotFoundException",
+    "RobotoNoOrgProvidedException",
+    "RobotoServiceException",
+    "RobotoUnauthorizedException",
+    "RobotoUnknownOperationException",
+]
