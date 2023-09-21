6bb994b5815b1c5946b5ef6fc5f8ab9574a80aaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 14:07:36 +0000

    Add package ai_engine_new/0.1.12/ai_engine_new-0.1.12.tar.gz

---
+++ packages/ai_engine_new/ai_engine_new-0.1.12.tar.gz/ai_engine_new-0.1.12/ai_engine_new/models/__init__.py
@@ -0,0 +1,15 @@
+from ai_engine_new.models.Dream import (DreamCheckTask, 
+                                    DreamCreateTask, 
+                                    DreamArtStyle)
+from ai_engine_new.models.Kandinsky import (KandinskyAskModel, 
+                                        KandinskyResponseModel, 
+                                        KandinskyStyleModel)
+
+__all__ = [
+    "DreamCheckTask",
+    "DreamCreateTask",
+    "DreamArtStyle",
+    "KandinskyAskModel", 
+    "KandinskyResponseModel", 
+    "KandinskyStyleModel"
+          ]
---
+++ packages/ai_engine_new/ai_engine_new-0.1.12.tar.gz/ai_engine_new-0.1.12/ai_engine_new/wombo/__init__.py
@@ -0,0 +1,10 @@
+from ai_engine_new.wombo.dream import Dream
+from ai_engine_new.wombo.adream import AsyncDream
+
+version = "1.0.0"
+
+__all__ = [
+    "AsyncDream",
+    "Dream",
+    "version"
+]
