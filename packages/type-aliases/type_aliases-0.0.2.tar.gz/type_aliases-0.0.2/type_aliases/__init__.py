06c75b090f880d7a189044d856b89088c75a9379
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:06:17 +0000

    Add package type-aliases/0.0.2/type_aliases-0.0.2.tar.gz

---
+++ packages/type-aliases/type_aliases-0.0.2.tar.gz/type_aliases-0.0.2/type_aliases/__init__.py
@@ -0,0 +1,19 @@
+from typing import TypeAlias
+import typing
+
+Any: TypeAlias = typing.Any
+Bool: TypeAlias = bool
+Dict = dict
+Float: TypeAlias = float
+Int: TypeAlias = int
+List = list
+Literal = typing.Literal
+LiteralString: TypeAlias = typing.LiteralString
+NamedTuple: TypeAlias = typing.NamedTuple
+Optional = typing.Optional
+Set = set
+Str: TypeAlias = str
+Tuple = tuple
+Type: TypeAlias = type
+TypeVar: TypeAlias = typing.TypeVar
+Union = typing.Union
