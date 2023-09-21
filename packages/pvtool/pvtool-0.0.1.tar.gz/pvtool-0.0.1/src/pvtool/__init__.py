d9046f3870ebbac148133b43871ecf174c1921a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:12 +0000

    Add package pvtool/0.0.1/pvtool-0.0.1.tar.gz

---
+++ packages/pvtool/pvtool-0.0.1.tar.gz/pvtool-0.0.1/src/pvtool/__init__.py
@@ -0,0 +1,10 @@
+"""
+src contains all your business logic
+"""
+
+
+def return_foo() -> str:
+    """
+    returns foo (this is just to have some business logic and a non null coverage ;)
+    """
+    return "foo"
