6a803ac8b6d1993ea8b89be3af9ab44412d36cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:38 +0000

    Add package dsw-command-queue/3.27.1/dsw_command_queue-3.27.1-py2.py3-none-any.whl

---
+++ packages/dsw-command-queue/dsw_command_queue-3.27.1-py2.py3-none-any.whl/dsw/command_queue/build_info.py
@@ -0,0 +1,17 @@
+# Generated file
+# - do not overwrite
+# - do not include in git
+from collections import namedtuple
+
+BuildInfo = namedtuple(
+    'BuildInfo',
+    ['version', 'built_at', 'sha', 'branch', 'tag'],
+)
+
+BUILD_INFO = BuildInfo(
+    version='v3.27.1~4be9747',
+    built_at='2023-09-20 13:02:34Z',
+    sha='4be974749848a367bb7558c64b2f732e85ed8d75',
+    branch='HEAD',
+    tag='v3.27.1',
+)
