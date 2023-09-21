788b72e7f06406fcf3a812a1b3eb45033295c658
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:29 +0000

    Add package iceaddr/0.5.7/iceaddr-0.5.7.tar.gz

---
+++ packages/iceaddr/iceaddr-0.5.7.tar.gz/iceaddr-0.5.7/src/iceaddr/__init__.py
@@ -0,0 +1,15 @@
+"""
+
+    iceaddr: Look up information about Icelandic streets, addresses,
+             placenames, landmarks, locations and postcodes.
+
+    Copyright (c) 2018-2023 Sveinbjorn Thordarson.
+
+"""
+
+__author__ = "Sveinbjorn Thordarson"
+__version__ = "0.5.7"  # Also update setup.py
+
+from .addresses import *  # noqa: F403
+from .placenames import *  # noqa: F403
+from .postcodes import *  # noqa: F403
