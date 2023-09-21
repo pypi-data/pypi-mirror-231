be09255637d0a2f0308144f788efc7a89700d82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:39 +0000

    Add package physics-lib24/0.1.1/physics-lib24-0.1.1.tar.gz

---
+++ packages/physics-lib24/physics-lib24-0.1.1.tar.gz/physics-lib24-0.1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="physics-lib24",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[],  # List any dependencies here
+    author="Kautilya Katariya",
+    author_email="katariya.i@gmail.com",
+    description="A Python physics library for 2D and 3D simulations",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
