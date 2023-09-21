7421f0b702f3ccdbc0b3275f3b7a88bb4a5dc587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:36 +0000

    Add package LIFEsim/0.2.32/LIFEsim-0.2.32.tar.gz

---
+++ packages/LIFEsim/LIFEsim-0.2.32.tar.gz/LIFEsim-0.2.32/lifesim/util/constants.py
@@ -0,0 +1,11 @@
+h = 6.62607e-34
+k = 1.380649e-23
+c = 2.99792e+8
+m_per_pc = 3.086e+16
+m_per_au = 1.49597870691e+11
+
+radius_sun = 6.947e+8
+temp_sun = 5778.
+
+radius_earth = 6.371e+6
+temp_earth = 276.
