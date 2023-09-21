9f5799e2266ac97c5eaf5d40f021f6d03e0c2652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:05:41 +0000

    Add package SimpleTopicModel/0.0.2/SimpleTopicModel-0.0.2.tar.gz

---
+++ packages/SimpleTopicModel/SimpleTopicModel-0.0.2.tar.gz/SimpleTopicModel-0.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+
+    name='SimpleTopicModel',
+
+    version='0.0.2',
+
+    author='decafsunrise',
+
+    author_email='alembichosting@gmail.com',
+
+    description='An NLP Package for generating Topic Models',
+    
+    long_description_content_type='text/markdown',
+    
+    long_description=open('README.md').read(),
+    
+    packages=find_packages(),
+
+)
