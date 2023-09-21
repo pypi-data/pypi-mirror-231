1b82208530a45abdd005a7d68a7b5d2a6a7aa9cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:07 +0000

    Add package pg-common/1.0.7/pg_common-1.0.7.tar.gz

---
+++ packages/pg-common/pg_common-1.0.7.tar.gz/pg_common-1.0.7/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from pg_common import VERSION
+
+DIST_NAME = "pg_common"
+__author__ = "baozilaji@gmail.com"
+
+setup(
+	name=DIST_NAME,
+	version=VERSION,
+	description="python game: common lib",
+	packages=['pg_common'],
+	author=__author__,
+	python_requires='>=3',
+	install_requires=[
+		'pycrypto==2.6.1'
+	]
+)
