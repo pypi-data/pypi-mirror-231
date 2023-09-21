b0509499ab4884d50b3c670802c3abe5768826d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:05 +0000

    Add package Flask-OpenAPI3-UI/1.8.0/Flask-OpenAPI3-UI-1.8.0.tar.gz

---
+++ packages/Flask-OpenAPI3-UI/Flask-OpenAPI3-UI-1.8.0.tar.gz/Flask-OpenAPI3-UI-1.8.0/src/flask_openapi/__init__.py
@@ -0,0 +1,17 @@
+# DO NOT CHANGE MANUALLY THIS IS CHANGED IN THE PIPELINES
+__version__ = '1.8.0'
+# Based on works of Bruno Rocha and the Flasgger open source community
+
+
+from jsonschema import ValidationError  # noqa
+
+from .base import (
+    BR_SANITIZER,
+    Flasgger,
+    MK_SANITIZER,
+    NO_SANITIZER,
+    Swagger,
+)
+from .constants import OPTIONAL_FIELDS  # noqa
+from .marshmallow_apispec import APISpec, fields, Schema, SwaggerView  # noqa
+from .utils import apispec_to_template, swag_from, validate  # noqa
