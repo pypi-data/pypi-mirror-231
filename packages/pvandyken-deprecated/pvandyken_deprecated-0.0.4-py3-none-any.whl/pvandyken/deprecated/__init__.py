378dbe2c59bc9be64e94c9d7499c6665f9e7247e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 14:06:49 +0000

    Add package pvandyken-deprecated/0.0.4/pvandyken_deprecated-0.0.4-py3-none-any.whl

---
+++ packages/pvandyken-deprecated/pvandyken_deprecated-0.0.4-py3-none-any.whl/pvandyken/deprecated/__init__.py
@@ -0,0 +1,15 @@
+from pvandyken.deprecated.deprecated import (
+    DeprecatedWarning,
+    UnsupportedWarning,
+    deprecated,
+    fail_if_not_removed,
+    set_config,
+)
+
+__all__ = [
+    "DeprecatedWarning",
+    "UnsupportedWarning",
+    "deprecated",
+    "fail_if_not_removed",
+    "set_config",
+]
