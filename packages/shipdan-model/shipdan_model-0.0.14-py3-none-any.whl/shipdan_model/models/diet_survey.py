dbec424a1e63dfac144748eb4f5d48fa2e4bf634
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:05:44 +0000

    Add package shipdan-model/0.0.14/shipdan_model-0.0.14-py3-none-any.whl

---
+++ packages/shipdan-model/shipdan_model-0.0.14-py3-none-any.whl/shipdan_model/models/diet_survey.py
@@ -0,0 +1,74 @@
+from django.db import models
+
+
+# todo 솔직히 아는 것 없어서 다 가져옴 - 검수 요망
+class DietOrderSurvey(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='diet_order_surveys')
+    question_category = models.IntegerField()
+    answer = models.JSONField(default=dict)
+    extra = models.JSONField(default=dict)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'diet_survey_dietordersurvey'
+
+
+class DietOrderSurveyComplete(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='diet_order_surveys_completed')
+    diet_order = models.OneToOneField(
+        'food_payment.FoodOrder', related_name='surveys_completed', on_delete=models.CASCADE
+    )
+    answer = models.IntegerField(help_text='complete 상태')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'diet_survey_dietordersurveycomplete'
+
+
+class DayDietSurveyComplete(models.Model):
+    user = models.ForeignKey('accounts.User', on_delete=models.CASCADE, related_name='day_diet_surveys_completed')
+    day_diet = models.OneToOneField('food_payment.OrderDayDietLog', related_name='survey_completed',
+                                    on_delete=models.CASCADE)
+    answer = models.IntegerField(help_text='complete 상태')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'diet_survey_daydietsurveycomplete'
+
+
+class DietOrderSurveyRelation(models.Model):
+    survey = models.OneToOneField(DietOrderSurvey, related_name='diet_order_relation', on_delete=models.CASCADE)
+    diet_order = models.ForeignKey('food_payment.FoodOrder', related_name='survey_relations', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'diet_survey_dietordersurveyrelation'
+
+
+class DietMealSurveyRelation(models.Model):
+    survey = models.OneToOneField(DietOrderSurvey, related_name='diet_meal_relation', on_delete=models.CASCADE)
+    day_diet = models.ForeignKey('food_payment.OrderDayDietLog', related_name='meal_survey_relations',
+                                 on_delete=models.CASCADE)
+    meal = models.IntegerField()
+
+    class Meta:
+        db_table = 'diet_survey_dietmealsurveyrelation'
+
+
+class DietFoodSurveyRelation(models.Model):
+    survey = models.OneToOneField(DietOrderSurvey, related_name='diet_food_relation', on_delete=models.CASCADE)
+    day_diet_food = models.ForeignKey('food_payment.OrderDayDietFoodLog', related_name='survey_relations',
+                                      on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'diet_survey_dietfoodsurveyrelation'
+
+
+class FoodProductSurveyRelation(models.Model):
+    survey = models.OneToOneField(DietOrderSurvey, related_name='food_product_relation', on_delete=models.CASCADE)
+    food_product = models.ForeignKey('food.FoodProduct', related_name='survey_relations', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'diet_survey_foodproductsurveyrelation'
