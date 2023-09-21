c6d5229127f4505b01ed457c891951250c14f768
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:00 +0000

    Add package waving-hands/0.1.0/waving_hands-0.1.0.tar.gz

---
+++ packages/waving-hands/waving_hands-0.1.0.tar.gz/waving_hands-0.1.0/README.md
@@ -0,0 +1,24 @@
+# WavingHands
+P2P Multiplayer Python implementation of Richard Bartle's Waving Hands (very WIP and buggy right now!
+
+This code was originally written in 2020 and I've learned a lot since then.
+
+### Development
+
+Setup your preferred python environment first:
+
+    python -m venv venv
+    source venv/bin/activate
+
+Then do a developer install. This will give you the `waving-hands` command you can use to start the client/server:
+
+    python setup.py develop
+    waving-hands
+
+
+### Testing
+
+Use pytest for testing:
+
+    pip install pytest
+    pytest
---
+++ packages/waving-hands/waving_hands-0.1.0.tar.gz/waving_hands-0.1.0/setup.py
@@ -0,0 +1,35 @@
+import os
+import setuptools
+
+with open("README.md") as f:
+    readme = f.read()
+
+setuptools.setup(
+    name="waving_hands",
+    version="0.1.0",
+    author="Alan Bailey",
+    author_email="",
+    description=(
+        "P2P Multiplayer Python implementation of Richard Bartle's Waving Hands"
+    ),
+    long_description=readme,
+    long_description_content_type="text/plain",
+    license="GNUv3",
+    keywords="waving hands turnbased",
+    url="https://github.com/alanb33/WavingHands",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": ["waving-hands = waving_hands.waving_hands:main",],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Framework :: Pytest",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Games/Entertainment :: Turn Based Strategy"
+    ],
+)
