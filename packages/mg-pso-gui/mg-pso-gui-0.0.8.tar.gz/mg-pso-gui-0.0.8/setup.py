351b8233550f513e9486731a02433d8f3124c189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 14:06:51 +0000

    Add package mg-pso-gui/0.0.8/mg-pso-gui-0.0.8.tar.gz

---
+++ packages/mg-pso-gui/mg-pso-gui-0.0.8.tar.gz/mg-pso-gui-0.0.8/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.8'
+DESCRIPTION = 'GUI for MG-PSO'
+LONG_DESCRIPTION = 'GUI for MG-PSO'
+
+# Setting up
+setup(
+    name="mg-pso-gui",
+    version=VERSION,
+    author="Robert Cordingly",
+    author_email="<rcording@uw.ed>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    package_data={
+        'mgpsogui': ['*.json', '*.png', 'images/*.png'],
+    },
+    include_package_data=True,
+    install_requires=['pandas', 
+                      'numpy', 
+                      'requests',
+                      'tk',
+                      'customtkinter',
+                      'plotly',
+                      'csip',
+                      'csip-cosu'],
+    keywords=['python', 'muti-group', 'pso', 'particle', 'swarm', 'optimization', 'gui'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
