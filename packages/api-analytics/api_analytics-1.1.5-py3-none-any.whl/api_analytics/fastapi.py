430a2bde9ab8f4004ba1284e53b2d1dee34d1988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:53 +0000

    Add package api-analytics/1.1.5/api_analytics-1.1.5-py3-none-any.whl

---
+++ packages/api-analytics/api_analytics-1.1.5-py3-none-any.whl/api_analytics/fastapi.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from time import time
+
+from api_analytics.core import log_request
+from starlette.middleware.base import (BaseHTTPMiddleware,
+                                       RequestResponseEndpoint)
+from starlette.requests import Request
+from starlette.responses import Response
+from starlette.types import ASGIApp
+
+
+class Analytics(BaseHTTPMiddleware):
+    def __init__(self, app: ASGIApp, api_key: str):
+        super().__init__(app)
+        self.api_key = api_key
+
+    async def dispatch(self, request: Request, call_next: RequestResponseEndpoint) -> Response:
+        start = time()
+        response = await call_next(request)
+
+        request_data = {
+            'hostname': request.url.hostname,
+            'ip_address': request.client.host,
+            'path': request.url.path,
+            'user_agent': request.headers['user-agent'],
+            'method': request.method,
+            'status': response.status_code,
+            'response_time': int((time() - start) * 1000),
+            'created_at': datetime.now().isoformat(),
+        }
+
+        log_request(self.api_key, request_data, 'FastAPI')
+        return response
