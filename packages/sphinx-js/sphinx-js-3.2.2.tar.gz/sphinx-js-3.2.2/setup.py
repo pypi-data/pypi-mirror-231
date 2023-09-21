272e959a670b39de5e9166477dea0c1306804b0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:07:20 +0000

    Add package sphinx-js/3.2.2/sphinx-js-3.2.2.tar.gz

---
+++ packages/sphinx-js/sphinx-js-3.2.2.tar.gz/sphinx-js-3.2.2/setup.py
@@ -0,0 +1,41 @@
+from io import open
+from setuptools import setup, find_packages
+
+
+setup(
+    name='sphinx-js',
+    version='3.2.2',
+    description='Support for using Sphinx on JSDoc-documented JS code',
+    long_description=open('README.rst', 'r', encoding='utf8').read(),
+    long_description_content_type="text/x-rst",
+    author='Erik Rose',
+    author_email='erikrose@grinchcentral.com',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup']),
+    url='https://github.com/mozilla/sphinx-js',
+    include_package_data=True,
+    install_requires=[
+        'Jinja2>2.0',
+        'parsimonious>=0.10.0,<0.11.0',
+        'Sphinx>=4.1.0',
+        # Pin markupsafe because of
+        # https://github.com/pallets/jinja/issues/1585
+        'markupsafe==2.0.1',
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        'Framework :: Sphinx :: Extension',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Software Development :: Documentation'
+    ],
+    keywords=['sphinx', 'documentation', 'docs', 'javascript', 'js', 'jsdoc', 'restructured', 'typescript', 'typedoc'],
+)
