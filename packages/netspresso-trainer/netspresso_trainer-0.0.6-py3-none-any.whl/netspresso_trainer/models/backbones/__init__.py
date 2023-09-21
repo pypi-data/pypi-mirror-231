203613463637d4a67a55f3de9e00d2b632602f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:05:48 +0000

    Add package netspresso-trainer/0.0.6/netspresso_trainer-0.0.6-py3-none-any.whl

---
+++ packages/netspresso-trainer/netspresso_trainer-0.0.6-py3-none-any.whl/netspresso_trainer/models/backbones/__init__.py
@@ -0,0 +1,6 @@
+# from .core import *
+from .experimental.efficientformer import efficientformer
+from .experimental.mobilevit import mobilevit
+from .experimental.resnet import resnet50
+from .experimental.segformer import segformer
+from .experimental.vit import vit
