dd2a73e13fdc691c67ca21f6434b2f77c3c859d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 03:08:55 +0000

    Add package pygwalker/0.3.7a0/pygwalker-0.3.7a0-py3-none-any.whl

---
+++ packages/pygwalker/pygwalker-0.3.7a0-py3-none-any.whl/pygwalker/templates/dist/dataSource/index.d.ts
@@ -0,0 +1,13 @@
+import type { IDataSourceProps } from "../interfaces";
+import type { IRow, IDataQueryPayload } from "@kanaries/graphic-walker/dist/interfaces";
+interface ICommPostDataMessage {
+    dataSourceId: string;
+    data?: IRow[];
+    total: number;
+    curIndex: number;
+}
+export declare function loadDataSource(props: IDataSourceProps): Promise<IRow[]>;
+export declare function postDataService(msg: ICommPostDataMessage): void;
+export declare function finishDataService(msg: any): void;
+export declare function getDatasFromKernel(payload: IDataQueryPayload): Promise<IRow[]>;
+export {};
