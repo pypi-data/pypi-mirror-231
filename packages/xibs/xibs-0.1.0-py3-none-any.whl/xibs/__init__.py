e8c31484acc4153bc25bb49f596616672937ce45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 04:08:20 +0000

    Add package xibs/0.1.0/xibs-0.1.0-py3-none-any.whl

---
+++ packages/xibs/xibs-0.1.0-py3-none-any.whl/xibs/__init__.py
@@ -0,0 +1,14 @@
+"""
+Exposes TfsDataFrame, read and write directly in tfs namespace, as well as the package version.
+"""
+from . import version
+
+__title__ = "xibs"
+__description__ = "Prototype Intra-Beam Scattering implementation for Xsuite."
+__url__ = "https://github.com/fsoubelet/xibs"
+__version__ = version.VERSION
+__author__ = "Felix Soubelet"
+__author_email__ = "felix.soubelet@cern.ch"
+__license__ = "MIT"
+
+# TODO: decide what to expose as top-level
