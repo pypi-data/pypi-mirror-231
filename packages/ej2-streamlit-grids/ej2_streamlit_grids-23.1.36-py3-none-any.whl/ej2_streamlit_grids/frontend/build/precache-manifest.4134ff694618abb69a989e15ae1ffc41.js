94a5359f79c3ad4911c7a1883080842075ca2d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 16:08:26 +0000

    Add package ej2-streamlit-grids/23.1.36/ej2_streamlit_grids-23.1.36-py3-none-any.whl

---
+++ packages/ej2-streamlit-grids/ej2_streamlit_grids-23.1.36-py3-none-any.whl/ej2_streamlit_grids/frontend/build/precache-manifest.4134ff694618abb69a989e15ae1ffc41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "168d195a1bac92979a11653aa63f3fbf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc06b51a0083359259f1",
+    "url": "./static/js/2.48fdf914.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.48fdf914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd0a916c77b83587238",
+    "url": "./static/js/main.21470ba7.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
