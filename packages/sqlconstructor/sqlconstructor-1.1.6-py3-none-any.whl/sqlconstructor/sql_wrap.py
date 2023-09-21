32a3fe42760f53e7475351374117690773b3e9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 15:05:17 +0000

    Add package sqlconstructor/1.1.6/sqlconstructor-1.1.6-py3-none-any.whl

---
+++ packages/sqlconstructor/sqlconstructor-1.1.6-py3-none-any.whl/sqlconstructor/sql_wrap.py
@@ -0,0 +1,45 @@
+# coding=utf-8
+"""
+Module of SqlFilter class.
+"""
+
+__author__ = 'https://github.com/akvilary'
+
+from .sql_container import SqlContainer
+from .utils.classes.filter_operator_manager import FilterOperatorManager
+from .utils.classes.string_convertible import StringConvertible
+from .utils.classes.container_convertible import ContainerConvertible
+from .utils.wrap_text import get_wrapped
+
+
+class SqlWrap(
+    FilterOperatorManager,
+    StringConvertible,
+    ContainerConvertible,
+):
+    """The class is invented to do wrapping of sql text more easier"""
+
+    def __init__(
+        self,
+        text: str | StringConvertible,
+        wrapper_text: str | StringConvertible = '',
+    ):
+        """Wrap text by parentheses and add wrapper_text after them.
+        wrapper_text could be empty string (in that case you get text wrapped only by parentheses).
+        Params:
+            - text: str - text to be wrapped
+            - wrapper_text: str - string to be added after parentheses enclosing "text" argument.
+        """
+        self.text = text
+        self.wrapper_text = wrapper_text
+
+    def __str__(self):
+        return get_wrapped(self.text, self.wrapper_text)
+
+    def inline(self) -> SqlContainer:
+        """Get container of wrapped sql text in inline"""
+        return SqlContainer(self.text, self.wrapper_text).wrap(multiline=False)
+
+    def multiline(self) -> SqlContainer:
+        """Get container of wrapped sql text in multiline"""
+        return SqlContainer(self.text, self.wrapper_text).wrap()
