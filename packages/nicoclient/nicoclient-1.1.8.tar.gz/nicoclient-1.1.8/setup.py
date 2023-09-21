21537243512f072cd738b3fd13d00c3e907d2f68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 16:07:52 +0000

    Add package nicoclient/1.1.8/nicoclient-1.1.8.tar.gz

---
+++ packages/nicoclient/nicoclient-1.1.8.tar.gz/nicoclient-1.1.8/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nicoclient",
+    version="v1.1.8",
+    author="Jaeseo Park",
+    author_email="jaeseopark@icloud.com",
+    description="A python client to interact with nicovideo.jp",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jaeseopark/nicoclient",
+    packages=setuptools.find_packages(exclude=["tests", "tests.*"]),
+    install_requires=['requests', 'beautifulsoup4', 'python-dateutil', 'pydantic'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Natural Language :: English"
+    ]
+)
