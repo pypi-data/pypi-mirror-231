2c13d478ef9090c30aa20c5985856d27ed45a81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-21 14:06:41 +0000

    Add package scian-ciiu/0.0.11/scian_ciiu-0.0.11.tar.gz

---
+++ packages/scian-ciiu/scian_ciiu-0.0.11.tar.gz/scian_ciiu-0.0.11/README.md
@@ -0,0 +1,2 @@
+# SCIAN-CIIU
+A library to search the corresponding mexican scian/ciiu given an input parameter
---
+++ packages/scian-ciiu/scian_ciiu-0.0.11.tar.gz/scian_ciiu-0.0.11/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("app/README.md") as f:
+    long_description = f.read()
+
+setup(
+    name = "scian_ciiu",
+    version = "0.0.11",
+    description = "A library to search the corresponding mexican scian or ciiu given an input parameter",
+    package_dir = {"":"app"},
+    packages = find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    author="Dimitrj Bonansea",
+    author_email="dimitrj22@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)
