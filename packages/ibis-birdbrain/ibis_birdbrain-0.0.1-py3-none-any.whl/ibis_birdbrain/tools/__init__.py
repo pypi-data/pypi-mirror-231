9d547c984692e68fc89613a4e5a09cc7626da454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 05:06:37 +0000

    Add package ibis-birdbrain/0.0.1/ibis_birdbrain-0.0.1-py3-none-any.whl

---
+++ packages/ibis-birdbrain/ibis_birdbrain-0.0.1-py3-none-any.whl/ibis_birdbrain/tools/__init__.py
@@ -0,0 +1,48 @@
+# imports
+from marvin.tools import tool
+
+from ibis_birdbrain.tools.internet import (
+    open_browser,
+    search_internet,
+    webpage_to_str,
+)
+from ibis_birdbrain.tools.text import summarize_text, translate_text
+from ibis_birdbrain.tools.code import text_to_python, fix_python_error, run_python_code
+from ibis_birdbrain.tools.filesystem import read_file, list_files_and_dirs, write_file
+from ibis_birdbrain.tools.ibis_birdbrain import list_tables, query_table, get_table_schema
+from ibis_birdbrain.tools.advanced import (
+    read_files_and_summarize,
+    read_webpage_and_summarize,
+)
+from ibis_birdbrain.tools.github import use_github_cli
+
+
+# tools
+tools = [
+    # internet
+    open_browser,
+    search_internet,
+    webpage_to_str,
+    # text
+    summarize_text,
+    translate_text,
+    # filesystem
+    read_file,
+    list_files_and_dirs,
+    write_file,
+    # code
+    text_to_python,
+    fix_python_error,
+    run_python_code,
+    # data
+    list_tables,
+    query_table,
+    get_table_schema,
+    # advanced
+    read_files_and_summarize,
+    read_webpage_and_summarize,
+    # github
+    # use_github_cli,
+]
+
+__all__ = ["tool", "tools"]
