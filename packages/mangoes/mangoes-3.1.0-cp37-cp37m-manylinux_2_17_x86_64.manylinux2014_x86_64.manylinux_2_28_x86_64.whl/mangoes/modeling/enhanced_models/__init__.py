e7f0262f5977a8de6fcacad9408acc4fd2613753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:08:47 +0000

    Add package mangoes/3.1.0/mangoes-3.1.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl

---
+++ packages/mangoes/mangoes-3.1.0-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_28_x86_64.whl/mangoes/modeling/enhanced_models/__init__.py
@@ -0,0 +1,5 @@
+from mangoes.modeling.enhanced_models.kepler import *
+from mangoes.modeling.enhanced_models.ernie import *
+from mangoes.modeling.enhanced_models.libert import *
+from mangoes.modeling.enhanced_models.kadapter import *
+from mangoes.modeling.enhanced_models.hck_transformer import *
