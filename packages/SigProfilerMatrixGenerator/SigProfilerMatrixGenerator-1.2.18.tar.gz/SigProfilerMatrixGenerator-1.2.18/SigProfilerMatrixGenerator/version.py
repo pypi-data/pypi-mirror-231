4b368623ea4345000451c7fbd72815e7fcacb41e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:08:03 +0000

    Add package SigProfilerMatrixGenerator/1.2.18/SigProfilerMatrixGenerator-1.2.18.tar.gz

---
+++ packages/SigProfilerMatrixGenerator/SigProfilerMatrixGenerator-1.2.18.tar.gz/SigProfilerMatrixGenerator-1.2.18/SigProfilerMatrixGenerator/version.py
@@ -0,0 +1,7 @@
+
+# THIS FILE IS GENERATED FROM SIGPROFILEMATRIXGENERATOR SETUP.PY
+short_version = '1.2.18'
+version = '1.2.18'
+Update = 'v1.2.18: Add CLI entry point (view PR #156 for more details)'
+
+	
