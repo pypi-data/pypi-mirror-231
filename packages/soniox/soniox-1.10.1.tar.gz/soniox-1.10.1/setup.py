72ac43da2af13ded6f61c038f05037d454cac816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:07:51 +0000

    Add package soniox/1.10.1/soniox-1.10.1.tar.gz

---
+++ packages/soniox/soniox-1.10.1.tar.gz/soniox-1.10.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="soniox",
+    version="1.10.1",
+    author="Soniox Inc",
+    author_email="support@soniox.com",
+    description="Soniox speech recognition service client library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://soniox.com/",
+    packages=setuptools.find_packages(),
+    package_data={"soniox": ["speech_service_pb2.pyi"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=["protobuf>=3.20.0", "grpcio>=1.46.0", "numpy", "SoundCard"],
+)
