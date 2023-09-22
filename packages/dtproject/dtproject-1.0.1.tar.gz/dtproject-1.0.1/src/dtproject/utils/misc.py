46969a1eadba4d722fe1873f036e7d521f61be5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:10:33 +0000

    Add package dtproject/1.0.1/dtproject-1.0.1.tar.gz

---
+++ packages/dtproject/dtproject-1.0.1.tar.gz/dtproject-1.0.1/src/dtproject/utils/misc.py
@@ -0,0 +1,47 @@
+import re
+import subprocess
+
+from ..constants import DOCKER_LABEL_DOMAIN, CANONICAL_ARCH
+
+
+def run_cmd(cmd):
+    cmd = " ".join(cmd)
+    return [line for line in subprocess.check_output(cmd, shell=True).decode("utf-8").split("\n") if line]
+
+
+def dtlabel(key, value=None):
+    label = f"{DOCKER_LABEL_DOMAIN}.{key.lstrip('.')}"
+    if value is not None:
+        label = f"{label}={value}"
+    return label
+
+
+def git_remote_url_to_https(remote_url):
+    ssh_pattern = "git@([^:]+):([^/]+)/(.+)"
+    res = re.search(ssh_pattern, remote_url, re.IGNORECASE)
+    if res:
+        return f"https://{res.group(1)}/{res.group(2)}/{res.group(3)}"
+    return remote_url
+
+
+def assert_canonical_arch(arch):
+    if arch not in CANONICAL_ARCH.values():
+        raise ValueError(
+            f"Given architecture {arch} is not supported. "
+            f"Valid choices are: {', '.join(list(set(CANONICAL_ARCH.values())))}"
+        )
+
+
+def canonical_arch(arch):
+    if arch not in CANONICAL_ARCH:
+        raise ValueError(
+            f"Given architecture {arch} is not supported. "
+            f"Valid choices are: {', '.join(list(set(CANONICAL_ARCH.values())))}"
+        )
+    # ---
+    return CANONICAL_ARCH[arch]
+
+
+def DEPRECATED(fcn):
+    """This is a decorator that does nothing, it is just used to label functions that are deprecated"""
+    return fcn
