9298a29453609bf1475eda0154f6d3d6aeb602b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 05:06:32 +0000

    Add package pydantic-bind/1.0.1/pydantic_bind-1.0.1.tar.gz

---
+++ packages/pydantic-bind/pydantic_bind-1.0.1.tar.gz/pydantic_bind-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+# ToDo: Add versioneer
+
+setup(name="pydantic_bind",
+      version="1.0.1",
+      description="C++/pybind generation from Pydantic classes",
+      author="Nick Young",
+      license=r"https://www.apache.org/licenses/LICENSE-2.0",
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=[
+          "pydantic>=2.0.2",
+          "pybind11"
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "Programming Language :: Python :: 3.11",
+          "Operating System :: OS Independent",
+          "License :: OSI Approved :: Apache Software License"
+      ])
