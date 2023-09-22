42aecf2e9fb8c2e0f1f70225b9943c549bf9912b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:09:12 +0000

    Add package SpiralFilm/0.1.20/SpiralFilm-0.1.20-py3-none-any.whl

---
+++ packages/SpiralFilm/SpiralFilm-0.1.20-py3-none-any.whl/spiralfilm/evaluator.py
@@ -0,0 +1,12 @@
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+# parserは、出力をパースするためのクラスです。
+class FilmEvaluator:
+    def __init__(
+        self,
+    ):
+        self.format = format
