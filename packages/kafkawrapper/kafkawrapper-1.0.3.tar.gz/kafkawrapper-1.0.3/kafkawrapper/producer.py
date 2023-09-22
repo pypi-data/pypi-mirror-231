560a3dbc8778e02abfc85d2dc911186ec466527d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:09:38 +0000

    Add package kafkawrapper/1.0.3/kafkawrapper-1.0.3.tar.gz

---
+++ packages/kafkawrapper/kafkawrapper-1.0.3.tar.gz/kafkawrapper-1.0.3/kafkawrapper/producer.py
@@ -0,0 +1,31 @@
+import json
+import os
+
+from kafka import KafkaProducer
+from kafka.errors import KafkaTimeoutError
+from dotenv import load_dotenv
+
+load_dotenv()
+
+kafka_brokers = os.environ.get('KAFKA_BROKERS')
+
+def sendData(topic_name, data, metadata = {}):
+    try:
+        error = ''
+        producer = KafkaProducer(bootstrap_servers=kafka_brokers)
+        input = {
+            'data': data,
+            'id': metadata['id'] if 'id' in metadata else '',
+            'source': metadata['trigger_source'] if 'trigger_source' in metadata else 'none'
+        }
+        message_value = json.dumps(input).encode('utf-8')
+        ack = producer.send(topic_name, value=message_value)
+        return ack.get()
+    except KafkaTimeoutError as e:
+        error = f'Timeout error in sending data to kafka topic {topic_name}'
+        raise error
+    except Exception as e:
+        error = f'An error occured in sending data to kafka topic {topic_name}'
+        raise error
+    finally:
+        producer.close()
