331de2ba8fbe9ddc3b02e1495be1242843c733de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:10:38 +0000

    Add package testranunculus/0.15.5/testranunculus-0.15.5.tar.gz

---
+++ packages/testranunculus/testranunculus-0.15.5.tar.gz/testranunculus-0.15.5/testranunculus/__init__.py
@@ -0,0 +1,31 @@
+from . import available_functions
+from . import blueprint_code
+from . import blueprint_text
+from . import documents_management
+from . import file_management
+from . import function_calling_spec_maker
+from . import function_management
+from . import helpers
+from . import llm_functions
+from . import menu
+from . import project_management
+from . import project_management_data
+from . import prompts
+from . import requirements_with_chat
+from . import session_state_management
+from . import simple_auth
+
+import sys
+import os
+from pathlib import Path
+
+# Get the directory containing your main.py
+main_dir = Path(__file__).parent.absolute()
+
+# Add the 'plugins' directory to the Python path
+sys.path.append(os.path.join(main_dir, 'plugins'))
+
+from testranunculus.main import main
+
+if __name__ == "__main__":
+    main()
