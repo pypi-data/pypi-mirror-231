6f489a098c09b905b6e3a4b9e19c259e2497684d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:09:09 +0000

    Add package pyenigma/0.4.1/pyenigma-0.4.1.tar.gz

---
+++ packages/pyenigma/pyenigma-0.4.1.tar.gz/pyenigma-0.4.1/pyenigma/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+from . import enigma
+from . import rotor
+
+__author__ = "Christophe Goessen, Cedric Bonhomme"
+__version__ = "0.4.0"
+__date__ = "$Date: 2010/01/29 $"
+__revision__ = "$Date: 2020/05/17 $"
+__copyright__ = "Copyright (c) Christophe Goessen, Cedric Bonhomme"
+__license__ = "GPLv3"
