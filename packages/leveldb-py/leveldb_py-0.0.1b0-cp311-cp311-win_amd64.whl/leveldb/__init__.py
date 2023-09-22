ba6af14f89200b7fa8124ac93e5f23a5e95bb717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:09:29 +0000

    Add package leveldb-py/0.0.1b0/leveldb_py-0.0.1b0-cp311-cp311-win_amd64.whl

---
+++ packages/leveldb-py/leveldb_py-0.0.1b0-cp311-cp311-win_amd64.whl/leveldb/__init__.py
@@ -0,0 +1,12 @@
+from ._leveldb import (
+    LevelDB,
+    LevelDBException,
+    LevelDBEncrypted,
+    LevelDBIteratorException,
+    Iterator,
+)
+
+# from . import _version
+
+# __version__ = _version.get_versions()["version"]
+__version__ = "0.0.1a"
