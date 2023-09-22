aee6f2c57d8add5dc16c490108ef1c2894ddb0b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 05:07:27 +0000

    Add package mymlpackage/1.0.2/mymlpackage-1.0.2-py3-none-any.whl

---
+++ packages/mymlpackage/mymlpackage-1.0.2-py3-none-any.whl/mymlpackage/__init__.py
@@ -0,0 +1,7 @@
+"""mymlpackage - init module - A collection of data science functions"""
+from .PCA import PCA
+from .svd import svd
+from .t_sne import estimate_sne
+from .k_medoids import KMedoids
+from .k_means import KMeans
+from .categorical_scatter import categorical_scatter_2d
