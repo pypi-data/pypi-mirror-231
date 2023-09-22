a45241f2f8741b8fbd717e11c9f1fc4a8d4fc493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 05:07:25 +0000

    Add package msnpdf/1.1/msnpdf-1.1-py3-none-any.whl

---
+++ packages/msnpdf/msnpdf-1.1-py3-none-any.whl/msnpdf/pdf2text.py
@@ -0,0 +1,21 @@
+"""
+This module provides functions to convert a PDF
+"""
+
+class Converter:
+    """
+        A simple convertor to convert PDFs to text
+    """
+
+    def convert(self, path):
+        """
+        Converts the given PDF to text
+
+        Paramters:
+        path (str): The path to PDF file
+
+        Returns:
+        str: The content of the PDF file as text
+        """
+
+        print("pdf")
