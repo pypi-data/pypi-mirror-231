06c37494e32a75c6dd461623aad8d9d2a9a34d3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 04:08:10 +0000

    Add package emptydataremover/0.4.0/emptydataremover-0.4.0.tar.gz

---
+++ packages/emptydataremover/emptydataremover-0.4.0.tar.gz/emptydataremover-0.4.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'emptydataremover',
+    packages = find_packages(),
+    version = '0.4.0',
+    description = 'This project detects and deletes non-labelled images',
+    author = 'yusufesat',
+    author_email = 'yusuf.esat0@hotmail.com',
+    url = 'https://github.com/yusufesatt/empty-data-remover',
+    keywords = ['empty data remover', 'non labelled images remover'],
+    classifiers = [],
+)
