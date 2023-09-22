729271b54ffab00e83e117d394c0eaae9e6c7da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 04:07:21 +0000

    Add package kafkawrapper/1.0.4/kafkawrapper-1.0.4.tar.gz

---
+++ packages/kafkawrapper/kafkawrapper-1.0.4.tar.gz/kafkawrapper-1.0.4/kafkawrapper/__init__.py
@@ -0,0 +1,8 @@
+# __init__.py
+
+# Define package-level variables
+PACKAGE_VERSION = "1.0.4"
+
+# Import modules to make them accessible when the package is imported
+from .producer import sendData
+
---
+++ packages/kafkawrapper/kafkawrapper-1.0.4.tar.gz/kafkawrapper-1.0.4/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+long_description = """
+kafkawrapper
+================
+
+KafkaWrapper is a Python library designed to simplify Kafka producer setup and data transmission to Kafka topics. This wrapper abstracts the complexities of configuring a Kafka broker and producer, allowing developers to seamlessly integrate Kafka into their projects.
+
+Features
+--------
+
+- kafkawrapper simplifies the process of setting up a Kafka broker and producer, saving you time and effort.
+- asily send data to Kafka topics without the need for extensive Kafka knowledge.
+- Customize your Kafka producer settings to suit your specific use case.
+
+Usage
+-----
+
+To send data to Kafka, follow these steps:
+
+1. Import the `sendData` modules.
+2. Use this module to send data to kafka.
+3. Argument are 
+   kafka_topic: Topic name
+   data: that you want to send to kafka
+   metadata(optional): id - unique id to fetch particular record from kafka
+
+License
+-------
+
+This project is licensed under the MIT License.
+"""
+
+setup(
+    name='kafkawrapper',
+    version='1.0.4',
+    description='A wrapper for Kafka producer',
+    author='Pradeep',
+    author_email='pradeep@incaendo.com',
+    packages=find_packages(),
+    install_requires=[
+        'kafka-python==2.0.2',
+    ],
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/x-rst'
+)
