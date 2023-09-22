3d963a05b59e8d1ca9a040fc73453c613f7984e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:08:02 +0000

    Add package plone.namedfile/6.0.3/plone.namedfile-6.0.3-py3-none-any.whl

---
+++ packages/plone.namedfile/plone.namedfile-6.0.3-py3-none-any.whl/plone/namedfile/tests/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+
+def getFile(filename):
+    """return contents of the file with the given name"""
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    with open(filename, "rb") as data_file:
+        return data_file.read()
