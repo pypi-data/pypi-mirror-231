498e2af13f9e347869a5bf908e49fb0dd82c07d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:10:41 +0000

    Add package ms-swift/1.1.0/ms-swift-1.1.0.tar.gz

---
+++ packages/ms-swift/ms-swift-1.1.0.tar.gz/ms-swift-1.1.0/swift/trainers/arguments.py
@@ -0,0 +1,25 @@
+# Copyright (c) Alibaba, Inc. and its affiliates.
+
+from dataclasses import dataclass
+
+from transformers.training_args import TrainingArguments as HfTrainingArguments
+from transformers.training_args_seq2seq import \
+    Seq2SeqTrainingArguments as HfSeq2SeqTrainingArguments
+
+
+@dataclass
+class SwiftArgumentsMixin:
+    # ckpt only save model
+    only_save_model: bool = False
+    train_sampler_random: bool = True
+
+
+@dataclass
+class TrainingArguments(SwiftArgumentsMixin, HfTrainingArguments):
+    pass
+
+
+@dataclass
+class Seq2SeqTrainingArguments(SwiftArgumentsMixin,
+                               HfSeq2SeqTrainingArguments):
+    pass
