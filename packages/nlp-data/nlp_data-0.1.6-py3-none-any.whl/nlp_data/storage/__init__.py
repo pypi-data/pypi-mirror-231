69640a41dae881bf2b49b717d54ff5389d3e6d41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:08:27 +0000

    Add package nlp-data/0.1.6/nlp_data-0.1.6-py3-none-any.whl

---
+++ packages/nlp-data/nlp_data-0.1.6-py3-none-any.whl/nlp_data/storage/__init__.py
@@ -0,0 +1,7 @@
+from .api import APIDocStore
+from .nlu import NLUDocStore
+from .embdding import EmbeddingDocStore
+from .dialogue import DialogueDocStore
+
+
+__all__ = ["APIDocStore", "NLUDocStore", "EmbeddingDocStore", "DialogueDocStore"]
