30de9bbee013a0153daee6a73c76fdc915e52763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:07:30 +0000

    Add package eclib/1.5.4/eclib-1.5.4.tar.gz

---
+++ packages/eclib/eclib-1.5.4.tar.gz/eclib-1.5.4/README.md
@@ -0,0 +1,53 @@
+<img src="https://github.com/KaoruTeranishi/EncryptedControl/blob/master/logo.png?raw=true" align="center" width="400" alt="header pic"/>
+
+# ECLib
+
+This is a Python library for numerical simulation of encrypted control.
+
+# Encrypted control
+
+Cybersecurity is a critical issue of networked control systems in a modern society.
+Encrypted control is a novel concept of control using cryptographic tools for secure computation, such as homomorphic encryption and secret sharing.
+ECLib helps researchers and students to implement their new idea of encrypted control using homomorphic encryption.
+
+# Supported encryption schemes
+- ElGamal<sup> [1]</sup>
+- Dynamic-key ElGamal<sup> [2]</sup>
+- Paillier<sup> [3]</sup>
+- Regev (LWE)<sup> [4]</sup>
+- GSW<sup> [5]</sup>
+- GSW-LWE<sup> [6]</sup>
+
+# Installation
+
+Run pip command on your terminal.
+
+`pip install eclib`
+
+# Usage
+
+See [tutorial_slide_1.pdf](https://github.com/KaoruTeranishi/EncryptedControl/blob/master/doc/tutorial_slide_1.pdf), [tutorial_slide_2.pdf](https://github.com/KaoruTeranishi/EncryptedControl/blob/master/doc/tutorial_slide_2.pdf), and [example codes](https://github.com/KaoruTeranishi/EncryptedControl/tree/master/examples).
+
+# License
+
+BSD License 2.0
+
+# Author
+
+Kaoru Teranishi
+- E-mail: teranishi (at) uec.ac.jp
+- Homepage: [https://kaoruteranishi.xyz](https://kaoruteranishi.xyz)
+
+# References
+
+[1] T. Elgamal, "A public key cryptosystem and a signature scheme based on discrete logarithms," IEEE Transactions on Information Theory, vol. 31, no. 4, pp. 469–472, 1985.
+
+[2] K. Teranishi, T. Sadamoto, A. Chakrabortty, and K. Kogiso, "Designing optimal key lengths and control laws for encrypted control systems based on sample identifying complexity and deciphering time," IEEE Transactions on Automatic Control (Early access)
+
+[3] P. Paillier, "Public-key cryptosystems based on composite degree residuosity classes," in International Conference on Theory and Application of Cryptographic Techniques, 1999, pp. 223–238.
+
+[4] O. Regev, "On lattices, learning with errors, random linear codes, and cryptography," Journal of the ACM, vol. 56, no. 6, pp. 1-40, 2009.
+
+[5] C. Gentry, A. Sahai, and B. Waters, "Homomorphic encryption from learning with errors: Conceptually-simpler, asymptotically-faster, attribute-based," Cryptology ePrint Archive, Paper 2013/340, 2013.
+
+[6] J. Kim, H. Shim, and K. Han, "Dynamic controller that operates over homomorphically encrypted data for infinite time horizon," IEEE Transactions on Automatic Control, vol. 68, no. 2, pp. 660–672, 2023.
---
+++ packages/eclib/eclib-1.5.4.tar.gz/eclib-1.5.4/eclib/regev.py
@@ -0,0 +1,260 @@
+#! /usr/bin/env python
+
+from eclib.numutils import *
+from eclib.randutils import *
+from eclib.primeutils import *
+from eclib.modutils import *
+from collections import namedtuple
+import numpy as np
+from math import floor, ceil, log2
+
+def keygen(n, t, q, sigma, m=None):
+    params = namedtuple('Parameters', ['n', 't', 'q', 'sigma', 'm'])
+    params.n = n
+    params.t = t
+    params.q = q
+    params.sigma = sigma
+    if m == None:
+        params.m = 2 * params.n * ceil(log2(params.q))
+    else:
+        params.m = m
+
+    A = np.array([[get_rand(0, params.q) for _ in range(params.m)] for _ in range(params.n)], dtype=object)
+    s = np.array([[get_rand(0, params.q)] for _ in range(params.n)], dtype=object)
+    e = np.array(get_int_gaussian(0, sigma, params.m), dtype=object).reshape(-1, 1)
+
+    pk = np.block([[(s.T @ A + e.T) % params.q], [A]])
+    sk = np.block([1, -s.T])
+
+    return params, pk, sk
+
+def encrypt(params, pk, m):
+    if isinstance(m, np.matrix) or isinstance(m, list):
+        m = np.array(m)
+
+    # scalar
+    if isinstance(m, int):
+        return _encrypt(params, pk, m)
+    # vector
+    elif isinstance(m[0], int):
+        c = np.zeros(m.shape, dtype=object)
+        for i in range(c.shape[0]):
+            c[i] = _encrypt(params, pk, m[i])
+        return c
+    # matrix
+    elif isinstance(m[0][0], int):
+        c = np.zeros(m.shape, dtype=object)
+        for i in range(c.shape[0]):
+            for j in range(c.shape[1]):
+                c[i][j] = _encrypt(params, pk, m[i][j])
+        return c
+    else:
+        print('error: encryption')
+        return None
+
+def decrypt(params, sk, c):
+    if isinstance(c, np.matrix) or isinstance(c, list):
+        c = np.array(c)
+
+    # scalar
+    if isinstance(c[0][0], int):
+        return _decrypt(params, sk, c)
+    # vector
+    elif isinstance(c[0][0][0], int):
+        m = np.zeros(c.shape, dtype=object)
+        for i in range(m.shape[0]):
+            m[i] = _decrypt(params, sk, c[i])
+        return m
+    # matrix
+    elif isinstance(c[0][0][0][0], int):
+        m = np.zeros(c.shape, dtype=object)
+        for i in range(m.shape[0]):
+            for j in range(m.shape[1]):
+                m[i][j] = _decrypt(params, sk, c[i][j])
+        return m
+    else:
+        print('error: decryption')
+        return None
+
+def add(params, c1, c2):
+    if isinstance(c1, np.matrix) or isinstance(c1, list):
+        c1 = np.array(c1)
+    if isinstance(c2, np.matrix) or isinstance(c2, list):
+        c2 = np.array(c2)
+
+    # scalar + scalar
+    if isinstance(c1[0][0], int) and isinstance(c2[0][0], int):
+        return _add(params, c1, c2)
+    # vector + vector
+    elif isinstance(c1[0][0][0], int) and isinstance(c2[0][0][0], int) and c1.shape == c2.shape:
+        c = np.zeros(c1.shape, dtype=object)
+        for i in range(c.shape[0]):
+            c[i] = _add(params, c1[i], c2[i])
+        return c
+    # matrix + matrix
+    elif isinstance(c1[0][0][0][0], int) and isinstance(c2[0][0][0][0], int) and c1.shape == c2.shape:
+        c = np.zeros(c1.shape, dtype=object)
+        for i in range(c.shape[0]):
+            for j in range(c.shape[1]):
+                c[i][j] = _add(params, c1[i][j], c2[i][j])
+        return c
+    else:
+        print('error: addition')
+        return None
+
+def elementwise_add(params, c1, c2):
+    if isinstance(c1, np.matrix) or isinstance(c1, list):
+        c1 = np.array(c1)
+    if isinstance(c2, np.matrix) or isinstance(c2, list):
+        c2 = np.array(c2)
+
+    # scalar + scalar
+    if isinstance(c1[0][0], int) and isinstance(c2[0][0], int):
+        return add(params, c1, c2)
+    # vector + vector
+    elif isinstance(c1[0][0][0], int) and isinstance(c2[0][0][0], int) and c1.shape == c2.shape:
+        return add(params, c1, c2)
+    # matrix + vector
+    elif isinstance(c1[0][0][0][0], int) and isinstance(c2[0][0][0], int) and c1.shape[1] == c2.shape[0]:
+        c = np.zeros(c1.shape, dtype=object)
+        for i in range(c.shape[0]):
+            for j in range(c.shape[1]):
+                c[i][j] = _add(params, c1[i][j], c2[j])
+        return c
+    # matrix + matrix
+    elif isinstance(c1[0][0][0][0], int) and isinstance(c2[0][0][0][0], int) and c1.shape == c2.shape:
+        return add(params, c1, c2)
+    else:
+        print('error: elementwise addtion')
+        return None
+
+def int_mult(params, m, c):
+    if isinstance(m, np.matrix) or isinstance(m, list):
+        m = np.array(m)
+    if isinstance(c, np.matrix) or isinstance(c, list):
+        c = np.array(c)
+
+    # scalar (plaintext) x scalar (ciphertext)
+    if isinstance(m, int) and isinstance(c[0][0], int):
+        return _int_mult(params, m, c)
+    # scalar (plaintext) x vector (ciphertext)
+    elif isinstance(m, int) and isinstance(c[0][0][0], int):
+        c_ = np.zeros(c.shape, dtype=object)
+        for i in range(c_.shape[0]):
+            c_[i] = _int_mult(params, m, c[i])
+        return c_
+    # scalar (plaintext) x matrix (ciphertext)
+    elif isinstance(m, int) and isinstance(c[0][0][0][0], int):
+        c_ = np.zeros(c.shape, dtype=object)
+        for i in range(c_.shape[0]):
+            for j in range(c_.shape[1]):
+                c_[i][j] = _int_mult(params, m, c[i][j])
+        return c_
+    # vector (plaintext) x vector (ciphertext)
+    elif isinstance(m[0], int) and isinstance(c[0][0][0], int) and m.shape == c.shape:
+        c_ = np.zeros(1, dtype=object)
+        for i in range(m.shape[0]):
+            c_ = _add(params, c_, _int_mult(params, m[i], c[i]))
+        return c_
+    # matrix (plaintext) x vector (ciphertext)
+    elif isinstance(m[0][0], int) and isinstance(c[0][0][0], int) and m.shape[1] == c.shape[0]:
+        c_ = np.zeros(m.shape[0], dtype=object)
+        for i in range(m.shape[0]):
+            for j in range(m.shape[1]):
+                c_[i] = _add(params, c_[i], _int_mult(params, m[i][j], c[j]))
+        return c_
+    # matrix (plaintext) x matrix (ciphertext)
+    elif isinstance(m[0][0], int) and isinstance(c[0][0][0][0], int) and m.shape[1] == c.shape[0]:
+        c_ = np.zeros([m.shape[0], c.shape[1]], dtype=object)
+        for i in range(m.shape[0]):
+            for j in range(c.shape[1]):
+                for k in range(m.shape[1]):
+                    c_[i][j] = _add(params, c_[i][j], _int_mult(params, m[i][k], c[k][j]))
+        return c_
+    else:
+        print('error: integer multiplication')
+        return None
+
+def elementwise_int_mult(params, m, c):
+    if isinstance(m, np.matrix) or isinstance(m, list):
+        m = np.array(m)
+    if isinstance(c, np.matrix) or isinstance(c, list):
+        c = np.array(c)
+
+    # scalar (plaintext) x scalar (ciphertext)
+    if isinstance(m, int) and isinstance(c[0][0], int):
+        return int_mult(params, m, c)
+    # scalar (plaintext) x vector (ciphertext)
+    elif isinstance(m, int) and isinstance(c[0][0][0], int):
+        return int_mult(params, m, c)
+    # scalar (plaintext) x matrix (ciphertext)
+    elif isinstance(m, int) and isinstance(c[0][0][0][0], int):
+        return int_mult(params, m, c)
+    # vector (plaintext) x vector (ciphertext)
+    elif isinstance(m[0], int) and isinstance(c[0][0][0], int) and m.shape == c.shape:
+        c_ = np.zeros(c.shape, dtype=object)
+        for i in range(c_.shape[0]):
+            c_[i] = _int_mult(params, m[i], c[i])
+        return c_
+    # matrix (plaintext) x vector (ciphertext)
+    elif isinstance(m[0][0], int) and isinstance(c[0][0][0], int) and m.shape[1] == c.shape[0]:
+        c_ = np.zeros(m.shape, dtype=object)
+        for i in range(c_.shape[0]):
+            for j in range(c_.shape[1]):
+                c_[i][j] = _int_mult(params, m[i][j], c[j])
+        return c_
+    # matrix (plaintext) x matrix (ciphertext)
+    elif isinstance(m[0][0], int) and isinstance(c[0][0][0][0], int) and m.shape == c.shape:
+        c_ = np.zeros(c.shape, dtype=object)
+        for i in range(c_.shape[0]):
+            for j in range(c_.shape[1]):
+                c_[i][j] = _int_mult(params, m[i][j], c[i][j])
+        return c_
+    else:
+        print('error: elementwise integer multiplication')
+        return None
+
+def encode(params, x, delta):
+    f = np.frompyfunc(_encode, 3, 1)
+    return f(params, x, delta)
+
+def decode(params, m, delta):
+    f = np.frompyfunc(_decode, 3, 1)
+    return f(params, m, delta)
+
+def enc(params, pk, x, delta):
+    return encrypt(params, pk, encode(params, x, delta))
+
+def dec(params, sk, c, delta):
+    return decode(params, decrypt(params, sk, c), delta)
+
+def _encrypt(params, pk, m):
+    r = np.array([[get_rand(0, 2)] for _ in range(params.m)], dtype=object)
+    return (pk @ r + floor(params.q / params.t) * m * np.block([[1], [np.zeros([params.n, 1], dtype=object)]])) % params.q
+
+def _decrypt(params, sk, c):
+    return floor((params.t / params.q) * ((sk @ c)[0][0] % params.q) + 0.5) % params.t
+
+def _add(params, c1, c2):
+    return (c1 + c2) % params.q
+
+def _int_mult(params, m, c):
+    return (m * c) % params.q
+
+def _encode(params, x, delta):
+    m = floor(x / delta + 0.5)
+
+    if m < 0:
+        if m < -((params.t - 1) // 2):
+            print('error: underflow')
+            return None
+        else:
+            m += params.t
+    elif m > (params.t // 2):
+        print('error: overflow')
+        return None
+
+    return m
+
+def _decode(params, m, delta):
+    return min_residue(m, params.t) * delta
