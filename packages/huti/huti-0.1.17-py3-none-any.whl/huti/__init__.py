83d6f5ba755d5fa7db681dfa2e9baa5ccb11ff17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:08:04 +0000

    Add package huti/0.1.17/huti-0.1.17-py3-none-any.whl

---
+++ packages/huti/huti-0.1.17-py3-none-any.whl/huti/__init__.py
@@ -0,0 +1,39 @@
+"""huti - Python helpers and utilities"""
+import huti.alias
+import huti.classes
+import huti.color
+import huti.constants
+import huti.datas
+import huti.enums
+import huti.exceptions
+import huti.functions
+import huti.meta
+import huti.path
+import huti.typings
+import huti.variables
+from huti.alias import *
+from huti.classes import *
+from huti.color import *
+from huti.constants import *
+from huti.datas import *
+from huti.enums import *
+from huti.exceptions import *
+from huti.functions import *
+from huti.meta import *
+from huti.path import *
+from huti.typings import *
+from huti.variables import *
+
+__all__ = \
+    huti.alias.__all__ + \
+    huti.classes.__all__ + \
+    huti.color.__all__ + \
+    huti.constants.__all__ + \
+    huti.datas.__all__ + \
+    huti.enums.__all__ + \
+    huti.exceptions.__all__ + \
+    huti.functions.__all__ + \
+    huti.meta.__all__ + \
+    huti.path.__all__ + \
+    huti.typings.__all__ + \
+    huti.variables.__all__
