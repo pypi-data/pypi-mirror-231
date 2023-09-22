3790c1dfba1a112379ec8b17e66cada2d75e8722
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:06:43 +0000

    Add package apifoncier/0.0.25/apifoncier-0.0.25-py3-none-any.whl

---
+++ packages/apifoncier/apifoncier-0.0.25-py3-none-any.whl/apifoncier/config.py
@@ -0,0 +1,23 @@
+CONFIG_INIT = {
+    "BASE_URL": "https://apidf-preprod.cerema.fr",
+    "TOKEN": None,
+    "PROXY": None,
+    "PROGRESS_BAR": True,
+    "MAX_ATTEMPTS": 3,
+}
+CONFIG = CONFIG_INIT.copy()
+
+
+def configure(**kwargs):
+    global CONFIG
+    CONFIG.update(kwargs)
+
+
+def get_param(value):
+    global CONFIG
+    return CONFIG.get(value)
+
+
+def reset():
+    global CONFIG
+    CONFIG = CONFIG_INIT.copy()
