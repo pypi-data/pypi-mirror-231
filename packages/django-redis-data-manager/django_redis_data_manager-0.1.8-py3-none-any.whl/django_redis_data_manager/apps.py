497d891d06cdc951bcdd09d9f72fdf8f1baf9e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:09:05 +0000

    Add package django-redis-data-manager/0.1.8/django_redis_data_manager-0.1.8-py3-none-any.whl

---
+++ packages/django-redis-data-manager/django_redis_data_manager-0.1.8-py3-none-any.whl/django_redis_data_manager/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
+
+class DjangoRedisDataManagerConfig(AppConfig):
+    name = "django_redis_data_manager"
+    verbose_name = _("Django Redis Data Manager")
