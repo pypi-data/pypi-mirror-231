33d747828487e41cf758c0bd59769b173920b6c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 04:08:02 +0000

    Add package flygym/0.1.1/flygym-0.1.1.tar.gz

---
+++ packages/flygym/flygym-0.1.1.tar.gz/flygym-0.1.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="flygym",
+    version="0.1.1",
+    author="Neuroengineering Laboratory, EPFL",
+    author_email="sibo.wang@epfl.ch",
+    description="Gym environments for NeuroMechFly in various physics simulators",
+    packages=find_packages(),
+    package_data={"flygym": ["data/*"]},
+    include_package_data=True,
+    python_requires=">=3.7",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    install_requires=[
+        "gymnasium",
+        "numpy",
+        "scipy",
+        "pyyaml",
+        "jupyter",
+        "mediapy",
+        "imageio",
+        "imageio[pyav]",
+        "imageio[ffmpeg]",
+        "tqdm",
+    ],
+    extras_require={
+        "mujoco": ["mujoco", "dm_control", "numba"],
+        "pybullet": ["pybullet"],
+        "dev": [
+            "sphinx",
+            "furo",
+            "numpydoc",
+            "pytest",
+            "ruff",
+            "black==23.3.0",
+            "black[jupyter]",
+            "shapely",
+            "rasterio",
+            "requests",
+            "opencv-python",
+        ],
+    },
+)
