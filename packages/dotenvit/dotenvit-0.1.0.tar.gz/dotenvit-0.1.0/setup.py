068a5894808bd8cb06d50b614ae4630379d63a85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 05:06:09 +0000

    Add package dotenvit/0.1.0/dotenvit-0.1.0.tar.gz

---
+++ packages/dotenvit/dotenvit-0.1.0.tar.gz/dotenvit-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dotenvit',
+    version='0.1.0',
+    description='A simple key store .env manager damnit',
+    url='',
+    author='plusleft',
+    packages=find_packages(),
+)
