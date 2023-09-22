42ef47c61e45ec08c42de663b3a627889608f399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:10:03 +0000

    Add package usfm-grammar/3.0.0b2/usfm_grammar-3.0.0b2-cp310-cp310-musllinux_1_1_x86_64.whl

---
+++ packages/usfm-grammar/usfm_grammar-3.0.0b2-cp310-cp310-musllinux_1_1_x86_64.whl/usfm_grammar/__init__.py
@@ -0,0 +1,9 @@
+'''Entry point of the package with its public values'''
+
+from usfm_grammar import usfm_parser
+
+Filter = usfm_parser.Filter
+Format = usfm_parser.Format
+USFMParser = usfm_parser.USFMParser
+
+__version__ = "3.0.0-beta.2"
