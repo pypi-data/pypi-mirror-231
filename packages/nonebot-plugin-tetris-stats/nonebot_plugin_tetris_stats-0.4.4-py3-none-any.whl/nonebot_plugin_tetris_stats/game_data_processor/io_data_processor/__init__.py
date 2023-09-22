2c08ac82f83a9d86561996bc68bff5b29c6f4ca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 04:08:19 +0000

    Add package nonebot-plugin-tetris-stats/0.4.4/nonebot_plugin_tetris_stats-0.4.4-py3-none-any.whl

---
+++ packages/nonebot-plugin-tetris-stats/nonebot_plugin_tetris_stats-0.4.4-py3-none-any.whl/nonebot_plugin_tetris_stats/game_data_processor/io_data_processor/__init__.py
@@ -0,0 +1,40 @@
+from re import I
+
+from nonebot import on_regex
+from nonebot.adapters.onebot.v11 import GROUP, MessageEvent
+from nonebot.matcher import Matcher
+
+from .processor import Processor
+
+IOBind = on_regex(pattern=r'^io绑定|^iobind', flags=I, permission=GROUP)
+IOStats = on_regex(pattern=r'^io查|^iostats', flags=I, permission=GROUP)
+IORank = on_regex(pattern=r'^io段位|^iorank', flags=I, permission=GROUP)
+
+@IOBind.handle()
+async def _(event: MessageEvent, matcher: Matcher):
+    await matcher.finish(
+        await Processor.handle_bind(
+            message=event.raw_message,
+            qq_number=event.sender.user_id
+        )
+    )
+
+
+@IOStats.handle()
+async def _(event: MessageEvent, matcher: Matcher):
+    if event.is_tome():
+        await matcher.finish('不能查询bot的信息')
+    await matcher.finish(
+        await Processor.handle_query(
+            message=event.raw_message,
+            qq_number=event.sender.user_id
+        )
+    )
+
+@IORank.handle()
+async def _(event: MessageEvent, matcher: Matcher):
+    await matcher.finish(
+        await Processor.handle_rank(
+            message=event.raw_message
+        )
+    )
