b14d7300f636c1049cf4588a2778b497d978fc17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:09:23 +0000

    Add package square-pass3/0.0.5/square-pass3-0.0.5.tar.gz

---
+++ packages/square-pass3/square-pass3-0.0.5.tar.gz/square-pass3-0.0.5/sqpass/main.py
@@ -0,0 +1,236 @@
+from getpass import getpass
+from sqpass.passwordManager import base
+from sqpass.passwordManager import argaction
+from sqpass.passwordManager.ciphers import hashuser, encrypt, decrypt
+from sqpass.passwordManager.cache import set_with_ttl, CACHE_DIR
+from sqpass.passwordManager.tools import is_process_running
+from sqpass.passwordManager.parser import run_parser
+from sqpass.passwordManager.base import readpass
+
+
+from os import urandom
+
+from subprocess import Popen
+
+import pickle
+
+pwdwrong = [
+    "Sorry that's not correct!",
+    "Not even close!",
+    "Nice Try!",
+    "Hold on and give the correct password!",
+]
+
+services = ["passw", "notes", "keys"]
+
+
+class UserArgManager:
+    def __init__(self, args):
+        self.userInp = None
+        self.db = None
+        self.args = args
+
+    def setup(self):
+        data = None
+        if os.path.exists(CACHE_DIR):
+            with open(CACHE_DIR, 'rb') as file:
+                data = pickle.load(file)
+        if data:
+            salt, iv, enc, rand_byte = data['upass'][0]
+            upass = decrypt(salt, iv, enc, rand_byte).decode()
+            self.userInp = upass
+            self.db = base.DatabaseManager(
+                self.userInp, hashuser(self.userInp))
+        else:
+            userpass = getpass(prompt="Enter Masterpass: ")
+            hashed_pass = hashuser(userpass)
+            while True:
+                if hashed_pass != readpass():
+                    userpass = getpass(prompt="Try again: ")
+                    hashed_pass = hashuser(userpass)
+                else:
+                    self.userInp = userpass
+                    break
+            self.db = base.DatabaseManager(
+                self.userInp, hashuser(self.userInp))
+
+    def show_usage(self, show_services=True, usage_msg=None, example=None, show_fields=False, arg=None):
+        print("")
+        if show_services:
+            print(f"Available Services -[ {' | '.join(services)} ]")
+        if usage_msg is not None:
+            print(f"usage: sq {arg} {usage_msg}")
+        if example is not None:
+            print(f"eg: sq {arg} {example}")
+        if show_fields:
+            pass
+        print("")
+
+    def handle_insert(self, passw=False, keypass=False, note=False):
+        if passw:
+            self.db.insert()
+        if keypass:
+            mode = False if self.args.normal else True
+            if self.args.keypass == "None":
+                self.db.keyins(silent=mode)
+            else:
+                self.db.keyins(self.args.keypass, silent=mode)
+        if note:
+            if note == "None":
+                self.db.noteins()
+            else:
+                self.db.noteins(self.args.note)
+
+    def generate_password(self):
+        passw = argaction.generate_password(self.args.generate)
+        if self.args.copy:
+            argaction.copy_to_clipboard(passw)
+        print(passw)
+
+    def handle_retrieve_data(self):
+        try:
+            idx = int(self.args.cat.split("/")[1])
+            service = self.args.cat.split("/")[0]
+            if service == "notes":
+                self.db.view_notes(noteid=idx)
+            elif service == "keys":
+                self.db.view_keys(keyid=idx)
+            elif service == "passw":
+                self.db.view_userpasses(userid=idx)
+            else:
+                print("Availables: ", " | ".join(services))
+        except (IndexError, ValueError):
+            print("Usage: sq -cat service/index")
+
+    def handle_count(self):
+        try:
+            mode = True if self.args.ignorecase else False
+            service = self.args.count[0].split("/")[0]
+            field = self.args.count[0].split("/")[1]
+            self.db.count(icase=mode, table=service,
+                          column=field, cred=self.args.count[1])
+        except Exception as err:
+            print(err)
+
+    def handle_showlist(self):
+        sort = "date" if self.args.recent else "id"
+        order = "DESC" if self.args.recent else "ASC"
+        mode = True if self.args.ignorecase else False
+        if self.args.username:
+            self.db.filter(icase=mode, username=self.args.username)
+        elif self.args.appname:
+            self.db.filter(icase=mode, appname=self.args.appname)
+        else:
+            if self.args.showlist == "passw":
+                self.db.view_userpasses(sort=sort, order=order)
+            elif self.args.showlist == "notes":
+                self.db.view_notes(sort=sort, order=order)
+
+            elif self.args.showlist == "keys":
+                self.db.view_keys(sort=sort, order=order)
+
+            else:
+                self.show_usage(arg="--ls", example="passw",
+                                usage_msg="<service>")
+
+    def handle_remove(self):
+        try:
+            service_name = self.args.remove.split("/")[0]
+            ind = int(self.args.remove.split("/")[1])
+            try:
+                self.db.remove_cd(table=service_name, u_id=ind)
+            except KeyError:
+                print("Available args:", " | ".join(services))
+        except IndexError:
+            print("Usage: sq --rm service/index")
+
+    def handle_bigbang(self):
+        if self.args.bigbang == "boom":
+            self.db.bigbang(boom=True)
+
+        elif self.args.bigbang == "passw":
+            self.db.bigbang(userpass=True)
+        elif self.args.bigbang == "keys":
+            self.db.bigbang(keys=True)
+        elif self.args.bigbang == "notes":
+            self.db.bigbang(notes=True)
+        else:
+            print("Available args: \nboom | passw | keys | notes")
+
+    def handle_update(self):
+        usage = r"sq -U <service>/<index>"
+        eg = "sq -U passw/2"
+        try:
+            service_name = self.args.update.split("/")[0]
+            index = int(self.args.update.split("/")[1])
+            if service_name not in services:
+                self.show_usage(show_services=True,
+                                usage_msg=usage, example=eg)
+                return
+            try:
+                self.db.update(table=service_name, id=index)
+            except Exception as e:
+                print(e)
+                print("Availables: \nusers | keys")
+
+        except Exception as e:
+            self.show_usage(show_services=True, usage_msg=usage, example=eg)
+
+    def handle_export(self):
+        try:
+            service_name = self.args.export[0]
+            fileform = self.args.export[1]
+        except IndexError:
+            fileform = "json"
+        if fileform == "csv":
+            self.db.export(service=service_name, csv=True)
+        elif fileform == "json":
+            self.db.export(service=service_name, json=True)
+
+    def handle_login(self):
+        if self.args.login:
+            cache_time = input("Time duration to remember <minutes> : ")
+            while True:
+                if not cache_time.isdigit():
+                    cache_time = input("Please Enter minutes in number: ")
+                else:
+                    break
+            rand_byte = urandom(16)
+            salt, iv, enc = encrypt(self.userInp.encode('utf-8'), rand_byte)
+            set_with_ttl('upass', [salt, iv, enc, rand_byte], int(cache_time))
+            cmd = ['python', 'observer.py']
+            if not is_process_running(cmd):
+                Popen(['python', 'observer.py'],
+                      start_new_session=True)
+
+    def initial_setup(self):
+        pass
+
+    def handle_functions(self):
+        actions = {
+            "update": self.handle_update,
+            "export": self.handle_export,
+            "bigbang": self.handle_bigbang,
+            "remove": self.handle_remove,
+            "showlist": self.handle_showlist,
+            "cat": self.handle_retrieve_data,
+            "note": lambda: self.handle_insert(note=self.args.note),
+            "passw": lambda: self.handle_insert(passw=True),
+            "keypass": lambda: self.handle_insert(keypass=True),
+            "generate": self.generate_password,
+            "login": self.handle_login,
+        }
+
+        for arg, action in actions.items():
+            if getattr(self.args, arg):
+                action()
+
+
+def main():
+    argmanager = UserArgManager(args=run_parser())
+    argmanager.setup()
+    argmanager.handle_functions()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/square-pass3/square-pass3-0.0.5.tar.gz/square-pass3-0.0.5/sqpass/passwordManager/base.py
@@ -0,0 +1,345 @@
+from sqpass.passwordManager.ciphers import encrypt, decrypt
+from sqpass.passwordManager import tools
+
+from prettytable import PrettyTable
+from contextlib import contextmanager
+import sqlite3
+import getpass
+import sys
+import os
+import base64
+import platform
+import json
+
+
+home_dir = os.path.expanduser("~")
+db_path = os.path.join(home_dir, ".local", "share", "pass.db")
+
+
+def readpass():
+    paths = {
+        'Linux': os.path.expanduser("~/.local/share/pass.key"),
+        'Windows': os.path.expanduser("~\\AppData\\pass.key")
+    }
+    pathname = paths.get(platform.system())
+    if not os.path.exists(pathname):
+        os.system('sq-init')
+    else:
+        with open(pathname, "r") as file:
+            return file.read()
+
+
+class DatabaseManager:
+    """ Class Doc Goes Here"""
+
+    def __init__(self, MasterPass, cryptedpass):
+        self.User_Masterpass = readpass()
+        check_pass = True if self.User_Masterpass == cryptedpass else False
+        self.cryptedpass = cryptedpass if check_pass else None
+        self.MasterPass = MasterPass if check_pass else None
+        self.db = db_path
+        if not check_pass:
+            print("Not matched")
+        try:
+            self.termlines = os.get_terminal_size().columns
+        except Exception:
+            self.termlines = 100
+
+    @contextmanager
+    def opendb(self):
+        conn = sqlite3.connect(self.db)
+        cur = conn.cursor()
+        try:
+            yield cur
+        except (sqlite3.DatabaseError, sqlite3.OperationalError) as e:
+            print("No Database Found")
+        finally:
+            conn.commit()
+            conn.close()
+
+    @contextmanager
+    def dbfetch(self, query, data=None):
+        with self.opendb() as cur:
+            try:
+                if data is None:
+                    cur.execute(query)
+                else:
+                    cur.execute(query, data)
+                yield cur.fetchall()
+            except sqlite3.DatabaseError as e:
+                raise
+            finally:
+                cur.close()
+
+    def update(self, table='passw', id=None):
+        local_pass = self.MasterPass
+        if self.cryptedpass == self.User_Masterpass:
+            if table == "passw":
+                lst = ["app_name", "username", "passwd"]
+                lstinp = ["App Name: ", "Username: ", "Password: "]
+            else:
+                lst = ["title", "key"]
+                lstinp = ["Title: ", "Key: "]
+            userinp = []
+
+            for idxlst, inp in enumerate(lst):
+                if inp == "passwd":
+                    # inpass = getpass.getpass(lstinp[idxlst])
+                    salt, iv, enc = encrypt(getpass.getpass("Password: ").encode(
+                        'utf-8'), local_pass.encode('utf-8'))
+                    enc = json.dumps({'salt': salt, 'iv': iv, 'enc': enc})
+                else:
+                    enc = input(lstinp[idxlst])
+                userinp.append(enc)
+
+            for idx, inp in enumerate(userinp):
+                if inp != "":
+                    try:
+                        with self.opendb() as cur:
+                            update_query = f"""UPDATE '{table}'
+                                                SET '{lst[idx]}' = '{inp}'
+                                                WHERE id={id};"""
+                            cur.execute(update_query)
+                    except sqlite3.Error as err:
+                        print(err)
+                else:
+                    pass
+
+    def count(self, icase=False, table=None, column=None, cred=None):
+        if self.cryptedpass != self.User_Masterpass:
+            return
+
+        if column == "passwd":
+            passlist = self._get_decrypted_passwords()
+            self._print_passwords(passlist, cred)
+        else:
+            readquery = "SELECT COUNT(*) FROM {} WHERE {} = '{}';"
+            if icase:
+                readquery = "SELECT COUNT(*) FROM {} WHERE {} = '{}' COLLATE NOCASE;"
+            with self.dbfetch(readquery.format(table, column, cred)) as row:
+                print(row[0][0])
+
+    def _get_decrypted_passwords(self):
+        local_pass = self.MasterPass
+        passlist = []
+        with self.dbfetch("SELECT username, app_name, passwd FROM passw;") as row:
+            for col in row:
+                data = json.loads(col[2])
+                salt, iv, ct = data.values()
+                decipher = decrypt(salt, iv, ct, local_pass.encode()).decode()
+                passlist.append(
+                    {'Username': col[0], 'App': col[1], 'Pass': decipher})
+        return passlist
+
+    def _print_passwords(self, passlist, cred):
+        headers = ['Username', 'App Name', 'Password']
+        t = PrettyTable(headers)
+        count = 0
+        for passw in passlist:
+            print()
+            if cred == passw['Pass']:
+                count += 1
+                t.add_row([passw['Username'], passw['App'], passw['Pass']])
+        col = ['' for _ in range(count-1)]
+        t.add_column("Count", [str(count), *col])
+        print(t)
+
+    def filter(self, icase=False, username=None, appname=None, state="OR"):
+        if username is None and appname is None:
+            print("Please Define A Service Parameter")
+        elif username is not None or appname is not None:
+            local_pass = self.MasterPass
+            if icase:
+                readquery = """SELECT * FROM passw WHERE username = '{}' COLLATE NOCASE {} app_name = '{}' COLLATE NOCASE;"""
+            else:
+                readquery = """SELECT * FROM passw WHERE username = '{}' {} app_name = '{}';"""
+            with self.dbfetch(readquery.format(username, state, appname)) as row:
+                tools.print_box(row, local_pass)
+
+    def view_notes(self, sort="id", order="ASC", markdown=False, noteid=None):
+        if self.cryptedpass == self.User_Masterpass:
+            if noteid is None:
+                readquery = f"SELECT * FROM notes ORDER BY {sort} {order};"
+            else:
+                readquery = f"SELECT * FROM notes WHERE id={noteid};"
+            if self.cryptedpass == self.User_Masterpass:
+                with self.dbfetch(readquery) as row:
+                    if noteid is None:
+                        notes = PrettyTable()
+                        notes.field_names = ["Index", "Title", "Content"]
+                        for idx, title, cont, dtime in row:
+                            notes.add_row(
+                                [idx, title, base64.b64decode(cont).decode()[:10]+"..."])
+                        print(notes)
+
+                    else:
+                        try:
+                            title = row[0][1]
+                            content = (
+                                (base64.b64decode(row[0][2])).decode()).strip()
+                            subtitle = row[0][3]
+
+                            tools.print_note(content, title, subtitle)
+                        except IndexError:
+                            print("Not Available")
+
+    def view_keys(self, sort="id", order="ASC", keyid=None, export=False):
+        local_pass = self.MasterPass
+        if keyid is None:
+            readquery = f"SELECT * FROM keys ORDER BY {sort} {order};"
+        else:
+            readquery = f"SELECT * FROM keys WHERE id = {keyid};"
+        if self.cryptedpass == self.User_Masterpass:
+            with self.dbfetch(readquery) as row:
+                if len(row) < 1:
+                    print("Not Available")
+                else:
+                    keys = PrettyTable()
+                    keys.field_names = ["Index", "Title", "Key"]
+                    for r in row:
+                        data = json.loads(r[2])
+                        salt, iv, ct = data.values()
+                        decipher = decrypt(
+                            salt, iv, ct, local_pass.encode()).decode()
+                        keys.add_row([r[0], r[1], decipher])
+                    if export:
+                        return keys
+                    print(keys)
+
+    def view_userpasses(self, sort="id", order="ASC", userid=None, countpass=False, export=False):
+        if userid is None:
+            readquery = f"SELECT * FROM passw ORDER BY {sort} {order};"
+        else:
+            readquery = f"SELECT * FROM passw WHERE id = {userid};"
+        local_pass = self.MasterPass
+        if self.cryptedpass == self.User_Masterpass:
+            with self.dbfetch(readquery) as row:
+                content_table = tools.print_box(row, local_pass)
+                if export:
+                    return content_table
+                print(content_table)
+
+    def insert(self, app_name=None, user_name=None):  # Pylint: disable=W0613
+        insert_query = """INSERT INTO passw (app_name,username, passwd)
+                            VALUES (:appname ,:u_name,:passw)"""
+        local_pass = self.MasterPass
+        if self.cryptedpass == self.User_Masterpass:
+            app_name = input("App Name:")
+            u_name = input("Username: ")
+            salt, iv, enc = encrypt(getpass.getpass("Password: ").encode(
+                'utf-8'), local_pass.encode('utf-8'))
+            data = json.dumps({'salt': salt, 'iv': iv, 'enc': enc})
+
+            try:
+                with self.opendb() as cur:
+                    cur.execute(insert_query, {
+                        "appname": app_name,
+                        "u_name": u_name,
+                        "passw": data
+                    }
+                    )
+                print(f"[+]Successfully Added for {u_name}")
+            except sqlite3.Error as error:
+                print("Failed to Insert Into Database", error)
+
+    def keyins(self, title=None, silent=True):  # Pylint: disable=W0613
+        insert_query = """INSERT INTO keys (title, key)
+                            VALUES (:key_title ,:key_pass)"""
+        local_pass = self.MasterPass
+        if self.cryptedpass == self.User_Masterpass:
+            if title is None:
+                title = input("Title: ")
+            if silent:
+                salt, iv, enc = encrypt(getpass.getpass(
+                    "Key: ").encode(), local_pass.encode())
+                data = json.dumps({'salt': salt, 'iv': iv, 'enc': enc})
+            else:
+                salt, iv, enc = encrypt(
+                    input("Key: ").encode(), local_pass.encode())
+                data = json.dumps({'salt': salt, 'iv': iv, 'enc': enc})
+            try:
+                with self.connection:
+                    self.cur.execute(insert_query, {
+                        "key_title": title,
+                        "key_pass": data
+                    }
+                    )
+                print()
+                print(f"[+]Stored Key for {title}")
+            except sqlite3.Error as error:
+                print("Failed to Insert Into Database", error)
+
+    def noteins(self, title=None):  # Pylint: disable=W0613
+        insert_query = """INSERT INTO notes (title,content, date)
+                            VALUES (:note_title ,:note_content, datetime())"""
+        if self.cryptedpass == self.User_Masterpass:
+            if title is None:
+                title = input("Title:")
+            centext = "BODY"
+            save_text = " [CTRL + D] "
+            discard = " [CTRL + C] "
+
+            eq = (self.termlines//2)-(len(save_text+centext+discard)//2)
+            last_put = " "*(eq)+centext+" "*(eq)
+            print("_"*self.termlines)
+            print(discard, end="")
+            print(last_put, end="")
+            print(save_text, end="")
+            print("-"*self.termlines)
+
+            note_content = "".join(sys.stdin.readlines())
+            contb64 = base64.b64encode(note_content.encode())
+            try:
+                with self.connection:
+                    self.cur.execute(insert_query, {
+                        "note_title": title,
+                        "note_content": contb64,
+                    }
+                    )
+                print()
+                print(f"[+]Note Added")
+            except sqlite3.Error as error:
+                print("Failed to Insert Into Database", error)
+
+    def remove_cd(self, table=None, u_id=None):
+
+        query = f"DELETE FROM {table} WHERE id={u_id}"
+
+        with self.connection:
+            self.cur.execute(query)
+
+    def bigbang(self, boom=False, userpass=None, keys=None, notes=None):
+        master_pass = self.MasterPass
+        if boom:
+            userpass = True
+            keys = True
+            notes = True
+        query = """DELETE FROM {}"""
+        if userpass is not None:
+            with self.connection:
+                self.cur.execute(query.format("passw"))
+        if keys is not None:
+            with self.connection:
+                self.cur.execute(query.format("keys"))
+        if notes is not None:
+            with self.connection:
+                self.cur.execute(query.format("notes"))
+
+    def export(self, service=None, json=False, csv=False):
+        backup = ""
+        ext = ".json" if json else ".csv"
+        if self.cryptedpass == self.User_Masterpass:
+            if service == "passw":
+                rawbac = self.view_userpasses(export=True)
+                backup = rawbac.get_json_string() if json else rawbac.get_csv_string()
+
+            elif service == "keys":
+                rawbac = self.view_keys(export=True)
+                backup = rawbac.get_json_string() if json else rawbac.get_csv_string()
+            else:
+                print("service name was not found")
+            print(backup)
+            with open("backup"+ext, "w") as f:
+                for line in backup:
+                    if line != "\n":
+                        f.write(line)
