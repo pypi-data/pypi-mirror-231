89097f64a48459b764d9b611b6dccf7aa14a893b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 04:08:09 +0000

    Add package uproot/5.0.12/uproot-5.0.12.tar.gz

---
+++ packages/uproot/uproot-5.0.12.tar.gz/uproot-5.0.12/tests/test_0088-read-with-http.py
@@ -0,0 +1,33 @@
+# BSD 3-Clause License; see https://github.com/scikit-hep/uproot5/blob/main/LICENSE
+
+import pytest
+
+import uproot
+
+
+@pytest.mark.network
+def test_issue176():
+    with uproot.open(
+        "https://starterkit.web.cern.ch/starterkit/data/advanced-python-2019/dalitzdata.root"
+    ) as f:
+        data = f["tree/Y1"].array(library="np")
+        assert len(data) == 100000
+
+
+@pytest.mark.network
+def test_issue176_again():
+    with uproot.open(
+        "https://starterkit.web.cern.ch/starterkit/data/advanced-python-2019/dalitzdata.root"
+    ) as f:
+        data = f["tree"].arrays(["Y1", "Y2"], library="np")
+        assert len(data["Y1"]) == 100000
+        assert len(data["Y2"]) == 100000
+
+
+@pytest.mark.network
+def test_issue121():
+    with uproot.open(
+        "https://github.com/CoffeaTeam/coffea/raw/master/tests/samples/nano_dy.root"
+    ) as f:
+        data = f["Events/MET_pt"].array(library="np")
+        assert len(data) == 40
