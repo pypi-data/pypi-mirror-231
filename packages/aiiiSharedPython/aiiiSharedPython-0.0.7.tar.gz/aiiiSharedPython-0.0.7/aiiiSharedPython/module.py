d8a747a59d6dc9590b8d070494a15ff059e5354d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:07:35 +0000

    Add package aiiiSharedPython/0.0.7/aiiiSharedPython-0.0.7.tar.gz

---
+++ packages/aiiiSharedPython/aiiiSharedPython-0.0.7.tar.gz/aiiiSharedPython-0.0.7/aiiiSharedPython/module.py
@@ -0,0 +1,22 @@
+import requests
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+def echo(value):
+    print(value)
+
+def send_ga_log(client_id: str, events: list, measurement_id: str, api_secret: str):
+    url = f"https://www.google-analytics.com/mp/collect?measurement_id={measurement_id}&api_secret={api_secret}"
+    body = {
+        "client_id": client_id,
+        "events": events
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+    response = requests.post(url, json=body, headers=headers)
+    response.raise_for_status()
+    return response.status_code
