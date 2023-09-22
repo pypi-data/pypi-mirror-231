485fcaf63b1ba0d30fa00c4e9af26870aa64d706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:09:27 +0000

    Add package ckanext-vip-portal/0.2.4/ckanext_vip_portal-0.2.4-py3-none-any.whl

---
+++ packages/ckanext-vip-portal/ckanext_vip_portal-0.2.4-py3-none-any.whl/ckanext/vip_portal/interfaces.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import enum
+from typing import Optional, Union
+from flask import Response
+from ckan.plugins import Interface
+
+
+class Access(enum.Enum):
+    allowed = enum.auto()
+    forbidden = enum.auto()
+    unknown = enum.auto()
+
+
+class IVipPortal(Interface):
+    def check_vip_access_for_endpoint(
+        self,
+        endpoint: Union[tuple[str, str], tuple[None, None]],
+        user: Optional[str],
+    ) -> Access:
+        return Access.unknown
+
+    def check_vip_access_for_path(
+        self, path: str, user: Optional[str]
+    ) -> Access:
+        return Access.unknown
+
+    def make_vip_rejection_response(
+        self, user: Optional[str]
+    ) -> Optional[Response]:
+        return None
+
+    def alter_vip_rejection_response(
+        self, resp: Response, user: Optional[str]
+    ) -> Response:
+        return resp
