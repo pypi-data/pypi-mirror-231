951a75a3eb891ad34c0d73b4d9ee18a5bf7c573d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:09:38 +0000

    Add package panax/0.0.29/panax-0.0.29-py3-none-any.whl

---
+++ packages/panax/panax-0.0.29-py3-none-any.whl/panax/database.py
@@ -0,0 +1,34 @@
+import sys
+import os
+import uuid
+import datetime
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
+from sqlalchemy import Column, String, Integer, DateTime
+
+sys.path.append(os.getcwd())
+from config import APP_SETTING
+
+
+engine = create_engine(APP_SETTING["connection"])
+conn = engine.connect()
+session = Session(engine)
+
+Base = declarative_base(engine)
+
+
+def gen_id():
+    return uuid.uuid4().hex
+
+
+class BaseModel(Base):
+    """基类模型"""
+    __abstract__ = True
+
+    id = Column(String(32), default=gen_id, primary_key=True, comment="主键ID")
+    created = Column(DateTime, nullable=True, default=datetime.datetime.now, comment="创建时间")
+    is_del = Column(Integer, comment="是否删除", default=0)
+
+    def to_dict(self):
+        return {c.name: getattr(self, c.name) for c in self.__table__.columns}
