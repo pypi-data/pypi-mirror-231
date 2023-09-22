d16b37438d490c986aa90e37505dff26c54a8698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:10:38 +0000

    Add package async-redis-uow/0.0.8/async_redis_uow-0.0.8.tar.gz

---
+++ packages/async-redis-uow/async_redis_uow-0.0.8.tar.gz/async_redis_uow-0.0.8/async_redis_uow/repository/paginated.py
@@ -0,0 +1,20 @@
+from typing import Generic, Optional
+from axsqlalchemy.repository.paginated import math
+from redis.commands.json.path import Path
+from .types import TIModel, TOModel
+from .all_getter import AllGetterRepo
+
+
+class PaginatedRepo(AllGetterRepo[TIModel, TOModel], Generic[TIModel, TOModel]):
+    __abstract__ = True
+
+    async def all_page_count(self, filters: str = '', count: Optional[int] = None) -> int:
+        all_count = await self.all_count(filters)
+        return math.ceil(all_count / count) if count else 1 
+
+    async def all_count(self, filters: str = ''):
+        return (await self.session.json().objlen(
+            self.hname, 
+            Path(f'${filters}').strPath,
+        ).execute())[-1][-1]  # type: ignore
+
