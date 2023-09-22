d7911dcabf97fe2f91605bccc536fd3e3ce8e9b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:10:49 +0000

    Add package haianh/0.0.1/haianh-0.0.1.tar.gz

---
+++ packages/haianh/haianh-0.0.1.tar.gz/haianh-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='haianh',
+    version='0.0.1',
+    author='dohaianh',
+    author_email='haianhdo193@gmail.com',
+    description='Setting up a python package',
+    # url='',
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'test']
+    # extras_require={'plotting': ['matplotlib>=2.2.0', 'jupyter']},
+    # setup_requires=['pytest-runner', 'flake8'],
+    # tests_require=['pytest'],
+    # entry_points={
+    #     'console_scripts': ['my-command=exampleproject.example:main']
+    # },
+    # package_data=find_packages()
+)
