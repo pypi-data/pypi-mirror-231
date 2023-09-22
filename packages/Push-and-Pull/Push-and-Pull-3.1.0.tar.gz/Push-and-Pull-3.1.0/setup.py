a51d1d861f313029bcff5c0b2d5de917dca1733e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:10:38 +0000

    Add package Push-and-Pull/3.1.0/Push-and-Pull-3.1.0.tar.gz

---
+++ packages/Push-and-Pull/Push-and-Pull-3.1.0.tar.gz/Push-and-Pull-3.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Push-and-Pull',
+    version='3.1.0',
+    description='Push and Pull algorithm for ARC problem implemented by fun._.alone from GIST DSLab',
+    author='fun._.alone',
+    author_email='limvictor@naver.com',
+    url='https://github.com/teddylee777/teddynote',
+    install_requires=['tqdm', 'pandas', 'scikit-learn',],
+    packages=find_packages(exclude=[]),
+    keywords=['fun._.alone', 'GIST', 'ARC', 'abstraction and reasoning corpus', 'DSLab'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
