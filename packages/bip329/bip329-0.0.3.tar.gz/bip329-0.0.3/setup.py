96ccbe3b91d097266a699b1142ca9db9f56ac391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 04:08:23 +0000

    Add package bip329/0.0.3/bip329-0.0.3.tar.gz

---
+++ packages/bip329/bip329-0.0.3.tar.gz/bip329-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bip329',
+    version='0.0.3',
+    packages=find_packages(),
+    install_requires=[
+        'py7zr',  
+    ],
+)
