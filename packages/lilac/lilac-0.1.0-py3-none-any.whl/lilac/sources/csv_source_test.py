4883773d30f909610f25c5ca3ae0559f77aaccaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:09:09 +0000

    Add package lilac/0.1.0/lilac-0.1.0-py3-none-any.whl

---
+++ packages/lilac/lilac-0.1.0-py3-none-any.whl/lilac/sources/csv_source_test.py
@@ -0,0 +1,42 @@
+"""Tests for the CSV source."""
+import csv
+import os
+import pathlib
+
+from ..schema import schema
+from ..source import SourceSchema
+from .csv_source import LINE_NUMBER_COLUMN, CSVSource
+
+
+def test_csv(tmp_path: pathlib.Path) -> None:
+  csv_rows = [{'x': 1, 'y': 'ten'}, {'x': 2, 'y': 'twenty'}]
+
+  filename = 'test-dataset.csv'
+  filepath = os.path.join(tmp_path, filename)
+  with open(filepath, 'w') as f:
+    writer = csv.DictWriter(f, fieldnames=list(csv_rows[0].keys()))
+    writer.writeheader()
+    writer.writerows(csv_rows)
+
+  source = CSVSource(filepaths=[filepath])
+  source.setup()
+
+  source_schema = source.source_schema()
+  assert source_schema == SourceSchema(
+    fields=schema({
+      LINE_NUMBER_COLUMN: 'int64',
+      'x': 'int64',
+      'y': 'string'
+    }).fields, num_items=2)
+
+  items = list(source.process())
+
+  assert items == [{
+    LINE_NUMBER_COLUMN: 0,
+    'x': 1,
+    'y': 'ten'
+  }, {
+    LINE_NUMBER_COLUMN: 1,
+    'x': 2,
+    'y': 'twenty'
+  }]
---
+++ packages/lilac/lilac-0.1.0-py3-none-any.whl/lilac/web/_app/immutable/chunks/navigation.a92775b7.js
@@ -0,0 +1,1 @@
+import{j as o}from"./singletons.4045da4a.js";const e=o("goto");export{e as g};
