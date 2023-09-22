6e335fa23a0e5bcd57d071895d4b288952af1669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 05:05:14 +0000

    Add package runhouse/0.0.12/runhouse-0.0.12-py3-none-any.whl

---
+++ packages/runhouse/runhouse-0.0.12-py3-none-any.whl/runhouse/main.py
@@ -0,0 +1,166 @@
+import shlex
+import subprocess
+import webbrowser
+from typing import Optional
+
+import typer
+from rich.console import Console
+
+import runhouse.rns.login
+
+from runhouse import __version__, cluster, configs
+
+# create an explicit Typer application
+app = typer.Typer(add_completion=False)
+state = {"verbose": False}
+
+# For printing with typer
+console = Console()
+
+
+@app.command()
+def login(
+    token: Optional[str] = typer.Argument(None, help="Your Runhouse API token"),
+    yes: Optional[bool] = typer.Option(
+        False, "--yes", "-y", help="Sets any confirmations to 'yes' automatically."
+    ),
+):
+    """Login to Runhouse. Validates token provided, with options to upload or download stored secrets or config between
+    local environment and Runhouse / Vault.
+    """
+    valid_token: str = (
+        runhouse.rns.login.login(
+            token=token,
+            download_config=True,
+            upload_config=True,
+            download_secrets=True,
+            upload_secrets=True,
+        )
+        if yes
+        else runhouse.rns.login.login(token=token, interactive=True, ret_token=True)
+    )
+
+    if valid_token:
+        webbrowser.open(f"{configs.get('dashboard_url')}/dashboard?token={valid_token}")
+        raise typer.Exit()
+    else:
+        raise typer.Exit(code=1)
+
+
+@app.command()
+def logout():
+    """Logout of Runhouse. Provides options to delete locally configured secrets and local Runhouse configs"""
+    runhouse.rns.login.logout(interactive=True)
+    raise typer.Exit()
+
+
+@app.command()
+def notebook(
+    cluster_name: str, up: bool = typer.Option(False, help="Start the cluster")
+):
+    """Open a Jupyter notebook on a cluster."""
+    c = cluster(name=cluster_name)
+    if up:
+        c.up_if_not()
+    if not c.is_up():
+        console.print(
+            f"Cluster {cluster_name} is not up. Please run `runhouse notebook {cluster_name} --up`."
+        )
+        raise typer.Exit(1)
+    c.notebook()
+
+
+@app.command()
+def ssh(cluster_name: str, up: bool = typer.Option(False, help="Start the cluster")):
+    """SSH into a cluster created elsewhere (so `ssh cluster` doesn't work out of the box) or not yet up."""
+    c = cluster(name=cluster_name)
+    if up:
+        c.up_if_not()
+    if not c.is_up():
+        console.print(
+            f"Cluster {cluster_name} is not up. Please run `runhouse ssh {cluster_name} --up`."
+        )
+        raise typer.Exit(1)
+    c.ssh()
+
+
+def load_cluster(cluster_name: str):
+    """Load a cluster from RNS into the local environment, e.g. to be able to ssh."""
+    c = cluster(name=cluster_name)
+    if not c.address:
+        c._update_from_sky_status(dryrun=True)
+
+
+def _start_server(restart, restart_ray, screen, create_logfile=True):
+    from runhouse.resources.hardware.cluster import Cluster
+
+    cmds = Cluster._start_server_cmds(
+        restart=restart,
+        restart_ray=restart_ray,
+        screen=screen,
+        create_logfile=create_logfile,
+    )
+
+    try:
+        # We do these one by one so it's more obvious where the error is if there is one
+        for cmd in cmds:
+            console.print(f"Executing `{cmd}`")
+            result = subprocess.run(shlex.split(cmd), text=True)
+            # We don't want to raise an error if the server kill fails, as it may simply not be running
+            if result.returncode != 0 and "pkill" not in cmd:
+                console.print(f"Error while executing `{cmd}`")
+                raise typer.Exit(1)
+    except FileNotFoundError:
+        console.print(
+            "python3 command was not found. Make sure you have python3 installed."
+        )
+        raise typer.Exit(1)
+
+
+@app.command()
+def start(
+    restart_ray: bool = typer.Option(False, help="Restart the Ray runtime"),
+    screen: bool = typer.Option(False, help="Start the server in a screen"),
+):
+    """Start the HTTP server on the cluster."""
+    _start_server(
+        restart=False, restart_ray=restart_ray, screen=screen, create_logfile=True
+    )
+
+
+@app.command()
+def restart(
+    name: str = typer.Option(None, help="A *saved* remote cluster object to restart."),
+    restart_ray: bool = typer.Option(True, help="Restart the Ray runtime"),
+    screen: bool = typer.Option(
+        True,
+        help="Start the server in a screen. Only relevant when restarting locally.",
+    ),
+    resync_rh: bool = typer.Option(
+        False,
+        help="Resync the Runhouse package. Only relevant when restarting remotely.",
+    ),
+):
+    """Restart the HTTP server on the cluster."""
+    if name:
+        c = cluster(name=name)
+        c.restart_server(resync_rh=resync_rh, restart_ray=restart_ray)
+        return
+
+    _start_server(
+        restart=True,
+        restart_ray=restart_ray,
+        screen=screen,
+        create_logfile=True,
+    )
+
+
+@app.callback()
+def main(verbose: bool = False):
+    """
+    Runhouse CLI
+    """
+    if verbose:
+        name = "runhouse"
+        console.print(f"{name}=={__version__}", style="bold green")
+        state["verbose"] = True
---
+++ packages/runhouse/runhouse-0.0.12-py3-none-any.whl/runhouse/servers/http/http_client.py
@@ -0,0 +1,244 @@
+import json
+import logging
+import time
+
+import requests
+
+from runhouse.resources.envs.utils import _get_env_from
+
+from runhouse.resources.resource import Resource
+from runhouse.servers.http.http_utils import handle_response, OutputType, pickle_b64
+
+logger = logging.getLogger(__name__)
+
+
+class HTTPClient:
+    """
+    Client for cluster RPCs
+    """
+
+    DEFAULT_PORT = 50052
+    MAX_MESSAGE_LENGTH = 1 * 1024 * 1024 * 1024  # 1 GB
+    CHECK_TIMEOUT_SEC = 10
+
+    def __init__(self, host, port=DEFAULT_PORT, auth=None):
+        self.host = host
+        self.port = port
+        self.auth = auth
+
+    def request(
+        self,
+        endpoint,
+        req_type="post",
+        data=None,
+        env=None,
+        stream_logs=True,
+        save=False,
+        key=None,
+        err_str=None,
+        timeout=None,
+    ):
+        req_fn = (
+            requests.get
+            if req_type == "get"
+            else requests.put
+            if req_type == "put"
+            else requests.delete
+            if req_type == "delete"
+            else requests.post
+        )
+        endpoint = endpoint.strip("/")
+        endpoint = (endpoint + "/") if "?" not in endpoint else endpoint
+        response = req_fn(
+            f"http://{self.host}:{self.port}/{endpoint}",
+            json={
+                "data": data,
+                "env": env,
+                "stream_logs": stream_logs,
+                "save": save,
+                "key": key,
+            },
+            timeout=timeout,
+            auth=self.auth,
+        )
+        if response.status_code != 200:
+            raise ValueError(
+                f"Error calling {endpoint} on server: {response.content.decode()}"
+            )
+        resp_json = response.json()
+        output_type = resp_json["output_type"]
+        return handle_response(resp_json, output_type, err_str)
+
+    def check_server(self, cluster_config=None):
+        self.request(
+            "check",
+            req_type="post",
+            data=json.dumps(cluster_config, indent=4),
+            timeout=self.CHECK_TIMEOUT_SEC,
+        )
+
+    def call_module_method(
+        self,
+        module_name,
+        method_name,
+        env=None,
+        stream_logs=True,
+        save=False,
+        run_name=None,
+        remote=False,
+        run_async=False,
+        args=None,
+        kwargs=None,
+        system=None,
+    ):
+        """
+        Client function to call the rpc for call_module_method
+        """
+        # Measure the time it takes to send the message
+        start = time.time()
+        logger.info(
+            f"{'Calling' if method_name else 'Getting'} {module_name}"
+            + (f".{method_name}" if method_name else "")
+        )
+        res = requests.post(
+            f"http://{self.host}:{self.port}/{module_name}/{method_name}/",
+            json={
+                "data": pickle_b64([args, kwargs]),
+                "env": env,
+                "stream_logs": stream_logs,
+                "save": save,
+                "key": run_name,
+                "remote": remote,
+                "run_async": run_async,
+            },
+            stream=not run_async,
+        )
+        if res.status_code != 200:
+            raise ValueError(
+                f"Error calling {method_name} on server: {res.content.decode()}"
+            )
+        error_str = f"Error calling {method_name} on {module_name} on server"
+
+        # We get back a stream of intermingled log outputs and results (maybe None, maybe error, maybe single result,
+        # maybe a stream of results), so we need to separate these out.
+        non_generator_result = None
+        res_iter = iter(res.iter_content(chunk_size=None))
+        for responses_json in res_iter:
+            resp = json.loads(responses_json)
+            output_type = resp["output_type"]
+            result = handle_response(resp, output_type, error_str)
+            if output_type in [OutputType.RESULT_STREAM, OutputType.SUCCESS_STREAM]:
+                # First time we encounter a stream result, we know the rest of the results will be a stream, so return
+                # a generator
+                def results_generator():
+                    # If this is supposed to be an empty generator, there's no first result to return
+                    if not output_type == OutputType.SUCCESS_STREAM:
+                        yield result
+                    for responses_json_inner in res_iter:
+                        resp_inner = json.loads(responses_json_inner)
+                        output_type_inner = resp_inner["output_type"]
+                        result_inner = handle_response(
+                            resp_inner, output_type_inner, error_str
+                        )
+                        # if output_type == OutputType.SUCCESS_STREAM:
+                        #     break
+                        if output_type_inner in [
+                            OutputType.RESULT_STREAM,
+                            OutputType.RESULT,
+                        ]:
+                            yield result_inner
+                    end_inner = time.time()
+                    if method_name:
+                        log_str = f"Time to call {module_name}.{method_name}: {round(end_inner - start, 2)} seconds"
+                    else:
+                        log_str = f"Time to get {module_name}: {round(end_inner - start, 2)} seconds"
+                    logging.info(log_str)
+
+                return results_generator()
+            elif output_type == OutputType.CONFIG:
+                # If this was a `.remote` call, we don't need to recreate the system and connection, which can be
+                # slow, we can just set it explicitly.
+                if (
+                    system
+                    and "system" in result
+                    and system.rns_address == result["system"]
+                ):
+                    result["system"] = system
+                non_generator_result = Resource.from_config(result, dryrun=True)
+
+            elif output_type == OutputType.RESULT:
+                # Finish iterating over logs before returning single result
+                non_generator_result = result
+
+        end = time.time()
+        if method_name:
+            log_str = f"Time to call {module_name}.{method_name}: {round(end - start, 2)} seconds"
+        else:
+            log_str = f"Time to get {module_name}: {round(end - start, 2)} seconds"
+        logging.info(log_str)
+        return non_generator_result
+
+    def put_object(self, key, value, env=None):
+        self.request(
+            "object",
+            req_type="post",
+            data=pickle_b64(value),
+            key=key,
+            env=env,
+            err_str=f"Error putting object {key}",
+        )
+
+    def put_resource(self, resource, env=None, state=None, dryrun=False):
+        if env and not isinstance(env, str):
+            env = _get_env_from(env)
+            env = env.name or env.env_name
+        return self.request(
+            "resource",
+            req_type="post",
+            # TODO wire up dryrun properly
+            data=pickle_b64((resource.config_for_rns, state, resource.dryrun)),
+            env=env,
+            err_str=f"Error putting resource {resource.name or type(resource)}",
+        )
+
+    def rename_object(self, old_key, new_key):
+        self.request(
+            "object",
+            req_type="put",
+            data=pickle_b64((old_key, new_key)),
+            err_str=f"Error renaming object {old_key}",
+        )
+
+    def delete(self, keys=None, env=None):
+        return self.request(
+            "object",
+            req_type="delete",
+            data=pickle_b64((keys or [])),
+            env=env,
+            err_str=f"Error deleting keys {keys}",
+        )
+
+    def cancel(self, key, force=False):
+        # Note key can be set to "all" to cancel all runs
+        return self.request(
+            "cancel",
+            req_type="post",
+            data=pickle_b64(force),
+            key=key,
+            err_str=f"Error cancelling runs {key}",
+        )
+
+    def keys(self, env=None):
+        if env is not None and not isinstance(env, str):
+            env = _get_env_from(env)
+            env = env.name
+        return self.request(f"keys/?env={env}" if env else "keys", req_type="get")
+
+    def add_secrets(self, secrets):
+        failed_providers = self.request(
+            "secrets",
+            req_type="post",
+            data=pickle_b64(secrets),
+            err_str="Error sending secrets",
+        )
+        return failed_providers
