8fca81de290db12b0d257472ee767146be510f7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:08:09 +0000

    Add package Mensajes-henry.snow17/6.0/Mensajes_henry.snow17-6.0-py3-none-any.whl

---
+++ packages/Mensajes-henry.snow17/Mensajes_henry.snow17-6.0-py3-none-any.whl/mensajes/adios/despedidas.py
@@ -0,0 +1,6 @@
+def despedir():
+    print("Adios, me despido desde despedidas.despedir()")
+
+class Despedida:
+    def __init__(self):
+        print("Adios, me despdio desde Despedida.__init__()")
