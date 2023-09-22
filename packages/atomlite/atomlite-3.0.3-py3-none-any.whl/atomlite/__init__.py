234b121186cc03f4a2fd4f09e530ec786e27d93c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:09:39 +0000

    Add package atomlite/3.0.3/atomlite-3.0.3-py3-none-any.whl

---
+++ packages/atomlite/atomlite-3.0.3-py3-none-any.whl/atomlite/__init__.py
@@ -0,0 +1,29 @@
+from atomlite._internal.database import (
+    Database,
+    Entry,
+    PropertyEntry,
+)
+from atomlite._internal.json import (
+    AromaticBonds,
+    Bonds,
+    Json,
+    Molecule,
+    json_from_rdkit,
+    json_to_rdkit,
+)
+
+Json = Json
+"""A JSON value."""
+
+
+__all__ = [
+    "Bonds",
+    "AromaticBonds",
+    "Database",
+    "Entry",
+    "PropertyEntry",
+    "Json",
+    "Molecule",
+    "json_to_rdkit",
+    "json_from_rdkit",
+]
