8573cd39120ad7a1db8ba6402baa8035835159fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 05:06:05 +0000

    Add package pki-tools/0.0.18/pki_tools-0.0.18-py3-none-any.whl

---
+++ packages/pki-tools/pki_tools-0.0.18-py3-none-any.whl/pki_tools/exceptions.py
@@ -0,0 +1,30 @@
+class Error(Exception):
+    pass
+
+
+class ExtensionMissing(Error):
+    pass
+
+
+class CertLoadError(Error):
+    pass
+
+
+class OcspFetchFailure(Error):
+    pass
+
+
+class OcspInvalidResponseStatus(Error):
+    pass
+
+
+class OcspIssuerFetchFailure(Error):
+    pass
+
+
+class CrlFetchFailure(Error):
+    pass
+
+
+class CrlLoadError(Error):
+    pass
