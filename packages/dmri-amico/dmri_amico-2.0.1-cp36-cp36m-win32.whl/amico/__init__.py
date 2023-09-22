bd4691a74425354a4dc8e2efde078f4e1f3eaac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 04:07:44 +0000

    Add package dmri-amico/2.0.1/dmri_amico-2.0.1-cp36-cp36m-win32.whl

---
+++ packages/dmri-amico/dmri_amico-2.0.1-cp36-cp36m-win32.whl/amico/__init__.py
@@ -0,0 +1,45 @@
+from __future__ import absolute_import, division, print_function
+
+
+
+# start delvewheel patch
+def _delvewheel_init_patch_1_1_4():
+    import ctypes
+    import os
+    import platform
+    import sys
+    libs_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, 'dmri_amico.libs'))
+    is_pyinstaller = getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS')
+    is_conda_cpython = platform.python_implementation() == 'CPython' and (hasattr(ctypes.pythonapi, 'Anaconda_GetVersion') or 'packaged by conda-forge' in sys.version)
+    if sys.version_info[:2] >= (3, 8) and not is_conda_cpython or sys.version_info[:2] >= (3, 10):
+        if not is_pyinstaller or os.path.isdir(libs_dir):
+            os.add_dll_directory(libs_dir)
+    else:
+        load_order_filepath = os.path.join(libs_dir, '.load-order-dmri_amico-2.0.1')
+        if not is_pyinstaller or os.path.isfile(load_order_filepath):
+            with open(os.path.join(libs_dir, '.load-order-dmri_amico-2.0.1')) as file:
+                load_order = file.read().split()
+            for lib in load_order:
+                lib_path = os.path.join(os.path.join(libs_dir, lib))
+                if not is_pyinstaller or os.path.isfile(lib_path):
+                    ctypes.WinDLL(lib_path)
+
+
+_delvewheel_init_patch_1_1_4()
+del _delvewheel_init_patch_1_1_4
+# end delvewheel patch
+
+
+from .core import Evaluation, setup
+from .util import set_verbose, get_verbose
+from . import core
+from . import scheme
+from . import lut
+from . import models
+from . import util
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
+__version__ = version('dmri-amico')
