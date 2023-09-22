2e4627933c8727bf712cd9b2f31e741d71fbf146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:07:34 +0000

    Add package stdflow/0.0.67/stdflow-0.0.67.tar.gz

---
+++ packages/stdflow/stdflow-0.0.67.tar.gz/stdflow-0.0.67/tests/execution/test_exec.py
@@ -0,0 +1,44 @@
+import os
+import random
+import string
+import unittest
+
+from stdflow.stdflow_utils.execution import run_function, run_notebook, run_python_file
+
+
+class TestEnvExport(unittest.TestCase):
+    def setUp(self):
+        self.env_vars = {
+            f'__stdflow__{"".join(random.choices(string.ascii_letters, k=5))}': str(
+                random.randint(0, 100)
+            )
+            for _ in range(10)
+        }
+        os.environ.update(self.env_vars)
+
+    def test_env_export_notebook(self):
+        run_notebook("tests/execution/env_export_notebook.ipynb", env_vars=self.env_vars)
+        with open("/tmp/env_notebook.txt", "r") as f:
+            content = f.read().splitlines()
+        for var, value in self.env_vars.items():
+            self.assertIn(f"{var}={value}", content)
+
+    def test_env_export_function(self):
+        run_function(
+            "tests/execution/env_export_function.py", "export_env_var", env_vars=self.env_vars
+        )
+        with open("/tmp/env_function.txt", "r") as f:
+            content = f.read().splitlines()
+        for var, value in self.env_vars.items():
+            self.assertIn(f"{var}={value}", content)
+
+    def test_env_export_script(self):
+        run_python_file("tests/execution/env_export_script.py", env_vars=self.env_vars)
+        with open("/tmp/env_script.txt", "r") as f:
+            content = f.read().splitlines()
+        for var, value in self.env_vars.items():
+            self.assertIn(f"{var}={value}", content)
+
+
+if __name__ == "__main__":
+    unittest.main()
