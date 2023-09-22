9e452a1a0e87fb10f122e76b888cadb794f11f96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 15:10:01 +0000

    Add package hungdq97-distributions/0.1/hungdq97_distributions-0.1.tar.gz

---
+++ packages/hungdq97-distributions/hungdq97_distributions-0.1.tar.gz/hungdq97_distributions-0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='hungdq97_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['hungdinh97_distributions'],
+      author_email='hungdinhquang97@gmail.com',
+      zip_safe=False)
