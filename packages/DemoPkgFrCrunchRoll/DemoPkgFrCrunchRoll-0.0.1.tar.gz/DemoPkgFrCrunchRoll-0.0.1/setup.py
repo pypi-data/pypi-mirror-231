16cdaa1974ac80598278083a5e2bc472a254b215
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 04:08:15 +0000

    Add package DemoPkgFrCrunchRoll/0.0.1/DemoPkgFrCrunchRoll-0.0.1.tar.gz

---
+++ packages/DemoPkgFrCrunchRoll/DemoPkgFrCrunchRoll-0.0.1.tar.gz/DemoPkgFrCrunchRoll-0.0.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="DemoPkgFrCrunchRoll",
+    version="0.0.1",
+    author="Ratnakar Kumbhar",
+    author_email="ratnakar.kumbhar@crunchyroll.com",
+    description="Calculate current Month",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
