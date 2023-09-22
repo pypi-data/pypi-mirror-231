b52423f47196f4bd16ee9bfbd33cf935765cbb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:09:09 +0000

    Add package testpypiitest/1.0.0/testpypiitest-1.0.0.tar.gz

---
+++ packages/testpypiitest/testpypiitest-1.0.0.tar.gz/testpypiitest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='testpypiitest',
+    version='1.0.0',
+    description='for test',
+    author='yusufesat',
+    author_email='yusuf.esat0@hotmail.com',
+    packages=find_packages(),
+)
