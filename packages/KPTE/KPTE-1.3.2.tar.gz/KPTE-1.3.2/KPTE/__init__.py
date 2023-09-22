8e496f3c2e1f4a62d24e8854def0342f5b87993e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 04:08:06 +0000

    Add package KPTE/1.3.2/KPTE-1.3.2.tar.gz

---
+++ packages/KPTE/KPTE-1.3.2.tar.gz/KPTE-1.3.2/KPTE/__init__.py
@@ -0,0 +1,7 @@
+# --$KaixinPyToExePackage>1.3.2$-- #
+# Produced by Kaixin(SongXinZhe, 宋昕哲) #
+# Time: 2023.9.21 #
+# Region: China, 中国 #
+# Editor: VSCode #
+# Language: Python==3.8 #
+# Copyright (c) 2023 SongXinZhe #
