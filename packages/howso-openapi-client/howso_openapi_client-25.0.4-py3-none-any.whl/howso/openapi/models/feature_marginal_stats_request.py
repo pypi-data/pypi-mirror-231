997e6de2ee57dd6b28505f28ea7495b7855bf6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:07:38 +0000

    Add package howso-openapi-client/25.0.4/howso_openapi_client-25.0.4-py3-none-any.whl

---
+++ packages/howso-openapi-client/howso_openapi_client-25.0.4-py3-none-any.whl/howso/openapi/models/feature_marginal_stats_request.py
@@ -0,0 +1,137 @@
+# coding: utf-8
+
+"""
+Howso API
+
+OpenAPI implementation for interacting with the Howso API. 
+"""
+
+try:
+    from inspect import getfullargspec
+except ImportError:
+    from inspect import getargspec as getfullargspec
+import pprint
+import re  # noqa: F401
+import six
+
+from howso.openapi.configuration import Configuration
+
+
+class FeatureMarginalStatsRequest(object):
+    """
+    Auto-generated OpenAPI type.
+
+    The body of a feature marginal stats request. 
+    """
+
+    """
+    Attributes:
+      openapi_types (dict): The key is attribute name
+                            and the value is attribute type.
+      attribute_map (dict): The key is attribute name
+                            and the value is json key in definition.
+    """
+    openapi_types = {
+        'weight_feature': 'str'
+    }
+
+    attribute_map = {
+        'weight_feature': 'weight_feature'
+    }
+
+    nullable_attributes = [
+    ]
+
+    discriminator = None
+
+    def __init__(self, weight_feature=None, local_vars_configuration=None):  # noqa: E501
+        """FeatureMarginalStatsRequest - a model defined in OpenAPI"""  # noqa: E501
+        if local_vars_configuration is None:
+            local_vars_configuration = Configuration.get_default_copy()
+        self.local_vars_configuration = local_vars_configuration
+
+        self._weight_feature = None
+
+        if weight_feature is not None:
+            self.weight_feature = weight_feature
+
+    @property
+    def weight_feature(self):
+        """Get the weight_feature of this FeatureMarginalStatsRequest.
+
+        When specified, will attempt to return stats that were computed using this weight_feature. 
+
+        :return: The weight_feature of this FeatureMarginalStatsRequest.
+        :rtype: str
+        """
+        return self._weight_feature
+
+    @weight_feature.setter
+    def weight_feature(self, weight_feature):
+        """Set the weight_feature of this FeatureMarginalStatsRequest.
+
+        When specified, will attempt to return stats that were computed using this weight_feature. 
+
+        :param weight_feature: The weight_feature of this FeatureMarginalStatsRequest.
+        :type weight_feature: str
+        """
+
+        self._weight_feature = weight_feature
+
+    def to_dict(self, serialize=False, exclude_null=False):
+        """Returns the model properties as a dict"""
+        result = {}
+
+        def convert(x):
+            if hasattr(x, "to_dict"):
+                args = getfullargspec(x.to_dict).args
+                if len(args) == 1:
+                    return x.to_dict()
+                elif 'exclude_null' in args:
+                    return x.to_dict(serialize, exclude_null)
+                else:
+                    return x.to_dict(serialize)
+            else:
+                return x
+
+        for attr, _ in six.iteritems(self.openapi_types):
+            value = getattr(self, attr)
+            attr = self.attribute_map.get(attr, attr) if serialize else attr
+            if isinstance(value, list):
+                result[attr] = list(map(
+                    lambda x: convert(x),
+                    value
+                ))
+            elif isinstance(value, dict):
+                result[attr] = dict(map(
+                    lambda item: (item[0], convert(item[1])),
+                    value.items()
+                ))
+            elif value is None and (exclude_null or attr not in self.nullable_attributes):
+                continue
+            else:
+                result[attr] = convert(value)
+
+        return result
+
+    def to_str(self):
+        """Returns the string representation of the model"""
+        return pprint.pformat(self.to_dict())
+
+    def __repr__(self):
+        """For `print` and `pprint`"""
+        return self.to_str()
+
+    def __eq__(self, other):
+        """Returns true if both objects are equal"""
+        if not isinstance(other, FeatureMarginalStatsRequest):
+            return False
+
+        return self.to_dict() == other.to_dict()
+
+    def __ne__(self, other):
+        """Returns true if both objects are not equal"""
+        if not isinstance(other, FeatureMarginalStatsRequest):
+            return True
+
+        return self.to_dict() != other.to_dict()
