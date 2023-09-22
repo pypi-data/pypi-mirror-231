2d5c98572bc964b35ab30c91e719dac756341712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:09:46 +0000

    Add package autoawq/0.1.0/autoawq-0.1.0-cp39-cp39-manylinux2014_x86_64.whl

---
+++ packages/autoawq/autoawq-0.1.0-cp39-cp39-manylinux2014_x86_64.whl/awq/models/__init__.py
@@ -0,0 +1,6 @@
+from .mpt import MptAWQForCausalLM
+from .llama import LlamaAWQForCausalLM
+from .opt import OptAWQForCausalLM
+from .falcon import FalconAWQForCausalLM
+from .bloom import BloomAWQForCausalLM
+from .gptj import GPTJAWQForCausalLM
