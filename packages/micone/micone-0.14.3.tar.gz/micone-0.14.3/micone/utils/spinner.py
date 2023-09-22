a3d68823b48f8789e415a39664163d075c8bafa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:08:12 +0000

    Add package micone/0.14.3/micone-0.14.3.tar.gz

---
+++ packages/micone/micone-0.14.3.tar.gz/micone-0.14.3/micone/utils/spinner.py
@@ -0,0 +1,74 @@
+"""
+    Custom spinner class
+"""
+
+from typing import Optional
+
+from click import echo
+from halo import Halo
+
+
+class Spinner:
+    """
+    A custom spinner class that makes use of `halo` and `click`
+
+    Parameters
+    ----------
+    text : str
+    spinner : str
+
+    Attributes
+    ----------
+    interactive : bool
+        True if the `Spinner` instance is interactive
+    """
+
+    def __init__(self, text: str, spinner: str, interactive: bool = True) -> None:
+        self.interactive = interactive
+        self._text = text
+        if self.interactive:
+            self._spinner = Halo(text=self._text, spinner=spinner)
+        else:
+            self._spinner = None
+
+    @property
+    def text(self) -> str:
+        """The text in the spinner"""
+        return self._text
+
+    @text.setter
+    def text(self, text: str) -> None:
+        self._text = text
+        if self.interactive:
+            self._spinner.text = self._text
+        else:
+            echo(self._text)
+
+    def start(self) -> None:
+        """Start the spinner"""
+        if self.interactive:
+            self._spinner.start()
+
+    def succeed(self, text: Optional[str] = None) -> None:
+        """Display text on success"""
+        if text:
+            self._text = text
+        if self.interactive:
+            self._spinner.succeed(self.text)
+        else:
+            echo(self.text)
+
+    def fail(self, text: Optional[str] = None) -> None:
+        """Display text on failure"""
+        if text:
+            self._text = text
+        if self.interactive:
+            self._spinner.fail(self.text)
+        else:
+            echo(self.text)
+
+    def stop(self) -> None:
+        """Stop the spinner"""
+        if self.interactive:
+            self._spinner.stop()
+        self._text = ""
