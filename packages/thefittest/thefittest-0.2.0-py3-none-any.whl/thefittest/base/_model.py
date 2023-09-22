815d811014c71d614f36f9e90bc43cceb37852a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 03:08:42 +0000

    Add package thefittest/0.2.0/thefittest-0.2.0-py3-none-any.whl

---
+++ packages/thefittest/thefittest-0.2.0-py3-none-any.whl/thefittest/base/_model.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+from typing import Any
+from typing import Tuple
+from typing import Union
+
+import numpy as np
+from numpy.typing import NDArray
+
+
+class Model:
+    def _fit(
+        self,
+        X: NDArray[np.float64],
+        y: NDArray[Union[np.float64, np.int64]],
+    ) -> Any:
+        pass
+
+    def _predict(self, X: NDArray[np.float64]) -> Any:
+        pass
+
+    def get_optimizers(self: Model) -> Tuple:
+        return tuple()
+
+    def fit(
+        self,
+        X: NDArray[np.float64],
+        y: NDArray[Union[np.float64, np.int64]],
+    ) -> Any:
+        return self._fit(X, y)
+
+    def predict(self, X: NDArray[np.float64]) -> NDArray[Union[np.float64, np.int64]]:
+        return self._predict(X)
