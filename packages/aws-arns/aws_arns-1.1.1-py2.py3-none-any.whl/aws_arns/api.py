27e4b537a557958151e28c0552a4325b11182ce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-22 14:07:49 +0000

    Add package aws-arns/1.1.1/aws_arns-1.1.1-py2.py3-none-any.whl

---
+++ packages/aws-arns/aws_arns-1.1.1-py2.py3-none-any.whl/aws_arns/api.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+"""
+Usage example::
+
+    import aws_arns.api as aws_arns
+"""
+
+from .constants import AwsPartitionEnum
+from .model import Arn
+from . import resource as res
