52ab89cd6ed7c2f8d47018a1082ce807ff53d604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:07:37 +0000

    Add package aryahelpers/0.0.2/aryahelpers-0.0.2.tar.gz

---
+++ packages/aryahelpers/aryahelpers-0.0.2.tar.gz/aryahelpers-0.0.2/setup.py
@@ -0,0 +1,27 @@
+"""Setup for aryahelpers modules"""
+from setuptools import setup, find_packages
+
+BASE_REPO = 'https://gitlab.leoforce.com/ratnadip.adhikari/arya-helpers'
+
+setup(
+    name='aryahelpers',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    version='0.0.2',
+    description='Arya Helpers Modules',
+    long_description='Helper Python modules for Arya + general projects',
+    author='Ratnadip Adhikari',
+    author_email='ratnadip.adhikari@leoforce.com',
+    url=BASE_REPO + '.git',
+    download_url=BASE_REPO,
+    keywords=['utilities', 'arya', 'helper modules'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3"
+    ],
+    platforms=['any'],
+    install_requires=[],
+    # include_package_data=True
+)
