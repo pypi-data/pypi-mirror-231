d2f2478e57c9a53088b14cad209cf874a434290c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:10:45 +0000

    Add package phoenix-trade/0.1.5/phoenix-trade-0.1.5.tar.gz

---
+++ packages/phoenix-trade/phoenix-trade-0.1.5.tar.gz/phoenix-trade-0.1.5/phoenix/order_subscribe_response.py
@@ -0,0 +1,67 @@
+from dataclasses import dataclass
+from traceback import StackSummary
+from typing import Optional, Union
+
+from phoenix.types.fifo_order_id import FIFOOrderId
+from phoenix.types.side import Ask, Bid
+from solders.pubkey import Pubkey
+
+
+@dataclass
+class FilledOrder:
+    exchange_order_id: FIFOOrderId
+    order_id: int
+    market_pubkey: Pubkey
+    sequence_number: int
+    slot: int
+    unix_timestamp_in_seconds: int
+    taker_side: Union[Bid, Ask]
+    base_lots_filled: int
+    base_lots_remaining: int
+    taker_pubkey: Pubkey
+    maker_pubkey: Pubkey
+
+
+@dataclass
+class OpenOrder:
+    exchange_order_id: FIFOOrderId
+    order_id: int
+    market_pubkey: Pubkey
+    sequence_number: int
+    slot: int
+    unix_timestamp_in_seconds: int
+    side: Union[Bid, Ask]
+    base_lots_placed: int
+    maker_pubkey: Pubkey
+
+
+@dataclass
+class CancelledOrder:
+    exchange_order_id: FIFOOrderId
+    order_id: int
+    market_pubkey: Pubkey
+    sequence_number: int
+    slot: int
+    unix_timestamp_in_seconds: int
+    side: Union[Bid, Ask]
+    base_lots_removed: int
+    base_lots_remaining: int
+    maker_pubkey: Pubkey
+
+
+@dataclass
+class OrderSubscribeError:
+    message: str
+    reconnection_count: int
+    reconnected: bool
+    error_count: int
+    traceback: Optional[StackSummary]
+    exception: Exception
+
+
+OrderSubscribeResponse = Union[
+    FilledOrder,
+    OpenOrder,
+    CancelledOrder,
+    OrderSubscribeError,
+]
