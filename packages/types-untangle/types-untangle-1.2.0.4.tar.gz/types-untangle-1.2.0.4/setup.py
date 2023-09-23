2c376985ff4035106859c39f09458522a825ae12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 15:07:55 +0000

    Add package types-untangle/1.2.0.4/types-untangle-1.2.0.4.tar.gz

---
+++ packages/types-untangle/types-untangle-1.2.0.4.tar.gz/types-untangle-1.2.0.4/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+
+name = "types-untangle"
+description = "Typing stubs for untangle"
+long_description = '''
+## Typing stubs for untangle
+
+This is a PEP 561 type stub package for the `untangle` package. It
+can be used by type-checking tools like
+[mypy](https://github.com/python/mypy/),
+[pyright](https://github.com/microsoft/pyright),
+[pytype](https://github.com/google/pytype/),
+PyCharm, etc. to check code that uses
+`untangle`. The source for this package can be found at
+https://github.com/python/typeshed/tree/main/stubs/untangle. All fixes for
+types and metadata should be contributed there.
+
+See https://github.com/python/typeshed/blob/main/README.md for more details.
+This package was generated from typeshed commit `6ca7505af71322818ff07c9d56c4376fd84450fb` and was tested
+with mypy 1.5.1, pyright 1.1.328, and
+pytype 2023.8.31.
+'''.lstrip()
+
+setup(name=name,
+      version="1.2.0.4",
+      description=description,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/python/typeshed",
+      project_urls={
+          "GitHub": "https://github.com/python/typeshed",
+          "Changes": "https://github.com/typeshed-internal/stub_uploader/blob/main/data/changelogs/untangle.md",
+          "Issue tracker": "https://github.com/python/typeshed/issues",
+          "Chat": "https://gitter.im/python/typing",
+      },
+      install_requires=[],
+      packages=['untangle-stubs'],
+      package_data={'untangle-stubs': ['__init__.pyi', 'METADATA.toml']},
+      license="Apache-2.0 license",
+      classifiers=[
+          "License :: OSI Approved :: Apache Software License",
+          "Programming Language :: Python :: 3",
+          "Typing :: Stubs Only",
+      ]
+)
