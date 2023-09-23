2073eff2512f230c22fa582868d70cdcd6c3ee53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 05:06:53 +0000

    Add package shell-whiz/1.3.3/shell_whiz-1.3.3.tar.gz

---
+++ packages/shell-whiz/shell_whiz-1.3.3.tar.gz/shell_whiz-1.3.3/shell_whiz/jsonschemas.py
@@ -0,0 +1,22 @@
+translation_json_schema = {
+    "type": "object",
+    "properties": {
+        "shell_command": {
+            "type": "string",
+            "description": "Shell command to perform the task, leave blank if it is not possible to perform the task in the command line",
+        }
+    },
+    "required": ["shell_command"],
+}
+
+dangerous_command_json_schema = {
+    "type": "object",
+    "properties": {
+        "dangerous_to_run": {"type": "boolean"},
+        "dangerous_consequences": {
+            "type": "string",
+            "description": "Brief explanation of the potential side effects of running the command. No more than 12 words.",
+        },
+    },
+    "required": ["dangerous_to_run"],
+}
