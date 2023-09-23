22eff494f798c7c6eb634fad448d815c4a4011d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:08:06 +0000

    Add package Antiland/0.76.6/antiland-0.76.6.tar.gz

---
+++ packages/Antiland/antiland-0.76.6.tar.gz/antiland-0.76.6/class_test.py
@@ -0,0 +1,38 @@
+import requests
+
+session_token="r:92fede4c6ad623f4dc2c41bf414fb19b"
+dialogue="TOhmT4cR0d"
+
+class Dialogue:
+    def __init__(self, data):
+        self.lang = data["lang"]
+        self.groupAdmins = data.get("groupAdmins")
+        self.lastmessage = data["lastmessage"]
+        self.objectId = data["objectId"]
+        self.guestname = data["guestname"]
+        self.foundername = data["foundername"]
+        self.founderId = data["founderId"]
+        self.private = data.get("private")
+        self.public = data.get("public")
+        self.humanLink = data.get("humanlink")
+        self.accepted = data["accepted"]
+        self.flags = data["flags"]
+
+def get_chat(dialogue,token):
+        url="https://mobile-elb.antich.at/functions/getDialogue"
+        json_payload={
+            "dialogueId": dialogue,
+            "v": 10001,
+            "_ApplicationId": "fUEmHsDqbr9v73s4JBx0CwANjDJjoMcDFlrGqgY5",
+            "_ClientVersion": "js1.11.1",
+            "_InstallationId": "3e355bb2-ce1f-0876-2e6b-e3b19adc4cef",
+            "_SessionToken": token
+            }
+        r=requests.post(url,json_payload)
+        data = r.json()
+        for data2 in data["result"]:
+            print(data["result"][data2])
+        dialogue = Dialogue(data["result"])
+        return dialogue
+
+print(get_chat(dialogue,session_token))
