b45a17d2b990ae08ac921b084763f8567e882a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 04:06:15 +0000

    Add package quahris/0.0.3.4/quahris-0.0.3.4.tar.gz

---
+++ packages/quahris/quahris-0.0.3.4.tar.gz/quahris-0.0.3.4/main.py
@@ -0,0 +1,6 @@
+from quahris import functions
+
+functions.qualys_cred("credt6aa1", "yDv4d$)VlcEJ2P)I9AyM!6NR2*")
+qids = "19568, 38169, 38173, 38628"
+vm = functions.listVm('70000', 'Active', '1', '0', None, None, None, None, None, "listVm.xml")
+print(vm)
