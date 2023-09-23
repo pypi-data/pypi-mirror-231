18a2769d511e8d3269c68c398b12643e81cea6ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:06:58 +0000

    Add package nafes/0.0.2/nafes-0.0.2.tar.gz

---
+++ packages/nafes/nafes-0.0.2.tar.gz/nafes-0.0.2/nafes/__init__.py
@@ -0,0 +1,12 @@
+"""Top-level package for nafes."""
+
+__author__ = """Nana Abeka Otoo"""
+__email__ = 'nanaabekaotoo@gmail.com'
+__version__ = '0.0.2'
+
+from .dataset import *
+from .mutate_labels import *
+from .mutated_validation import *
+from .nafes import *
+
+
