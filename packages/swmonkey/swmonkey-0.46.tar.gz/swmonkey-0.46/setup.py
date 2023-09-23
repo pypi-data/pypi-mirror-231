32a1b602754e79bf47a7f835db5e959bb551cbc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:09:10 +0000

    Add package swmonkey/0.46/swmonkey-0.46.tar.gz

---
+++ packages/swmonkey/swmonkey-0.46.tar.gz/swmonkey-0.46/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+scriptFolder = os.path.dirname(os.path.realpath(__file__))
+os.chdir(scriptFolder)
+
+with open('src/swmonkey/main.py', encoding='utf-8') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            version = line.split('=')[1].strip().strip("'")
+            break
+
+print('version: ', version)
+setup(
+    name='swmonkey',
+    version=version,
+    packages=find_packages(where='src'),
+    author='Li Saifei',
+    author_email='waltermitty121906@gmail.com',
+    package_dir={'': 'src'},
+    description='A tool for monkey test on Linux GUI',
+    entry_points={
+        'console_scripts': [
+            'swmonkey = swmonkey.main:swmonkey',
+            'swmonkey_runner = swmonkey.runner:main'
+        ]
+    },
+    install_requires=[
+        'pyautogui',
+        'psutil',
+        'pywinctl'
+    ]
+)
