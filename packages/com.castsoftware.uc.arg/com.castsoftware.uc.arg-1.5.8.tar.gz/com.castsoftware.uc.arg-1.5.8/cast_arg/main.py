3e1e865fdcfcdce8664bcf75712ec5743d13dbf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:08:02 +0000

    Add package com.castsoftware.uc.arg/1.5.8/com.castsoftware.uc.arg-1.5.8.tar.gz

---
+++ packages/com.castsoftware.uc.arg/com.castsoftware.uc.arg-1.5.8.tar.gz/com.castsoftware.uc.arg-1.5.8/cast_arg/main.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+from cast_arg.convert import GeneratePPT
+from cast_arg.config import Config
+# from convert import GeneratePPT
+# from config import Config
+
+__author__ = "Nevin Kaplan"
+__email__ = "n.kaplan@castsoftware.com"
+__copyright__ = "Copyright 2023, CAST Software"
+
+if __name__ == '__main__':
+    print('\nCAST Assessment Deck Generation Tool (ARG)')
+    print('Copyright (c) 2023 CAST Software Inc.\n')
+    print('If you need assistance, please contact oneclick@castsoftware.com\n')
+
+    parser = ArgumentParser(description='Assessment Report Generation Tool')
+    parser.add_argument('-c','--config', required=True, help='Configuration properties file')
+    args = parser.parse_args()
+    ppt = GeneratePPT(Config(args.config))
+    ppt.save_ppt()
+
