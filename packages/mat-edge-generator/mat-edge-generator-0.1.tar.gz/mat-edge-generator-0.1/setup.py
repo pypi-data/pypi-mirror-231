bf3b2cb578bf35b2158390e33b67392ec9445c27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:08:20 +0000

    Add package mat-edge-generator/0.1/mat-edge-generator-0.1.tar.gz

---
+++ packages/mat-edge-generator/mat-edge-generator-0.1.tar.gz/mat-edge-generator-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mat-edge-generator',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pyyaml"
+    ],
+    author='Filippo Ghelfi',
+    author_email='f.ghelfi@40-factory.com',
+    description='This library is used for creating a MAT edge starting from a simple configuration file',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
