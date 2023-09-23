34612c450a06268fa6de6b2c11f8bca7f1016dc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:10:39 +0000

    Add package pkscreener/0.41.20230922.2/pkscreener-0.41.20230922.2-py3-none-any.whl

---
+++ packages/pkscreener/pkscreener-0.41.20230922.2-py3-none-any.whl/pkscreener/classes/Fetcher.py
@@ -0,0 +1,433 @@
+"""
+    The MIT License (MIT)
+
+    Copyright (c) 2023 pkjmesra
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+"""
+"""
+ *  Project             :   Screenipy
+ *  Author              :   Pranjal Joshi
+ *  Created             :   28/04/2021
+ *  Description         :   Class for handling networking for fetching stock codes and data
+"""
+
+import csv
+import os
+import random
+import sys
+import urllib
+import warnings
+from datetime import timedelta
+from io import StringIO
+
+warnings.simplefilter("ignore", DeprecationWarning)
+warnings.simplefilter("ignore", FutureWarning)
+import pandas as pd
+import requests
+import requests_cache
+import yfinance as yf
+from requests.exceptions import ConnectTimeout, ReadTimeout
+from requests_cache import CachedSession
+from urllib3.exceptions import ReadTimeoutError
+
+from pkscreener.classes.ColorText import colorText
+from pkscreener.classes.log import default_logger
+from pkscreener.classes.SuppressOutput import SuppressOutput
+
+requests.packages.urllib3.util.connection.HAS_IPV6 = False
+session = CachedSession(
+    "pkscreener_cache",
+    expire_after=timedelta(hours=6),
+    stale_if_error=True,
+)
+
+# Exception class if yfinance stock delisted
+
+
+class StockDataEmptyException(Exception):
+    pass
+
+
+# This Class Handles Fetching of Stock Data over the internet
+
+class tools:
+    def __init__(self, configManager):
+        self.configManager = configManager
+        self._proxy = None
+        pass
+    
+    @property
+    def proxyServer(self):
+        if self._proxy is None:
+            self._proxy = self._getProxyServer()
+        return self._proxy
+    
+    def _getProxyServer(self):
+        # Get system wide proxy for networking
+        try:
+            proxy = urllib.request.getproxies()["http"]
+            proxy = {"https": proxy}
+        except KeyError as e:
+            default_logger().debug(e, exc_info=True)
+            proxy = None
+        return proxy
+
+    def postURL(self, url, data=None, headers={}, trial=1):
+        try:
+            response = None
+            requestor = session
+            # We should try to switch to requests lib if cached_session 
+            # begin to give some problem after we've tried for
+            # 50% of the configured retrials.
+            if trial >= int(self.configManager.maxNetworkRetryCount/2):
+                requestor = requests
+            response = requestor.post(
+                            url,
+                            proxies=self.proxyServer,
+                            data = data,
+                            headers=headers,
+                            timeout=trial*self.configManager.generalTimeout,
+                        )
+        except (ConnectTimeout,ReadTimeoutError,ReadTimeout) as e:
+            default_logger().debug(e, exc_info=True)
+            if trial <= int(self.configManager.maxNetworkRetryCount):
+                print(colorText.BOLD + colorText.FAIL + f"[+] Network Request timed-out. Going for {trial} of {self.configManager.maxNetworkRetryCount}th trial..." + colorText.END, end="")
+                return self.postURL(url, data=data, headers=headers,trial=trial+1)
+        except Exception as e:
+            # Something went wrong with the CachedSession.
+            default_logger().debug(e, exc_info=True)
+            if trial <= int(self.configManager.maxNetworkRetryCount):
+                if trial <= 1:
+                    # Let's try and restart the cache
+                    self.configManager.restartRequestsCache()
+                elif trial > 1 and requests_cache.is_installed():
+                    # REstarting didn't fix it. We need to disable the cache altogether.
+                    requests_cache.clear()
+                    requests_cache.uninstall_cache()
+                print(colorText.BOLD + colorText.FAIL + f"[+] Network Request failed. Going for {trial} of {self.configManager.maxNetworkRetryCount}th trial..." + colorText.END, end="")
+                return self.postURL(url, data=data, headers=headers,trial=trial+1)
+        if trial > 1 and not requests_cache.is_installed():
+            # Let's try and re-enable the caching behaviour before exiting.
+            # Maybe there was something wrong with this request, but the next
+            # request should have access to cache.
+            self.configManager.restartRequestsCache()
+        return response
+
+    def fetchURL(self, url, stream=False, trial=1):
+        try:
+            response = None
+            requestor = session
+            # We should try to switch to requests lib if cached_session 
+            # begin to give some problem after we've tried for
+            # 50% of the configured retrials.
+            if trial >= int(self.configManager.maxNetworkRetryCount/2):
+                requestor = requests
+            response = requestor.get(
+                            url,
+                            proxies=self.proxyServer,
+                            stream = stream,
+                            timeout=trial*self.configManager.generalTimeout,
+                        ) 
+        except (ConnectTimeout,ReadTimeoutError,ReadTimeout) as e:
+            default_logger().debug(e, exc_info=True)
+            if trial <= int(self.configManager.maxNetworkRetryCount):
+                print(colorText.BOLD + colorText.FAIL + f"[+] Network Request timed-out. Going for {trial} of {self.configManager.maxNetworkRetryCount}th trial..." + colorText.END, end="")
+                return self.fetchURL(url, stream=stream, trial=trial+1)
+        except Exception as e:
+            # Something went wrong with the CachedSession.
+            default_logger().debug(e, exc_info=True)
+            if trial <= int(self.configManager.maxNetworkRetryCount):
+                if trial <= 1:
+                    # Let's try and restart the cache
+                    self.configManager.restartRequestsCache()
+                elif trial > 1 and requests_cache.is_installed():
+                    # REstarting didn't fix it. We need to disable the cache altogether.
+                    requests_cache.clear()
+                    requests_cache.uninstall_cache()
+                print(colorText.BOLD + colorText.FAIL + f"[+] Network Request failed. Going for {trial} of {self.configManager.maxNetworkRetryCount}th trial..." + colorText.END, end="")
+                return self.fetchURL(url, stream=stream, trial=trial+1)
+        if trial > 1 and not requests_cache.is_installed():
+            # Let's try and re-enable the caching behaviour before exiting.
+            # Maybe there was something wrong with this request, but the next
+            # request should have access to cache.
+            self.configManager.restartRequestsCache()
+        return response
+                
+    def fetchCodes(self, tickerOption):
+        listStockCodes = []
+        if tickerOption == 12:
+            url = "https://archives.nseindia.com/content/equities/EQUITY_L.csv"
+            res = self.fetchURL(url)
+            if res is None or res.status_code != 200:
+                return listStockCodes
+            try:
+                data = pd.read_csv(StringIO(res.text))
+                return list(data["SYMBOL"].values)
+            except Exception as e:
+                default_logger().debug(e, exc_info=True)
+                return listStockCodes
+            
+        tickerMapping = {
+            1: "https://archives.nseindia.com/content/indices/ind_nifty50list.csv",
+            2: "https://archives.nseindia.com/content/indices/ind_niftynext50list.csv",
+            3: "https://archives.nseindia.com/content/indices/ind_nifty100list.csv",
+            4: "https://archives.nseindia.com/content/indices/ind_nifty200list.csv",
+            5: "https://archives.nseindia.com/content/indices/ind_nifty500list.csv",
+            6: "https://archives.nseindia.com/content/indices/ind_niftysmallcap50list.csv",
+            7: "https://archives.nseindia.com/content/indices/ind_niftysmallcap100list.csv",
+            8: "https://archives.nseindia.com/content/indices/ind_niftysmallcap250list.csv",
+            9: "https://archives.nseindia.com/content/indices/ind_niftymidcap50list.csv",
+            10: "https://archives.nseindia.com/content/indices/ind_niftymidcap100list.csv",
+            11: "https://archives.nseindia.com/content/indices/ind_niftymidcap150list.csv",
+            14: "https://archives.nseindia.com/content/fo/fo_mktlots.csv",
+        }
+
+        url = tickerMapping.get(tickerOption)
+
+        try:
+            res = self.fetchURL(url)
+            if res is None or res.status_code != 200:
+                return listStockCodes
+            cr = csv.reader(res.text.strip().split("\n"))
+
+            if tickerOption == 14:
+                for i in range(5):
+                    next(cr)  # skipping first line
+                for row in cr:
+                    listStockCodes.append(row[1])
+            else:
+                next(cr)  # skipping first line
+                for row in cr:
+                    listStockCodes.append(row[2])
+        except Exception as e:
+            default_logger().debug(e, exc_info=True)
+
+        return listStockCodes
+
+    # Fetch all stock codes from NSE
+    def fetchStockCodes(self, tickerOption, stockCode=None):
+        listStockCodes = []
+        if tickerOption == 0:
+            stockCode = None
+            while stockCode is None or stockCode == "":
+                stockCode = str(
+                    input(
+                        colorText.BOLD
+                        + colorText.BLUE
+                        + "[+] Enter Stock Code(s) for screening (Multiple codes should be seperated by ,): "
+                    )
+                ).upper()
+            stockCode = stockCode.replace(" ", "")
+            listStockCodes = stockCode.split(",")
+        else:
+            print(colorText.BOLD + "[+] Getting Stock Codes From NSE... ", end="")
+            listStockCodes = self.fetchCodes(tickerOption)
+            if len(listStockCodes) > 10:
+                print(
+                    colorText.GREEN
+                    + ("=> Done! Fetched %d stock codes." % len(listStockCodes))
+                    + colorText.END
+                )
+                if self.configManager.shuffleEnabled:
+                    random.shuffle(listStockCodes)
+                    print(
+                        colorText.BLUE
+                        + "[+] Stock shuffling is active."
+                        + colorText.END
+                    )
+                else:
+                    print(
+                        colorText.FAIL
+                        + "[+] Stock shuffling is inactive."
+                        + colorText.END
+                    )
+                if self.configManager.stageTwo:
+                    print(
+                        colorText.BLUE
+                        + "[+] Screening only for the stocks in Stage-2! Edit User Config to change this."
+                        + colorText.END
+                    )
+                else:
+                    print(
+                        colorText.FAIL
+                        + "[+] Screening only for the stocks in all Stages! Edit User Config to change this."
+                        + colorText.END
+                    )
+
+            else:
+                input(
+                    colorText.FAIL
+                    + "=> Error getting stock codes from NSE! Press <Enter> to exit!"
+                    + colorText.END
+                )
+
+        return listStockCodes
+
+    # Fetch stock price data from Yahoo finance
+    def fetchStockData(
+        self,
+        stockCode,
+        period,
+        duration,
+        proxyServer,
+        screenResultsCounter,
+        screenCounter,
+        totalSymbols,
+        printCounter=False,
+    ):
+        with SuppressOutput(suppress_stdout=True, suppress_stderr=True):
+            data = yf.download(
+                tickers=stockCode + ".NS",
+                period=period,
+                interval=duration,
+                proxy=proxyServer,
+                progress=False,
+                timeout=self.configManager.longTimeout,
+            )
+        if printCounter:
+            sys.stdout.write("\r\033[K")
+            try:
+                print(
+                    colorText.BOLD
+                    + colorText.GREEN
+                    + (
+                        "[%d%%] Screened %d, Found %d. Fetching data & Analyzing %s..."
+                        % (
+                            int((screenCounter.value / totalSymbols) * 100),
+                            screenCounter.value,
+                            screenResultsCounter.value,
+                            stockCode,
+                        )
+                    )
+                    + colorText.END,
+                    end="",
+                )
+            except ZeroDivisionError as e:
+                default_logger().debug(e, exc_info=True)
+                pass
+            except Exception as e:
+                default_logger().debug(e, exc_info=True)
+                pass
+            if len(data) == 0:
+                print(
+                    colorText.BOLD
+                    + colorText.FAIL
+                    + "=> Failed to fetch!"
+                    + colorText.END,
+                    end="\r",
+                    flush=True,
+                )
+                raise StockDataEmptyException
+                return None
+            print(
+                colorText.BOLD + colorText.GREEN + "=> Done!" + colorText.END,
+                end="\r",
+                flush=True,
+            )
+        return data
+
+    # Get Daily Nifty 50 Index:
+    def fetchLatestNiftyDaily(self, proxyServer=None):
+        data = yf.download(
+            tickers="^NSEI",
+            period="5d",
+            interval="1d",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        return data
+
+    # Get Data for Five EMA strategy
+    def fetchFiveEmaData(self, proxyServer=None):
+        nifty_sell = yf.download(
+            tickers="^NSEI",
+            period="5d",
+            interval="5m",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        banknifty_sell = yf.download(
+            tickers="^NSEBANK",
+            period="5d",
+            interval="5m",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        nifty_buy = yf.download(
+            tickers="^NSEI",
+            period="5d",
+            interval="15m",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        banknifty_buy = yf.download(
+            tickers="^NSEBANK",
+            period="5d",
+            interval="15m",
+            proxy=proxyServer,
+            progress=False,
+            timeout=self.configManager.longTimeout,
+        )
+        return nifty_buy, banknifty_buy, nifty_sell, banknifty_sell
+
+    # Load stockCodes from the watchlist.xlsx
+    def fetchWatchlist(self):
+        createTemplate = False
+        data = pd.DataFrame()
+        try:
+            data = pd.read_excel("watchlist.xlsx")
+        except FileNotFoundError as e:
+            default_logger().debug(e, exc_info=True)
+            print(
+                colorText.BOLD
+                + colorText.FAIL
+                + f"[+] watchlist.xlsx not found in {os.getcwd()}"
+                + colorText.END
+            )
+            createTemplate = True
+        try:
+            if not createTemplate:
+                data = data["Stock Code"].values.tolist()
+        except KeyError as e:
+            default_logger().debug(e, exc_info=True)
+            print(
+                colorText.BOLD
+                + colorText.FAIL
+                + '[+] Bad Watchlist Format: First Column (A1) should have Header named "Stock Code"'
+                + colorText.END
+            )
+            createTemplate = True
+        if createTemplate:
+            sample = {"Stock Code": ["SBIN", "INFY", "TATAMOTORS", "ITC"]}
+            sample_data = pd.DataFrame(sample, columns=["Stock Code"])
+            sample_data.to_excel("watchlist_template.xlsx", index=False, header=True)
+            print(
+                colorText.BOLD
+                + colorText.BLUE
+                + f"[+] watchlist_template.xlsx created in {os.getcwd()} as a referance template."
+                + colorText.END
+            )
+            return None
+        return data
