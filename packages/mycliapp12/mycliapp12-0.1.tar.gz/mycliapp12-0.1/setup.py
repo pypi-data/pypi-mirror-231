72948ea96cd5da0e3c6c3463bff301e6898aff9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:09:38 +0000

    Add package mycliapp12/0.1/mycliapp12-0.1.tar.gz

---
+++ packages/mycliapp12/mycliapp12-0.1.tar.gz/mycliapp12-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mycliapp12',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        'click',
+        'databricks-sql-connector'
+    ],
+    entry_points={
+        'console_scripts': [
+            'mycliapp12 = mycliapp.main:hello',
+        ],
+    },
+)
