466721df4cd1d2d8d0094cf23bc76b9d45bbb0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 05:07:05 +0000

    Add package autodatap/1.2.8/autodatap-1.2.8.tar.gz

---
+++ packages/autodatap/autodatap-1.2.8.tar.gz/autodatap-1.2.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.2.8'
+DESCRIPTION = 'Automating Data Preprocessing'
+
+# Setting up
+setup(
+    name="autodatap",
+    version=VERSION,
+    author="SyabAhmad",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=['pandas', 'scikit-learn'],  # Remove 'time'
+    keywords=['python', 'machine learning', 'data science', 'data', 'preprocessing', 'AI'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    entry_points={
+        'console_scripts': [
+            'autodatap = autodatap.main:mainMethod',
+        ],
+    },
+)
