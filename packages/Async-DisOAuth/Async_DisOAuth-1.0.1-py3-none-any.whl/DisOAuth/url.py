fc09e30ac9176c5302c596df894c3c8e9b32e6c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:08:17 +0000

    Add package Async-DisOAuth/1.0.1/Async_DisOAuth-1.0.1-py3-none-any.whl

---
+++ packages/Async-DisOAuth/Async_DisOAuth-1.0.1-py3-none-any.whl/DisOAuth/url.py
@@ -0,0 +1,89 @@
+import json
+from typing import List
+
+import requests
+
+from .common import generate_token, getToken, htmlEncode, joinUrl
+
+apiUrl = "https://discord.com/api"
+
+
+class AuthUrl:
+    def __init__(self,
+                 client_id: str,
+                 scope: List[str],
+                 redirect_uri: str) -> None:
+        """Makes and returns a url that is used to authorize users
+
+        Keyword arguments:
+        client_id -- The client ID of your discord app
+        scope -- A list of scopes you want to use
+        redirect_uri -- The redirect uri you want to use
+        """
+        self._client_id = client_id
+        self._scope = scope
+        self._redirect_uri = redirect_uri
+
+    async def makeUrl(self) -> str:
+        scope = self._scope
+        redirect_uri = self._redirect_uri
+        client_id = self._client_id
+        state = await generate_token()
+        x = 0
+        strScope = ""
+        while x < len(scope):
+            strScope = strScope.join(f"{scope[x]} ")
+            x += 1
+        _strScope = strScope[:-1]
+        _scope = _strScope.replace(" ", "%20")
+        redirectUri = await htmlEncode(redirect_uri)
+        url = await joinUrl(client_id, _scope, redirectUri, state)
+        return url
+
+
+class discordApi:
+
+    def __init__(self,
+                 client_id,
+                 client_secret,
+                 scope: list[str],
+                 redirect_uri) -> None:
+        """Where you can get an access code, use the api links to get info
+
+        client_id - The client id of your application
+        client_secret - The client secret of your application
+        scope - a list of the scopes you authorized for
+        redirect_uri -- the redirect_uri you want to use
+        """
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.scope = scope
+        self.redirect_uri = redirect_uri
+
+    async def accessToken(self, code) -> dict[str, str]:
+        """Takes values input into discordApi, and the code
+        returns the response as a dictionary
+
+        Keyword Arguments:
+        code -- The code you got when the user authorized
+        """
+        tokenDict = getToken(code,
+                             self.scope,
+                             self.redirect_uri,
+                             self.client_id,
+                             self.client_secret)
+        return await tokenDict
+
+
+    class User:
+        def __init__(self, access_token):
+            self.access_token = access_token
+
+        async def get_current_user(self):
+            url = apiUrl + "/users/@me"
+            headers = {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Authorization': 'Bearer ' + self.access_token
+            }
+            r = requests.get(url, headers=headers)
+            return r.json()
