1dc6a7a9a1076ee807b73eba8a3c4ca2673986b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 04:08:15 +0000

    Add package pykooler/1.0.0/pykooler-1.0.0.tar.gz

---
+++ packages/pykooler/pykooler-1.0.0.tar.gz/pykooler-1.0.0/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from setuptools.command.install import install
+from pathlib import Path
+
+
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Cool package.'
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
+class InstallCommand(install):
+
+    def run(self):
+        try:
+            print("Installing..")
+        except:
+            pass
+        install.run(self)
+
+
+setup(
+    name="pykooler",
+    version=VERSION,
+    author="HW",
+    author_email="",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+    cmdclass={
+        'install': InstallCommand
+    }
+)
