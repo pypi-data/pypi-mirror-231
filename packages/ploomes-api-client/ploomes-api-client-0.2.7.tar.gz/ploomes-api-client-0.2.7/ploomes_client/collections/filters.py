2146305fec3743883914435978fe1774fe3d2d84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:10:54 +0000

    Add package ploomes-api-client/0.2.7/ploomes-api-client-0.2.7.tar.gz

---
+++ packages/ploomes-api-client/ploomes-api-client-0.2.7.tar.gz/ploomes-api-client-0.2.7/ploomes_client/collections/filters.py
@@ -0,0 +1,52 @@
+import json
+from ploomes_client.core.ploomes_client import PloomesClient
+
+
+class Filters:
+    def __init__(self, client: PloomesClient) -> None:
+        self.client = client
+        self.path = "/Filters"
+
+    def post_filter(
+        self,
+        payload,
+        filter_=None,
+        expand=None,
+        top=None,
+        inlinecount=None,
+        orderby=None,
+        select=None,
+        skip=None,
+    ):
+        """
+        Creates a new filter using the provided payload and filters.
+
+        Args:
+            payload (dict): The data for the filter to be created.
+            filter_ (str, optional): OData filter string.
+            expand (str, optional): Expand related entities.
+            top (int, optional): Maximum number of results to return.
+            inlinecount (str, optional): Option for inline count.
+            orderby (str, optional): Order by clause.
+            select (str, optional): Select specific properties.
+            skip (int, optional): Number of results to skip.
+
+        Returns:
+            Response: The response object containing the result of the POST request.
+        """
+        filters = {
+            "$filter": filter_,
+            "$inlinecount": inlinecount,
+            "$orderby": orderby,
+            "$select": select,
+            "$skip": skip,
+            "$top": top,
+            "$expand": expand,
+        }
+        payload_json = json.dumps(payload)
+        return self.client.request(
+            "POST",
+            self.path,
+            filters={k: v for k, v in filters.items() if v is not None},
+            payload=payload_json,
+        )
---
+++ packages/ploomes-api-client/ploomes-api-client-0.2.7.tar.gz/ploomes-api-client-0.2.7/ploomes_client/collections/tables.py
@@ -0,0 +1,138 @@
+import json
+from ploomes_client.core.ploomes_client import PloomesClient
+
+
+class Tables:
+    def __init__(self, client: PloomesClient) -> None:
+        self.client = client
+        self.path = "/Tables"
+
+    def get_tables(
+        self,
+        filter_=None,
+        expand=None,
+        top=None,
+        inlinecount=None,
+        orderby=None,
+        select=None,
+        skip=None,
+    ):
+        """
+        Retrieves tables based on the provided filters.
+
+        Args:
+            filter_ (str, optional): OData filter string.
+            expand (str, optional): Expand related entities.
+            top (int, optional): Maximum number of results to return.
+            inlinecount (str, optional): Option for inline count.
+            orderby (str, optional): Order by clause.
+            select (str, optional): Select specific properties.
+            skip (int, optional): Number of results to skip.
+
+        Returns:
+            Response: The response object containing the result of the GET request.
+        """
+        filters = {
+            "$filter": filter_,
+            "$inlinecount": inlinecount,
+            "$orderby": orderby,
+            "$select": select,
+            "$skip": skip,
+            "$top": top,
+            "$expand": expand,
+        }
+        return self.client.request(
+            "GET",
+            self.path,
+            filters={k: v for k, v in filters.items() if v is not None},
+        )
+
+    def post_table(
+        self,
+        payload,
+        filter_=None,
+        expand=None,
+        top=None,
+        inlinecount=None,
+        orderby=None,
+        select=None,
+        skip=None,
+    ):
+        """
+        Creates a new table using the provided payload and filters.
+
+        Args:
+            payload (dict): The data for the table to be created.
+            filter_ (str, optional): OData filter string.
+            expand (str, optional): Expand related entities.
+            top (int, optional): Maximum number of results to return.
+            inlinecount (str, optional): Option for inline count.
+            orderby (str, optional): Order by clause.
+            select (str, optional): Select specific properties.
+            skip (int, optional): Number of results to skip.
+
+        Returns:
+            Response: The response object containing the result of the POST request.
+        """
+        filters = {
+            "$filter": filter_,
+            "$inlinecount": inlinecount,
+            "$orderby": orderby,
+            "$select": select,
+            "$skip": skip,
+            "$top": top,
+            "$expand": expand,
+        }
+        payload_json = json.dumps(payload)
+        return self.client.request(
+            "POST",
+            self.path,
+            filters={k: v for k, v in filters.items() if v is not None},
+            payload=payload_json,
+        )
+
+    def patch_table(
+        self,
+        id_: int,
+        payload: dict,
+        filter_=None,
+        expand=None,
+        top=None,
+        inlinecount=None,
+        orderby=None,
+        select=None,
+        skip=None,
+    ):
+        """
+        Updates a table by its ID with specific fields.
+
+        Args:
+            id_ (int): The ID of the table to be updated.
+            payload (dict): Fields to be updated in the table.
+            filter_ (str, optional): OData filter string.
+            expand (str, optional): Expand related entities.
+            top (int, optional): Maximum number of results to return.
+            inlinecount (str, optional): Option for inline count.
+            orderby (str, optional): Order by clause.
+            select (str, optional): Select specific properties.
+            skip (int, optional): Number of results to skip.
+
+        Returns:
+            dict: The JSON response from the server.
+        """
+        filters = {
+            "$filter": filter_,
+            "$inlinecount": inlinecount,
+            "$orderby": orderby,
+            "$select": select,
+            "$skip": skip,
+            "$top": top,
+            "$expand": expand,
+        }
+        payload_json = json.dumps(payload)
+        return self.client.request(
+            "PATCH",
+            self.path + f"({id_})",
+            filters={k: v for k, v in filters.items() if v is not None},
+            payload=payload_json,
+        )
