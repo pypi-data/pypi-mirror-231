42435d62a2ff8cc4a90c52bb2db857016844dd25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:06:57 +0000

    Add package paraty-commit-jlvillada/0.1.1/paraty_commit_jlvillada-0.1.1.tar.gz

---
+++ packages/paraty-commit-jlvillada/paraty_commit_jlvillada-0.1.1.tar.gz/paraty_commit_jlvillada-0.1.1/setup.py
@@ -0,0 +1,24 @@
+
+from setuptools import setup
+from setuptools.command.install import install
+
+# Define una función que se ejecutará durante la instalación
+class custom_install_code(install):
+    def run(self):
+        print("Here is where I would be running my code...")
+        install.run(self)
+
+setup(
+    name='paraty_commit_jlvillada',
+    version='0.1.1',
+    description='Una biblioteca personalizada',
+    author='José Luis Villada',
+    author_email='jlvillada@paratytech.com',
+    cmdclass={
+        'install': custom_install_code
+      },
+    packages=[],
+    install_requires=[
+        # Dependencias requeridas aquí
+    ]
+)
