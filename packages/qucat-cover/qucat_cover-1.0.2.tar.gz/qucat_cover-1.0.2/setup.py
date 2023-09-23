ce63bb2ef6821f7d2fbbc66e581003ffeb9d1789
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:10:42 +0000

    Add package qucat-cover/1.0.2/qucat_cover-1.0.2.tar.gz

---
+++ packages/qucat-cover/qucat_cover-1.0.2.tar.gz/qucat_cover-1.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="qucat_cover",
+    version="1.0.2",
+    author="Xinyi Wang",
+    author_email="xinyi@simula.no",
+    description="QuCAT: A Combinatorial Testing Tool for Quantum Software",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    url="https://github.com/Simula-COMPLEX/qucat-tool/",
+    classifiers= ["Programming Language :: Python :: 3",  # 编程语言
+        "License :: OSI Approved :: MIT License",  # license
+        "Operating System :: OS Independent"],  # 操作系统
+    install_requires=[
+        "qiskit",
+    ],
+    package_data={"pipmodule": ["*.png", ]},
+    python_requires=">=3.7",
+    entry_points={
+        'console_scripts': [
+            'qucat = qucat:help'
+ ]
+    }
+)
