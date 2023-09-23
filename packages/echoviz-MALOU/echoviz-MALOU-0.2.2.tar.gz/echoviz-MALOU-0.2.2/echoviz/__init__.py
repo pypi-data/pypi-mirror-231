db6d2505b96924f564b7957e2c4e7ba8aca0cc62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 04:06:19 +0000

    Add package echoviz-MALOU/0.2.2/echoviz-MALOU-0.2.2.tar.gz

---
+++ packages/echoviz-MALOU/echoviz-MALOU-0.2.2.tar.gz/echoviz-MALOU-0.2.2/echoviz/__init__.py
@@ -0,0 +1,10 @@
+from echoviz.threed import (animated_3d, sdf_animated_3d, asd_animated_3d,
+                            interactive_3d, sdf_interactive_3d, asd_interactive_3d,
+                            static_3d, sdf_static_3d, asd_static_3d)
+from echoviz.twod import plot_slice, sliced_sequence, sliced_volume
+from echoviz.utils import VoxelGrid
+
+
+
+__author__ = "Maïlys Hau"
+__version__ = "0.2.2"
