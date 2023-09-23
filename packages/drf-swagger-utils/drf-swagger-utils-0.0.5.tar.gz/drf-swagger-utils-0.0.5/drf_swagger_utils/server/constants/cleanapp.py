b3622a2cbb8d87743c01a784a34aa06837227198
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:07:00 +0000

    Add package drf-swagger-utils/0.0.5/drf-swagger-utils-0.0.5.tar.gz

---
+++ packages/drf-swagger-utils/drf-swagger-utils-0.0.5.tar.gz/drf-swagger-utils-0.0.5/drf_swagger_utils/server/constants/cleanapp.py
@@ -0,0 +1,6 @@
+directory_list = ["adapters", "api_specs", "conf", "constants", "dtos",
+                  "entities", "exceptions", "models", "tests", "utils", "signals", "interactors", "presenters", "storages"]
+
+tests_directory_list = ["entities", "interactors", "presenters", "storages", "views"]
+
+interactors_directory = ["presenters", "storages"]
---
+++ packages/drf-swagger-utils/drf-swagger-utils-0.0.5.tar.gz/drf-swagger-utils-0.0.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drf-swagger-utils',
+    version='0.0.5',
+    description='Your package description',
+    author='Chinni Raja Ammela',
+    packages=find_packages(),
+    install_requires=[
+          'django',
+    ],
+    entry_points={
+        'console_scripts': [
+            'create_app = drf_swagger_utils.Command:main',
+            'build = drf_swagger_utils.Command:main'
+        ],
+    }
+)
