764892011540b8b9d0b025f16b4fa981231dc8f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 05:07:03 +0000

    Add package torchmedia/0.1.0/torchmedia-0.1.0.tar.gz

---
+++ packages/torchmedia/torchmedia-0.1.0.tar.gz/torchmedia-0.1.0/setup.py
@@ -0,0 +1,14 @@
+"""Python setup.py for torchmedia package"""
+from setuptools import find_packages, setup
+
+setup(
+    name="torchmedia",
+    version="0.1.0",
+    description="",
+    url="https://pypi.org/project/torchmedia/",
+    long_description="TorchMedia",
+    long_description_content_type="text/markdown",
+    author="flat_hill",
+    packages=find_packages(),
+    install_requires=[],
+)
