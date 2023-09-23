28137a04cf854a9502972e7cf43675f1709a6369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:08:05 +0000

    Add package fishdata/1.4/fishdata-1.4.tar.gz

---
+++ packages/fishdata/fishdata-1.4.tar.gz/fishdata-1.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="fishdata",
+    version="1.4",
+    description="A package for managing fish data",
+    author="Carl-Emil",
+    author_email="carlemilkrag@email.com",
+    packages=["fishdata"],
+)
