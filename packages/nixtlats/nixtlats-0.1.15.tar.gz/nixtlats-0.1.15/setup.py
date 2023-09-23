e39f36205ddca375296a3ee207b84caf6b623c06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 15:07:58 +0000

    Add package nixtlats/0.1.15/nixtlats-0.1.15.tar.gz

---
+++ packages/nixtlats/nixtlats-0.1.15.tar.gz/nixtlats-0.1.15/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+dev = ["black", "nbdev", "plotly", "python-dotenv", "openbb", "statsforecast", "utilsforecast"]
+distributed = ["dask[complete]", "fugue[ray]", "pyspark"]
+
+setuptools.setup(
+    name="nixtlats",
+    version="0.1.15",
+    description="TimeGPT SDK",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Nixtla/nixtla",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+    install_requires=["requests", "pandas", "httpx", "pydantic<2"],
+    extras_require={
+        "dev": dev + distributed,
+        "distributed": distributed,
+    },
+)
