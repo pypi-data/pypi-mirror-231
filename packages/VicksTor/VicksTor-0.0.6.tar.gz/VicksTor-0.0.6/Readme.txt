dfcf670b35950fb91db9f855e0b158a6c9fec96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 04:06:57 +0000

    Add package VicksTor/0.0.6/VicksTor-0.0.6.tar.gz

---
+++ packages/VicksTor/VicksTor-0.0.6.tar.gz/VicksTor-0.0.6/Readme.txt
@@ -0,0 +1,6 @@
+
+https://raw.githubusercontent.com/imvickykumar999/Flask-XAMPP-Onion-Host/main/Tutorial%20Files/VicksTor.py
+
+# just add this 1 line.
+
+from VicksTor import VicksTor 
---
+++ packages/VicksTor/VicksTor-0.0.6.tar.gz/VicksTor-0.0.6/VicksTor/__init__.py
@@ -0,0 +1,17 @@
+
+how_to = '''
+# just add this 1 line.
+from VicksTor import VicksTor 
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
+'''
+
+print(how_to)
