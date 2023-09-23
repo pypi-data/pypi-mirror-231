5f926b91e09983582ef81571fc4414382dcf4aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:09:41 +0000

    Add package photoshop-python-api/0.22.1/photoshop_python_api-0.22.1-py3-none-any.whl

---
+++ packages/photoshop-python-api/photoshop_python_api-0.22.1-py3-none-any.whl/photoshop/api/constants.py
@@ -0,0 +1,11 @@
+# The photoshop version to COM progid mappings.
+PHOTOSHOP_VERSION_MAPPINGS = {
+    "2024": "180",
+    "2023": "170",
+    "2022": "160",
+    "2021": "150",
+    "2020": "140",
+    "2019": "130",
+    "2018": "120",
+    "2017": "110",
+}
