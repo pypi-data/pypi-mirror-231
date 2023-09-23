9bb3b2b99e9d36764db3043c9f8fb774f08f8232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:08:07 +0000

    Add package few-shot-priming/0.1.73/few_shot_priming-0.1.73-py3-none-any.whl

---
+++ packages/few-shot-priming/few_shot_priming-0.1.73-py3-none-any.whl/few_shot_priming/syntax_similarity.py
@@ -0,0 +1,11 @@
+import fkassim.FastKassim as fkassim
+import numpy as np
+def calc_syntactic_similarity(df_test, df_training):
+    similarities = np.zeros(((len(df_test),len(df_training))))
+    FastKassim = fkassim.FastKassim(fkassim.FastKassim.LTK)
+    for i, test_text in enumerate(df_test["text"]):
+        for j, train_text in enumerate(df_training["text"]):
+            similarity = FastKassim.compute_similarity(test_text, train_text)
+            similarities[i, j] = similarity
+    return similarities
+
