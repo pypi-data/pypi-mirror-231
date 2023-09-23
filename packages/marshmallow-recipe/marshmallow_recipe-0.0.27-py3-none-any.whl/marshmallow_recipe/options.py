f980aebe8b3d2a96fd4d9d98e58b31487327d2f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:08:53 +0000

    Add package marshmallow-recipe/0.0.27/marshmallow_recipe-0.0.27-py3-none-any.whl

---
+++ packages/marshmallow-recipe/marshmallow_recipe-0.0.27-py3-none-any.whl/marshmallow_recipe/options.py
@@ -0,0 +1,43 @@
+import dataclasses
+import enum
+
+from .naming_case import NamingCase
+
+_OPTIONS_KEY = "__marshmallow_recipe_options__"
+
+
+class NoneValueHandling(str, enum.Enum):
+    IGNORE = "IGNORE"
+    INCLUDE = "INCLUDE"
+
+    def __str__(self) -> str:
+        return self.value
+
+
+@dataclasses.dataclass(kw_only=True, slots=True, frozen=True)
+class DataclassOptions:
+    none_value_handling: NoneValueHandling | None = None
+    naming_case: NamingCase | None = None
+
+
+def options(
+    *,
+    none_value_handling: NoneValueHandling | None = None,
+    naming_case: NamingCase | None = None,
+):
+    def wrap(cls: type) -> type:
+        setattr(
+            cls,
+            _OPTIONS_KEY,
+            DataclassOptions(
+                none_value_handling=none_value_handling,
+                naming_case=naming_case,
+            ),
+        )
+        return cls
+
+    return wrap
+
+
+def try_get_options_for(type: type) -> DataclassOptions | None:
+    return getattr(type, _OPTIONS_KEY, None)
