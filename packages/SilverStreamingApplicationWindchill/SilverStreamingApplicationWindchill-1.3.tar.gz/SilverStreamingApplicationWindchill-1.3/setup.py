3df955555208335f68ca6012ad59c5b3ec85dc32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:08:19 +0000

    Add package SilverStreamingApplicationWindchill/1.3/SilverStreamingApplicationWindchill-1.3.tar.gz

---
+++ packages/SilverStreamingApplicationWindchill/SilverStreamingApplicationWindchill-1.3.tar.gz/SilverStreamingApplicationWindchill-1.3/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SilverStreamingApplicationWindchill',
+    version='1.3',
+    packages=['com.phida.main', 'com.phida.tests'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='Support for Windchill added'
+)
