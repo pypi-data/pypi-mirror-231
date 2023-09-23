966613a43efae90b323594f6d6262095863cb60a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:07:12 +0000

    Add package dhole/0.0.1/dhole-0.0.1.tar.gz

---
+++ packages/dhole/dhole-0.0.1.tar.gz/dhole-0.0.1/README.md
@@ -0,0 +1,3 @@
+# dhole
+
+Testing
---
+++ packages/dhole/dhole-0.0.1.tar.gz/dhole-0.0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Currently just a test lib'
+LONG_DESCRIPTION = 'Currently just a test lib'
+
+# Setting up
+setup(
+    name="dhole",
+    version=VERSION,
+    author="Berislav Paradžik",
+    author_email="<bparadzik.zg@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
