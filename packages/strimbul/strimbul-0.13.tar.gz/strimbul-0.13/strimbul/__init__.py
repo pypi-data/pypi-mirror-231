2306fd4b9496ebbbf12a77da3b682814a3255829
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 03:10:46 +0000

    Add package strimbul/0.13/strimbul-0.13.tar.gz

---
+++ packages/strimbul/strimbul-0.13.tar.gz/strimbul-0.13/strimbul/__init__.py
@@ -0,0 +1,7 @@
+# from .new_page import print_new_page
+from . import new_page
+from . import main
+import os 
+
+if __name__ == "__main__":
+    os.system("streamlit run strimbul/main.py")
---
+++ packages/strimbul/strimbul-0.13.tar.gz/strimbul-0.13/strimbul/__main__.py
@@ -0,0 +1,5 @@
+# __main__.py
+from strimbul.main import main
+
+if __name__ == "__main__":
+    main()
