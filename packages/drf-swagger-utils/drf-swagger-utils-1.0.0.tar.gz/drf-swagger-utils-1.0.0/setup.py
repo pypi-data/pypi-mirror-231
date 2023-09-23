4e736b9097e52af524c77cfe747d3f92a3a07d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 04:06:51 +0000

    Add package drf-swagger-utils/1.0.0/drf-swagger-utils-1.0.0.tar.gz

---
+++ packages/drf-swagger-utils/drf-swagger-utils-1.0.0.tar.gz/drf-swagger-utils-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='drf-swagger-utils',
+    version='1.0.0',
+    description='Your package description',
+    author='Chinni Raja Ammela',
+    packages=find_packages(),
+)
