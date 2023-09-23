1af530bfc870bd66a2bc5b91cb16c44eefdf9af9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:07:12 +0000

    Add package bradleystevenson2015-database/0.0.9/bradleystevenson2015_database-0.0.9.tar.gz

---
+++ packages/bradleystevenson2015-database/bradleystevenson2015_database-0.0.9.tar.gz/bradleystevenson2015_database-0.0.9/src/bradleystevenson2015_database/table.py
@@ -0,0 +1,22 @@
+class Table:
+
+    def __init__(self):
+        pass
+
+
+    def generate_from_database(self):
+        self.data = self.database.execute_select_statement(self._generate_select_all_string())
+
+
+    def append(self, insert_dict):
+        new_dict = {}
+        for column in self.columns:
+            new_dict[column] = insert_dict[column]
+        self.data.append(new_dict)
+
+
+    def _generate_select_all_string(self):
+        return_string = 'SELECT '
+        for column in self.columns:
+            return_string += column + ', '
+        return_string = return_string[:-2] + ' FROM ' + self.table_name
