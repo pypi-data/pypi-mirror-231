c2fa0f7843bcc6266d8a216d2541caf01311dd1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 04:06:10 +0000

    Add package helpsk/0.1.55/helpsk-0.1.55-py3-none-any.whl

---
+++ packages/helpsk/helpsk-0.1.55-py3-none-any.whl/helpsk/__init__.py
@@ -0,0 +1,11 @@
+"""
+init file
+"""
+import helpsk.color  # noqa
+import helpsk.conversions
+import helpsk.date
+import helpsk.pandas
+import helpsk.string
+import helpsk.text
+import helpsk.utility
+import helpsk.validation  # noqa
---
+++ packages/helpsk/helpsk-0.1.55-py3-none-any.whl/helpsk/sklearn_pipeline.py
@@ -0,0 +1,67 @@
+"""This module contains helper functions when working with sklearn (scikit-learn) objects;
+in particular, for building pipelines"""
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import OrdinalEncoder
+
+
+class TransformerChooser(BaseEstimator, TransformerMixin):
+    """Transformer that wraps another Transformer. This allows different transformer objects to be
+    tuned.
+    """
+    def __init__(self, transformer=None):
+        """
+        Args:
+            transformer:
+                Transformer object (e.g. StandardScaler, MinMaxScaler)
+        """
+        self.transformer = transformer
+
+    def fit(self, X, y=None):
+        """fit implementation
+        """
+        if self.transformer is None:
+            return self
+
+        return self.transformer.fit(X, y)
+
+    def transform(self, X):
+        """transform implementation
+        """
+        if self.transformer is None:
+            return X
+
+        return self.transformer.transform(X)
+
+
+class CustomOrdinalEncoder(BaseEstimator, TransformerMixin):
+    """First replaces missing values with '<missing>' then applies OrdinalEncoder
+    """
+
+    def __init__(self, handle_unknown='use_encoded_value'):
+        self._ordinal_encoder = OrdinalEncoder(handle_unknown=handle_unknown, unknown_value=-1)
+        # unknown_value=-1,
+        # handle_unknown='use_encoded_value')
+        self.handle_unknown = handle_unknown
+        self._missing_value = '<missing>'
+
+    def _fill_na(self, X):
+        """Helper function that fills missing values with strings before calling OrdinalEncoder"""
+        for column in X.columns.values:
+            if X[column].dtype.name == 'category':
+                if self._missing_value not in X[column].cat.categories:
+                    X[column] = X[column].cat.add_categories(self._missing_value)
+                X[column] = X[column].fillna(self._missing_value)
+
+        return X
+
+    def fit(self, X, y=None):
+        """fit implementation"""
+        X = self._fill_na(X)
+        self._ordinal_encoder.fit(X)
+        return self
+
+    def transform(self, X):
+        """transform implementation"""
+        X = self._fill_na(X)
+        return self._ordinal_encoder.transform(X)
