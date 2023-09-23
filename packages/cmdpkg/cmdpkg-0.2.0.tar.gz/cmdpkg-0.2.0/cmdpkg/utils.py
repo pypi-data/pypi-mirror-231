a0252f1ef895e52114a6a3fc1c63d358861e239b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 05:06:53 +0000

    Add package cmdpkg/0.2.0/cmdpkg-0.2.0.tar.gz

---
+++ packages/cmdpkg/cmdpkg-0.2.0.tar.gz/cmdpkg-0.2.0/cmdpkg/utils.py
@@ -0,0 +1,12 @@
+from typing import Iterable, Type
+
+
+def filter_none_values(values: Iterable) -> filter:
+    return filter(lambda value: value is not None, values)
+
+
+def is_all_items_instance_of(args: Iterable, _type: Type) -> bool:
+    for arg in args:
+        if not isinstance(arg, _type):
+            return False
+    return True
