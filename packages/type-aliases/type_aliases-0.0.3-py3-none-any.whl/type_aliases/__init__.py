54ca6bfcb8d10476f30be246c57a2a93c52a40c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-23 14:06:56 +0000

    Add package type-aliases/0.0.3/type_aliases-0.0.3-py3-none-any.whl

---
+++ packages/type-aliases/type_aliases-0.0.3-py3-none-any.whl/type_aliases/__init__.py
@@ -0,0 +1,22 @@
+import typing
+from typing import TypeAlias
+
+Any: TypeAlias = typing.Any
+Bool: TypeAlias = bool
+Bytes: TypeAlias = bytes
+Dict = dict
+Float: TypeAlias = float
+Int: TypeAlias = int
+Iterable = typing.Iterable
+Iterator = typing.Iterator
+List = list
+Literal = typing.Literal
+LiteralString: TypeAlias = typing.LiteralString
+NamedTuple: TypeAlias = typing.NamedTuple
+Optional = typing.Optional
+Set = set
+Str: TypeAlias = str
+Tuple = tuple
+Type: TypeAlias = type
+TypeVar: TypeAlias = typing.TypeVar
+Union = typing.Union
