bfbd4e029e2e22c60a277b56d6067636d591978c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:09:58 +0000

    Add package poethepoet/0.23.0/poethepoet-0.23.0.tar.gz

---
+++ packages/poethepoet/poethepoet-0.23.0.tar.gz/poethepoet-0.23.0/poethepoet/exceptions.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+
+class PoeException(RuntimeError):
+    cause: Optional[str]
+
+    def __init__(self, msg, *args):
+        self.msg = msg
+        self.cause = args[0].args[0] if args else None
+        self.args = (msg, *args)
+
+
+class CyclicDependencyError(PoeException):
+    pass
+
+
+class ExpressionParseError(PoeException):
+    pass
+
+
+class ExecutionError(RuntimeError):
+    cause: Optional[str]
+
+    def __init__(self, msg, *args):
+        self.msg = msg
+        self.cause = args[0].args[0] if args else None
+        self.args = (msg, *args)
+
+
+class PoePluginException(PoeException):
+    pass
