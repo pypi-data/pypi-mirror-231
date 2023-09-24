b3aab1ce49e18ae8525e2a0c570ca5cc2406573c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:09:41 +0000

    Add package turbocrawler/0.0.1rc2/turbocrawler-0.0.1rc2-py3-none-any.whl

---
+++ packages/turbocrawler/turbocrawler-0.0.1rc2-py3-none-any.whl/turbocrawler/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+logger = logging.getLogger('turbocrawler')
+logger.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+formatter = logging.Formatter('[%(asctime)s] [%(levelname)s] %(message)s', datefmt='%m-%d-%Y %H:%M:%S')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
---
+++ packages/turbocrawler/turbocrawler-0.0.1rc2-py3-none-any.whl/turbocrawler/queues/crawler_queues.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+from turbocrawler.engine import CrawledQueueABC, CrawlerQueueABC
+from turbocrawler.engine.models import CrawlerRequest
+
+
+class FIFOMemoryQueue(CrawlerQueueABC):
+    """The queue is a FIFO"""
+
+    def __init__(self, crawled_queue: CrawledQueueABC = None, save_crawled_queue: bool = False):
+        super().__init__(crawled_queue=crawled_queue, save_crawled_queue=save_crawled_queue)
+        self.__crawler_queue = deque()
+
+    def _insert_queue(self, crawler_request: CrawlerRequest):
+        self.__crawler_queue.append(crawler_request)
+
+    def _get_and_remove_request_from_queue(self) -> CrawlerRequest:
+        return self.__crawler_queue.popleft()
+
+    def _is_queue_empty(self) -> bool:
+        return not bool(self.__crawler_queue)
