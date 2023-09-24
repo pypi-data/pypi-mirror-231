481a03c7167724eadb778913ab9075372b5ba4f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:09:14 +0000

    Add package vecked/1.6.0/vecked-1.6.0-py3-none-any.whl

---
+++ packages/vecked/vecked-1.6.0-py3-none-any.whl/vecked/vector2/__init__.py
@@ -0,0 +1,9 @@
+from vecked.vector2.vector2 import Vector2
+from vecked.vector2.vector2f import Vector2f
+from vecked.vector2.vector2i import Vector2i
+
+__all__ = [
+    "Vector2",
+    "Vector2f",
+    "Vector2i",
+]
