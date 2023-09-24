3d1e967015a299ccf1edd16fc8c678494b8ab3d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:09:49 +0000

    Add package notion-python-client/0.0.1b1/notion_python_client-0.0.1b1.tar.gz

---
+++ packages/notion-python-client/notion_python_client-0.0.1b1.tar.gz/notion_python_client-0.0.1b1/notion_python_client/models/properties/number.py
@@ -0,0 +1,20 @@
+from typing import Dict, Union, Optional
+from pydantic import Field
+
+from notion_python_client.models.properties.properties_base import PropertiesBase
+from notion_python_client.models.properties.properties_base_dict import PropertiesDictBase
+
+
+class Number(PropertiesDictBase, PropertiesBase):
+    number: Optional[Union[float, int]] = Field(default=None)
+
+    def create_object(self, property_name: str) -> Dict:
+        number = {
+            property_name: {
+                "number": self.number
+            }
+        }
+
+        number_cleaned = self.clean_none(number)
+
+        return number_cleaned
