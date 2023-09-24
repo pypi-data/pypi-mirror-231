e4600299341fc9bc84196fc3a37b2260e229a01c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:09:57 +0000

    Add package metastock/0.1.2/metastock-0.1.2-py3-none-any.whl

---
+++ packages/metastock/metastock-0.1.2-py3-none-any.whl/metastock/modules/core/util/http_client.py
@@ -0,0 +1,33 @@
+import requests
+
+from metastock.modules.core.logging.logger import Logger
+
+
+class HttpClient:
+    instance = None
+
+    @staticmethod
+    def get(url, config=None):
+        if config is None:
+            config = {}
+        response = requests.get(url)
+
+        return response
+
+    @staticmethod
+    def post(url, data: dict, config: dict = None):
+        response = requests.post(url, json=data)
+        # response.raise_for_status()
+        return response
+
+    @staticmethod
+    def patch(url, data: dict, config: dict = None):
+        response = requests.patch(url, json=data)
+        # response.raise_for_status()
+        return response
+
+
+def http_client() -> HttpClient:
+    if HttpClient.instance is None:
+        HttpClient.instance = HttpClient()
+    return HttpClient.instance
