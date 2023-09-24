40a2b09b5c59bb8202065d92b4f18b553cb29a05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:07:31 +0000

    Add package replicator-agent/0.0.1/replicator_agent-0.0.1.tar.gz

---
+++ packages/replicator-agent/replicator_agent-0.0.1.tar.gz/replicator_agent-0.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['replicate']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['swarms']
+
+setup_kwargs = {
+    'name': 'replicator-agent',
+    'version': '0.0.1',
+    'description': 'TheReplicator - Pytorch',
+    'long_description': "# The Replicator\n\n# Install\n`pip install \n\nToday, Artificial Intelligence (AI) research and development is highly centralised, slow, and mostly manual. It involves talented researchers and engineers iterating on models, creating new architectures, testing different hyperparameters, and validating their results in a labor-intensive process. \n\nImagine a world where AI research could be conducted autonomously, at the speed of thought. Where Multi-Modal AI models could be iteratively improved, expanded, and adapted by intelligent swarms of autonomous agents, thus making the process of AI research and development more scalable, efficient, and widespread.\n\nYet this hasn't been achieved because building such a system faces immense challenges - the complexity of managing and orchestrating an autonomous swarm, the need for reliable evaluation and validation of AI models, and the issue of iterating at the speed of computation rather than human thought.\n\nHowever, there is a massive opportunity here. Solving these challenges would be a revolutionary step in the world of AI. It requires the capability to orchestrate an intelligent swarm, a reliable evaluation and validation system for AI models, and the ability to iterate quickly and autonomously. But the reward, an AI system that conducts its own research and development, would be a groundbreaking achievement.\n\nEnter The Replicator. Our secret sauce is making iterations at lightspeed. Using an autonomous swarm approach, we are creating a system that conducts Multi-Modal AI research autonomously. By developing new underlying mathematical operations and models, the swarm can improve and adapt AI models at a computational speed, leaving human limitations behind.\n\nWhy are we the ones to make this happen? Agora has 1,500 team members with extensive experience in AI research and development, distributed systems, and fast iteration methodologies. We understand the complexities and nuances of this task and are prepared to tackle them head-on. With our expertise, dedication, and innovative approach, we're primely positioned to make this revolutionary step in AI research and development a reality.\n\n# The Replicator\n*An Autonomous Swarm that Conducts Multi-Modal AI Research by Creating New Underlying Mathematical Operations and Models*\n\nWelcome to The Replicator. \n\n## Features\n- **Autonomous Multi-Modal AI Research:** Using swarm intelligence, The Replicator conducts autonomous AI research, developing new models and algorithms to improve AI capabilities.\n\n- **Innovative Swarm Intelligence:** Our system is designed as a swarm of autonomous agents, capable of working together to conduct complex AI research tasks at a computational speed.\n\n- **Rapid Iteration:** Our system makes iterations at lightspeed, allowing us to quickly adapt and improve AI models based on real-time insights.\n\n- **Versatile Research Scope:** The Replicator is capable of conducting research on multi-modal AI, making it possible to develop and improve AI models across different modalities.\n\n- **Reliable Evaluation and Validation:** The system has an inbuilt evaluation and validation mechanism that ensures the quality and reliability of the AI models it develops.\n\nJoin us on this exciting journey to revolutionize AI research and development, making it more autonomous, efficient, and far-reaching. Be a part of this groundbreaking venture and let's reshape the world of AI together!\n\n## Usage\nPlease refer to our [Getting Started Guide](./Getting-Started.md) for instructions on how to install and use The Replicator. \n\n## Contributing\nWe welcome contributors! Please see our [Contributing Guide](./Contributing.md) for more information on how to get involved.\n\n## License\nThe Replicator is licensed under the [MIT License](./LICENSE.md).\n\n## Acknowledgements\nA big thank you to our incredible team, contributors, and users who make The Replicator possible. Your support and dedication drive us forward in this revolutionary journey.\n\n## Resources\n\n* [SuperAlignment by OPENAI](https://openai.com/blog/introducing-superalignment)",
+    'author': 'Kye Gomez',
+    'author_email': 'kye@apac.ai',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/kyegomez/The-Replicator',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
