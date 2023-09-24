860f6c1c38b42593f387aee6c5bdd2cbca497e59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:04 +0000

    Add package qldev/1.0.6.2/qldev-1.0.6.2.tar.gz

---
+++ packages/qldev/qldev-1.0.6.2.tar.gz/qldev-1.0.6.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from setuptools import find_packages
+
+VERSION = '1.0.6.2'
+AUTHOR='eegion'
+EMAIL='hehuajun@eegion.com'
+
+option = {
+    "build_exe": {
+        "excludes":["test", "main"],
+        'packages':['utils','device','x7base','network']
+    }
+}
+
+setup(
+    name='qldev',  # package name
+    version=VERSION,  # package version
+    author=AUTHOR,
+    author_email=EMAIL,
+    description='api for x7 box since v1.2.2',  # package description
+    packages=find_packages(),
+    package_data={
+        "":["*.txt", "*.md"]
+    },
+    zip_safe=False
+)
