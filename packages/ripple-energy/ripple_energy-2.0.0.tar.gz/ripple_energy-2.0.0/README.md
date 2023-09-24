67c7a1da385beec337fbc14d62e4ca2dc4f1f8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:26 +0000

    Add package ripple-energy/2.0.0/ripple_energy-2.0.0.tar.gz

---
+++ packages/ripple-energy/ripple_energy-2.0.0.tar.gz/ripple_energy-2.0.0/README.md
@@ -0,0 +1,72 @@
+# Ripple Energy
+
+[![PyPI - Version](https://img.shields.io/pypi/v/ripple-energy.svg)](https://pypi.org/project/ripple-energy)
+
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/ripple-energy.svg)](https://pypi.org/project/ripple-energy)
+
+-----
+
+**Table of Contents**
+
+ - [Known Bugs And Issues](#known-bugs-and-issues)
+ - [To-Do](#to-do)  
+ - [Installation](#installation)
+ - [Example](#example)
+ - [License](#license)
+
+## Known Bugs And Issues
+
+ - ~~Any (all?) GraphQL query functions fail~~ solved by dapalex/py-graphql-mapper#25
+ - ~~list_Date type clash warning~~ solved by dapalex/py-graphql-mapper#25
+ - ~~Unsure of header format for token auth~~ now confirmed working
+ - insightsChartData optional in response but mandatory in framework
+
+## To-Do
+
+ - Complete function wrappers for all GraphQL queries
+ - Complete function wrappers for all GraphQL mutations
+ - Create function exceptions
+ - ~~Put functions into classes~~
+ - Add linting
+ - Create tests
+ - Create build pipeline 
+ - Publish to PyPi 
+ - Make Async?
+
+## Installation
+
+At present, this package has not been published to PyPi - You will therefore need to clone this repository to use it.  
+
+```console
+git clone https://github.com/danieledwardgeorgehitchcock/ripple-energy.git
+```
+This project leverages the use of [Hatch](https://hatch.pypa.io/latest/) for project management - please follow the installation instructions there before continuing.
+
+Once the above has completed, enter in to the project directory
+
+```console
+cd ripple-energy
+```
+As this package is managed by Hatch, you can start an environment which automatically pulls the project dependencies
+  
+  ```console
+hatch shell start
+```
+
+You should then be able to use the package from within this environment.
+
+## Example
+Me:
+  ```python
+from  ripple_energy  import RippleEnergy
+
+ripple  = RippleEnergy(email  =  "YOUR_RIPPLE_EMAIL", password  =  "YOUR_RIPPLE_PASSWORD")
+
+data = ripple.me()
+
+print(data)
+```
+
+## License
+
+`ripple-energy` is distributed under the terms of the [MIT](https://spdx.org/licenses/MIT.html) license.
---
+++ packages/ripple-energy/ripple_energy-2.0.0.tar.gz/ripple_energy-2.0.0/src/ripple_energy/ripple_energy.py
@@ -0,0 +1,128 @@
+"""Python client for fetching data from Ripple Energy API."""
+from requests import post
+from pygqlmap.network import GQLResponse
+import graphql.queries as gql_query
+import graphql.mutations as gql_mutation
+import graphql.gql_types as gql_type
+
+class RippleEnergy():
+    # init method or constructor
+    def __init__(self, email: str = None, password: str = None):
+        if email is None:
+            return #Replace with exception
+        if password is None:
+            return #Replace with exception
+        if not hasattr(self, "token"):
+            self.token = self.token_auth(email = email, password = password)
+
+    def request(self, query = None, url: str = None, headers: dict[str, str] = {}, timeout: int = None):
+        """Create a request to the Ripple Energy API, parse and validate response."""
+        if query is None:
+            return #Replace with exception
+        if url is None:
+            url = "https://rippleenergy.com/graphql"
+        if not hasattr(headers, "Content-Type"):
+            headers.update({"Content-Type": "application/json"})
+        if hasattr(self, "token") and not hasattr(headers, "Authorization"):
+            headers.update({"Authorization": self.token})
+        if timeout is None:
+            timeout = 10
+
+        response = post(url = url, timeout = timeout, headers = headers, json = {"query": query.export_gql_source})
+
+        gql_response = GQLResponse(response)
+
+        gql_response.print_msg_out()
+
+        gql_response.map_gqldata_to_obj(query.type)
+
+        data = gql_response.result_obj
+
+        return data
+
+    def token_auth(self, email: str = None, password: str = None):
+        """Generate authentication token with Ripple Energy API"""
+        if email is None:
+            return #Replace with exception
+        if password is None:
+            return #Replace with exception
+
+        mutation = gql_mutation.tokenAuth()
+        mutation.name = "TokenAuth"
+
+        mutation_input = gql_type.TokenAuthenticationInput()
+        mutation_input.email = email
+        mutation_input.password = password
+
+        mutation._args.input = mutation_input
+
+        data = self.request(query = mutation)
+
+        token = f"JWT {data.token}"
+
+        return token
+
+    def auth_login_session(self, email: str = None, password: str = None):
+        """Create login session with Ripple Energy API"""
+        if email is None:
+            return #Replace with exception
+        if password is None:
+            return #Replace with exception
+
+        mutation = gql_mutation.authLoginSession()
+        mutation.name = "AuthLoginSession"
+
+        mutation_input = gql_type.AuthLoginSessionInputType()
+        mutation_input.email = email
+        mutation_input.password = password
+
+        mutation._args.input = mutation_input
+
+        data = self.request(query = mutation)
+
+        return data
+
+    def auth_logout_session(self):
+        """Destroy login session with Ripple Energy API"""
+        mutation = gql_mutation.authLogoutSession()
+        mutation.name = "AuthLogoutSession"
+
+        data = self.request(query = mutation)
+
+        return data
+
+    def me(self):
+        """User information from Ripple Energy API"""
+        query = gql_query.me()
+        query.name = "Me"
+
+        data = self.request(query = query)
+
+        return data
+
+    def version(self):
+        """Version information from Ripple Energy API""" 
+        query = gql_query.version()
+        query.name = "Version"
+
+        data = self.request(query = query)
+
+        return data
+
+    def branding(self):
+        """Branding information from Ripple Energy API""" 
+        query = gql_query.branding()
+        query.name = "Branding"
+
+        data = self.request(query = query)
+
+        return data
+
+    def products(self):
+        """Products information from Ripple Energy API""" 
+        query = gql_query.products()
+        query.name = "Products"
+
+        data = self.request(query = query)
+
+        return data
