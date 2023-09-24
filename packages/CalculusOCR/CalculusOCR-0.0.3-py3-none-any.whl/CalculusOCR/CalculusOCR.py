9296db947a0d0ffc57826a6a97b88b1cf38cd150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:09:46 +0000

    Add package CalculusOCR/0.0.3/CalculusOCR-0.0.3-py3-none-any.whl

---
+++ packages/CalculusOCR/CalculusOCR-0.0.3-py3-none-any.whl/CalculusOCR/CalculusOCR.py
@@ -0,0 +1,48 @@
+import re
+import sympy
+import argparse
+from sympy import *
+from PIL import Image
+from pix2tex.cli import LatexOCR
+from sympy.parsing.latex import parse_latex
+
+def sympy_solve(sympy_equation):
+
+    words = re.findall(r'\b[A-Za-z]+\b', str(sympy_equation))
+    var_list = []
+    for word in words:
+        if word not in dir(sympy) and word not in var_list:
+            var_list.append(word)
+
+    sympy_symbols = symbols(" ".join(var_list))
+    solution = sympy_equation.doit()
+    return solution
+
+def getlatex(file):
+
+    img = Image.open(file)
+    model = LatexOCR()
+    return model(img)
+
+def solveimage(file):
+
+    latex_string = getlatex(file)
+    sympy_equation = parse_latex(latex_string)
+    return sympy_equation, sympy_solve(sympy_equation)
+
+
+def main():
+
+    desc_str = "CalculusOCR"
+    parser = argparse.ArgumentParser(description=desc_str)
+    parser.add_argument('--file', dest='filename', required=True)
+    filename = parser.parse_args().filename
+
+    eqn, sol = solveimage(filename)
+    print("Equation : ", eqn)
+    print("Solution : {}".format(sol))
+
+
+if __name__ == '__main__':
+
+    main()
---
+++ packages/CalculusOCR/CalculusOCR-0.0.3-py3-none-any.whl/CalculusOCR/__init__.py
@@ -0,0 +1,16 @@
+import subprocess
+import importlib
+import subprocess
+
+
+requirements_file=['antlr4-python3-runtime==4.11.0','pix2tex','sympy']
+def install_package(package_name):
+    try:
+        importlib.import_module(package_name)
+    except ImportError:
+        subprocess.check_call(["pip", "install", package_name])
+
+for package in requirements_file:
+    install_package(package)
+
+from .CalculusOCR import solveimage, sympy_solve, getlatex
