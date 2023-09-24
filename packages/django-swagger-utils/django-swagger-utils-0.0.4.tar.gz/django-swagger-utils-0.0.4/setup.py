620346ec32ea6c8195abbdb3bdb89edf64330809
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:38 +0000

    Add package django-swagger-utils/0.0.4/django-swagger-utils-0.0.4.tar.gz

---
+++ packages/django-swagger-utils/django-swagger-utils-0.0.4.tar.gz/django-swagger-utils-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-swagger-utils',
+    version='0.0.4',
+    description='Your package description',
+    author='Chinni Raja Ammela',
+    packages=find_packages()
+)
