01c4851a8a3fa7bb08633b8c1fed193ae0f75a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:11 +0000

    Add package so-ml-tools/0.1.49/so-ml-tools-0.1.49.tar.gz

---
+++ packages/so-ml-tools/so-ml-tools-0.1.49.tar.gz/so-ml-tools-0.1.49/so_ml_tools/tf/model/augment.py
@@ -0,0 +1,31 @@
+import keras as _keras
+
+
+def create_augmentation_layer(random_flip_h=True,
+                              random_flip_v=True,
+                              random_rotate=True,
+                              random_height=False,
+                              random_width=False,
+                              random_zoom=True) -> _keras.Sequential:
+    steps = []
+    if random_flip_h or random_flip_v:
+        if random_flip_h and random_flip_v:
+            steps.append(_keras.layers.RandomFlip('horizontal_and_vertical'))
+        elif random_flip_h:
+            steps.append(_keras.layers.RandomFlip('horizontal'))
+        elif random_flip_v:
+            steps.append(_keras.layers.RandomFlip('vertical'))
+
+    if random_rotate:
+        steps.append(_keras.layers.RandomRotation(.2))
+
+    if random_height:
+        steps.append(_keras.layers.RandomHeight(.2))
+
+    if random_width:
+        steps.append(_keras.layers.RandomWidth(.2))
+
+    if random_zoom:
+        steps.append(_keras.layers.RandomZoom(.2))
+
+    return _keras.Sequential(steps, name='data_augmentation')
