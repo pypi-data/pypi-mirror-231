9c4a4b4d3c910084f298079193576ef970feda90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:29 +0000

    Add package fastapi-walletauth/1.0.0b0/fastapi_walletauth-1.0.0b0.tar.gz

---
+++ packages/fastapi-walletauth/fastapi_walletauth-1.0.0b0.tar.gz/fastapi_walletauth-1.0.0b0/fastapi_walletauth/common.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
+from pydantic_settings import BaseSettings
+
+
+class SupportedChains(Enum):
+    Solana = "SOL"
+    Ethereum = "ETH"
+
+
+class AuthType(Enum):
+    Bearer = "Bearer"
+    JWT = "JWT"
+
+
+class NotAuthorizedError(Exception):
+    pass
+
+
+class Settings(BaseSettings):
+    APP: str = "fastapi_walletauth"
+    PRIVATE_KEY: bytes = Ed25519PrivateKey.generate().private_bytes_raw()
+    PUBLIC_KEY: bytes = Ed25519PrivateKey.from_private_bytes(PRIVATE_KEY).public_key().public_bytes_raw()
+    AUTH_TYPE: str = AuthType.JWT.value
+    TOKEN_TTL: int = 24 * 60 * 60  # 24 hours
+    CHALLENGE_TTL: int = 10 * 60  # 10 minutes
+
+    model_config = {
+        "env_prefix": "FASTAPI_WALLETAUTH_",
+        "case_sensitive": False,
+        "env_file": ".env",
+    }
+
+
+settings = Settings()
