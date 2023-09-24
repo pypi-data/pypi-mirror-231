94b831c74fdf26044a72daadd42fa0b01c6c1b33
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:09 +0000

    Add package strimbul/0.19/strimbul-0.19.tar.gz

---
+++ packages/strimbul/strimbul-0.19.tar.gz/strimbul-0.19/strimbul/__init__.py
@@ -0,0 +1,17 @@
+import os
+import glob
+
+# Get the list of all Python files in the current directory (excluding __init__.py)
+modules = [
+    os.path.basename(f)[:-3]  # Remove the ".py" extension
+    for f in glob.glob(os.path.join(os.path.dirname(__file__), '*.py'))
+    if not f.endswith('__init__.py')
+]
+
+# # Import all modules listed in the 'modules' list
+# for module in modules:
+#     __import__(module, globals(), locals())
+
+# Define __all__ to include all the modules
+print(modules)
+__all__ = modules
