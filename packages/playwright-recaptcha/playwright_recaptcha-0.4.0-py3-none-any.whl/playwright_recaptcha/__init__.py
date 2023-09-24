cacc179a668433110277c4d683f2d559ec912105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:08:07 +0000

    Add package playwright-recaptcha/0.4.0/playwright_recaptcha-0.4.0-py3-none-any.whl

---
+++ packages/playwright-recaptcha/playwright_recaptcha-0.4.0-py3-none-any.whl/playwright_recaptcha/__init__.py
@@ -0,0 +1,31 @@
+"""A library for solving reCAPTCHA v2 and v3 with Playwright."""
+
+__author__ = "Xewdy444"
+__version__ = "0.4.0"
+__license__ = "MIT"
+
+from playwright_recaptcha.errors import (
+    CapSolverError,
+    RecaptchaError,
+    RecaptchaNotFoundError,
+    RecaptchaRateLimitError,
+    RecaptchaSolveError,
+    RecaptchaTimeoutError,
+)
+from playwright_recaptcha.recaptchav2.async_solver import AsyncSolver as AsyncSolverV2
+from playwright_recaptcha.recaptchav2.sync_solver import SyncSolver as SyncSolverV2
+from playwright_recaptcha.recaptchav3.async_solver import AsyncSolver as AsyncSolverV3
+from playwright_recaptcha.recaptchav3.sync_solver import SyncSolver as SyncSolverV3
+
+__all__ = [
+    "CapSolverError",
+    "RecaptchaError",
+    "RecaptchaNotFoundError",
+    "RecaptchaRateLimitError",
+    "RecaptchaSolveError",
+    "RecaptchaTimeoutError",
+    "AsyncSolverV2",
+    "SyncSolverV2",
+    "AsyncSolverV3",
+    "SyncSolverV3",
+]
