c33403d512c4962a7523b82309a40236a510ac97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:20 +0000

    Add package speakWin/0.0.2/speakWin-0.0.2.tar.gz

---
+++ packages/speakWin/speakWin-0.0.2.tar.gz/speakWin-0.0.2/setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(name='speakWin',
+        version='0.0.2',
+        description='Text to Speech Windows Based.',
+        packages=['speakWin'],
+        author_email='ranjitmaity95@gmail.com',
+        zip_safe=False,
+        long_description_content_type="text/markdown",
+        long_description=open('README.md').read(),
+        )
