0d778605e4f7ca872da02a20859d0d2fb9e87f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 14:07:32 +0000

    Add package pyfuf/0.0.3/pyfuf-0.0.3.tar.gz

---
+++ packages/pyfuf/pyfuf-0.0.3.tar.gz/pyfuf-0.0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyfuf',
+    version='0.0.3',
+    description='support for fuff in python',
+    author='plusleft',
+    packages=["pyfuf"],
+    long_description_content_type="text/markdown",
+    long_description=open('README.md').read(),
+)
