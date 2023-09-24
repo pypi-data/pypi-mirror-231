84167f460208588ca5b548169f5f8414731067bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:48 +0000

    Add package django-swagger-utils/0.0.5/django-swagger-utils-0.0.5.tar.gz

---
+++ packages/django-swagger-utils/django-swagger-utils-0.0.5.tar.gz/django-swagger-utils-0.0.5/django_swagger_utils/management/commands/create_app.py
@@ -0,0 +1,25 @@
+import argparse
+from django.core.management.base import BaseCommand
+from django_swagger_utils.server.generators.create_app import create_app
+
+
+class Command(BaseCommand):
+    help = 'My custom Django management command'
+
+    def add_arguments(self, parser):
+        # Define the 'command' argument with choices for sub-commands
+        parser.add_argument("command", choices=["create_app"], help="The command to run")
+
+        # Add other command-specific options as needed
+        parser.add_argument("--app", help="Command-specific option")
+
+    def handle(self, *args, **kwargs):
+        # 'command' is automatically available as self.command
+        if self.command == "build":
+            create_app(self.options)
+        else:
+            print("Invalid command. Use 'build' as the command.")
+
+
+if __name__ == "__main__":
+    Command().execute()
