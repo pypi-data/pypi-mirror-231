b476e4e7b91e9cc9c8f03edea77e72e7fcd19d26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:07 +0000

    Add package pyfuf/0.0.1/pyfuf-0.0.1.tar.gz

---
+++ packages/pyfuf/pyfuf-0.0.1.tar.gz/pyfuf-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyfuf',
+    version='0.0.1',
+    description='support for fuff in python',
+    author='plusleft',
+    packages=["pyfuf"],
+)
