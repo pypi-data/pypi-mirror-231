854cfff38fa0a6e3ae5c92634051f05e2f97fc8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:12 +0000

    Add package idli-coco/0.0.3/idli_coco-0.0.3.tar.gz

---
+++ packages/idli-coco/idli_coco-0.0.3.tar.gz/idli_coco-0.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='idli_coco',
+    version='0.0.3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'streamlit',
+    ],
+    entry_points={
+        'console_scripts': [
+            'idli_coco = idli_coco.main:run',
+        ],
+    },
+)
