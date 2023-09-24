fda7951646e0129c2cc75c7e131122c99487fa7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:10:14 +0000

    Add package AleksFolt/0.1/AleksFolt-0.1.tar.gz

---
+++ packages/AleksFolt/AleksFolt-0.1.tar.gz/AleksFolt-0.1/aleksfolt/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="AleksFolt",
+    version="0.1",
+    description="Полезная библиотека",
+    author="AleksFolt",
+    author_email="dfdfdd039@gmail.com",
+    packages=["aleksfolt"],
+)
