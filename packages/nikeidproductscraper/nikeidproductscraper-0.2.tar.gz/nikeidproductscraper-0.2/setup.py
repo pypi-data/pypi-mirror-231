829da32d3cbf3d8b2d6a13d4212169a9d6713d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:09:32 +0000

    Add package nikeidproductscraper/0.2/nikeidproductscraper-0.2.tar.gz

---
+++ packages/nikeidproductscraper/nikeidproductscraper-0.2.tar.gz/nikeidproductscraper-0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nikeidproductscraper',
+    version='0.2',
+    description='A Python package for scraping Nike Indonesia website.',
+    author='hasan ash ',
+    author_email='hasan40p30m@gmail.com',
+    license='MIT',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'pandas>=2.1',
+        'httpx>=0.25.0',
+        'playwright>=1.38.0',
+        'selectolax>=0.3.16',
+    ],
+    setup_requires=['setuptools>=44'],
+)
