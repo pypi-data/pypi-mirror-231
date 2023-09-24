4d4eefd6686f2f70129189431d9010d3261adbd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-09-24 03:08:19 +0000

    Add package specex/0.7.2/specex-0.7.2.tar.gz

---
+++ packages/specex/specex-0.7.2.tar.gz/specex-0.7.2/src/specex/exceptions.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 11:45:22 2023
+
+@author: daddona
+"""
+
+
+try:
+    import IPython
+    from IPython.core import ultratb
+except Exception:
+    HAS_IPYTHON = False
+else:
+    HAS_IPYTHON = True
+
+
+def exception_handler(exception_type, value, traceback):
+    global HAS_IPYTHON
+
+    if HAS_IPYTHON:
+        traceback_formatter = ultratb.FormattedTB(
+            mode='Verbose', color_scheme='Linux', call_pdb=1
+        )
+        return traceback_formatter(exception_type, value, traceback)
+    else:
+        sys.__excepthook__(exception_type, value, traceback)
+        print("\n*** IPython not installed, cannot start the debugger! ***\n")
+
+    sys.exit(1)
